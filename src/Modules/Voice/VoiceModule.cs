6ae76f2cbb929c5f6c865169ee6997a7db8752af
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-02 20:31:04 +0300

    feat: voice records module

---
+++ src/Modules/Voice/VoiceModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Stl.Fusion;
+using Stl.Fusion.EntityFramework;
+using Stl.Fusion.EntityFramework.Npgsql;
+using Stl.Fusion.Extensions;
+using Stl.Fusion.Operations.Internal;
+
+namespace ActualChat.Voice
+{
+    public class VoiceModule : Module
+    {
+        public VoiceModule(IServiceCollection services, IServiceProvider moduleBuilderServices) 
+            : base(services, moduleBuilderServices) { }
+
+        public override void Use()
+        {
+            base.Use();
+            
+            var isDevelopmentInstance = HostInfo.IsDevelopmentInstance;
+            var settings = Services.BuildServiceProvider().GetRequiredService<VoiceSettings>();
+
+            var fusion = Services.AddFusion();
+            fusion.AddSandboxedKeyValueStore();
+
+            Services.AddDbContextFactory<VoiceDbContext>(builder => {
+                builder.UseNpgsql(settings.Db);
+                if (isDevelopmentInstance)
+                    builder.EnableSensitiveDataLogging();
+            });
+            Services.AddDbContextServices<VoiceDbContext>(b => {
+                Services.AddSingleton(new CompletionProducer.Options {
+                    IsLoggingEnabled = true,
+                });
+            });
+        }
+    }
+}
