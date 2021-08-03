dcd307dae9d343e5b3038d38d06b522e9f82ed3b
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-03 16:34:05 -0700

    refactor: models in Transcription

--- src/Modules/Transcription.Contracts/ITranscriber.cs
+++ src/Modules/Transcription.Contracts/ITranscriber.cs
@@ -2,103 +2,27 @@
 using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
-using Stl.Fusion.Authentication;
-using Stl.Serialization;
+using ActualChat.Audio;
+using Stl.Fusion;
+using Stl.Fusion.Extensions;
+using Stl.Text;
 
 namespace ActualChat.Transcription
 {
-    public enum AudioFormat
-    {
-        WAV = 0,
-        FLAC,
-        OPUS
-    }
-
-    public enum AudioLanguage
-    {
-        English = 0,
-        Russian,
-        Spanish,
-        Chinese,
-        German
-    }
-    
-    public record TranscriptionSession(Ulid Id)
-    {
-        public TranscriptionSession() : this(Ulid.NewUlid()) { }
-    }
-
-    public record StartTranscriptionSessionCommand(
-        Session Session, 
-        AudioFormat Format,
-        AudioLanguage Language,
-        AudioLanguage[] AlternativeLanguages,
-        int NumberOfChannels,
-        int SampleRate, 
-        int MaxAlternatives,
-        bool EnableDiarization,
-        int DiarizationSpeakerCount,
-        bool EnablePunctuation,
-        ImmutableList<string> TranscriptionHistory)
-    {
-        public StartTranscriptionSessionCommand() 
-            : this(
-                Session.Null, 
-                AudioFormat.WAV,
-                AudioLanguage.English,
-                Array.Empty<AudioLanguage>(),
-                1,
-                8000,
-                3,
-                false,
-                1,
-                true,
-                ImmutableList<string>.Empty) { }
-    }
-
-    public record AppendAudioDataCommand(Session Session, Ulid TranscriptionSessionId, Base64Encoded Data)
-    {
-        public AppendAudioDataCommand() : this(Session.Null, Ulid.Empty, default)
-        { }
-    }
-
-    public record EndTranscriptionSessionCommand(Session Session, Ulid TranscriptionSessionId)
-    {
-        public EndTranscriptionSessionCommand() : this(Session.Null, Ulid.Empty)
-        { }
-    }
-
-    public record TranscriptionResult(Ulid TranscriptionSessionId, IImmutableList<Transcript> Alternatives)
-    {
-        public TranscriptionResult() : this (Ulid.Empty, ImmutableList<Transcript>.Empty)
-        { }
-    }
-
-    public record Transcript(string Text, float Confidence, TimeSpan StartOffset, TimeSpan EndOffset,
-        IImmutableList<Word> Words)
-    {
-        public Transcript() : this (string.Empty, 0, TimeSpan.Zero, TimeSpan.Zero, ImmutableList<Word>.Empty)
-        { }
-    }
-
-    public record Word(string Text, TimeSpan StartOffset, TimeSpan EndOffset, string SpeakerId)
-    {
-        public Word() : this (string.Empty, TimeSpan.Zero, TimeSpan.Zero, string.Empty)
-        { }
-    }
-
-    public record TranscriptionSessionStats(Ulid TranscriptionSessionId, TimeSpan Duration)
-    {
-        public TranscriptionSessionStats() : this (Ulid.Empty, TimeSpan.Zero)
-        { }
-    }
-
     public interface ITranscriber
     {
-        Task<TranscriptionSession> StartSession(StartTranscriptionSessionCommand startSessionCommand, CancellationToken cancellationToken = default);
+        Task<Symbol> BeginTranscription(BeginTranscriptionCommand command, CancellationToken cancellationToken = default);
+        Task AppendTranscription(AppendTranscriptionCommand command, CancellationToken cancellationToken = default);
+        Task EndTranscription(EndTranscriptionCommand command, CancellationToken cancellationToken = default);
 
-        Task<TranscriptionResult> Append(AppendAudioDataCommand appendAudioDataCommand, CancellationToken cancellationToken = default);
-
-        Task<TranscriptionSessionStats> EndSession(EndTranscriptionSessionCommand endTranscriptionSessionCommand, CancellationToken cancellationToken = default);
+        [ComputeMethod]
+        Task<Transcript> GetTranscript(Symbol transcriptId, CancellationToken cancellationToken = default);
+        [ComputeMethod]
+        Task<TranscriptSummary> GetSummary(Symbol transcriptId, CancellationToken cancellationToken = default);
+        [ComputeMethod]
+        Task<TranscriptAudioSummary> GetAudioSummary(Symbol transcriptId, CancellationToken cancellationToken = default);
+        [ComputeMethod]
+        Task<ImmutableArray<TranscriptFragment>> GetFragments(
+            Symbol transcriptId, PageRef<int> page, CancellationToken cancellationToken = default);
     }
 }
---
+++ src/Modules/Transcription.Contracts/TranscriptionOptions.cs
@@ -0,0 +1,12 @@
+using System.Collections.Immutable;
+
+namespace ActualChat.Transcription
+{
+    public record TranscriptionOptions
+    {
+        public string Language { get; init; } = "en-us";
+        public bool IsPunctuationEnabled { get; init; } = true;
+        public bool IsDiarizationEnabled { get; init; } = false;
+        public int? MaxSpeakerCount { get; init; }
+    }
+}
