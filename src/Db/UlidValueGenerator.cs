f2be3f55c49435f9d9162312006eeab776211b76
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-03 19:05:59 +0300

    feat: introduce Ulid<->string converter for EF Core

---
+++ src/Db/UlidValueGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace ActualChat.Db
+{
+    public sealed class UlidValueGenerator : ValueGenerator<Ulid>
+    {
+        public override Ulid Next(EntityEntry entry) => Ulid.NewUlid();
+
+        public override bool GeneratesTemporaryValues => false;
+    }
+}
