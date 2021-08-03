047120a6b8c7a79299070ed93ebc7776e8566f42
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-03 20:41:28 +0300

    feat: voice transcription test stub page

---
+++ src/Modules/Voice.UI.Blazor/VoiceBlazorUIModule.cs
@@ -0,0 +1,14 @@
+﻿using ActualChat.Hosting;
+using ActualChat.UI.Blazor;
+using Stl.DependencyInjection;
+using Stl.Plugins;
+
+namespace ActualChat.Voice.UI.Blazor
+{
+    public class VoiceBlazorUIModule: HostModule, IBlazorUIModule
+    {
+        public VoiceBlazorUIModule(IPluginInfoProvider.Query _) : base(_) { }
+        [ServiceConstructor]
+        public VoiceBlazorUIModule(IPluginHost plugins) : base(plugins) { }
+    }
+}
