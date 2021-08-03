2c226ebb08f2028eed47f60670fe31203f901fab
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-03 19:05:59 +0300

    feat: introduce Ulid<->string converter for EF Core

---
+++ src/Db/UlidToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace ActualChat.Db
+{
+    public class UlidToStringConverter : ValueConverter<Ulid, string>
+    {
+        private static readonly ConverterMappingHints DefaultHints = new ConverterMappingHints(size: 26, unicode:false);
+
+        public UlidToStringConverter(ConverterMappingHints mappingHints = null!)
+            : base(
+                convertToProviderExpression: x => x.ToString(),
+                convertFromProviderExpression: x => Ulid.Parse(x),
+                mappingHints: DefaultHints.With(mappingHints))
+        {
+        }
+    }
+}
