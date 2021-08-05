6d0ec6244906d4f96742a25624e1f089c22123d0
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-05 21:26:30 +0300

    chore: working on audio recording data buffering

--- src/Modules/Audio/Service/AudioRecorder.cs
+++ src/Modules/Audio/Service/AudioRecorder.cs
@@ -1,30 +1,49 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
+using ActualChat.Storage;
 using Stl.Fusion;
 using Stl.Fusion.Authentication;
+using Stl.Fusion.EntityFramework;
+using Stl.Serialization;
 using Stl.Text;
+using Stl.Time;
 
 namespace ActualChat.Audio
 {
     [RegisterComputeService(typeof(IAudioRecorder))]
-    public class AudioRecorder : IAudioRecorder
+    public class AudioRecorder : DbServiceBase<AudioDbContext>, IAudioRecorder
     {
         private readonly IAuthService _authService;
+        private readonly IBlobStorage _blobStorage;
+        private readonly ConcurrentDictionary<string, ConcurrentQueue<(Moment, Base64Encoded)>> _dataCapacitor;
 
-        public AudioRecorder(IAuthService authService)
+        public AudioRecorder(IServiceProvider services, IAuthService authService, IBlobStorage blobStorage) : base(services)
         {
             _authService = authService;
+            _blobStorage = blobStorage;
+            _dataCapacitor = new ConcurrentDictionary<string, ConcurrentQueue<(Moment, Base64Encoded)>>();
         }
 
         public virtual async Task<Symbol> Initialize(InitializeAudioRecorderCommand command, CancellationToken cancellationToken = default)
         {
             if (Computed.IsInvalidating()) return default!;
-            
+           
             var user = await _authService.GetUser(command.Session, cancellationToken);
             user.MustBeAuthenticated();
+            
+            // @AY,. this code below doesn't work throwing exception
+            //  await using var dbContext = await CreateCommandDbContext(cancellationToken);
+            // at 
+            // protected Task<TDbContext> CreateCommandDbContext(bool readWrite = true, CancellationToken cancellationToken = default)
+            //    ...
+            //    var operationScope = commandContext.Items.Get<DbOperationScope<TDbContext>>();
+            // please check
 
-            return Ulid.NewUlid().ToString();
+
+            var recordingId = IdGenerator.NewSymbol();
+            return recordingId;
         }
 
         public virtual async Task AppendAudio(AppendAudioCommand command, CancellationToken cancellationToken = default)
@@ -32,6 +51,11 @@ namespace ActualChat.Audio
             if (Computed.IsInvalidating()) return;
             
             var (recordingId, offset, data) = command;
+            // _dataCapacitor.AddOrUpdate(
+            //     recordingId, 
+            //     (r, q) => { return null },
+            //     ()
+            // );
 
             await Task.CompletedTask;
         }
