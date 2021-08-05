95365e196b6978e1b50286473f3d0fab55328e4e
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-05 16:58:40 +0300

    feat: static Id generator

---
+++ src/Core/IdGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Stl.Text;
+
+namespace ActualChat
+{
+    public static class IdGenerator
+    {
+        // Probably it's not so strong as 
+        // https://github.com/lemire/testingRNG/blob/master/source/splitmix64.h
+        // but it doesn't require state and battle-tested
+        public static long NewLong() => Math.Abs((long)Guid.NewGuid().GetHashCode() * Guid.NewGuid().GetHashCode());
+        
+        public static Symbol NewSymbol() => Ulid.NewUlid().ToString();
+    }
+}
