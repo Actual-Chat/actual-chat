c75836d93e39e23399d06c881014c0a5c851b730
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-05 21:26:30 +0300

    chore: working on audio recording data buffering

--- src/Modules/Audio/Contract/Commands.cs
+++ src/Modules/Audio/Contract/Commands.cs
@@ -12,7 +12,7 @@ namespace ActualChat.Audio
         public Session Session { get; init; } = Session.Null;
         public AudioFormat AudioFormat { get; init; } = new();
         public string Language { get; init; } = "en-us";
-        public Moment ClientStartTime { get; init; } = CpuClock.Now;
+        public Moment ClientStartOffset { get; init; } = CpuClock.Now;
     }
 
     public record AppendAudioCommand(Symbol RecordingId, Moment ClientOffset, Base64Encoded Data) : ICommand<Unit>
