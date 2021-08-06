7189b191d1e78c68a76dfa740ee17a08c4629bba
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-06 20:38:20 +0300

    feat: audio recordings database model

--- src/Modules/Audio/Service/AudioRecorder.cs
+++ src/Modules/Audio/Service/AudioRecorder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ActualChat.Audio.Db;
@@ -19,21 +21,24 @@ namespace ActualChat.Audio
     [RegisterComputeService(typeof(IAudioRecorder))]
     public class AudioRecorder : DbServiceBase<AudioDbContext>, IAudioRecorder
     {
+        private static readonly TimeSpan CleanupInterval = new TimeSpan(0, 0, 10);
+        private static readonly TimeSpan SegmentLength = new TimeSpan(0, 1, 0);
+        
         private readonly IAuthService _authService;
-        private readonly IBlobStorage _blobStorage;
+        // private readonly IBlobStorage _blobStorage;
         private readonly Generator<string> _idGenerator;
         // AY:
         // - ConcurrentQueue is a bit of excess here, I guess - a normal queue would be fine
         // - It requires robust maintenance (mem leaks are easy to happen here), so
         //   prob. it's ok to just write for now
-        private readonly ConcurrentDictionary<Symbol, ConcurrentQueue<(Moment, Base64Encoded)>> _dataCapacitor;
+        private readonly ConcurrentDictionary<Symbol, Queue<(int, Moment, Base64Encoded)>> _dataCapacitor;
 
         public AudioRecorder(IServiceProvider services, IAuthService authService, IBlobStorage blobStorage) : base(services)
         {
             _authService = authService;
-            _blobStorage = blobStorage;
+            // _blobStorage = blobStorage;
             _idGenerator = new RandomStringGenerator(16, RandomStringGenerator.Base32Alphabet);
-            _dataCapacitor = new ConcurrentDictionary<Symbol, ConcurrentQueue<(Moment, Base64Encoded)>>();
+            _dataCapacitor = new ConcurrentDictionary<Symbol, Queue<(int, Moment, Base64Encoded)>>();
         }
 
         public virtual async Task<Symbol> Initialize(InitializeAudioRecorderCommand command, CancellationToken cancellationToken = default)
@@ -48,6 +53,17 @@ namespace ActualChat.Audio
 
             var recordingId = _idGenerator.Next();
             Log.LogInformation($"{nameof(Initialize)}, RecordingId = {{RecordingId}}", recordingId);
+            dbContext.AudioRecordings.Add(new DbAudioRecording {
+                Id = recordingId,
+                UserId = user.Id,
+                RecordingStartedUtc = command.ClientStartOffset,
+                AudioCodec = command.AudioFormat.Codec,
+                ChannelCount = command.AudioFormat.ChannelCount,
+                SampleRate = command.AudioFormat.SampleRate,
+                Language = command.Language,
+                RecordingDuration = 0
+            });
+
             return recordingId;
         }
 
@@ -55,14 +71,50 @@ namespace ActualChat.Audio
         {
             if (Computed.IsInvalidating()) return;
 
-            var (recordingId, offset, data) = command;
-            // _dataCapacitor.AddOrUpdate(
-            //     recordingId,
-            //     (r, q) => { return null },
-            //     ()
-            // );
+            var (recordingId, index, offset, data) = command;
+            Log.LogTrace($"{nameof(AppendAudio)}, RecordingId = {{RecordingId}}, Index = {{Index}}, DataLength = {{DataLength}}",
+                recordingId.Value,
+                command.Index,
+                command.Data.Count);
+            
+            // Push to real-time pipeline
+            // TBD
+            
+            
+            // Buffering...
+            _dataCapacitor.AddOrUpdate(
+                recordingId,
+                r => new Queue<(int, Moment, Base64Encoded)>(new []{ (index, offset, data) }),
+                (r, q) => {
+                    q.Enqueue((index, offset, data));
+                    return q; 
+                }
+            );
 
             await Task.CompletedTask;
+        }
+
+        private async Task BackgroundCycle(CancellationToken cancellationToken = default)
+        {
+            while (true) {
+                await Task.Delay(CleanupInterval, cancellationToken);
+                if (cancellationToken.IsCancellationRequested) return;
+                
+                await FlushBufferedSegments();
+            }
+        }
+
+        private Task FlushBufferedSegments()
+        {
+            foreach (var (recordingId, queue) in _dataCapacitor) {
+                // we definitely will have multi-threading issues with this implementation .. will do something
+                
+                // if (queue.TryPeek(out var tuple)) {
+                //     var (index, offset, data) = tuple;
+                //     
+                // }
+            }
+            return Task.CompletedTask;
         }
     }
 }
