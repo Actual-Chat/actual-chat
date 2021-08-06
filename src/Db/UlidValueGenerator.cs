f5bfba310bb421f4f1a7c7515c97349bc6fbceb3
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-06 10:02:02 -0700

    chore: formatting fixes in Ulid*

--- src/Db/UlidValueGenerator.cs
+++ src/Db/UlidValueGenerator.cs
@@ -6,8 +6,7 @@ namespace ActualChat.Db
 {
     public sealed class UlidValueGenerator : ValueGenerator<Ulid>
     {
-        public override Ulid Next(EntityEntry entry) => Ulid.NewUlid();
-
         public override bool GeneratesTemporaryValues => false;
+        public override Ulid Next(EntityEntry entry) => Ulid.NewUlid();
     }
 }
