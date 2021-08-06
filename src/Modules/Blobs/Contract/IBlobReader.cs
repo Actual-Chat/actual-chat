1ad3022cfe9a8da9d957801c2dcc96d638837ec1
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-05 20:29:18 -0700

    fix: Add [ComputeMethod] to IBlobReader.Read

--- src/Modules/Blobs/Contract/IBlobReader.cs
+++ src/Modules/Blobs/Contract/IBlobReader.cs
@@ -12,6 +12,7 @@ namespace ActualChat.Blobs
         Task<BlobInfo?> GetInfo(Session session, string blobId, CancellationToken cancellationToken = default);
         [ComputeMethod]
         Task<Base64Encoded> ReadTail(Session session, string blobId, long maxLength, CancellationToken cancellationToken = default);
+        [ComputeMethod] // It's a compute method only because there is Session; we may add non-compute version later
         Task<Base64Encoded> Read(Session session, string blobId, long start, long maxLength, CancellationToken cancellationToken = default);
     }
 }
