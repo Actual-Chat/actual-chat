60ffb8ae63c9fdb58b4c58c14ebfd30e32cc4d1b
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-08 20:37:48 -0700

    fix: exclude unnecessary DbOperationScopeProviders

--- src/Services/Audio/Service/Module/AudioModule.cs
+++ src/Services/Audio/Service/Module/AudioModule.cs
@@ -3,10 +3,13 @@ using ActualChat.Audio.Db;
 using ActualChat.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Stl.CommandR;
+using Stl.CommandR.Configuration;
 using Stl.DependencyInjection;
 using Stl.Fusion;
 using Stl.Fusion.EntityFramework;
 using Stl.Fusion.EntityFramework.Npgsql;
+using Stl.Fusion.EntityFramework.Operations;
 using Stl.Fusion.Extensions;
 using Stl.Fusion.Operations.Internal;
 using Stl.Plugins;
@@ -44,6 +47,18 @@ namespace ActualChat.Audio.Module
                     o.UnconditionalWakeUpPeriod = TimeSpan.FromSeconds(isDevelopmentInstance ? 60 : 5);
                 });
                 b.AddNpgsqlDbOperationLogChangeTracking();
+            });
+            services.AddCommander().AddHandlerFilter((handler, commandType) => {
+                // 1. Check if this is DbOperationScopeProvider<AudioDbContext> handler
+                if (handler is not InterfaceCommandHandler<ICommand> ich)
+                    return true;
+                if (ich.ServiceType != typeof(DbOperationScopeProvider<AudioDbContext>))
+                    return true;
+                // 2. Make sure it's intact only for local commands
+                var commandAssembly = commandType.Assembly;
+                if (commandAssembly == typeof(IAudioRecorder).Assembly)
+                    return true;
+                return false;
             });
         }
     }
