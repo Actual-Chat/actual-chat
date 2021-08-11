4cf292dd27617c94c6b25c2e12ff44776bf43404
iqmulator <iqmulator@gmail.com>
2021-08-11 09:29:23 +0500

    WIP ChatService

---
+++ src/Services/Chat/Contract/ChatPage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+using ActualChat.Users;
+
+namespace ActualChat.Chat
+{
+    public record ChatPage(string ChatId, int Limit)
+    {
+        public ImmutableList<ChatMessage> Messages { get; init; } = ImmutableList<ChatMessage>.Empty;
+        public ImmutableDictionary<long, Speaker> Users { get; init; } = ImmutableDictionary<long, Speaker>.Empty;
+
+        public ChatPage() : this("", 0) { }
+    }
+}
