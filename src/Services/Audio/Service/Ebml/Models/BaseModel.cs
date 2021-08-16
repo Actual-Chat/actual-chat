994f81085b763688c1128ed61b9c2821419e26be
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-16 21:29:08 +0300

    feat: adding variable-length read methods

--- src/Services/Audio/Service/Ebml/Models/BaseModel.cs
+++ src/Services/Audio/Service/Ebml/Models/BaseModel.cs
@@ -3,10 +3,17 @@ namespace ActualChat.Audio.Ebml.Models
 {
     public abstract class BaseModel
     {
+        public static readonly BaseModel Empty = new EmptyModel();
+        
         [MatroskaElementDescriptor(MatroskaSpecification.Void)]
         public byte[]? Void { get; set; }
 
         [MatroskaElementDescriptor(MatroskaSpecification.CRC32)]
         public byte[]? CRC32 { get; set; }
+    }
+
+    public sealed class EmptyModel : BaseModel
+    {
+        
     }
 }
