e73de66b39ee343a72db6bcd2934b740b66c3609
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-17 16:59:45 -0700

    feat: UserState.IsOnline should work now

--- src/Services/Users/Contract/IUserStateService.cs
+++ src/Services/Users/Contract/IUserStateService.cs
@@ -6,7 +6,7 @@ namespace ActualChat.Users
 {
     public interface IUserStateService
     {
-        [ComputeMethod(KeepAliveTime = 10)]
+        [ComputeMethod(KeepAliveTime = 30)]
         Task<bool> IsOnline(string userId, CancellationToken cancellationToken = default);
     }
 }
