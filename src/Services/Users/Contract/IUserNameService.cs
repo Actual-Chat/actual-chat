760dd7321c7f55ded752ba464322e6d309eaeb1a
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-19 15:51:56 -0700

    feat: Markup & MarkupParser

--- src/Services/Users/Contract/IUserNameService.cs
+++ src/Services/Users/Contract/IUserNameService.cs
@@ -6,6 +6,6 @@ namespace ActualChat.Users
     public interface IUserNameService
     {
         ValidationException? ValidateName(in ReadOnlySpan<char> name);
-        ReadOnlySpan<char> ParseName(ref ReadOnlySpan<char> text);
+        ReadOnlySpan<char> ParseName(ReadOnlySpan<char> text);
     }
 }
