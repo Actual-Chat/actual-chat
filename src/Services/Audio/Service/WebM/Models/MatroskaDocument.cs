fc225f7d86fa09f428874c75414715dc00f0afbd
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-23 15:52:37 +0300

    feat: new we have bit-perfect match after read-write cycle

--- src/Services/Audio/Service/WebM/Models/MatroskaDocument.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ActualChat.Audio.WebM.Models
-{
-    public sealed class MatroskaDocument
-    {
-        public EBML Ebml { get; set; } = null!;
-
-        public Segment Segment { get; set; } = null!;
-    }
-}
