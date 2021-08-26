fa87fba00e0cb4e42ee72a7010f18f21952b491e
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-26 11:45:44 +0300

    feat: calculate common prefix with just one previous transcription result

--- tests/Tests.Transcription/StablePrefixCutterTest.cs
+++ tests/Tests.Transcription/StablePrefixCutterTest.cs
@@ -17,9 +17,21 @@ namespace ActualChat.Tests.Transcription
         {
             var cutter = new StablePrefixCutter();
 
-            cutter.CutMemoized(new("раз", 1.86));
-            cutter.CutMemoized(new("работа", 1.98));
-            cutter.CutMemoized(new("разбор", 2.04));
+            var r1 = cutter.CutMemoized(new("раз", 1.86));
+            r1.Text.Should().Be("раз");
+            r1.TextIndex.Should().Be(0);
+            r1.StartOffset.Should().Be(0);
+            r1.Duration.Should().Be(1.86);
+            var r2 = cutter.CutMemoized(new("работа", 1.98));
+            r2.Text.Should().Be("бота");
+            r2.TextIndex.Should().Be(2);
+            r2.StartOffset.Should().Be(1.24);
+            r2.Duration.Should().Be(0.74);
+            var r3 = cutter.CutMemoized(new("разбор", 2.04));
+            r3.Text.Should().Be("збор");
+            r3.TextIndex.Should().Be(2);
+            r3.StartOffset.Should().Be(0.66);
+            r3.Duration.Should().Be(1.38);
             cutter.CutMemoized(new("работа", 2.22));
             cutter.CutMemoized(new("раз-два-три", 2.28));
             cutter.CutMemoized(new("раз-два-три-четыре", 2.76));
@@ -30,9 +42,9 @@ namespace ActualChat.Tests.Transcription
             r9.Text.Should().Be("раз-два-три-четыре-пять");
             cutter.CutMemoized(new("раз-два-три-четыре-пять", 4.32));
             var r11 = cutter.CutMemoized(new("раз-два-три-четыре-пять Вышел", 4.92));
-            r11.Text.Should().Be("Вышел");
+            r11.Text.Should().Be(" Вышел");
             var r12 = cutter.CutMemoized(new("раз-два-три-четыре-пять Вышел зайчик", 5.52));
-            r12.Text.Should().Be("зайчик");
+            r12.Text.Should().Be(" зайчик");
         }
     }
 }
