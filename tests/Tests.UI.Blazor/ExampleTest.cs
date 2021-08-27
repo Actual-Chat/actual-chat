1307e9465d8863250969e546d347a7d1b13f993b
iqmulator <72043982+iqmulator@users.noreply.github.com>
2021-08-27 10:55:07 +0500

    Remove todo (#5)
    * wip: remove Todo module
    
    * removed everything related to Todo

--- tests/Tests.UI.Blazor/ExampleTest.cs
+++ tests/Tests.UI.Blazor/ExampleTest.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using ActualChat.Chat.UI.Blazor;
 using ActualChat.Testing;
-using ActualChat.Todos;
-using ActualChat.Todos.UI.Blazor;
 using Blazorise;
 using Bunit;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using Stl.Fusion;
 using Stl.Fusion.Authentication;
+using Stl.Fusion.Blazor;
+using Stl.Fusion.Client;
 using Stl.Fusion.UI;
 using Stl.Testing;
 using Xunit;
@@ -21,43 +24,6 @@ namespace ActualChat.Tests.UI.Blazor
         public ExampleTest(ITestOutputHelper @out) : base(@out) { }
 
         [Fact]
-        public async Task TodoSummaryBadgeTest()
-        {
-            using var appHost = await TestHostFactory.NewAppHost();
-            using var blazorTester = appHost.NewBlazorTester();
-            var session = blazorTester.Session;
-            var uiCommandRunner = blazorTester.ScopedAppServices.GetRequiredService<UICommandRunner>();
-
-            // Create user & sign in
-            var user = await blazorTester.SignIn(new User("", "Alex"));
-            user.Id.Value.Should().NotBeNullOrEmpty();
-            user.Name.Should().Be("Alex");
-            user.Identities.Count.Should().Be(1);
-
-            // Render summary badge
-            var badge = blazorTester.RenderComponent<TodoSummaryBadge>(
-                parameters => parameters.AddAuthenticationState(blazorTester));
-            badge.RenderCount.Should().Be(1);
-            var badges = badge.FindComponents<Badge>();
-            badges.Count.Should().Be(2);
-            badges.First().MarkupMatches("<span class=\"badge badge-success\"><b>0</b> done</span>");
-            badges.Last().MarkupMatches("<span class=\"badge badge-primary\"><b>0</b> total</span>");
-
-            // Add item
-            var (todo1, _) = await uiCommandRunner.Run(
-                new AddOrUpdateTodoCommand(session, new Todo() { Title = "1" }));
-            todo1.Id.Should().NotBeNullOrEmpty();
-            await Task.Delay(100); // Must be enough for user-induced re-render to complete
-
-            // Check content again
-            badge.RenderCount.Should().BeGreaterThan(1);
-            badges = badge.FindComponents<Badge>();
-            badges.Count.Should().Be(2);
-            badges.First().MarkupMatches("<span class=\"badge badge-success\"><b>0</b> done</span>");
-            badges.Last().MarkupMatches("<span class=\"badge badge-primary\"><b>1</b> total</span>");
-        }
-
-        [Fact]
         public async Task SessionTest()
         {
             using var appHost = await TestHostFactory.NewAppHost();
@@ -67,6 +33,21 @@ namespace ActualChat.Tests.UI.Blazor
 
             Assert.NotNull(sessionA);
             sessionA.ToString().Length.Should().BeGreaterOrEqualTo(16);
+        }
+
+        [Fact]
+        public async Task ChatPageTest()
+        {
+            using var appHost = await TestHostFactory.NewAppHost();
+            using var blazorTester = appHost.NewBlazorTester();
+            var user = await blazorTester.SignIn(new User("", "Bob"));
+            user.Id.Value.Should().NotBeNullOrEmpty();
+            user.Name.Should().Be("user-Bob");
+            var page = blazorTester.RenderComponent<ChatPage>(parameters 
+                => parameters.AddAuthenticationState(blazorTester));
+            page.RenderCount.Should().Be(1);
+            var badges = page.FindComponents<Badge>();
+            badges.Count.Should().Be(1);
         }
     }
 }
