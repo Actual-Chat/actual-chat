2d7abc4ee48f3d0d5c40d2c5567b2eec682ab529
iqmulator <iqmulator@gmail.com>
2021-08-27 17:39:59 +0500

    WIP: test MS.Playwright

---
+++ tests/Tests.UI.Blazor/WebServerFixture.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using ActualChat.Testing;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+using Microsoft.Playwright;
+
+namespace ActualChat.Tests.UI.Blazor
+{
+    public class WebServerFixture : IAsyncLifetime, IDisposable
+    {
+        private readonly IHost _host;
+        private IPlaywright Playwright { get; set; }
+        public IBrowser Browser { get; private set; }
+        public string BaseUrl { get; }
+
+        public WebServerFixture()
+        {
+            var appHost = TestHostFactory.NewAppHost().Result;
+            BaseUrl = appHost.ServerUrls;
+            _host = appHost.Host;
+        }
+
+        public async Task InitializeAsync()
+        {
+            Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            Browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{ Headless = true });
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _host.StopAsync();
+            _host?.Dispose();
+            Playwright?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _host?.Dispose();
+            Playwright?.Dispose();
+        }
+        
+        private static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Any, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+    }
+}
