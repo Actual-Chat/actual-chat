de38afc0c8ecb02845dcecc1631ac4c083574bb9
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-30 16:42:20 -0700

    wip: virtual scroll

---
+++ src/UI.Blazor.Core/wwwroot/scripts/VirtualScroll.js
@@ -0,0 +1,31 @@
+export function create(elementRef, backendRef) {
+    return new VirtualScroll(elementRef, backendRef)
+}
+
+export class VirtualScroll {
+    constructor(elementRef, backendRef) {
+        this.elementRef = elementRef;
+        this.backendRef = backendRef;
+        this.abortController = new AbortController();
+
+        let pushStateUpdate = e => this.pushStateUpdate();
+        let listenerOptions = { signal: this.abortController.signal };
+        elementRef.addEventListener("scroll", pushStateUpdate, listenerOptions);
+        window.addEventListener("resize", pushStateUpdate, listenerOptions);
+    };
+
+    dispose() {
+        this.abortController.abort();
+    }
+
+    pushStateUpdate() {
+        this.backendRef.invokeMethodAsync("UpdateClientSideState", this.getState());
+    }
+
+    getState() {
+        return {
+            scrollTop: this.elementRef.scrollTop,
+            clientHeight: this.elementRef.clientHeight
+        };
+    }
+}
