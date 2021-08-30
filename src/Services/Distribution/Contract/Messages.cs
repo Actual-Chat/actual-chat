01ffb860c25ea7a9615859a87b826b25cdc393c0
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-08-30 12:02:16 +0300

    chore: wrap to chop at .editorconfig

--- src/Services/Distribution/Contract/Messages.cs
+++ src/Services/Distribution/Contract/Messages.cs
@@ -18,22 +18,5 @@ namespace ActualChat.Distribution
         [property: Key(1)] string TextIndex,
         [property: Key(2)] double StartOffset, 
         [property: Key(3)] double Duration);
-    //
-    // [Serializable]
-    // public class MessageVariant : Variant<ChatMessage>
-    // {
-    //     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), Newtonsoft.Json.JsonIgnore]
-    //     public AudioChatMessage? Audio { get => Get<AudioChatMessage>(); init => Set(value); }
-    //     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), Newtonsoft.Json.JsonIgnore]
-    //     public ChatEntryChatMessage? Chat { get => Get<ChatEntryChatMessage>(); init => Set(value); }
-    //     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), Newtonsoft.Json.JsonIgnore]
-    //     public TranscriptChatMessage? Transcript { get => Get<TranscriptChatMessage>(); init => Set(value); }
-    //     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), Newtonsoft.Json.JsonIgnore]
-    //     public ChatMessage? Other { get => Get<ChatMessage>(); init => Set(value); }
-    //
-    //     [JsonConstructor]
-    //     public MessageVariant() { }
-    //     [Newtonsoft.Json.JsonConstructor]
-    //     public MessageVariant(ChatMessage? value) : base(value) { }
-    // }
+
 }
