87f09c1272e54aefc8d5d894e00153bf26b8b841
Alex Yakunin <alex.yakunin@gmail.com>
2021-08-31 07:32:56 -0700

    feat: Playwright integration in tests

---
+++ src/Core/UriMapper.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ActualChat
+{
+    public class UriMapper
+    {
+        public virtual Uri BaseUri { get; }
+
+        public UriMapper(Uri baseUri)
+            => BaseUri = baseUri;
+
+        public virtual Uri ToAbsolute(string relativeUri)
+            => new(BaseUri, relativeUri);
+    }
+}
