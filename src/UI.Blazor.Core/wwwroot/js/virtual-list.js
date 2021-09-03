80e05cac246bcb19f16ac50edcfac16b5766d486
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-02 21:54:23 -0700

    wip: VirtualList (doesn't work yet)

--- src/UI.Blazor.Core/wwwroot/js/virtual-list.js
+++ src/UI.Blazor.Core/wwwroot/js/virtual-list.js
@@ -8,30 +8,40 @@ export class VirtualList {
         this.backendRef = backendRef;
         this.abortController = new AbortController();
 
-        let pushStateUpdate = e => this.pushStateUpdate();
+        let pushMeasurements = e => this.afterRender(true, true, false, 0);
         let listenerOptions = { signal: this.abortController.signal };
-        elementRef.addEventListener("scroll", pushStateUpdate, listenerOptions);
-        window.addEventListener("resize", pushStateUpdate, listenerOptions);
+        elementRef.addEventListener("scroll", pushMeasurements, listenerOptions);
+        window.addEventListener("resize", pushMeasurements, listenerOptions);
     };
 
     dispose() {
         this.abortController.abort();
     }
 
-    pushStateUpdate() {
-        // TODO(AY): Make sure measurement happens only for what's needed
-        // TODO(AY): Use ResizeObserver to track item size changes
-        this.backendRef.invokeMethodAsync("UpdateClientSideState", this.getState());
-    }
-
-    afterRender(mustMeasure, mustScroll, scrollOffset) {
-        // TODO(AY): Implement this
-    }
-
-    getState() {
-        return {
-            scrollTop: this.elementRef.scrollTop,
-            clientHeight: this.elementRef.clientHeight
+    afterRender(mustMeasure, mustMeasureAll, mustScroll, viewportOffset) {
+        let spacerSize = this.elementRef.querySelector(".spacer").getBoundingClientRect().height;
+        if (mustScroll)
+            this.elementRef.scrollTo(0, viewportOffset + spacerSize);
+        let state = {
+            renderIndex: parseInt(this.elementRef.dataset["renderIndex"]),
+            viewportOffset: this.elementRef.scrollTop - spacerSize,
+            viewportSize: this.elementRef.clientHeight,
+            itemSizes: {}
         };
+        if (mustMeasure) {
+            let items = this.elementRef.querySelectorAll(".measured .item").values();
+            for (let item of items) {
+                let key = item.dataset["key"];
+                state.itemSizes[key] = item.getBoundingClientRect().height;
+            }
+        }
+        if (mustMeasureAll) {
+            let items = this.elementRef.querySelectorAll(".shown .item").values();
+            for (let item of items) {
+                let key = item.dataset["key"];
+                state.itemSizes[key] = item.getBoundingClientRect().height;
+            }
+        }
+        this.backendRef.invokeMethodAsync("UpdateClientSideState", state);
     }
 }
