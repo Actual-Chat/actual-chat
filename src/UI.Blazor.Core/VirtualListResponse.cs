9cae1a17862ba130f4701bb0c497e21a59ac877a
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-06 23:58:07 -0700

    fix: bugs in VirtualList (wip)

--- src/UI.Blazor.Core/VirtualListResponse.cs
+++ src/UI.Blazor.Core/VirtualListResponse.cs
@@ -5,8 +5,8 @@ namespace ActualChat.UI.Blazor
 {
     public record VirtualListResponse<TItem>(List<KeyValuePair<string, TItem>> Items)
     {
-        public bool HasVeryFirstItem { get; init; } = false;
-        public bool HasVeryLastItem { get; init; } = false;
+        public bool HasVeryFirstItem { get; init; }
+        public bool HasVeryLastItem { get; init; }
 
         public VirtualListResponse() : this(new List<KeyValuePair<string, TItem>>()) { }
     }
