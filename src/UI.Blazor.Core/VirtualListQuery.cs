3ce415534f79a481656e251afbdfae57eb044779
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-06 23:58:07 -0700

    fix: bugs in VirtualList (wip)

--- src/UI.Blazor.Core/VirtualListQuery.cs
+++ src/UI.Blazor.Core/VirtualListQuery.cs
@@ -1,6 +1,6 @@
 namespace ActualChat.UI.Blazor
 {
-    public record VirtualListQuery(Range<string> KeyRange)
+    public record VirtualListQuery(Range<string> InclusiveKeyRange)
     {
         public double ExpandStartBy { get; init; }
         public double ExpandEndBy { get; init; }
