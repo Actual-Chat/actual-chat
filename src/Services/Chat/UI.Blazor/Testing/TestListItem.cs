112d7a5123de9621160aeee627db23e817fa003c
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-08 11:21:21 -0700

    wip: VirtualList.razor

---
+++ src/Services/Chat/UI.Blazor/Testing/TestListItem.cs
@@ -0,0 +1,9 @@
+namespace ActualChat.Chat.UI.Blazor.Testing
+{
+    public record TestListItem(
+        int Key,
+        string Title,
+        string Description = "",
+        double FontSize = 1
+    ) { }
+}
--- src/UI.Blazor.Core/VirtualListResponse.cs
+++ src/UI.Blazor.Core/VirtualListResponse.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace ActualChat.UI.Blazor
 {
@@ -9,5 +11,36 @@ namespace ActualChat.UI.Blazor
         public bool HasVeryLastItem { get; init; }
 
         public VirtualListResponse() : this(new List<KeyValuePair<string, TItem>>()) { }
+    }
+
+    public static class VirtualListResponse
+    {
+        public static VirtualListResponse<TItem> New<TItem>(
+            List<KeyValuePair<string, TItem>> items,
+            bool hasVeryFirstItem = false,
+            bool hasVeryLastItem = false)
+            => new(items) {
+                HasVeryFirstItem = hasVeryFirstItem,
+                HasVeryLastItem = hasVeryLastItem,
+            };
+
+        public static VirtualListResponse<TItem> New<TItem>(
+            IEnumerable<KeyValuePair<string, TItem>> items,
+            bool hasVeryFirstItem = false,
+            bool hasVeryLastItem = false)
+            => new(items.ToList()) {
+                HasVeryFirstItem = hasVeryFirstItem,
+                HasVeryLastItem = hasVeryLastItem,
+            };
+
+        public static VirtualListResponse<TItem> New<TItem>(
+            IEnumerable<TItem> items,
+            Func<TItem, string> keySelector,
+            bool hasVeryFirstItem = false,
+            bool hasVeryLastItem = false)
+            => new(items.Select(item => KeyValuePair.Create(keySelector.Invoke(item), item)).ToList()) {
+                HasVeryFirstItem = hasVeryFirstItem,
+                HasVeryLastItem = hasVeryLastItem,
+            };
     }
 }
