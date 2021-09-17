9d15aa1d0bd5591fedc8105984808c5e0e236385
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-16 23:01:37 -0700

    fix: warnings

--- src/Streaming.Server/StreamIdExt.cs
+++ src/Streaming.Server/StreamIdExt.cs
@@ -3,6 +3,6 @@ namespace ActualChat.Streaming.Server
     public static class StreamIdExt
     {
         public static string GetRedisChannelName(this StreamId streamId)
-            => $"stream-{streamId.Value}";
+            => $"stream-{(string) streamId}";
     }
 }
