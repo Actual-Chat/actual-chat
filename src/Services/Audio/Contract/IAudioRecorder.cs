60cc3171db5d7c23c064abaacf0e8891ab628f47
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-21 14:46:54 -0700

    refactor: remove IRecorder<T>

--- src/Services/Audio/Contract/IAudioRecorder.cs
+++ src/Services/Audio/Contract/IAudioRecorder.cs
@@ -1,7 +1,17 @@
-using ActualChat.Streaming;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using ActualChat.Blobs;
+using Stl.Fusion.Authentication;
 
 namespace ActualChat.Audio
 {
-    public interface IAudioRecorder : IRecorder<AudioRecord>
-    { }
+    public interface IAudioRecorder
+    {
+        Task Record(
+            Session session,
+            AudioRecord record,
+            ChannelReader<BlobPart> content,
+            CancellationToken cancellationToken);
+    }
 }
