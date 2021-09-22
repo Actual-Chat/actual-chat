6f31cc9e1959c3c07870122bd77bbb5938ba18c4
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-09-22 09:04:10 +0300

    feat: fail-safe audio record reader interface

---
+++ src/Core/Streaming/RecordWorkItem.cs
@@ -0,0 +1,16 @@
+using System.Runtime.Serialization;
+using Stl;
+
+namespace ActualChat.Streaming
+{
+    [DataContract]
+    public record RecordWorkItem<TRecordId, TRecord>(
+        [property: DataMember(Order = 0)] TRecord Record,
+        [property: DataMember(Order = 1)] RecordReadProgress<TRecordId>? CurrentProgress)
+        where TRecordId : struct
+        where TRecord : class, IHasId<TRecordId>
+    {
+        public RecordWorkItem() : this(default!, default)
+        { }
+    }
+}
