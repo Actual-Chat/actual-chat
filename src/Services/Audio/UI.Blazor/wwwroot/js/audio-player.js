cec9103d95ae4e8095c48b40454c987829ee047e
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-09-27 20:51:39 +0300

    wip: base64->Uint8Array

--- src/Services/Audio/UI.Blazor/wwwroot/js/audio-player.js
+++ src/Services/Audio/UI.Blazor/wwwroot/js/audio-player.js
@@ -2,6 +2,13 @@ let mediaSource = null;
 let sourceBuffer = null;
 let audio = null;
 
+async function base64ToBlob(base64) {
+    let dataUrl = "data:application/octet-binary;base64," + base64;
+    let res = await fetch(dataUrl);
+    let buffer = await res.arrayBuffer();
+    return new Uint8Array(buffer);
+}
+
 export async function initialize() {
     audio = document.getElementById('player');
     mediaSource = new MediaSource();
@@ -15,9 +22,10 @@ export async function initialize() {
     audio.src = URL.createObjectURL(mediaSource);
 }
 
-export async function appendAudio(blob){
+export async function appendAudio(base64){
     if (mediaSource === null)
         return;
     
+    let blob = await base64ToBlob(base64);
     sourceBuffer.appendBuffer(blob);
 }
