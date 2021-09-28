8bb9e20e5ecadd818cc611524b900818a97df724
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-09-28 19:43:05 +0300

    feat: basic audio player that works only once

--- src/Services/Audio/UI.Blazor/wwwroot/js/audio-player.js
+++ src/Services/Audio/UI.Blazor/wwwroot/js/audio-player.js
@@ -1,31 +1,56 @@
-let mediaSource = null;
-let sourceBuffer = null;
-let audio = null;
+export function create(elementRef, backendRef) {
+    return new AudioPlayer(elementRef, backendRef);
+}
+
+export class AudioPlayer {
+    constructor(elementRef, backendRef) {
+        this.elementRef = elementRef;
+        this.backendRef = backendRef;
+        this.sourceBuffer = null;
+        this.bufferQueue = [];
+        this.mediaSource = new MediaSource();
+
+        this.mediaSource.addEventListener('sourceopen', e => {
+            URL.revokeObjectURL(this.elementRef.src);
+
+            let mime = 'audio/webm; codecs=opus';
+            this.sourceBuffer = this.mediaSource.addSourceBuffer(mime);
+            this.sourceBuffer.addEventListener('updateend', sbe => {
+               if (this.bufferQueue.length) {
+                   this.sourceBuffer.appendBuffer(this.bufferQueue.shift());
+               } 
+            });
+        });
+        
+        this.elementRef.src = URL.createObjectURL(this.mediaSource);
+    }
+    
+    dispose() {
+        this.mediaSource.endOfStream();
+    }
+    
+    async appendAudio(base64) {
+        let blob = await base64ToBlob(base64);
+        if (this.sourceBuffer.updating) {
+            this.bufferQueue.push(blob);
+        }
+        else {
+            this.sourceBuffer.appendBuffer(blob);
+        }
+    }
+    
+    stop() {
+        this.mediaSource.endOfStream();
+    }
+}
+
+// let mediaSource = null;
+// let sourceBuffer = null;
+// let audio = null;
 
 async function base64ToBlob(base64) {
     let dataUrl = "data:application/octet-binary;base64," + base64;
     let res = await fetch(dataUrl);
     let buffer = await res.arrayBuffer();
     return new Uint8Array(buffer);
-}
-
-export async function initialize() {
-    audio = document.getElementById('player');
-    mediaSource = new MediaSource();
-    
-    mediaSource.addEventListener('sourceopen', e => {
-        URL.revokeObjectURL(audio.src);
-        
-        let mime = 'audio/webm; codecs=opus';
-        sourceBuffer = mediaSource.addSourceBuffer(mime);
-    });
-    audio.src = URL.createObjectURL(mediaSource);
-}
-
-export async function appendAudio(base64){
-    if (mediaSource === null)
-        return;
-    
-    let blob = await base64ToBlob(base64);
-    sourceBuffer.appendBuffer(blob);
 }
