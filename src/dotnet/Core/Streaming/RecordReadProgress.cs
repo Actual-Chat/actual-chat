f1e274fda0e6bd31990f630e106aecc084af4b69
Alex Yakunin <alex.yakunin@gmail.com>
2021-09-29 19:19:46 -0700

    fix: a few more fixes unrelated to merge

--- src/dotnet/Core/Streaming/RecordReadProgress.cs
+++ src/dotnet/Core/Streaming/RecordReadProgress.cs
@@ -1,17 +1,13 @@
-using System.Runtime.Serialization;
-using Stl;
+namespace ActualChat.Streaming;
 
-namespace ActualChat.Streaming
+[DataContract]
+public record RecordReadProgress<TRecordId>(
+    [property: DataMember(Order = 0)] TRecordId Id,
+    [property: DataMember(Order = 1)] int StreamNumber,
+    [property: DataMember(Order = 2)] StreamId StreamId,
+    [property: DataMember(Order = 3)] int StreamStartedWithBlobPartNumber,
+    [property: DataMember(Order = 4)] int CurrentBlobPartNumber) : IHasId<TRecordId> where TRecordId : struct
 {
-    [DataContract]
-    public record RecordReadProgress<TRecordId>(
-        [property: DataMember(Order = 0)] TRecordId Id,
-        [property: DataMember(Order = 1)] int StreamNumber,
-        [property: DataMember(Order = 2)] StreamId StreamId,
-        [property: DataMember(Order = 3)] int StreamStartedWithBlobPartNumber,
-        [property: DataMember(Order = 4)] int CurrentBlobPartNumber) : IHasId<TRecordId> where TRecordId : struct
-    {
-        public RecordReadProgress() : this(default, 0, StreamId.None, 0, 0) 
-        { }
-    }
+    public RecordReadProgress() : this(default, 0, StreamId.None, 0, 0)
+    { }
 }
--- src/dotnet/Core/Streaming/RecordWorkItem.cs
+++ src/dotnet/Core/Streaming/RecordWorkItem.cs
@@ -1,15 +1,11 @@
-using System.Runtime.Serialization;
-using Stl;
+namespace ActualChat.Streaming;
 
-namespace ActualChat.Streaming
+[DataContract]
+public record RecordWorkItem<TRecordId, TRecord>(
+    [property: DataMember(Order = 0)] TRecord Record,
+    [property: DataMember(Order = 1)] RecordReadProgress<TRecordId>? CurrentProgress)
+    where TRecordId : struct
+    where TRecord : class, IHasId<TRecordId>
 {
-    [DataContract]
-    public record RecordWorkItem<TRecordId, TRecord>(
-        [property: DataMember(Order = 0)] TRecord Record,
-        [property: DataMember(Order = 1)] RecordReadProgress<TRecordId>? CurrentProgress)
-        where TRecordId : struct
-        where TRecord : class, IHasId<TRecordId>
-    {
-        public RecordWorkItem() : this(default!, default) { }
-    }
+    public RecordWorkItem() : this(default!, default) { }
 }
