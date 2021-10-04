05e20e3005d53c189fcc59456a52a276db3f3051
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-10-04 19:53:36 +0300

    feat: media frame extractor

---
+++ tests/Audio.UnitTests/MediaFrameExtractorTest.cs
@@ -0,0 +1,59 @@
+using System.Buffers;
+using System.Threading.Channels;
+using ActualChat.Blobs;
+using Grpc.Core;
+using Stl.Time;
+using Channel = System.Threading.Channels.Channel;
+
+namespace ActualChat.Audio.UnitTests;
+
+
+public class MediaFrameExtractorTest
+{
+    [Fact]
+    public async Task ExtractFramesFromFile()
+    {
+        var mediaFrameExtractor = new MediaFrameExtractor<AudioFrame>(MomentClockSet.Default);
+        var blobChannel = Channel.CreateUnbounded<BlobPart>();
+        var audioFrameReader = mediaFrameExtractor.ExtractMediaFrames(blobChannel.Reader, CancellationToken.None);
+
+        _ = ReadFromFile(blobChannel.Writer);
+
+        var offset = TimeSpan.Zero;
+        while (await audioFrameReader.WaitToReadAsync())
+        while (audioFrameReader.TryRead(out var audioFrame))
+            offset = audioFrame.Offset > offset
+                ? audioFrame.Offset
+                : offset;
+
+        offset.Should().Be(TimeSpan.FromMilliseconds(12120));
+    }
+
+
+    private static async Task<int> ReadFromFile(ChannelWriter<BlobPart> writer)
+    {
+        var size = 0;
+        await using var inputStream = new FileStream(
+            Path.Combine(Environment.CurrentDirectory, @"data", "file.webm"),
+            FileMode.Open,
+            FileAccess.Read);
+        using var readBufferLease = MemoryPool<byte>.Shared.Rent(1 * 1024);
+        var readBuffer = readBufferLease.Memory;
+        var index = 0;
+        var bytesRead = await inputStream.ReadAsync(readBuffer);
+        size += bytesRead;
+        while (bytesRead > 0) {
+            var command = new BlobPart(
+                index++,
+                readBuffer[..bytesRead].ToArray());
+            await writer.WriteAsync(command, CancellationToken.None);
+
+            // await Task.Delay(300); //emulate real-time speech delay
+            bytesRead = await inputStream.ReadAsync(readBuffer);
+            size += bytesRead;
+        }
+
+        writer.Complete();
+        return size;
+    }
+}
