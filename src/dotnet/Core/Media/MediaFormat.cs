1a16aafa8b8f97570fb220812e0d78d3f20239ed
Alex Yakunin <alex.yakunin@gmail.com>
2021-10-04 23:01:59 -0700

    chore: MediaFormat.cs

--- src/dotnet/Core/Media/MediaFormat.cs
+++ src/dotnet/Core/Media/MediaFormat.cs
@@ -6,7 +6,5 @@ public abstract record MediaFormat
     public abstract MediaType Type { get; }
 
     [DataMember(Order = 0)]
-    public int ChannelCount { get; init; }
-    [DataMember(Order = 1)]
-    public int SampleRate { get; init; }
+    public int ChannelCount { get; init; } = 1;
 }
