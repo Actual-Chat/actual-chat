5154d6063c987626fbe9ef89d2eddf6802e9bdb0
Vladimir Chirikov <vovanchig@gmail.com>
2021-10-07 02:10:23 +0300

    fix: add DisposeSilentAsync that mutes JSDisconnectedException

---
+++ src/dotnet/UI.Blazor/IJSObjectReferenceExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.JSInterop;
+
+namespace ActualChat.UI.Blazor;
+
+public static class IJSObjectReferenceExtensions
+{
+    /// <summary>
+    /// Workaround for blazor server after <seealso href="https://github.com/dotnet/aspnetcore/pull/32901"/>
+    /// Mutes only <see cref="Microsoft.JSInterop.JSDisconnectedException" />
+    /// </summary>
+    public static async ValueTask DisposeSilentAsync(this IJSObjectReference jsRef)
+    {
+        try {
+            await jsRef.DisposeAsync().ConfigureAwait(true);
+        }
+        catch (JSDisconnectedException) { }
+    }
+}
