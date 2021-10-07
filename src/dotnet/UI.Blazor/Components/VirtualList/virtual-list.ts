0c8db9b55efe8b3f9c6d7b3805743c09e5e93c64
Vladimir Chirikov <vovanchig@gmail.com>
2021-10-07 14:22:56 +0300

    refactor: migrate virtual list to ts

--- src/dotnet/UI.Blazor/Components/VirtualList/virtual-list.ts
+++ src/dotnet/UI.Blazor/Components/VirtualList/virtual-list.ts
@@ -1,34 +1,46 @@
 import './virtual-list.css'
 
 export class VirtualList {
-    static create(elementRef, backendRef) {
+    private _elementRef: HTMLElement;
+    private _blazorRef: DotNet.DotNetObject;
+    private _spacerRef: HTMLElement;
+    private _displayedItemsRef: HTMLElement;
+    private _abortController: AbortController;
+
+    private _resizeObserver: ResizeObserver;
+    private _resizedOnce: Map<Element, boolean>;
+    private _updateClientSideStateTask: Promise<unknown> | null;
+    private _onScrollStoppedTimeout: any;
+    private _onResizeTimeout: any;
+
+    static create(elementRef: HTMLElement, backendRef: DotNet.DotNetObject) {
         return new VirtualList(elementRef, backendRef)
     }
 
-    constructor(elementRef, backendRef) {
-        this.elementRef = elementRef;
-        this.backendRef = backendRef;
-        this.abortController = new AbortController();
-        this.spacerRef = this.elementRef.querySelector(".spacer");
-        this.displayedItemsRef = this.elementRef.querySelector(".items-displayed");
-        this._updateClientSideStateTask = null;
-        this._onScrollStoppedTimeout = null;
+    constructor(elementRef: HTMLElement, backendRef: DotNet.DotNetObject) {
+        this._elementRef = elementRef;
+        this._blazorRef = backendRef;
+        this._abortController = new AbortController();
+        this._spacerRef = this._elementRef.querySelector(".spacer")!;
+        this._displayedItemsRef = this._elementRef.querySelector(".items-displayed") as HTMLElement;
+        this._updateClientSideStateTask = null!;
+        this._onScrollStoppedTimeout = null!;
         this._resizeObserver = new ResizeObserver(entries => this.onResize(entries));
-        this._resizedOnce = { };
+        this._resizedOnce = new Map<Element, boolean>();
 
-        let listenerOptions = { signal: this.abortController.signal };
+        let listenerOptions = { signal: this._abortController.signal };
         elementRef.addEventListener("scroll", _ => this.updateClientSideStateAsync(), listenerOptions);
     };
 
     dispose() {
-        this.abortController.abort();
+        this._abortController.abort();
         this._resizeObserver.disconnect();
     }
 
     afterRender(mustScroll, viewOffset, mustNotifyWhenScrollStops) {
         let spacerSize = this.getSpacerSize();
         if (mustScroll)
-            this.elementRef.scrollTo(0, viewOffset + spacerSize);
+            this._elementRef.scrollTo(0, viewOffset + spacerSize);
         let _ = this.updateClientSideStateAsync()
         this.setupResizeTracking();
         this.setupScrollTracking(mustNotifyWhenScrollStops);
@@ -59,20 +71,22 @@ export class VirtualList {
 
     setupResizeTracking() {
         this._resizeObserver.disconnect();
-        this._resizedOnce = { };
+        this._resizedOnce = new Map<Element, boolean>();
         if (this._onResizeTimeout != null)
             clearTimeout(this._onResizeTimeout);
-        let items = this.elementRef.querySelectorAll(".items-displayed .item").values();
-        this._resizeObserver.observe(this.elementRef);
+        let items = this._elementRef.querySelectorAll(".items-displayed .item").values();
+        this._resizeObserver.observe(this._elementRef);
         for (let item of items)
             this._resizeObserver.observe(item);
     }
 
-    onResize(entries) {
+    onResize(entries: ResizeObserverEntry[]) {
         let mustIgnore = false;
         for (let entry of entries) {
-            mustIgnore |= this._resizedOnce[entry.target] != null;
-            this._resizedOnce[entry.target] = true;
+            if (this._resizedOnce.has(entry.target)) {
+                mustIgnore = true;
+            }
+            this._resizedOnce.set(entry.target, true);
         }
         if (mustIgnore)
             return;
@@ -94,35 +108,35 @@ export class VirtualList {
         }
         let spacerSize = this.getSpacerSize();
         let state = {
-            renderIndex: parseInt(this.elementRef.dataset["renderIndex"]),
+            renderIndex: parseInt(this._elementRef.dataset["renderIndex"]!),
             isScrollStopped: isScrollStopped,
-            viewOffset: this.elementRef.scrollTop - spacerSize,
-            viewSize: this.elementRef.getBoundingClientRect().height,
+            viewOffset: this._elementRef.scrollTop - spacerSize,
+            viewSize: this._elementRef.getBoundingClientRect().height,
             itemSizes: {}
         };
         if (!isScrollStopped) {
-            let items = this.elementRef.querySelectorAll(".items-unmeasured .item").values();
+            let items = this._elementRef.querySelectorAll(".items-unmeasured .item").values() as IterableIterator<HTMLElement>;
             for (let item of items) {
                 let key = item.dataset["key"];
                 state.itemSizes[key] = item.getBoundingClientRect().height;
             }
-            items = this.elementRef.querySelectorAll(".items-displayed .item").values();
+            items = this._elementRef.querySelectorAll(".items-displayed .item").values() as IterableIterator<HTMLElement>;
             for (let item of items) {
                 let key = item.dataset["key"];
-                let knownSize = parseFloat(item.dataset["size"])
+                let knownSize = parseFloat(item.dataset["size"]!)
                 let size = item.getBoundingClientRect().height;
                 if (Math.abs(size - knownSize) >= 0.001)
                     state.itemSizes[key] = size;
             }
         }
-        this._updateClientSideStateTask = this.backendRef.invokeMethodAsync("UpdateClientSideState", state);
+        this._updateClientSideStateTask = this._blazorRef.invokeMethodAsync("UpdateClientSideState", state);
     }
 
     // Helpers
 
     getSpacerSize() {
-        let entriesTop = this.displayedItemsRef.getBoundingClientRect().top;
-        let spacerTop = this.spacerRef.getBoundingClientRect().top;
+        let entriesTop = this._displayedItemsRef.getBoundingClientRect().top;
+        let spacerTop = this._spacerRef.getBoundingClientRect().top;
         return entriesTop - spacerTop;
     }
 }
