1c31e20c65ada5514e202a38475e53d99f4b5748
Alex Yakunin <alex.yakunin@gmail.com>
2021-10-06 21:25:23 -0700

    fix: remove unused setting

--- src/dotnet/Audio.Service/AudioSettings.cs
+++ src/dotnet/Audio.Service/AudioSettings.cs
@@ -5,5 +5,4 @@ public class AudioSettings
     public string Db { get; set; } =
         "Server=localhost;Database=ac_dev_audio;Port=5432;User Id=postgres;Password=ActualChat.Dev.2021.07";
     public string Redis { get; set; } = "localhost";
-    public bool UseMessagePackWithSignalR { get; set; } = false;
 }
