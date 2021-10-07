9261d1ef74eafbef759be47de7b2a412fbec9149
Alex Yakunin <alex.yakunin@gmail.com>
2021-10-06 18:44:04 -0700

    misc: update .cmd files

--- src/dotnet/UI.Blazor/IBlazorUIModule.cs
+++ src/dotnet/UI.Blazor/IBlazorUIModule.cs
@@ -2,9 +2,12 @@ namespace ActualChat.UI.Blazor;
 
 public interface IBlazorUIModule
 {
+// NOTE(AY): Temporarily commented out - abstract static members aren't supported by Rider EAP yet
+/*
     /// <summary>
     /// The lowercase name of the module in a webpack bundle <br/>
     /// Must be in sync with <c>src/nodejs/index.ts</c> import ); <br/>
     /// </summary>
     static abstract string ImportName { get; }
+*/
 }
