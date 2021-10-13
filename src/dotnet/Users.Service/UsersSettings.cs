ed226e3db1e60ec59761f29a3157f4d8b2bafd5c
Alex Yakunin <alex.yakunin@gmail.com>
2021-10-12 19:51:49 -0700

    chore: minor changes

--- src/dotnet/Users.Service/UsersSettings.cs
+++ src/dotnet/Users.Service/UsersSettings.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Text;
-using Castle.Core.Configuration;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+namespace ActualChat.Users;
 
-namespace ActualChat.Users
+public class UsersSettings
 {
-    public class UsersSettings
-    {
-        // DBs
-        public string Db { get; set; } = null!;
+    // DBs
+    public string Db { get; set; } = null!;
 
-        // Auth provider settings
-        public string MicrosoftAccountClientId { get; set; } = null!;
-        public string MicrosoftAccountClientSecret { get; set; } = null!;
-        public string GitHubClientId { get; set; } = null!;
-        public string GitHubClientSecret { get; set; } = null!;
-    }
+    // Auth provider settings
+    public string MicrosoftAccountClientId { get; set; } = null!;
+    public string MicrosoftAccountClientSecret { get; set; } = null!;
+    public string GitHubClientId { get; set; } = null!;
+    public string GitHubClientSecret { get; set; } = null!;
 }
