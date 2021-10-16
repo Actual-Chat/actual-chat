0d56c86e5a0ebe03c38e6eb9910a064b48afefe9
Vladimir Chirikov <vovanchig@gmail.com>
2021-10-17 02:05:52 +0300

    build: downgrade the build project to net5.0

--- build/CliWrapCommandExtensions.cs
+++ build/CliWrapCommandExtensions.cs
@@ -1,35 +1,39 @@
+using System;
+using System.IO;
 using CliWrap;
 using static Crayon.Output;
 
-namespace Build;
-
-internal static class CliWrapCommandExtensions
+namespace Build
 {
-    private static readonly Stream _stdout = Console.OpenStandardOutput();
-    private static readonly Stream _stderr = Console.OpenStandardError();
 
-    internal static Command ToConsole(this Command command) => command | (_stdout, _stderr);
-    internal static Command ToConsole(this Command command, string prefix) => command | (s => Console.WriteLine(prefix + Colorize(s)), s => Console.Error.WriteLine(prefix + Red(s)));
-
-    /// <summary>
-    /// Stdout redirect won't use colors from the original output, we should use pseudo console to correct redirect colors.
-    /// <see href="https://github.com/microsoft/terminal/blob/main/samples/ConPTY/MiniTerm/MiniTerm/Program.cs#L16" />
-    /// For now we don't do this and use the simplest solution.
-    /// </summary>
-    public static string Colorize(string str)
+    internal static class CliWrapCommandExtensions
     {
-        if (str.Contains("error", StringComparison.OrdinalIgnoreCase))
-            return Red(str);
+        private static readonly Stream _stdout = Console.OpenStandardOutput();
+        private static readonly Stream _stderr = Console.OpenStandardError();
 
-        if (str.Contains("Exception:", StringComparison.OrdinalIgnoreCase))
-            return Red(str);
+        internal static Command ToConsole(this Command command) => command | (_stdout, _stderr);
+        internal static Command ToConsole(this Command command, string prefix) => command | (s => Console.WriteLine(prefix + Colorize(s)), s => Console.Error.WriteLine(prefix + Red(s)));
 
-        if (str.Contains(" warning ", StringComparison.OrdinalIgnoreCase))
-            return Yellow(str);
+        /// <summary>
+        /// Stdout redirect won't use colors from the original output, we should use pseudo console to correct redirect colors.
+        /// <see href="https://github.com/microsoft/terminal/blob/main/samples/ConPTY/MiniTerm/MiniTerm/Program.cs#L16" />
+        /// For now we don't do this and use the simplest solution.
+        /// </summary>
+        public static string Colorize(string str)
+        {
+            if (str.Contains("error", StringComparison.OrdinalIgnoreCase))
+                return Red(str);
 
-        if (str.Contains("watch :", StringComparison.OrdinalIgnoreCase))
-            return Underline(Magenta(str));
+            if (str.Contains("Exception:", StringComparison.OrdinalIgnoreCase))
+                return Red(str);
 
-        return str;
+            if (str.Contains(" warning ", StringComparison.OrdinalIgnoreCase))
+                return Yellow(str);
+
+            if (str.Contains("watch :", StringComparison.OrdinalIgnoreCase))
+                return Underline(Magenta(str));
+
+            return str;
+        }
     }
 }
