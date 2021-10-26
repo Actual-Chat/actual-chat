abe0eca50fba9c958515fbc1c1790ed519cd8c11
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-10-26 20:51:45 +0300

    refactor(core): get rid of double at ConvertingSizeMeasure

--- src/dotnet/Core/Mathematics/LogCover.cs
+++ src/dotnet/Core/Mathematics/LogCover.cs
@@ -4,17 +4,17 @@ namespace ActualChat.Mathematics;
 
 public static class LogCover
 {
+    public static ConvertingLogTileCover<TPoint, TSize> New<TPoint, TSize>(
+        LongLogTileCover baseTiles,
+        ConvertingSizeMeasure<TPoint, TSize> sizeMeasure)
+        where TPoint : notnull
+        where TSize : notnull
+        => new (baseTiles, sizeMeasure);
+
     public static class Default
     {
         public static LogTileCover<long, long> Long { get; } = new LongLogTileCover();
         public static LogTileCover<double, double> Double { get; } = new DoubleLogTileCover();
         public static LogTileCover<Moment, TimeSpan> Moment { get; } = new MomentLogTileCover();
     }
-
-    public static ConvertingLogTileCover<TPoint, TSize> New<TPoint, TSize>(
-        DoubleLogTileCover baseTiles,
-        ConvertingSizeMeasure<TPoint, TSize> sizeMeasure)
-        where TPoint : notnull
-        where TSize : notnull
-        => new(baseTiles, sizeMeasure);
 }
