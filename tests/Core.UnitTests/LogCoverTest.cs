660959cb684ee1a86c64e36939e024a584b5fe8b
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-10-26 20:24:04 +0300

    chore: extra test for MomentLogCover

--- tests/Core.UnitTests/LogCoverTest.cs
+++ tests/Core.UnitTests/LogCoverTest.cs
@@ -1,81 +1,104 @@
 using ActualChat.Mathematics;
 
-namespace ActualChat.Core.UnitTests
+namespace ActualChat.Core.UnitTests;
+
+public class LogCoverTest : TestBase
 {
-    public class LogCoverTest : TestBase
+    public LogCoverTest(ITestOutputHelper @out) : base(@out) { }
+
+    [Fact]
+    public void LongLogCoverTest()
     {
-        public LogCoverTest(ITestOutputHelper @out) : base(@out) { }
+        var c = LogCover.Default.Long;
+        c.MinTileSize.Should().Be(16);
+        c.TileSizeFactor.Should().Be(4);
+        c.TileSizes.First().Should().Be(c.MinTileSize);
+        c.TileSizes.Last().Should().Be(c.MaxTileSize);
+        (c.TileSizes[1] / c.MinTileSize).Should().Be(c.TileSizeFactor);
+        c.TileSizes.Length.Should().Be(6);
 
-        [Fact]
-        public void MomentLogCoverTest()
-        {
-            var c = LogCover.Default.Moment;
-            c.MinTileSize.Should().Be(TimeSpan.FromMinutes(3));
-            c.TileSizeFactor.Should().Be(4);
-            c.TileSizes.First().Should().Be(c.MinTileSize);
-            c.TileSizes.Last().Should().Be(c.MaxTileSize);
-            (c.TileSizes[1] / c.MinTileSize).Should().Be(c.TileSizeFactor);
-            c.TileSizes.Length.Should().Be(11);
+        c.GetTileStart(-16, 0)
+            .Should()
+            .Be(-16);
 
-            c.GetTileStart(c.Zero - TimeSpan.FromMinutes(1), 0)
-                .Should().Be(c.Zero - TimeSpan.FromMinutes(3));
+        c.GetTileStart(1, 0)
+            .Should()
+            .Be(0);
+        c.GetTileStart(17, 0)
+            .Should()
+            .Be(16);
+        c.GetTileStart(16, 1)
+            .Should()
+            .Be(0);
+        c.GetTileStart(257, 1)
+            .Should()
+            .Be(256);
 
-            c.GetTileStart(c.Zero + TimeSpan.FromMinutes(1), 0)
-                .Should().Be(c.Zero);
-            c.GetTileStart(c.Zero + TimeSpan.FromMinutes(4), 0)
-                .Should().Be(c.Zero + TimeSpan.FromMinutes(3));
-            c.GetTileStart(c.Zero + TimeSpan.FromMinutes(4), 1)
-                .Should().Be(c.Zero + TimeSpan.FromMinutes(0));
-            c.GetTileStart(c.Zero + TimeSpan.FromMinutes(25), 1)
-                .Should().Be(c.Zero + TimeSpan.FromMinutes(24));
+        c.GetMinCoveringTile((-16, 0))
+            .Should()
+            .Be((Range<long>)(-16, 0));
+        c.GetMinCoveringTile((-17, 0))
+            .Should()
+            .Be((Range<long>)(-64, 0));
 
-            c.GetMinCoveringTile((Range<Moment>) (c.Zero - TimeSpan.FromMinutes(1), c.Zero))
-                .Should().Be((Range<Moment>) (c.Zero - TimeSpan.FromMinutes(3), c.Zero));
-        }
+        c.GetTileCover((-17, 257))
+            .Should()
+            .BeEquivalentTo(new Range<long>[] {
+                (-32, -16),
+                (-16, 0),
+                (0, 256),
+                (256, 256 + 16),
+            });
 
-        [Fact]
-        public void LongLogCoverTest()
-        {
-            var c = LogCover.Default.Long;
-            c.MinTileSize.Should().Be(16);
-            c.TileSizeFactor.Should().Be(4);
-            c.TileSizes.First().Should().Be(c.MinTileSize);
-            c.TileSizes.Last().Should().Be(c.MaxTileSize);
-            (c.TileSizes[1] / c.MinTileSize).Should().Be(c.TileSizeFactor);
-            c.TileSizes.Length.Should().Be(6);
+        c.GetTileCover((-65, 257))
+            .Should()
+            .BeEquivalentTo(new Range<long>[] {
+                (-80, -64),
+                (-64, 0),
+                (0, 256),
+                (256, 256 + 16),
+            });
+    }
 
-            c.GetTileStart(-16, 0)
-                .Should().Be(-16);
+    [Fact]
+    public void MomentLogCoverGetCoveringTilesTest()
+    {
+        var c = LogCover.Default.Moment;
+        var coveringTiles = c.GetCoveringTiles(new Moment(16348332675742660));
+        foreach (var tile in coveringTiles)
+            c.AssertIsTile(tile);
+    }
 
-            c.GetTileStart(1, 0)
-                .Should().Be(0);
-            c.GetTileStart(17, 0)
-                .Should().Be(16);
-            c.GetTileStart(16, 1)
-                .Should().Be(0);
-            c.GetTileStart(257, 1)
-                .Should().Be(256);
+    [Fact]
+    public void MomentLogCoverTest()
+    {
+        var c = LogCover.Default.Moment;
+        c.MinTileSize.Should().Be(TimeSpan.FromMinutes(3));
+        c.TileSizeFactor.Should().Be(4);
+        c.TileSizes.First().Should().Be(c.MinTileSize);
+        c.TileSizes.Last().Should().Be(c.MaxTileSize);
+        (c.TileSizes[1] / c.MinTileSize).Should().Be(c.TileSizeFactor);
+        c.TileSizes.Length.Should().Be(11);
 
-            c.GetMinCoveringTile((-16, 0))
-                .Should().Be((Range<long>) (-16, 0));
-            c.GetMinCoveringTile((-17, 0))
-                .Should().Be((Range<long>) (-64, 0));
+        c.GetTileStart(c.Zero - TimeSpan.FromMinutes(1), 0)
+            .Should()
+            .Be(c.Zero - TimeSpan.FromMinutes(3));
 
-            c.GetTileCover((-17, 257))
-                .Should().BeEquivalentTo(new Range<long>[] {
-                    (-32, -16),
-                    (-16, 0),
-                    (0, 256),
-                    (256, 256 + 16),
-                });
+        c.GetTileStart(c.Zero + TimeSpan.FromMinutes(1), 0)
+            .Should()
+            .Be(c.Zero);
+        c.GetTileStart(c.Zero + TimeSpan.FromMinutes(4), 0)
+            .Should()
+            .Be(c.Zero + TimeSpan.FromMinutes(3));
+        c.GetTileStart(c.Zero + TimeSpan.FromMinutes(4), 1)
+            .Should()
+            .Be(c.Zero + TimeSpan.FromMinutes(0));
+        c.GetTileStart(c.Zero + TimeSpan.FromMinutes(25), 1)
+            .Should()
+            .Be(c.Zero + TimeSpan.FromMinutes(24));
 
-            c.GetTileCover((-65, 257))
-                .Should().BeEquivalentTo(new Range<long>[] {
-                    (-80, -64),
-                    (-64, 0),
-                    (0, 256),
-                    (256, 256 + 16),
-                });
-        }
+        c.GetMinCoveringTile((c.Zero - TimeSpan.FromMinutes(1), c.Zero))
+            .Should()
+            .Be((Range<Moment>)(c.Zero - TimeSpan.FromMinutes(3), c.Zero));
     }
 }
