c70d7bbb29b572359a42f25e98d2f93dcd3db6a5
Alex Yakunin <alex.yakunin@gmail.com>
2021-11-10 01:56:09 -0800

    feat: add buffering where it makes sense

--- src/dotnet/Audio.Service/AudioSourceStreamer.cs
+++ src/dotnet/Audio.Service/AudioSourceStreamer.cs
@@ -6,6 +6,7 @@ namespace ActualChat.Audio;
 
 public class AudioSourceStreamer : IAudioSourceStreamer
 {
+    private const int StreamBufferSize = 64;
     private readonly RedisDb _redisDb;
 
     public AudioSourceStreamer(RedisDb<AudioContext> audioRedisDb)
@@ -24,7 +25,7 @@ public class AudioSourceStreamer : IAudioSourceStreamer
         CancellationToken cancellationToken)
     {
         var streamer = _redisDb.GetStreamer<AudioStreamPart>(streamId);
-        var audioStream = streamer.Read(cancellationToken);
+        var audioStream = streamer.Read(cancellationToken).Buffer(StreamBufferSize, cancellationToken);
         var audio = new AudioSource(audioStream, cancellationToken);
         await audio.WhenFormatAvailable.ConfigureAwait(false);
         return audio;
@@ -36,6 +37,6 @@ public class AudioSourceStreamer : IAudioSourceStreamer
         CancellationToken cancellationToken)
     {
         var streamer = _redisDb.GetStreamer<AudioStreamPart>(streamId);
-        return streamer.Read(cancellationToken);
+        return streamer.Read(cancellationToken).Buffer(StreamBufferSize, cancellationToken);
     }
 }
