ace2cd8b7dc49be874a876177746e7026a559bfa
Alex Yakunin <alex.yakunin@gmail.com>
2021-11-10 02:44:49 -0800

    feat: net6 release

---
+++ tests/Chat.UnitTests/ChatAuthorSerialization.cs
@@ -0,0 +1,15 @@
+﻿namespace ActualChat.Chat.UnitTests;
+
+public class ChatAuthorSerialization
+{
+    [Fact]
+    public void BasicTest()
+    {
+        SerializationTestExt.SystemJsonOptions = SystemJsonSerializer.DefaultOptions;
+        var ca = new ChatAuthor() {
+            Name = "Alex",
+        };
+        ca.PassThroughSystemJsonSerializer().Should().Be(ca);
+    }
+
+}
