95f2a6dbaa7e9cfb0aeef7d78d00765cdefe6ecf
iqmulator <iqmulator@gmail.com>
2021-11-15 11:46:26 +0500

    readertest works fine

--- tests/Chat.IntegrationTests/ChatEntryReaderTest.cs
+++ tests/Chat.IntegrationTests/ChatEntryReaderTest.cs
@@ -4,6 +4,7 @@ using ActualChat.Testing.Host;
 using ActualChat.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Stl.Fusion.Authentication.Commands;
 using Stl.Fusion.EntityFramework;
 using Stl.Time.Testing;
 using Stl.Versioning;
@@ -22,25 +23,23 @@ public class ChatEntryReaderTest
     {
         using var appHost = await TestHostFactory.NewAppHost();
         using var tester = appHost.NewWebClientTester();
+        var user = tester.SignIn(new User("", "reader-test-user")).ConfigureAwait(false);
         var session = tester.Session;
 
-        var chats = tester.ClientServices.GetRequiredService<IChats>();
-        var idRange = await chats.GetIdRange(session, ChatId, CancellationToken.None);
-        idRange.Start.Should().Be(131);
-        idRange.End.Should().Be(142);
+        var chats = tester.AppServices.GetRequiredService<IChats>();
 
         var chat = await chats.Get(session, ChatId, CancellationToken.None);
         chat.Should().NotBeNull();
         chat?.Title.Should().Be("The Actual One");
-        chat?.CreatedAt.Date.Should().Be(DateTime.Now.Date);
 
-        var dbContextFactory = tester.AppServices.GetRequiredService<IDbContextFactory<ChatDbContext>>();
-        var dbContext = await dbContextFactory.CreateDbContextAsync();
+        await AddChatEntries(chats, session, ChatId, CancellationToken.None);
 
-        var dbChat = await dbContext.Chats.FirstOrDefaultAsync(c => c.Id == ChatId);
-
-        var dbAuthor = await dbContext.ChatAuthors.FirstOrDefaultAsync(CancellationToken.None);
-        await AddTextMessages(dbContext, dbChat, dbAuthor, CancellationToken.None);
+        var idRange = await chats.GetIdRange(session, ChatId, CancellationToken.None);
+        idRange.Start.Should().Be(131);
+        var chuckBerryId = idRange.End;
+        chuckBerryId.Should().BeGreaterThan(idRange.Start);
+        var nirvanaId = chuckBerryId - 1;
+        var acDcId = nirvanaId - 1;
 
         var reader = new ChatEntryReader(chats) {
             ChatId = ChatId,
@@ -62,51 +61,27 @@ public class ChatEntryReaderTest
 
         var entryPoint = new Moment(DateTime.Parse("2021-11-05T16:41:30.0043140Z"));
 
-        var nextEntryId = await reader.GetNextEntryId(entryPoint, CancellationToken.None);
-        nextEntryId.Should().Be(133);
-
-        entry = await reader.Get(145, CancellationToken.None);
+        entry = await reader.Get(acDcId, CancellationToken.None);
         entry.Should().NotBeNull();
         entry?.Content.Should().Be("back in black i hit the sack");
 
-        entry = await reader.Get(146, CancellationToken.None);
+        entry = await reader.Get(nirvanaId, CancellationToken.None);
         entry.Should().NotBeNull();
         entry?.Content.Should().Be("rape me rape me my friend");
 
-        entry = await reader.Get(147, CancellationToken.None);
+        entry = await reader.Get(chuckBerryId, CancellationToken.None);
         entry.Should().NotBeNull();
         entry?.Content.Should().Be("it was a teenage wedding and the all folks wished them well");
     }
 
-    private async Task AddTextMessages(
-        DbContext dbContext,
-        DbChat dbChat,
-        DbChatAuthor dbAuthor,
-        CancellationToken cancellationToken)
+    private async Task AddChatEntries(IChats chats, Session session, ChatId chatId, CancellationToken cancellationToken)
     {
         var phrases = new[] {
             "back in black i hit the sack",
             "rape me rape me my friend",
             "it was a teenage wedding and the all folks wished them well",
         };
-        var id = 145;
         foreach (var text in phrases)
-        {
-            var dbChatEntry = new DbChatEntry() {
-                CompositeId = DbChatEntry.GetCompositeId(dbChat.Id, id),
-                ChatId = dbChat.Id,
-                Id = id,
-                Version = VersionGenerator.NextVersion(),
-                BeginsAt = Clocks.SystemClock.Now,
-                EndsAt = Clocks.SystemClock.Now,
-                Type = ChatEntryType.Text,
-                Content = text,
-                AuthorId = dbAuthor.Id,
-            };
-            dbContext.Add(dbChatEntry);
-            id++;
-        }
-
-        await dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            _ = await chats.CreateEntry(new (session, chatId, text), cancellationToken).ConfigureAwait(false);
     }
 }
