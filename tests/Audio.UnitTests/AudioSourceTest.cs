fd38365d4d3239e99d1eff93fcc8a5f74698f4fb
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-11-15 19:41:29 +0300

    fix: adding checks for broken file headers

--- tests/Audio.UnitTests/AudioSourceTest.cs
+++ tests/Audio.UnitTests/AudioSourceTest.cs
@@ -1,4 +1,3 @@
-using System.Buffers;
 using ActualChat.Blobs;
 using Stl.IO;
 
@@ -58,6 +57,23 @@ public class AudioSourceTest
         await WriteToFile(audio, default, "file-with-offset.webm");
     }
 
+    [Fact]
+    public async Task ExtractFromFileWithWrongByteOffset()
+    {
+        var audio = await GetAudio("file.webm", TimeSpan.Zero, 1);
+        var offset = TimeSpan.Zero;
+        await foreach (var frame in audio.GetFrames(default)) {
+            frame.Data.Should().NotBeNull();
+            frame.Data.Should().NotBeEmpty();
+            frame.Offset.Should().BeGreaterOrEqualTo(offset);
+            offset = frame.Offset > offset
+                ? frame.Offset
+                : offset;
+        }
+
+        offset.Should().Be(TimeSpan.FromMilliseconds(12240));
+    }
+
     [Fact(Skip = "Not fixed yet")]
     public async Task ExtractFromLargeFileWithOffset()
     {
@@ -89,17 +105,15 @@ public class AudioSourceTest
         await WriteToFile(audio, TimeSpan.FromSeconds(40), "result-large-file.webm");
     }
 
-    // Private methods
-
-    private static Task WriteToFile(AudioSource source, TimeSpan skipTo, FilePath fileName)
+    private async Task<AudioSource> GetAudio(
+        FilePath fileName,
+        TimeSpan skipTo = default,
+        int skipBytes = 0,
+        CancellationToken cancellationToken = default)
     {
-        var stream = new FileStream(GetAudioFilePath(fileName), FileMode.OpenOrCreate, FileAccess.ReadWrite);
-        return stream.WriteBlobStream(source.GetBlobStream(default), true, default);
-    }
-
-    private async Task<AudioSource> GetAudio(FilePath fileName, TimeSpan skipTo = default, CancellationToken cancellationToken = default)
-    {
-        var blobStream = GetAudioFilePath(fileName).ReadBlobStream(cancellationToken);
+        var blobStream = GetAudioFilePath(fileName)
+            .ReadBlobStream(cancellationToken)
+            .SkipBytes(skipBytes, cancellationToken);
         var audio = new AudioSource(blobStream, skipTo, null, cancellationToken);
         await audio.WhenFormatAvailable.ConfigureAwait(false);
         return audio;
@@ -107,4 +121,31 @@ public class AudioSourceTest
 
     private static FilePath GetAudioFilePath(FilePath fileName)
         => new FilePath(Environment.CurrentDirectory) & "data" & fileName;
+
+    // Private methods
+
+    private static Task WriteToFile(AudioSource source, TimeSpan skipTo, FilePath fileName)
+    {
+        var stream = new FileStream(GetAudioFilePath(fileName), FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        return stream.WriteBlobStream(source.GetBlobStream(default), true);
+    }
+}
+
+public static class BlobStreamExt
+{
+    public static async IAsyncEnumerable<BlobPart> SkipBytes(
+        this IAsyncEnumerable<BlobPart> that,
+        int bytes,
+        [EnumeratorCancellation]
+        CancellationToken cancellationToken)
+    {
+        await foreach (var blobPart in that.WithCancellation(cancellationToken))
+            if (bytes >= blobPart.Data.Length)
+                bytes -= blobPart.Data.Length;
+            else {
+                yield return blobPart with { Data = blobPart.Data[bytes..] };
+
+                bytes = 0;
+            }
+    }
 }
