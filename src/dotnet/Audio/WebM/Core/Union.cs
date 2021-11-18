4b3d8114deab1168ba72eaaab0e67f8417bb796a
Alex Yakunin <alex.yakunin@gmail.com>
2021-11-18 04:37:23 -0800

    fix: cosmetic stuff

--- src/dotnet/Audio/WebM/Core/Union.cs
+++ src/dotnet/Audio/WebM/Core/Union.cs
@@ -1,18 +1,15 @@
-using System.Runtime.InteropServices;
+namespace ActualChat.Audio.WebM;
 
-namespace ActualChat.Audio.WebM
+[StructLayout(LayoutKind.Explicit)]
+internal struct Union
 {
-    [StructLayout(LayoutKind.Explicit)]
-    internal struct Union
-    {
-        [FieldOffset(0)]
-        public uint UInt;
-        [FieldOffset(0)]
-        public float Float;
+    [FieldOffset(0)]
+    public uint UInt;
+    [FieldOffset(0)]
+    public float Float;
 
-        [FieldOffset(0)]
-        public ulong ULong;
-        [FieldOffset(0)]
-        public double Double;
-    }
+    [FieldOffset(0)]
+    public ulong ULong;
+    [FieldOffset(0)]
+    public double Double;
 }
