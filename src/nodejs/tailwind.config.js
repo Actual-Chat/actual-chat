4df22ba3e91a2101be330434e9e62abf49fbf0fd
iqmulator <iqmulator@gmail.com>
2021-12-01 14:19:24 +0500

    wip: apply main theme

--- src/nodejs/tailwind.config.js
+++ src/nodejs/tailwind.config.js
@@ -300,6 +300,7 @@ module.exports = ctx => {
           ],
         },
         fontSize: {
+          xxs: ['0.6rem', { lineHeight: '0.75rem' }],
           xs: ['0.75rem', { lineHeight: '1rem' }],
           sm: ['0.875rem', { lineHeight: '1.25rem' }],
           base: ['1rem', { lineHeight: '1.5rem' }],
