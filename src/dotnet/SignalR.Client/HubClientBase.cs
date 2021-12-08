0b9f02d9aefff99ae8a11397791130c11a595e43
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-08 16:44:11 +0300

    chore: additional logging for WASM audio client

--- src/dotnet/SignalR.Client/HubClientBase.cs
+++ src/dotnet/SignalR.Client/HubClientBase.cs
@@ -23,35 +23,48 @@ public abstract class HubClientBase
 
     protected HubConnection CreateHubConnection()
     {
-        var builder = new HubConnectionBuilder()
-            .WithUrl(HubUrl)
-            .WithAutomaticReconnect();
-        if (!Debugging.SignalR.DisableMessagePackProtocol)
-            builder = builder.AddMessagePackProtocol();
-        return builder.Build();
+        Log.LogDebug("CreateHubConnection: Time: {Time}", Clocks.CpuClock.UtcNow.TimeOfDay);
+        try {
+            var builder = new HubConnectionBuilder()
+                .WithUrl(HubUrl)
+                .WithAutomaticReconnect();
+            if (!Debugging.SignalR.DisableMessagePackProtocol)
+                builder = builder.AddMessagePackProtocol();
+            return builder.Build();
+        }
+        finally {
+            Log.LogDebug("CreateHubConnection: Exited; Time: {Time}", Clocks.CpuClock.UtcNow.TimeOfDay);
+        }
     }
 
     protected async ValueTask EnsureConnected(CancellationToken cancellationToken)
     {
-        if (HubConnection.State == HubConnectionState.Connected)
-            return;
+        Log.LogDebug("EnsureConnected: Time: {Time}", Clocks.CpuClock.UtcNow.TimeOfDay);
+        try {
+            if (HubConnection.State == HubConnectionState.Connected)
+                return;
 
-        var retryDelay = 0.5d;
-        var attempt = 0;
-        while (HubConnection.State != HubConnectionState.Connected || attempt < 10)
-            try {
-                attempt++;
-                if (HubConnection.State == HubConnectionState.Disconnected)
-                    await HubConnection.StartAsync(cancellationToken).ConfigureAwait(false);
-                else
-                    await Clocks.CpuClock.Delay(TimeSpan.FromSeconds(0.5), cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception e) when (e is not OperationCanceledException) {
-                Log.LogError(e,
-                    "EnsureConnected failed to reconnect SignalR Hub, will retry after {RetryDelay}s", retryDelay);
-                await Clocks.CpuClock.Delay(TimeSpan.FromSeconds(retryDelay), cancellationToken)
-                    .ConfigureAwait(false);
-                retryDelay = Math.Min(10d, retryDelay * (1 + Random.Shared.NextDouble())); // Exp. growth to 10s
-            }
+            var retryDelay = 0.5d;
+            var attempt = 0;
+            while (HubConnection.State != HubConnectionState.Connected || attempt < 10)
+                try {
+                    attempt++;
+                    if (HubConnection.State == HubConnectionState.Disconnected)
+                        await HubConnection.StartAsync(cancellationToken).ConfigureAwait(false);
+                    else
+                        await Clocks.CpuClock.Delay(TimeSpan.FromSeconds(0.5), cancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception e) when (e is not OperationCanceledException) {
+                    Log.LogError(e,
+                        "EnsureConnected failed to reconnect SignalR Hub, will retry after {RetryDelay}s",
+                        retryDelay);
+                    await Clocks.CpuClock.Delay(TimeSpan.FromSeconds(retryDelay), cancellationToken)
+                        .ConfigureAwait(false);
+                    retryDelay = Math.Min(10d, retryDelay * (1 + Random.Shared.NextDouble())); // Exp. growth to 10s
+                }
+        }
+        finally {
+            Log.LogDebug("EnsureConnected: Exited; Time: {Time}", Clocks.CpuClock.UtcNow.TimeOfDay);
+        }
     }
 }
