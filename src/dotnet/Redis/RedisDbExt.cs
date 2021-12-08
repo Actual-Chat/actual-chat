c3b079009fbef2ca0d1ae50ac89fdf4e51e23d67
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-08 20:20:12 +0300

    fix: use different timeouts at RedisQueue for pub\sub and error retry

--- src/dotnet/Redis/RedisDbExt.cs
+++ src/dotnet/Redis/RedisDbExt.cs
@@ -6,4 +6,7 @@ public static class RedisDbExt
 {
     public static RedisStreamer<T> GetStreamer<T>(this RedisDb redisDb, string key, RedisStreamer<T>.Options? settings = null)
         => new(redisDb, key, settings);
+
+    public static RedisQueue<T> GetQueue<T>(this RedisDb redisDb, string key, RedisQueue<T>.Options? settings = null)
+        => new (redisDb, key, settings);
 }
