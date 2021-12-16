120e56e29d2d47e1929253eb609c54f9e6b3ed84
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-16 14:33:19 +0300

    chore: add caching for webm and wasm files

--- src/dotnet/Host/Module/AppHostModule.cs
+++ src/dotnet/Host/Module/AppHostModule.cs
@@ -77,7 +77,16 @@ public class AppHostModule : HostModule<HostSettings>, IWebModule
 
         // Static + Swagger
         app.UseBlazorFrameworkFiles();
-        app.UseStaticFiles();
+        app.UseStaticFiles(new StaticFileOptions {
+                OnPrepareResponse = context => {
+                    if (!(context.File.Name?.ToLowerInvariant() ?? "").EndsWith(".wasm", StringComparison.InvariantCultureIgnoreCase))
+                        return;
+
+                    context.Context.Response.Headers.Append("Cache-Control", "public,max-age=86400");
+                    context.Context.Response.Headers.Append("Expires", DateTime.UtcNow.AddDays(1).ToString("R", CultureInfo.InvariantCulture));
+                },
+            }
+            );
         app.UseSwagger();
         app.UseSwaggerUI(c => {
             c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
