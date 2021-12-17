99b75550738571c04605e39e664c214771e50680
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-17 13:19:17 +0300

    fix(audio): collapse gaps

--- tests/Audio.UnitTests/AudioActivityPeriodExtractorTest.cs
+++ tests/Audio.UnitTests/AudioActivityPeriodExtractorTest.cs
@@ -158,8 +158,6 @@ public class AudioActivityPeriodExtractorTest : TestBase
                     yield return new BlobPart(blobPart.Index + increment++, blobPart.Data[..nextBlockStartsAt]);
                     yield return new BlobPart(blobPart.Index + increment++, new byte[]{0, 0, 0, 0});
 
-                    await Task.Delay(2 * 1000);
-
                     yield return new BlobPart(blobPart.Index + increment++, new byte[]{1, 1, 1, 1});
                     yield return new BlobPart(blobPart.Index + increment++, blobPart.Data[nextBlockStartsAt..]);
                 }
@@ -225,8 +223,6 @@ public class AudioActivityPeriodExtractorTest : TestBase
                     yield return new BlobPart(blobPart.Index + increment++, blobPart.Data[..nextBlockStartsAt]);
                     yield return new BlobPart(blobPart.Index + increment++, new byte[]{0, 0, 0, 0});
 
-                    await Task.Delay(2 * 1000);
-
                 }
                 else if (blobPart.Index is > 20 and <= 23) { }
                 else if (blobPart.Index == 24) {
@@ -243,6 +239,76 @@ public class AudioActivityPeriodExtractorTest : TestBase
         }
     }
 
+
+    [Fact]
+    public async Task SkipGapTest()
+    {
+        var record = new AudioRecord(
+            "test-id",
+            "1",
+            "1",
+            new AudioFormat { CodecKind = AudioCodecKind.Opus, ChannelCount = 1, SampleRate = 48_000 },
+            "RU-ru",
+            CpuClock.Now.EpochOffset.TotalSeconds);
+        var audioFilePath = GetAudioFilePath("file.webm");
+        var fileSize = audioFilePath.GetFileInfo().Length;
+        var blobStream = audioFilePath.ReadBlobStream();
+        var blobStreamWithBoundaries = InsertSpeechBoundaries(blobStream);
+        var services = new ServiceCollection().BuildServiceProvider();
+        var audioActivityExtractor = new AudioSplitter(services);
+        var openAudioSegments = audioActivityExtractor.GetSegments(record, blobStreamWithBoundaries, default);
+        var size = 0L;
+        var index = 0;
+        await foreach (var openAudioSegment in openAudioSegments) {
+            openAudioSegment.Index.Should().Be(index++);
+            openAudioSegment.AudioRecord.Should().Be(record);
+            var audio = openAudioSegment.Audio;
+            await audio.WhenFormatAvailable;
+
+            size += audio.Format.ToBlobPart().Data.Length;
+            var sum = 0;
+            var offset = TimeSpan.Zero;
+            await foreach (var f in audio.GetFrames(default)) {
+                sum += f.Data.Length;
+
+                (f.Offset - offset).Should().BeLessThan(TimeSpan.FromMilliseconds(80));
+                if (offset > TimeSpan.Zero)
+                    (f.Offset - offset).Should().BeGreaterThan(TimeSpan.FromMilliseconds(19));
+                offset = f.Offset;
+            }
+            size += sum;
+
+            var closedAudioSegment = await openAudioSegment.ClosedSegmentTask;
+            closedAudioSegment.Audio.Should().NotBeNull();
+            closedAudioSegment.Duration.Should().BeGreaterThan(TimeSpan.Zero);
+        }
+        size.Should().BeLessThan(fileSize + 161 - (3 * 1024)); // + format of next segment
+
+        async IAsyncEnumerable<BlobPart> InsertSpeechBoundaries(IAsyncEnumerable<BlobPart> bs)
+        {
+            var increment = 0;
+            await foreach (var blobPart in bs) {
+                if (blobPart.Index == 20) {
+                    var nextBlockStartsAt = blobPart.Data
+                        .Select((b, i) => (b == 0xA3 && blobPart.Data[i + 3] == 0x81, i))
+                        .First(x => x.Item1).i;
+
+                    yield return new BlobPart(blobPart.Index + increment++, blobPart.Data[..nextBlockStartsAt]);
+
+                }
+                else if (blobPart.Index is > 20 and <= 23) { }
+                else if (blobPart.Index == 24) {
+                    var nextBlockStartsAt = blobPart.Data
+                        .Select((b, i) => (b == 0xA3 && blobPart.Data[i + 3] == 0x81, i))
+                        .First(x => x.Item1).i;
+
+                    yield return new BlobPart(blobPart.Index + increment++, blobPart.Data[nextBlockStartsAt..]);
+                }
+                else
+                    yield return new BlobPart(blobPart.Index + increment, blobPart.Data);
+            }
+        }
+    }
 
     private async Task<AudioSource> GetAudio(FilePath fileName, CancellationToken cancellationToken = default)
     {
