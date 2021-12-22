c59fcb0db1bfb5166d1ba7383c4319873707c3b1
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-22 19:50:23 +0300

    chore: revert debug toggles

--- src/dotnet/Core/Constants.DebugMode.cs
+++ src/dotnet/Core/Constants.DebugMode.cs
@@ -13,8 +13,8 @@ public static partial class Constants
         public static bool AudioProcessing { get; } = false;
         public static bool AudioPlayback { get; } = false;
         public static bool AudioPlaybackPlayMyOwnAudio => HostInfo.IsDevelopmentInstance;
-        public static bool AudioRecording { get; } = true;
-        public static bool AudioRecordingStream { get; } = true;
+        public static bool AudioRecording { get; } = false;
+        public static bool AudioRecordingStream { get; } = false;
         public static bool Transcription { get; } = false;
         public static bool GoogleTranscriber { get; } = false;
         public static bool WebMReader { get; } = false;
