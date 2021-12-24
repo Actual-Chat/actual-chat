3086e362f44d0f8ac290d2fa652fddf63e31ffa8
Vladimir Chirikov <vovanchig@gmail.com>
2021-12-24 15:49:57 +0300

    feat: remove initialize() call and awaiting of it

--- src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/IAudioPlayer.ts
+++ src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/IAudioPlayer.ts
@@ -1,9 +1,8 @@
 export interface IAudioPlayer {
     onStartPlaying?: () => void;
     onInitialized?: () => void;
-    initialize(byteArray: Uint8Array): Promise<void>;
     dispose(): void;
-    appendAudioAsync(byteArray: Uint8Array, offset: number): Promise<void>;
+    appendAudio(byteArray: Uint8Array, offset: number): Promise<void>;
     endOfStream(): void;
     stop(error: EndOfStreamError | null): void;
 }
