d5840dd201636525db69a82857c223f96d674c32
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-24 14:10:43 +0300

    refactor(audio): use frames instead of AudioStreamPart - it was designed only for stream serialization

--- src/dotnet/Core/Media/MediaStreamPartExt.cs
+++
@@ -1,7 +0,0 @@
-namespace ActualChat.Media;
-
-public static class MediaStreamPartExt
-{
-    public static byte[] Serialize(this IMediaStreamPart mediaStreamPart)
-        => mediaStreamPart.Format?.Serialize() ?? mediaStreamPart.Frame!.Data;
-}
