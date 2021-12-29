fe6a2016fe1679a45d67739672536081dd5e1b35
Alexey Kochetov <alexis.kochetov@gmail.com>
2021-12-29 13:39:40 +0300

    chore: reformat and cast self to Worker interface

--- src/dotnet/Audio.UI.Blazor/Components/AudioRecorder/workers/opus-encoder-worker.ts
+++ src/dotnet/Audio.UI.Blazor/Components/AudioRecorder/workers/opus-encoder-worker.ts
@@ -13,62 +13,63 @@ interface Encoder {
     flush(): ArrayBuffer[];
     close(): void;
 }
+
 let encoder: Encoder;
-let self: Worker;
+const worker = self as unknown as Worker;
 
-self.onmessage = (ev: MessageEvent) => {
-  const { command } : EncoderCommand = ev.data;
-  switch (command) {
-    case 'loadEncoder':
-      const { mimeType, wasmPath }: LoadEncoderCommand = ev.data;
-      // Setting encoder module
-      const mime = mimeType.toLowerCase();
-      let encoderModule;
-      if (mime.indexOf("audio/webm") >= 0) {
-        encoderModule = WebMOpusEncoder;
-      }
-      // Override Emscripten configuration
-      let moduleOverrides = {};
-      if (wasmPath) {
-        moduleOverrides['locateFile'] = function (path, scriptDirectory) {
-          return path.match(/.wasm/) ? wasmPath : (scriptDirectory + path);
-        };
-      }
-      // Initialize the module
-      encoderModule(moduleOverrides).then(Module => {
-        encoder = Module;
-        // Notify the host ready to accept 'init' message.
-        self.postMessage({ command: 'readyToInit' });
-      });
-      break;
+worker.onmessage = (ev: MessageEvent) => {
+    const {command}: EncoderCommand = ev.data;
+    switch (command) {
+        case 'loadEncoder':
+            const {mimeType, wasmPath}: LoadEncoderCommand = ev.data;
+            // Setting encoder module
+            const mime = mimeType.toLowerCase();
+            let encoderModule;
+            if (mime.indexOf("audio/webm") >= 0) {
+                encoderModule = WebMOpusEncoder;
+            }
+            // Override Emscripten configuration
+            let moduleOverrides = {};
+            if (wasmPath) {
+                moduleOverrides['locateFile'] = function (path, scriptDirectory) {
+                    return path.match(/.wasm/) ? wasmPath : (scriptDirectory + path);
+                };
+            }
+            // Initialize the module
+            encoderModule(moduleOverrides).then(Module => {
+                encoder = Module;
+                // Notify the host ready to accept 'init' message.
+                self.postMessage({command: 'readyToInit'});
+            });
+            break;
 
-    case 'init':
-      const { sampleRate, channelCount, bitsPerSecond }: InitCommand = ev.data;
-      encoder.init(sampleRate, channelCount, bitsPerSecond);
-      break;
+        case 'init':
+            const {sampleRate, channelCount, bitsPerSecond}: InitCommand = ev.data;
+            encoder.init(sampleRate, channelCount, bitsPerSecond);
+            break;
 
-    case 'pushInputData':
-      const { channelBuffers } : PushInputDataCommand = ev.data; // eslint-disable-line
-      encoder.encode(channelBuffers);
-      break;
+        case 'pushInputData':
+            const {channelBuffers}: PushInputDataCommand = ev.data; // eslint-disable-line
+            encoder.encode(channelBuffers);
+            break;
 
-    case 'getEncodedData':
-    case 'done':
-      if (command === 'done') {
-        encoder.close();
-      }
+        case 'getEncodedData':
+        case 'done':
+            if (command === 'done') {
+                encoder.close();
+            }
 
-      const buffers = encoder.flush();
-      const message: EncoderMessage = {
-          command: command === 'done' ? 'lastEncodedData' : 'encodedData',
-          buffers
-      };
-      self.postMessage(message, buffers);
+            const buffers = encoder.flush();
+            const message: EncoderMessage = {
+                command: command === 'done' ? 'lastEncodedData' : 'encodedData',
+                buffers
+            };
+            worker.postMessage(message, buffers);
 
-      break;
+            break;
 
-    default:
-      // Ignore
-      break;
-  }
+        default:
+            // Ignore
+            break;
+    }
 };
