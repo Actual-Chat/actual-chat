84cd26165d1f2238a8609d4821d8f87acee09e9a
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-01-10 14:04:26 +0300

    chore: use on* handlers at the worker

--- src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/workers/opus-decoder-worker.ts
+++ src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/workers/opus-decoder-worker.ts
@@ -14,76 +14,87 @@ const decoderMap = new Map<string,OpusDecoder>();
 const worker = self as unknown as Worker;
 
 worker.onmessage = async (ev: MessageEvent) => {
-    const { command }: DecoderCommand = ev.data;
-    switch (command) {
-        case 'loadDecoder':
-            decoderPool.push(await OpusDecoder.create(release));
-            decoderPool.push(await OpusDecoder.create(release));
-            decoderPool.push(await OpusDecoder.create(release));
-            break;
-
-        case 'init': {
-            const {playerId, buffer, offset, length}: InitCommand = ev.data;
-            const workletPort = ev.ports[0];
-            let decoder = decoderPool.shift();
-            if (decoder == null) {
-                decoder = await OpusDecoder.create(release);
-            }
-            await decoder.init(playerId, workletPort, buffer, offset, length);
-            decoderMap.set(playerId, decoder);
-
-            const initCompletedMessage: DecoderWorkerMessage = {topic: 'initCompleted', playerId: playerId};
-            worker.postMessage(initCompletedMessage);
-            break;
-        }
-
-        case 'pushData': {
-            const {playerId, buffer, offset, length}: PushDataCommand = ev.data;
-            const decoder = decoderMap.get(playerId);
-            if (decoder == null) {
-                console.error(`pushData: can't find decoder for player=${playerId}`);
+    try {
+        const {command}: DecoderCommand = ev.data;
+        switch (command) {
+            case 'loadDecoder':
+                await onLoadDecoder();
                 break;
-            }
 
-            decoder.pushData(buffer, offset, length);
-            break;
-        }
-
-        case 'endOfStream': {
-            const {playerId}: EndOfStreamCommand = ev.data;
-            const decoder = decoderMap.get(playerId);
-            if (decoder == null) {
-                console.error(`endOfStream: can't find decoder for player=${playerId}`);
+            case 'init':
+                await onInit(ev.data, ev.ports[0]);
                 break;
-            }
 
-            decoder.pushEndOfStream();
-            break;
-        }
-
-        case 'stop': {
-            const {playerId}: StopCommand = ev.data;
-            const decoder = decoderMap.get(playerId);
-            if (decoder == null) {
-                // already processed by handling 'endOfStream' at the decoder
+            case 'pushData':
+                await onPushData(ev.data);
                 break;
-            }
 
-            await decoder.stop();
-            break;
+            case 'endOfStream':
+                await onEndOfStream(ev.data);
+                break;
+
+            case 'stop':
+                await onStop(ev.data);
+                break;
         }
     }
-
+    catch (error) {
+        // TODO(AK): implement centralized logging for client, like Sentry, etc.
+        console.error(error);
+    }
 };
+
+async function onLoadDecoder(): Promise<void> {
+    decoderPool.push(await OpusDecoder.create(release));
+    decoderPool.push(await OpusDecoder.create(release));
+    decoderPool.push(await OpusDecoder.create(release));
+}
+
+async function onInit(command: InitCommand, port: MessagePort): Promise<void> {
+    const { playerId, buffer, offset, length } = command;
+    const workletPort = port;
+    let decoder = decoderPool.shift();
+    if (decoder == null) {
+        decoder = await OpusDecoder.create(release);
+    }
+    await decoder.init(playerId, workletPort, buffer, offset, length);
+    decoderMap.set(playerId, decoder);
+
+    const initCompletedMessage: DecoderWorkerMessage = {topic: 'initCompleted', playerId: playerId};
+    worker.postMessage(initCompletedMessage);
+}
+
+async function onPushData(command: PushDataCommand): Promise<void> {
+    const { playerId, buffer, offset, length } = command;
+    const decoder = getDecoder(playerId);
+    decoder.pushData(buffer, offset, length);
+}
+
+async function onEndOfStream(command: EndOfStreamCommand): Promise<void> {
+    const {playerId} = command;
+    const decoder = getDecoder(playerId);
+    decoder.pushEndOfStream();
+}
+
+async function onStop(command: StopCommand): Promise<void> {
+    const {playerId} = command;
+    const decoder = decoderMap.get(playerId);
+    if (decoder == null) {
+        // already processed by handling 'endOfStream' at the decoder
+        return;
+    }
+
+    await decoder.stop();
+}
 
 function release(decoder: OpusDecoder): void {
     decoderMap.delete(decoder.playerId);
     decoderPool.push(decoder);
 }
 
-let getTimestamp;
-if (typeof performance === 'undefined' || typeof performance.now === 'undefined') {
-    getTimestamp = Date.now;
-} else {
-    getTimestamp = performance.now.bind(performance);
+function getDecoder(playerId: string): OpusDecoder {
+    const decoder = decoderMap.get(playerId);
+    if (decoder == null)
+        throw new Error(`Can't find decoder for player=${playerId}`);
+    return decoder;
 }
