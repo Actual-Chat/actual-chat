20c879ec1a1a44fbc4915d9172a01a2923440e4c
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-01-10 14:50:34 +0300

    chore: remove MSE audio player

--- src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/IAudioPlayer.ts
+++
@@ -1,8 +0,0 @@
-export interface IAudioPlayer {
-    onStartPlaying?: () => void;
-    onInitialized?: () => void;
-    init(byteArray: Uint8Array): Promise<void>;
-    appendAudio(byteArray: Uint8Array, offset: number): Promise<void>;
-    endOfStream(): void;
-    stop(error: EndOfStreamError | null): void;
-}
