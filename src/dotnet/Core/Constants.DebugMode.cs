b6790b51fb3385d6054899d2d9d094d433c0c55a
Alex Yakunin <alex.yakunin@gmail.com>
2022-01-10 23:40:14 -0800

    fix: Constants.DebugMode.cs

--- src/dotnet/Core/Constants.DebugMode.cs
+++ src/dotnet/Core/Constants.DebugMode.cs
@@ -10,10 +10,10 @@ public static partial class Constants
     {
         public static bool VirtualList { get; } = false;
         public static bool AudioSource { get; } = false;
-        public static bool AudioProcessing { get; } = true;
-        public static bool AudioPlayback { get; } = true;
+        public static bool AudioProcessing { get; } = false;
+        public static bool AudioPlayback { get; } = false;
         public static bool AudioPlaybackPlayMyOwnAudio => HostInfo.IsDevelopmentInstance;
-        public static bool AudioRecording { get; } = true;
+        public static bool AudioRecording { get; } = false;
         public static bool AudioRecordingStream { get; } = false;
         public static bool Transcription { get; } = false;
         public static bool TranscriptionProcessing { get; } = false;
