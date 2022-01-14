7362e0cfad3e7ec9967bc85fd5061856899a1963
Dmitry Filippov <crui3er@gmail.com>
2022-01-14 18:27:04 +0500

    feat: use gravatar picture for chat message avatar

--- src/dotnet/Users.Contracts/IUserInfos.cs
+++ src/dotnet/Users.Contracts/IUserInfos.cs
@@ -8,4 +8,7 @@ public interface IUserInfos
     // TODO(AY): Move to backend; the proper impl. of this method will end up hitting every user cluster
     [ComputeMethod(KeepAliveTime = 10)]
     Task<UserInfo?> GetByName(string name, CancellationToken cancellationToken);
+
+    [ComputeMethod(KeepAliveTime = 10)]
+    Task<string?> GetGravatarHash(string userId, CancellationToken cancellationToken);
 }
