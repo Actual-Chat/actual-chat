8f8e9c9d8f62fcf78127a96d51b9a4081f97609f
iqmulator <iqmulator@gmail.com>
2022-01-26 15:45:02 +0500

    Menu and MenuItem components

---
+++ src/dotnet/UI.Blazor/Components/Menu/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿namespace ActualChat.UI.Blazor.Components;
+
+public partial class MenuItem
+{
+    [Parameter]
+    public string? Title { get; set; }
+    [Parameter]
+    public string? Icon { get; set; } = "";
+    [Parameter]
+    public bool IsTitleVisible { get; set; } = true;
+    [Parameter]
+    public bool IsIconVisible { get; set; }
+    [Parameter]
+    public string? CommonStyle { get; set; }
+    [Parameter]
+    public string? Name { get; set; }
+}
