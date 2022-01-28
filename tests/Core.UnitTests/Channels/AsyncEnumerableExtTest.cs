575e810b233e62c0d5c235ff9274162c22fdb1d1
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-01-28 19:40:57 +0300

    chore: more tests

--- tests/Core.UnitTests/Channels/AsyncEnumerableExtTest.cs
+++ tests/Core.UnitTests/Channels/AsyncEnumerableExtTest.cs
@@ -49,4 +49,60 @@ public class AsyncEnumerableExtTest
             yield return 6;
         }
     }
+
+
+    [Fact]
+    public async Task MergeCancellationTest()
+    {
+        var cts = new CancellationTokenSource();
+        var left = Left(cts.Token);
+        var right = Right(cts.Token);
+        var result = left.Merge(right);
+        await foreach (var n in result) {
+            if (n == 10)
+                cts.Cancel();
+        }
+
+        async IAsyncEnumerable<int> Left([EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            yield return 0;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+
+            yield return 10;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+
+            yield return 20;
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await Task.Delay(TimeSpan.FromMilliseconds(300));
+
+            yield return 30;
+        }
+
+        async IAsyncEnumerable<int> Right([EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(20));
+
+            yield return 1;
+            yield return 2;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(150));
+
+            yield return 3;
+            yield return 4;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+
+            yield return 5;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            yield return 6;
+        }
+    }
 }
