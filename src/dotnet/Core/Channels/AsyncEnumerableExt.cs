d2ab94cf171fac6f2a96c4425c92e31311d71417
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-01-28 21:04:48 +0300

    fix: proper exception handling at merge

--- src/dotnet/Core/Channels/AsyncEnumerableExt.cs
+++ src/dotnet/Core/Channels/AsyncEnumerableExt.cs
@@ -147,121 +147,77 @@ public static class AsyncEnumerableExt
         {
             var count = sources.Length + 1;
 
-            var enumerators = new IAsyncEnumerator<TSource>[count];
+            var enumerators = new IAsyncEnumerator<TSource>?[count];
             var moveNextTasks = new ValueTask<bool>[count];
-
+            var errors = null as List<Exception>;
             try {
-                IAsyncEnumerator<TSource> sourceEnumerator = source.GetAsyncEnumerator(cancellationToken);
+                var sourceEnumerator = source.GetAsyncEnumerator(cancellationToken);
                 enumerators[0] = sourceEnumerator;
                 moveNextTasks[0] = sourceEnumerator.MoveNextAsync();
 
                 for (var i = 1; i < count; i++) {
-                    IAsyncEnumerator<TSource> enumerator = sources[i - 1].GetAsyncEnumerator(cancellationToken);
+                    var enumerator = sources[i - 1].GetAsyncEnumerator(cancellationToken);
                     enumerators[i] = enumerator;
-
-                    // REVIEW: This follows the lead of the original implementation where we kick off MoveNextAsync
-                    //         operations immediately. An alternative would be to do this in a separate stage, thus
-                    //         preventing concurrency across MoveNextAsync and GetAsyncEnumerator calls and avoiding
-                    //         any MoveNextAsync calls before all enumerators are acquired (or an exception has
-                    //         occurred doing so).
-
                     moveNextTasks[i] = enumerator.MoveNextAsync();
                 }
 
                 var whenAny = TaskExt.WhenAny(moveNextTasks);
                 int active = count;
                 while (active > 0) {
-                    int index = await whenAny;
+                    var index = await whenAny;
+                    var enumerator = enumerators[index];
+                    var moveNextTask = moveNextTasks[index];
+                    bool moved = false;
+                    try {
+                        moved = await moveNextTask.ConfigureAwait(false);
+                    }
+                    catch (Exception e) {
+                        errors ??= new List<Exception>();
+                        errors.Add(e);
+                    }
 
-                    IAsyncEnumerator<TSource> enumerator = enumerators[index];
-                    ValueTask<bool> moveNextTask = moveNextTasks[index];
-
-                    if (!await moveNextTask.ConfigureAwait(false)) {
-                        //
-                        // Replace the task in our array by a completed task to make finally logic easier. Note that
-                        // the WhenAnyValueTask object has a reference to our array (i.e. no copy is made), so this
-                        // gets rid of any resources the original task may have held onto. However, we *don't* call
-                        // whenAny.Replace to set this value, because it'd attach an awaiter to the already completed
-                        // task, causing spurious wake-ups when awaiting whenAny.
-                        //
-
-                        moveNextTasks[index] = new ValueTask<bool>();
-
+                    if (!moved) {
                         enumerators[index] = null!; // NB: Avoids attempt at double dispose in finally if disposing fails.
-                        await enumerator.DisposeAsync().ConfigureAwait(false);
+
+                        if (enumerator != null)
+                            await enumerator.DisposeAsync().ConfigureAwait(false);
 
                         active--;
                     }
-                    else
-                    {
-                        TSource item = enumerator.Current;
+                    else {
+                        if (enumerator == null) continue;
 
-                        //
-                        // Replace the task using whenAny.Replace, which will write it to the moveNextTasks array, and
-                        // will start awaiting the task. Note we don't have to write to moveNextTasks ourselves because
-                        // the whenAny object has a reference to it (i.e. no copy is made).
-                        //
-
+                        var item = enumerator.Current;
                         whenAny.Replace(index, enumerator.MoveNextAsync());
 
                         yield return item;
                     }
                 }
             }
-            finally
-            {
-                var errors = default(List<Exception>);
-
+            finally {
                 for (var i = count - 1; i >= 0; i--) {
-                    ValueTask<bool> moveNextTask = moveNextTasks[i];
-                    IAsyncEnumerator<TSource> enumerator = enumerators[i];
+                    var enumerator = enumerators[i];
 
                     try {
-                        try {
-                            //
-                            // Await the task to ensure outstanding work is completed prior to performing a dispose
-                            // operation. Note that we don't have to do anything special for tasks belonging to
-                            // enumerators that have finished; we swapped in a placeholder completed task.
-                            //
-
-                            // REVIEW: This adds an additional continuation to all of the pending tasks (note that
-                            //         whenAny also has registered one). The whenAny object will be collectible
-                            //         after all of these complete. Alternatively, we could drain via whenAny, by
-                            //         awaiting it until the active count drops to 0. This saves on attaching the
-                            //         additional continuations, but we need to decide on order of dispose. Right
-                            //         now, we dispose in opposite order of acquiring the enumerators, with the
-                            //         exception of enumerators that were disposed eagerly upon early completion.
-                            //         Should we care about the dispose order at all?
-
-                            _ = await moveNextTask.ConfigureAwait(false);
-                        }
-                        finally
-                        {
-                            if (enumerator != null)
-                            {
-                                await enumerator.DisposeAsync().ConfigureAwait(false);
-                            }
-                        }
+                        if (enumerator != null)
+                            await enumerator.DisposeAsync().ConfigureAwait(false);
                     }
-                    catch (Exception ex)
-                    {
-                        if (errors == null)
-                            errors = new List<Exception>();
-
+                    catch (Exception ex) {
+                        errors ??= new List<Exception>();
                         errors.Add(ex);
                     }
                 }
-
-                // NB: If we had any errors during cleaning (and awaiting pending operations), we throw these exceptions
-                //     instead of the original exception that may have led to running the finally block. This is similar
-                //     to throwing from any finally block (except that we catch all exceptions to ensure cleanup of all
-                //     concurrent sequences being merged).
-
-                if (errors != null)
- #pragma warning disable MA0072
-                    throw new AggregateException(errors);
- #pragma warning restore MA0072
             }
+            if (errors != null)
+                switch (errors.Count)
+                {
+                    case 1:
+                        throw errors[0];
+                    case > 1 when errors.All(e => e is OperationCanceledException):
+                        throw errors[0];
+                    case > 1:
+                        throw new AggregateException(errors);
+                }
         }
     }
 
