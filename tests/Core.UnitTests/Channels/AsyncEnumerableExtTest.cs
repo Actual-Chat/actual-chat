6477025b3f3b43d8e1595a49bbcc5d891189141c
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-01-29 16:18:38 +0300

    chore: use TestClock for delays

--- tests/Core.UnitTests/Channels/AsyncEnumerableExtTest.cs
+++ tests/Core.UnitTests/Channels/AsyncEnumerableExtTest.cs
@@ -1,10 +1,14 @@
+using Stl.Time.Testing;
+
 namespace ActualChat.Core.UnitTests.Channels;
 
 public class AsyncEnumerableExtTest
 {
+
     [Fact]
     public async Task BasicMergeTest()
     {
+        var clock = new TestClock();
         var left = Left();
         var right = Right();
         var result = left.Merge(right);
@@ -15,36 +19,36 @@ public class AsyncEnumerableExtTest
         {
             yield return 0;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            await clock.Delay(100);
 
             yield return 10;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            await clock.Delay(100);
 
             yield return 20;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(300));
+            await clock.Delay(300);
 
             yield return 30;
         }
 
         async IAsyncEnumerable<int> Right()
         {
-            await Task.Delay(TimeSpan.FromMilliseconds(20));
+            await clock.Delay(20);
 
             yield return 1;
             yield return 2;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(150));
+            await clock.Delay(150);
 
             yield return 3;
             yield return 4;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(10));
+            await clock.Delay(10);
 
             yield return 5;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            await clock.Delay(100);
 
             yield return 6;
         }
@@ -54,6 +58,7 @@ public class AsyncEnumerableExtTest
     [Fact]
     public async Task MergeCancellationTest()
     {
+        var clock = new TestClock();
         var cts = new CancellationTokenSource();
         var left = Left(cts.Token);
         var right = Right(cts.Token);
@@ -67,38 +72,38 @@ public class AsyncEnumerableExtTest
         {
             yield return 0;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            await clock.Delay(100);
 
             yield return 10;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            await clock.Delay(100);
 
             yield return 20;
 
             cancellationToken.ThrowIfCancellationRequested();
 
-            await Task.Delay(TimeSpan.FromMilliseconds(300));
+            await clock.Delay(300);
 
             yield return 30;
         }
 
         async IAsyncEnumerable<int> Right([EnumeratorCancellation] CancellationToken cancellationToken)
         {
-            await Task.Delay(TimeSpan.FromMilliseconds(20));
+            await clock.Delay(20);
 
             yield return 1;
             yield return 2;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(150));
+            await clock.Delay(150);
 
             yield return 3;
             yield return 4;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(10));
+            await clock.Delay(10);
 
             yield return 5;
 
-            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            await clock.Delay(100);
 
             cancellationToken.ThrowIfCancellationRequested();
 
