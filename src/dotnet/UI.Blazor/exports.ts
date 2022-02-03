3facec28c34aa5c870b0bafc80ac1487e7719333
iqmulator <iqmulator@gmail.com>
2022-02-03 14:51:26 +0500

    chat message menu as Menu component

--- src/dotnet/UI.Blazor/exports.ts
+++ src/dotnet/UI.Blazor/exports.ts
@@ -3,3 +3,4 @@ export * from './Components/Dropdown/dropdown';
 import './Components/Navbar/navbar.css'
 import './Components/AccountDropdown/account-dropdown.css'
 import './Components/Dropdown/dropdown.css'
+import './Components/tooltip.css'
