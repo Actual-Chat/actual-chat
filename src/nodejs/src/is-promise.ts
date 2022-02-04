00e0c0ff582aac9ccf4b2fb63adda91dd47e9a1e
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-02-04 18:02:58 +0300

    chore: fixing some lint issues

--- src/nodejs/src/is-promise.ts
+++ src/nodejs/src/is-promise.ts
@@ -1,3 +1,4 @@
 export function isPromise<T, S>(obj: PromiseLike<T> | S): obj is PromiseLike<T> {
+    // @ts-ignore
     return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj["then"] === 'function';
 }
