c6152bc0622a5c8be68ad399bd335bbb97590d3b
iqmulator <iqmulator@gmail.com>
2022-02-04 22:46:32 +0500

    Dropdown and Menu components, attach menu, message menu, message submenu

--- src/dotnet/UI.Blazor/exports.ts
+++ src/dotnet/UI.Blazor/exports.ts
@@ -2,5 +2,5 @@ export * from './Controls/VirtualList/virtual-list';
 export * from './Components/Dropdown/dropdown';
 import './Components/Navbar/navbar.css'
 import './Components/AccountDropdown/account-dropdown.css'
-import './Components/Dropdown/dropdown.css'
+import './Components/Menu/menu.css'
 import './Components/tooltip.css'
