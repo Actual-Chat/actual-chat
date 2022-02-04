d8f2376b7ae3a55aee2374ec6721779c5a266588
Dmitry Filippov <crui3er@gmail.com>
2022-02-04 15:55:09 +0500

    feat: added EF migration for UserAvatars

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20220204101911_UserAvatars.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations.Migrations
+{
+    public partial class UserAvatars : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "ChatAuthors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "ChatAuthors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
