a80b6fc29d52a329e36aaf1c8c50c1c945fcf63f
Vladimir Chirikov <vovanchig@gmail.com>
2022-02-08 12:10:20 +0300

    feat: new lib for prometheus metrics

--- src/dotnet/Host/Module/HostSettings.cs
+++ src/dotnet/Host/Module/HostSettings.cs
@@ -8,15 +8,5 @@ public class HostSettings
     /// Path to the folder or google cloud storage bucket <br/>(example: <c>gs://BUCKET_NAME/OBJECT_NAME</c>)
     /// </summary>
     public string DataProtection { get; set; } = "";
-    public PrometheusSettings? Prometheus { get; set; }
-}
-
-public class PrometheusSettings
-{
-    public string Endpoint { get; set; } = "/metrics";
-    public bool Gc { get; set; }
-    public bool Contention { get; set; }
-    public bool Jit { get; set; }
-    public bool ThreadPool { get; set; }
-    public bool Exceptions { get; set; }
+    public string PrometheusEndpoint { get; set; } = "";
 }
