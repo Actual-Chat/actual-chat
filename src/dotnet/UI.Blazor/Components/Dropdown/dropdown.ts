6c326975d247de5e9710f133d32525cb344d6671
iqmulator <iqmulator@gmail.com>
2022-02-08 14:09:07 +0500

    refactor dropdown and menu components, add dispose to chat-message-editor.ts

--- src/dotnet/UI.Blazor/Components/Dropdown/dropdown.ts
+++ src/dotnet/UI.Blazor/Components/Dropdown/dropdown.ts
@@ -1,52 +1,52 @@
 ﻿export class Dropdown {
 
-    private _blazorRef: DotNet.DotNetObject;
-    private _dropdown: HTMLDivElement;
-    private _contentDiv: HTMLDivElement;
-    private _contentBody: HTMLDivElement;
-    private _controlBtn: HTMLButtonElement;
+    private blazorRef: DotNet.DotNetObject;
+    private dropdown: HTMLDivElement;
+    private contentDiv: HTMLDivElement;
+    private contentBody: HTMLDivElement;
+    private controlBtn: HTMLButtonElement;
 
-    static create(dropdown: HTMLDivElement, backendRef: DotNet.DotNetObject): Dropdown {
-        return new Dropdown(dropdown, backendRef);
+    static create(dropdown: HTMLDivElement, blazorRef: DotNet.DotNetObject): Dropdown {
+        return new Dropdown(dropdown, blazorRef);
     }
 
     constructor(dropdown: HTMLDivElement, blazorRef: DotNet.DotNetObject) {
-        this._blazorRef = blazorRef;
-        this._dropdown = dropdown;
-        this._contentDiv = this._dropdown.querySelector('.dropdown-content');
-        this._contentBody = this._contentDiv.querySelector('.dropdown-content-body');
-        this._controlBtn = this._dropdown.querySelector('.dropdown-button');
+        this.blazorRef = blazorRef;
+        this.dropdown = dropdown;
+        this.contentDiv = this.dropdown.querySelector('.dropdown-content');
+        this.contentBody = this.contentDiv.querySelector('.dropdown-content-body');
+        this.controlBtn = this.dropdown.querySelector('.dropdown-button');
 
         window.addEventListener('mouseup', this.mouseListener)
         document.addEventListener('keydown', this.escapeListener);
     }
 
-    public mouseListener = ((event: MouseEvent & {target: Element; }) => {
-        let menu = this._contentDiv;
-        let control = this._controlBtn;
-        let dropdown = this._dropdown;
+    private mouseListener = ((event: MouseEvent & {target: Element; }) => {
+        const menu = this.contentDiv;
+        const control = this.controlBtn;
+        const dropdown = this.dropdown;
         if (control.contains(event.target))
             return;
         if (!dropdown.contains(event.target) && menu.classList.contains('dropdown-content-opened')) {
-            this.HideContent(this._blazorRef);
+            this.hideContent();
         }
     })
 
-    public escapeListener = ((event: KeyboardEvent & {target: Element; }) => {
+    private escapeListener = ((event: KeyboardEvent & {target: Element; }) => {
         if (event.keyCode == 27 || event.key == "Escape" || event.key == "Esc") {
-            let content = this._contentDiv;
+            const content = this.contentDiv;
             if (content.classList.contains('dropdown-content-opened')) {
-                this.HideContent(this._blazorRef);
+                this.hideContent();
             }
         }
     })
 
-    public dispose() {
+    private dispose() {
         window.removeEventListener('mouseup', this.mouseListener);
         document.removeEventListener('keydown', this.escapeListener);
     }
 
-    public HideContent(ref: DotNet.DotNetObject) {
-        ref.invokeMethodAsync('HideContent');
+    private hideContent() {
+        this.blazorRef.invokeMethodAsync('HideContent');
     }
 }
