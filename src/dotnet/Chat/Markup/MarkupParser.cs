1557bd78892b10897085efec7ca334fd5f40adbe
Dmitry Filippov <crui3er@gmail.com>
2022-02-10 00:04:29 +0500

    wip: Markup. Preserv new lines.

--- src/dotnet/Chat/Markup/MarkupParser.cs
+++ src/dotnet/Chat/Markup/MarkupParser.cs
@@ -4,7 +4,11 @@ namespace ActualChat.Chat;
 
 public sealed class MarkupParser
 {
-    private static readonly MarkdownPipeline _pipeline = new MarkdownPipelineBuilder().UseAutoLinks().UsePreciseSourceLocation().Build();
+    private static readonly MarkdownPipeline _pipeline = new MarkdownPipelineBuilder()
+        .UseAutoLinks()
+        .UsePreciseSourceLocation()
+        .DisableHeadings()
+        .Build();
 
     public ValueTask<Markup> Parse(string text, LinearMap textToTimeMap = default)
     {
