4985ed9781002cd95e06c79476dfcecb4bff33f5
iqmulator <iqmulator@gmail.com>
2022-02-21 12:26:31 +0500

    little updates for listening chats panel

--- src/dotnet/Chat.UI.Blazor/Services/ChatAuthorListeningChats.cs
+++ src/dotnet/Chat.UI.Blazor/Services/ChatAuthorListeningChats.cs
@@ -17,14 +17,20 @@ public class ChatAuthorListeningChats
     public async Task AddChat(string chatId, CancellationToken cancellationToken)
     {
         var chat = await Chats.Get(Session, chatId, cancellationToken).ConfigureAwait(false);
-        if (chat != null && !ListeningChats.Contains(chat))
+        if (chat != null && !ListeningChats.Contains(chat)) {
             ListeningChats.Add(chat);
+            Updated?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public async Task RemoveChat(string chatId, CancellationToken cancellationToken)
     {
         var chat = await Chats.Get(Session, chatId, cancellationToken).ConfigureAwait(false);
-        if (chat != null && ListeningChats.Contains(chat))
+        if (chat != null && ListeningChats.Contains(chat)) {
             ListeningChats.Remove(chat);
+            Updated?.Invoke(this, EventArgs.Empty);
+        }
     }
+
+    public event EventHandler? Updated;
 }
