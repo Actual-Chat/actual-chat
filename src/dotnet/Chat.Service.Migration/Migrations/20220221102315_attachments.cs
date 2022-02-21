cd525786ec37e4e423b88227ff6879fdc24c315f
Dmitry Filippov <crui3er@gmail.com>
2022-02-21 15:43:11 +0500

    feat: ef migration for text entry attachments

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20220221102315_attachments.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations.Migrations
+{
+    public partial class attachments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasAttachments",
+                table: "ChatEntries",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "TextEntryAttachments",
+                columns: table => new
+                {
+                    CompositeId = table.Column<string>(type: "text", nullable: false),
+                    ChatId = table.Column<string>(type: "text", nullable: false),
+                    EntryId = table.Column<long>(type: "bigint", nullable: false),
+                    Index = table.Column<int>(type: "integer", nullable: false),
+                    Version = table.Column<long>(type: "bigint", nullable: false),
+                    ContentId = table.Column<string>(type: "text", nullable: false),
+                    Length = table.Column<long>(type: "bigint", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    ContentType = table.Column<string>(type: "text", nullable: false),
+                    Width = table.Column<int>(type: "integer", nullable: false),
+                    Height = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TextEntryAttachments", x => x.CompositeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TextEntryAttachments");
+
+            migrationBuilder.DropColumn(
+                name: "HasAttachments",
+                table: "ChatEntries");
+        }
+    }
+}
