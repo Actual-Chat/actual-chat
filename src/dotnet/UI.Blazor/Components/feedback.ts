67ac90322ebc01944b6687693342dc6ddfa0c1ae
iqmulator <iqmulator@gmail.com>
2022-03-03 16:07:13 +0500

    textarea update

---
+++ src/dotnet/UI.Blazor/Components/feedback.ts
@@ -0,0 +1,21 @@
+﻿import './feedback.css';
+
+export class Feedback {
+    private blazorRef: DotNet.DotNetObject;
+    private feedbackDiv: HTMLDivElement;
+
+    static create(feedbackDiv: HTMLDivElement, blazorRef: DotNet.DotNetObject): Feedback {
+        return new Feedback(feedbackDiv, blazorRef);
+    }
+
+    constructor(feedbackDiv: HTMLDivElement, blazorRef: DotNet.DotNetObject) {
+        this.feedbackDiv = feedbackDiv;
+
+        this.feedbackDiv.addEventListener('mouseover', this.starHoverListener);
+
+    }
+
+    private starHoverListener = ((event: Event & { target: Element; }) => {
+        console.log(Element.length);
+    })
+}
---
+++ src/dotnet/UI.Blazor/IFeatureBackend.cs
@@ -0,0 +1,8 @@
+﻿namespace ActualChat.UI.Blazor;
+
+/// <summary>
+/// Is used from js, part of workaround <see href="https://github.com/dotnet/aspnetcore/issues/9974"/>
+/// </summary>
+public interface IFeatureBackend
+{
+}
