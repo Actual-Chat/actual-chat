de471586f06e8a566c2f51931054f8b9041c519d
Dmitry Filippov <crui3er@gmail.com>
2022-03-11 19:10:12 +0500

    update feature modal, stars, close button

--- src/dotnet/UI.Blazor/Services/FeedbackService.cs
+++ src/dotnet/UI.Blazor/Services/FeedbackService.cs
@@ -25,7 +25,7 @@ public class FeedbackService
 
         var parameters = new ModalParameters();
         parameters.Add(nameof(FeatureRequestFeedback.FeatureTitle), featureTitle);
-        var modalOptions = new ModalOptions { HideHeader = true};
+        var modalOptions = new ModalOptions { HideHeader = true, DisableBackgroundCancel = true, Class = "feature-modal"};
         _modalReference = _modalService.Show<FeatureRequestFeedback>("", parameters, modalOptions);
         var result = await _modalReference.Result.ConfigureAwait(false);
         _modalReference = null;
