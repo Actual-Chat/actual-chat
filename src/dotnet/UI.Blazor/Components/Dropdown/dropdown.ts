7e720ef83e962c90e2f2cc564203108f03dce5c8
Vladimir Chirikov <vovanchig@gmail.com>
2022-03-18 12:25:00 +0300

    fix: eslint issues

--- src/dotnet/UI.Blazor/Components/Dropdown/dropdown.ts
+++ src/dotnet/UI.Blazor/Components/Dropdown/dropdown.ts
@@ -3,10 +3,9 @@
     private blazorRef: DotNet.DotNetObject;
     private dropdown: HTMLDivElement;
     private contentDiv: HTMLDivElement;
-    private contentBody: HTMLDivElement;
     private controlBtn: HTMLButtonElement;
 
-    static create(dropdown: HTMLDivElement, blazorRef: DotNet.DotNetObject): Dropdown {
+    public static create(dropdown: HTMLDivElement, blazorRef: DotNet.DotNetObject): Dropdown {
         return new Dropdown(dropdown, blazorRef);
     }
 
@@ -14,37 +13,40 @@
         this.blazorRef = blazorRef;
         this.dropdown = dropdown;
         this.contentDiv = this.dropdown.querySelector('.dropdown-content');
-        this.contentBody = this.contentDiv.querySelector('.dropdown-content-body');
         this.controlBtn = this.dropdown.querySelector('.dropdown-button');
 
-        window.addEventListener('mouseup', this.mouseListener)
+        window.addEventListener('mouseup', this.mouseListener);
         document.addEventListener('keydown', this.escapeListener);
     }
 
-    private mouseListener = ((event: MouseEvent & {target: Element; }) => {
-        const {contentDiv, controlBtn, dropdown} = this;
+    private mouseListener = ((event: MouseEvent & { target: Element; }) => {
+        const { contentDiv, controlBtn, dropdown } = this;
         if (controlBtn.contains(event.target))
             return;
         if (!dropdown.contains(event.target) && contentDiv.classList.contains('dropdown-content-opened')) {
-            this.hideContent();
+            void this.hideContent();
         }
-    })
+    });
 
-    private escapeListener = ((event: KeyboardEvent & {target: Element; }) => {
-        if (event.keyCode == 27 || event.key == "Escape" || event.key == "Esc") {
+    private escapeListener = ((event: KeyboardEvent & { target: Element; }) => {
+        if (event.keyCode === 27 || event.key === 'Escape' || event.key === 'Esc') {
             const content = this.contentDiv;
             if (content.classList.contains('dropdown-content-opened')) {
-                this.hideContent();
+                void this.hideContent();
             }
         }
-    })
+    });
 
-    private dispose() {
+    public dispose() {
         window.removeEventListener('mouseup', this.mouseListener);
         document.removeEventListener('keydown', this.escapeListener);
+        this.blazorRef = null;
+        this.contentDiv = null;
+        this.dropdown = null;
+        this.controlBtn = null;
     }
 
-    private hideContent() {
-        this.blazorRef.invokeMethodAsync('HideContent');
+    private async hideContent(): Promise<void> {
+        await this.blazorRef.invokeMethodAsync('HideContent');
     }
 }
