96bd4b21e97ebca828b73b508f07da93dbee8d67
iqmulator <iqmulator@gmail.com>
2022-03-18 22:59:19 +0500

    navbar menu update

--- src/dotnet/UI.Blazor/exports.ts
+++ src/dotnet/UI.Blazor/exports.ts
@@ -1,9 +1,11 @@
 export * from './Controls/VirtualList/virtual-list';
 export * from './Components/Dropdown/dropdown';
 export * from './Components/feedback';
+export * from './Components/Navbar/navbar';
 import './Components/Navbar/navbar.css'
 import './Components/AccountDropdown/account-dropdown.css'
 import './Components/Menu/menu.css'
 import './Components/tooltip.css'
 import './Components/feedback.css'
 import './Components/feedback'
+import './Components/Navbar/navbar'
