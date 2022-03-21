566ebbd535e474ccb0479e4ece94e9458deeefe7
Alex Yakunin <alex.yakunin@gmail.com>
2022-03-21 16:52:09 -0700

    chore: simplify ChatPlayers

--- src/dotnet/Chat.UI.Blazor/Services/ChatPlayers.cs
+++ src/dotnet/Chat.UI.Blazor/Services/ChatPlayers.cs
@@ -3,7 +3,7 @@
 /// <summary> Must be scoped service. </summary>
 public class ChatPlayers : IAsyncDisposable
 {
-    private readonly ConcurrentDictionary<Symbol, Lazy<ChatPlayer>> _players = new();
+    private readonly ConcurrentDictionary<Symbol, ChatPlayer> _players = new();
     private readonly IServiceProvider _services;
     private readonly ILogger<ChatPlayers> _log;
     private int _isDisposed;
@@ -16,21 +16,15 @@ public class ChatPlayers : IAsyncDisposable
 
     [ComputeMethod]
     public virtual Task<ChatPlayer?> GetPlayer(Symbol chatId)
-    {
-        if (!_players.TryGetValue(chatId, out var player))
-            return Task.FromResult((ChatPlayer?)null);
-        return Task.FromResult((ChatPlayer?)player.Value);
-    }
+        => Task.FromResult(_players.GetValueOrDefault(chatId));
 
     public virtual ChatPlayer ActivatePlayer(Symbol chatId)
     {
         if (_isDisposed == 1)
             throw new ObjectDisposedException(nameof(ChatPlayers));
-        var player = _players.GetOrAdd(
-            chatId,
-            static (key, self) => new Lazy<ChatPlayer>(() => self._services.Activate<ChatPlayer>(key)),
-            this
-        ).Value;
+        var player = _players.GetOrAdd(chatId,
+            static (key, self) => self._services.Activate<ChatPlayer>(key),
+            this);
 
         using (Computed.Invalidate()) {
             _ = GetPlayer(chatId);
@@ -44,8 +38,8 @@ public class ChatPlayers : IAsyncDisposable
         if (_isDisposed == 1)
             throw new ObjectDisposedException(nameof(ChatPlayers));
         if (_players.TryRemove(chatId, out var player)) {
-            await player.Value.DisposeAsync().ConfigureAwait(false);
-            _log.LogDebug("Disposed player for chatId:{chatId}", chatId);
+            await player.DisposeAsync().ConfigureAwait(false);
+            _log.LogDebug("Disposed player for chat #{ChatId}", chatId);
         }
 
         using (Computed.Invalidate()) {
@@ -69,7 +63,7 @@ public class ChatPlayers : IAsyncDisposable
         }
 
         var playerDisposeTasks = players
-            .Select(kv => DisposePlayer(kv.Key, kv.Value.Value))
+            .Select(kv => DisposePlayer(kv.Key, kv.Value))
             .ToArray();
 
         if (playerDisposeTasks.Length > 0)
@@ -79,10 +73,10 @@ public class ChatPlayers : IAsyncDisposable
         {
             try {
                 await player.DisposeAsync().ConfigureAwait(true);
-                _log.LogDebug("Disposed player for chatId:{chatId}", chatId);
+                _log.LogDebug("Disposed player for chat #{ChatId}", chatId);
             }
             catch (Exception e) {
-                _log.LogError(e, "Can't dispose player for chatId:{chatId}", chatId);
+                _log.LogError(e, "Can't dispose player for chat #{ChatId}", chatId);
             }
         }
     }
