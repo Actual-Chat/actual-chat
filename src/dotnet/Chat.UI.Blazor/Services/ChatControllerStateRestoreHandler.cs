e67e33ba3ab4024bda1e6ee34f67127fc577e338
Dmitry Filippov <crui3er@gmail.com>
2022-03-24 01:57:50 +0400

    feat: do not restore listening/recording on chat if no enough permissions

--- src/dotnet/Chat.UI.Blazor/Services/ChatControllerStateRestoreHandler.cs
+++ src/dotnet/Chat.UI.Blazor/Services/ChatControllerStateRestoreHandler.cs
@@ -7,18 +7,24 @@ public class ChatControllerStateRestoreHandler : StateRestoreHandler<string[]>
 {
     private readonly ListeningChats _listeningChats;
     private readonly ChatController _chatController;
+    private readonly IChats _chats;
+    private readonly Session _session;
     private readonly InteractionUI _interactionUi;
 
     public ChatControllerStateRestoreHandler(
         ListeningChats listeningChats,
         ChatController chatController,
         InteractionUI interactionUi,
+        IChats chats,
+        Session session,
         IServiceProvider services)
         : base(services)
     {
         _listeningChats = listeningChats;
         _chatController = chatController;
         _interactionUi = interactionUi;
+        _chats = chats;
+        _session = session;
     }
 
     protected override string StoreItemKey => "listeningChats";
@@ -26,7 +32,10 @@ public class ChatControllerStateRestoreHandler : StateRestoreHandler<string[]>
     protected override async Task Restore(string[]? itemValue)
     {
         var listeningChats = itemValue;
-        if (listeningChats == null || listeningChats.Length <= 0)
+        if (listeningChats == null || listeningChats.Length == 0)
+            return;
+        listeningChats = await FilterCanRead(listeningChats).ConfigureAwait(false);
+        if (listeningChats.Length == 0)
             return;
         await _interactionUi.RequestInteraction().ConfigureAwait(false);
         var tasks = new List<Task>();
@@ -40,6 +49,18 @@ public class ChatControllerStateRestoreHandler : StateRestoreHandler<string[]>
     protected override async Task<string[]> Compute(CancellationToken cancellationToken)
     {
         var chatIds = await _listeningChats.GetChatIds(default).ConfigureAwait(false);
+        return chatIds.ToArray();
+    }
+
+    private async Task<string[]> FilterCanRead(string[] listeningChats)
+    {
+        var chatIds = new List<string>();
+        foreach (var chatId in listeningChats) {
+            var permissions = await _chats.GetPermissions(_session, chatId, default).ConfigureAwait(false);
+            if (!permissions.HasFlag(ChatPermissions.Read))
+                continue;
+            chatIds.Add(chatId);
+        }
         return chatIds.ToArray();
     }
 }
--- src/dotnet/Chat.UI.Blazor/Services/RecorderControllerStateRestoreHandler.cs
+++ src/dotnet/Chat.UI.Blazor/Services/RecorderControllerStateRestoreHandler.cs
@@ -8,14 +8,20 @@ public class RecorderControllerStateRestoreHandler : StateRestoreHandler<string>
 {
     private readonly AudioRecorderService _recorderService;
     private readonly InteractionUI _interactionUi;
+    private readonly IChats _chats;
+    private readonly Session _session;
 
     public RecorderControllerStateRestoreHandler(
         AudioRecorderService recorderService,
         InteractionUI interactionUi,
+        IChats chats,
+        Session session,
         IServiceProvider services)
         : base(services)
     {
         _recorderService = recorderService;
+        _chats = chats;
+        _session = session;
         _interactionUi = interactionUi;
     }
 
@@ -24,10 +30,15 @@ public class RecorderControllerStateRestoreHandler : StateRestoreHandler<string>
     protected override async Task Restore(string? itemValue)
     {
         var audioRecorderChat = itemValue;
-        if (!string.IsNullOrEmpty(audioRecorderChat)) {
-            await _interactionUi.RequestInteraction().ConfigureAwait(false);
-            await _recorderService.StartRecording(audioRecorderChat).ConfigureAwait(false);
-        }
+        if (string.IsNullOrEmpty(audioRecorderChat))
+            return;
+
+        var permissions = await _chats.GetPermissions(_session, audioRecorderChat, default).ConfigureAwait(false);
+        if (!permissions.HasFlag(ChatPermissions.Write))
+            return;
+
+        await _interactionUi.RequestInteraction().ConfigureAwait(false);
+        await _recorderService.StartRecording(audioRecorderChat).ConfigureAwait(false);
     }
 
     protected override async Task<string> Compute(CancellationToken cancellationToken)
