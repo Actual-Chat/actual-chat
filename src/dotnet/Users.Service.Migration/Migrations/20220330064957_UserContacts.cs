ed0aa71f57e4df87d1f0860aa32fa42e82362ec6
Dmitry Filippov <crui3er@gmail.com>
2022-03-30 10:52:20 +0400

    feat: add migrations for UserContracts

---
+++ src/dotnet/Users.Service.Migration/Migrations/20220330064957_UserContacts.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Users.Migrations.Migrations
+{
+    public partial class UserContacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserContacts",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    OwnerUserId = table.Column<string>(type: "text", nullable: false),
+                    TargetUserId = table.Column<string>(type: "text", nullable: false),
+                    Version = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserContacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContacts_OwnerUserId",
+                table: "UserContacts",
+                column: "OwnerUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserContacts");
+        }
+    }
+}
