b0eec428b56cb6f259c52fab7652ac0cd9de137c
Dmitry Filippov <crui3er@gmail.com>
2022-04-20 14:05:53 +0400

    wip: Like button on slate-editor

--- src/dotnet/Chat.UI.Blazor/Components/SlateEditor/slate-editor.ts
+++ src/dotnet/Chat.UI.Blazor/Components/SlateEditor/slate-editor.ts
@@ -9,8 +9,35 @@ export class SlateEditor {
     constructor(editorDiv: HTMLDivElement, blazorRef: DotNet.DotNetObject) {
         this.editorDiv = editorDiv;
         this.blazorRef = blazorRef;
+
+        const e = React.createElement;
+        const root = ReactDOM.createRoot(editorDiv);
+        root.render(e(LikeButton));
     }
 
     private dispose() {
+    }
+}
+
+const e = React.createElement;
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false
+        };
+    }
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+        return e(
+            'button', {
+                onClick: () => this.setState({
+                                                 liked: true
+                                             })
+            },
+            'Like'
+        );
     }
 }
