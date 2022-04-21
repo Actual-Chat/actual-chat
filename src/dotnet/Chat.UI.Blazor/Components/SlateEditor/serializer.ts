9c83181f2315f1ae1b4a154a0bc301e7f18a4f47
Dmitry Filippov <crui3er@gmail.com>
2022-04-21 15:13:06 +0400

    wip: SlateEditor improver GetText function

--- src/dotnet/Chat.UI.Blazor/Components/SlateEditor/serializer.ts
+++ src/dotnet/Chat.UI.Blazor/Components/SlateEditor/serializer.ts
@@ -6,13 +6,23 @@ export const serialize = node => {
         return node.text
     }
 
-    const children = node.children.map(n => serialize(n)).join('')
+    let children : string = ''
+    let i = 0;
+    for (const child of node.children) {
+        const child = node.children[i];
+        const isLast = i === node.children.length - 1;
+        const s = serialize(child)
+        children += s;
+        if (child.type == 'paragraph' && !isLast)
+            children += '\n'
+        i++
+    }
 
     switch (node.type) {
         case 'mention':
             return `<@${node.character}>`
         case 'paragraph':
-            return (children + '\n')
+            return children
         default:
             return children
     }
