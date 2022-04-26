5c42a78f2a102d0e66b5ac6a34390ea53e8986c5
Dmitry Filippov <crui3er@gmail.com>
2022-04-26 23:19:08 +0400

    refactoring: extract and simplify 'calculateMentionTarget' function in slate-editor-core

--- src/dotnet/Chat.UI.Blazor/Components/SlateEditor/slate-editor-core.tsx
+++ src/dotnet/Chat.UI.Blazor/Components/SlateEditor/slate-editor-core.tsx
@@ -14,7 +14,6 @@ import { serialize } from './serializer';
 
 export const createSlateEditorCore = (handle : SlateEditorHandle, debug : boolean) => {
     const [target, setTarget] = useState<Range | undefined>()
-    const [search, setSearch] = useState('')
     const [placeholder, setPlaceholder] = useState('')
     const [hasContent, setHasContent] = useState(false)
     const renderElement = useCallback(props => <Element {...props} />, [])
@@ -23,39 +22,12 @@ export const createSlateEditorCore = (handle : SlateEditorHandle, debug : boolea
         []
     )
 
-    const trimLeftSpecial = (str : string) : string => {
-        if (str.length===0)
-            return str;
-        // after first character is typed, char with code 65279 is added in the start of the string
-        // so let's remove it
-        if (str.charCodeAt(0)===65279)
-            return str.substring(1);
-        return str;
-    }
-
     handle.getText = () => trimLeftSpecial(serialize(editor));
     handle.setPlaceholder = setPlaceholder;
 
-    const resetEditor = () => {
-        if (debug) console.log('reset editor started')
-
-        Transforms.delete(editor, {
-            at: {
-                anchor: Editor.start(editor, []),
-                focus: Editor.end(editor, []),
-            },
-        });
-        editor.history = {
-            undos: [],
-            redos: []
-        };
-
-        if (debug) console.log('reset editor completed')
-    }
-
     handle.clearText = () => {
         if (debug) console.log('clear text')
-        resetEditor()
+        resetEditor(editor)
     }
 
     handle.insertMention = (mention : any) => {
@@ -99,10 +71,11 @@ export const createSlateEditorCore = (handle : SlateEditorHandle, debug : boolea
                 }
             }
         },
-        [search, target]
+        [target]
     )
 
     useEffect(() => {
+        const search = target ? Editor.string(editor, target).substring(1) : ''
         if (debug) {
             console.log("search: " + search)
             console.log("target: " + (target ? JSON.stringify(target) : 'null'))
@@ -111,106 +84,22 @@ export const createSlateEditorCore = (handle : SlateEditorHandle, debug : boolea
             handle.getMention.show(search)
         else
             handle.getMention.close()
-    }, [search, target])
+    }, [target])
 
     useEffect(() => {
         if (debug) console.log("hasContent: " + hasContent)
         handle.onHasContentChanged(hasContent)
     }, [hasContent])
 
-    const calculateHasContent = (children : Descendant[]) : boolean =>
-    {
-        if (!children || children.length===0)
-            return false;
-        if (children.length > 1)
-            return true;
-        const paragraph = children[0] as ParagraphElement;
-        if (!paragraph)
-            return true;
-        const children2 = paragraph.children;
-        if (!children2 || children2.length===0)
-            return false;
-        if (children2.length > 1)
-            return true;
-        const text = children2[0] as CustomText;
-        if (!text)
-            return false;
-        if (text.text.length > 1)
-            return true;
-        return trimLeftSpecial(text.text).length > 0;
-    }
-
     return (
         <Slate
             editor={editor}
             value={editorEmptyValue}
             onChange={() => {
-                const { selection, children } = editor
-
-                setHasContent(calculateHasContent(children))
-
-                if (selection && Range.isCollapsed(selection)) {
-                    const [start] = Range.edges(selection)
-                    const wordBefore = Editor.before(editor, start, { unit: 'word' })
-                    const wordBeforeRange = wordBefore && Editor.range(editor, wordBefore, start)
-                    const wordBeforeText = wordBeforeRange && Editor.string(editor, wordBeforeRange)
-                    if (debug && wordBeforeText) {
-                        console.log("wordBeforeText: " + wordBeforeText)
-                    }
-                    let searchCandidate = undefined
-                    let targetCandidate = undefined
-                    if (wordBeforeText && wordBeforeText.includes('@')) {
-                        if (wordBeforeText.length == 1) {
-                            searchCandidate = ''
-                            targetCandidate = wordBeforeRange
-                        }
-                        else {
-                            const before1 = wordBefore && Editor.before(editor, start)
-                            const before1Range = before1 && Editor.range(editor, before1, start)
-                            const before2 = before1 && Editor.before(editor, before1)
-                            const beforeTextRange = before2
-                                                    ? Editor.range(editor, before2, start)
-                                                    :before1Range;
-                            const beforeText = beforeTextRange && Editor.string(editor, beforeTextRange)
-                            const beforeMatch = beforeText && beforeText.match(/(^|\s)@$/)
-
-                            if (beforeMatch) {
-                                searchCandidate = ''
-                                targetCandidate = before1Range
-                            }
-                        }
-                    }
-                    else {
-                        const before1 = wordBefore && Editor.before(editor, wordBefore)
-                        const before1Range = before1
-                                             ? Editor.range(editor, before1, start)
-                                             : wordBefore && Editor.range(editor, wordBefore, start)
-                        const before2 = before1 && Editor.before(editor, before1)
-                        const beforeTextRange = before2
-                                                ? Editor.range(editor, before2, start)
-                                                :before1Range;
-                        const beforeText = beforeTextRange && Editor.string(editor, beforeTextRange)
-                        const beforeMatch = beforeText && beforeText.match(/(^|\s)@(\w*)$/)
-
-                        if (beforeMatch) {
-                            searchCandidate = beforeMatch[2]
-                            targetCandidate = before1Range
-                        }
-                    }
-
-                    const after = Editor.after(editor, start)
-                    const afterRange = Editor.range(editor, start, after)
-                    const afterText = Editor.string(editor, afterRange)
-                    const afterMatch = afterText.match(/^(\s|$)/)
-
-                    if (searchCandidate !== undefined && targetCandidate && afterMatch) {
-                        setSearch(searchCandidate)
-                        setTarget(targetCandidate)
-                        return
-                    }
-                }
-
-                setTarget(null)
+                const hasContent1 = calculateHasContent(editor.children)
+                setHasContent(hasContent1)
+                const target1 = calculateMentionTarget(editor, debug)
+                setTarget(target1)
             }}
         >
             <Editable
@@ -220,6 +109,106 @@ export const createSlateEditorCore = (handle : SlateEditorHandle, debug : boolea
             />
         </Slate>
     )
+}
+
+const trimLeftSpecial = (str : string) : string => {
+    if (str.length===0)
+        return str;
+    // after first character is typed, char with code 65279 is added in the start of the string
+    // so let's remove it
+    if (str.charCodeAt(0)===65279)
+        return str.substring(1);
+    return str;
+}
+
+const resetEditor = (editor: CustomEditor) => {
+    Transforms.delete(editor, {
+        at: {
+            anchor: Editor.start(editor, []),
+            focus: Editor.end(editor, []),
+        },
+    });
+    editor.history = {
+        undos: [],
+        redos: []
+    };
+}
+
+const calculateHasContent = (children : Descendant[]) : boolean =>
+{
+    if (!children || children.length===0)
+        return false;
+    if (children.length > 1)
+        return true;
+    const paragraph = children[0] as ParagraphElement;
+    if (!paragraph)
+        return true;
+    const children2 = paragraph.children;
+    if (!children2 || children2.length===0)
+        return false;
+    if (children2.length > 1)
+        return true;
+    const text = children2[0] as CustomText;
+    if (!text)
+        return false;
+    if (text.text.length > 1)
+        return true;
+    return trimLeftSpecial(text.text).length > 0;
+}
+
+const calculateMentionTarget = (editor : CustomEditor, debug : boolean) : Range => {
+    const { selection } = editor
+    if (selection && Range.isCollapsed(selection)) {
+        const [cursorPosition] = Range.edges(selection)
+        // get a range that ends at cursor position and starts at nearest word boundary
+        const wordBefore = Editor.before(editor, cursorPosition, { unit: 'word' })
+        const wordBeforeRange = wordBefore && Editor.range(editor, wordBefore, cursorPosition)
+        const wordBeforeText = wordBeforeRange && Editor.string(editor, wordBeforeRange)
+
+        let targetCandidate: Range | undefined = undefined
+
+        if (wordBeforeText) {
+            if (debug) console.log("wordBeforeText: " + wordBeforeText)
+
+            if (wordBeforeText && wordBeforeText.endsWith('@')) {
+                // cursor is right after '@' character
+                // check if there is a space or nothing before it
+                const beforeMatch = wordBeforeText.match(/(^|\s)@$/)
+
+                if (beforeMatch) {
+                    const before1 = Editor.before(editor, cursorPosition)
+                    targetCandidate = Editor.range(editor, before1, cursorPosition)
+                }
+            } else {
+                // try to take 2 character before word start boundary
+                const before1 = Editor.before(editor, wordBefore)
+                const before2 = before1 && Editor.before(editor, before1)
+                const before = before2 || before1 || wordBefore;
+
+                const beforeTextRange = Editor.range(editor, before, cursorPosition)
+                const beforeText = beforeTextRange && Editor.string(editor, beforeTextRange)
+                const beforeMatch = beforeText && beforeText.match(/(^|\s)@(\w*)$/)
+
+                if (beforeMatch) {
+                    targetCandidate = Editor.range(editor, before1, cursorPosition)
+                }
+            }
+        }
+
+        if (targetCandidate) {
+            // check if there is a space or nothing after cursor
+            const after = Editor.after(editor, cursorPosition)
+            const afterRange = Editor.range(editor, cursorPosition, after)
+            const afterText = Editor.string(editor, afterRange)
+            const afterMatch = afterText.match(/^(\s|$)/)
+
+            if (afterMatch) {
+                return targetCandidate
+            }
+        }
+    }
+
+    return null
 }
 
 const withMentions = (editor : CustomEditor) : CustomEditor  => {
