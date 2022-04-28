c3915aa27287161961c5fad2366443e2c6c0d85a
iqmulator <iqmulator@gmail.com>
2022-04-28 15:27:57 +0500

    update delete message modal for messages with file and image

--- src/dotnet/Chat.UI.Blazor/Services/ChatUI.cs
+++ src/dotnet/Chat.UI.Blazor/Services/ChatUI.cs
@@ -23,7 +23,7 @@ public class ChatUI
         _modalService.Show<ChatAuthorCard>(null, modalParameters, options);
     }
 
-    public void ShowDeleteMessageRequest(ChatEntry entry, ChatAuthor requestAuthor)
+    public void ShowDeleteMessageRequest(ChatMessageModel model, ChatAuthor requestAuthor)
     {
         var modalParameters = new ModalParameters();
         var options = new ModalOptions() {
@@ -32,7 +32,7 @@ public class ChatUI
             Class = "custom-modal-class",
         };
 
-        modalParameters.Add(nameof(DeleteMessageModal.Entry), entry);
+        modalParameters.Add(nameof(DeleteMessageModal.Model), model);
         modalParameters.Add(nameof(DeleteMessageModal.RequestAuthor), requestAuthor);
         _modalService.Show<DeleteMessageModal>("Delete Message", modalParameters, options);
     }
