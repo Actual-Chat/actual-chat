3d9e78657c0d7a1d79a3d7fbf207b67d5e57f29c
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2022-05-13 18:12:41 +0300

    WIP

--- src/dotnet/Db/StringExtensions.cs
+++ src/dotnet/Db/StringExtensions.cs
@@ -4,11 +4,15 @@ namespace ActualChat.Db;
 
 internal static class StringExtensions
 {
+    private static readonly Regex _camelCaseRegex = new (@"([a-z0-9])([A-Z])", RegexOptions.Compiled);
+
     public static string ToSnakeCase(this string input)
     {
-        if (string.IsNullOrEmpty(input)) { return input; }
+        if (string.IsNullOrEmpty(input))
+            return input;
 
-        var startUnderscores = Regex.Match(input, @"^_+");
-        return startUnderscores + Regex.Replace(input, @"([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        return _camelCaseRegex.Replace(input, "$1_$2")
+            .ToLower(CultureInfo.InvariantCulture)
+            .Replace("__", "_", StringComparison.InvariantCulture);
     }
 }
---
+++ src/dotnet/Invite.Service.Migration/Migrations/20220513151104_Initial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Invite.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "_operations",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    agent_id = table.Column<string>(type: "text", nullable: false),
+                    start_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    commit_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    command_json = table.Column<string>(type: "text", nullable: false),
+                    items_json = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_operations", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "invites",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    version = table.Column<long>(type: "bigint", nullable: false),
+                    search_key = table.Column<string>(type: "text", nullable: false),
+                    remaining = table.Column<int>(type: "integer", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expires_on = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    details_json = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_invites", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_commit_time",
+                table: "_operations",
+                column: "commit_time");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_start_time",
+                table: "_operations",
+                column: "start_time");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_invites_search_key_remaining",
+                table: "invites",
+                columns: new[] { "search_key", "remaining" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "_operations");
+
+            migrationBuilder.DropTable(
+                name: "invites");
+        }
+    }
+}
