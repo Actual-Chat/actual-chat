32af383e9ac7a754265e0b97326ec8d412aabf73
Alex Yakunin <alex.yakunin@gmail.com>
2022-05-13 22:37:26 -0700

    refactor: improve DbShardLocalIdGenerator API

---
+++ src/dotnet/Db/DbShardLocalIdGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq.Expressions;
+using ActualChat.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Stl.Fusion.EntityFramework;
+using Stl.Redis;
+
+namespace ActualChat.Db;
+
+public interface IDbShardLocalIdGenerator<TEntity, in TShardKey>
+    where TShardKey : class
+{
+    Task<long> Next(DbContext dbContext, TShardKey shardKey, CancellationToken cancellationToken);
+}
+
+public class DbShardLocalIdGenerator<TDbContext, TEntity, TShardKey> : IDbShardLocalIdGenerator<TEntity, TShardKey>
+    where TDbContext : DbContext
+    where TEntity : class
+    where TShardKey : class
+{
+    private readonly IThreadSafeLruCache<Symbol, long> _maxLocalIdCache;
+    private readonly RedisSequenceSet<TEntity> _idSequences;
+    private readonly Func<TDbContext, DbSet<TEntity>> _dbSetExtractor;
+    private readonly Expression<Func<TEntity, TShardKey, bool>> _shardKeyFilter;
+    private readonly Expression<Func<TEntity, long>> _localIdSelector;
+
+    public DbShardLocalIdGenerator(RedisSequenceSet<TEntity> idSequences,
+        Func<TDbContext,DbSet<TEntity>> dbSetExtractor,
+        Expression<Func<TEntity, TShardKey, bool>> shardKeyFilter,
+        Expression<Func<TEntity, long>> localIdSelector,
+        Func<IThreadSafeLruCache<Symbol, long>>? maxLocalIdCacheFactory = null)
+    {
+        _idSequences = idSequences;
+        _dbSetExtractor = dbSetExtractor;
+        _shardKeyFilter = shardKeyFilter;
+        _localIdSelector = localIdSelector;
+        _maxLocalIdCache = maxLocalIdCacheFactory?.Invoke() ?? new ConcurrentLruCache<Symbol, long>(16384);
+    }
+
+    public async Task<long> Next(
+        DbContext dbContext,
+        TShardKey shardKey,
+        CancellationToken cancellationToken)
+    {
+        // This method must be thread-safe!
+
+        var idSequenceKey = new Symbol(shardKey?.ToString() ?? "");
+        var maxLocalId = _maxLocalIdCache.GetValueOrDefault(idSequenceKey);
+        if (maxLocalId == 0) {
+            var shardFilterExpr = CreateShardFilterExpression(shardKey);
+            _maxLocalIdCache[idSequenceKey] = maxLocalId =
+                await _dbSetExtractor((TDbContext) dbContext).ForUpdate() // To serialize inserts
+                    .Where(shardFilterExpr)
+                    .OrderByDescending(_localIdSelector)
+                    .Select(_localIdSelector)
+                    .FirstOrDefaultAsync(cancellationToken)
+                    .ConfigureAwait(false);
+        }
+
+        var localId = await _idSequences.Next(idSequenceKey, maxLocalId).ConfigureAwait(false);
+        _maxLocalIdCache[idSequenceKey] = localId;
+        return localId;
+    }
+
+    private Expression<Func<TEntity, bool>> CreateShardFilterExpression(TShardKey shardKey)
+    {
+        var pShardKey = _shardKeyFilter.Parameters[1];
+        var eShardKey = Expression.Constant(shardKey);
+        return _shardKeyFilter.InlineParameter<Func<TEntity, TShardKey, bool>, Func<TEntity, bool>>(pShardKey, eShardKey);
+    }
+}
--- src/dotnet/Db/ModelBuilderExt.cs
+++ src/dotnet/Db/ModelBuilderExt.cs
@@ -3,25 +3,24 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace ActualChat.Db;
 
-public static class ModelBuilderExtensions
+public static class ModelBuilderExt
 {
     public static void UseSnakeCaseNaming(this ModelBuilder modelBuilder)
     {
-        foreach (var entity in modelBuilder.Model.GetEntityTypes())
-        {
-            entity.SetTableName(entity.GetTableName().ToSnakeCase());
+        foreach (var entity in modelBuilder.Model.GetEntityTypes()) {
+            entity.SetTableName(entity.GetTableName()!.ToSnakeCase());
 
             foreach (var property in entity.GetProperties()) {
-                var identifier = StoreObjectIdentifier.Table(entity.GetTableName(), entity.GetSchema());
+                var identifier = StoreObjectIdentifier.Table(entity.GetTableName()!, entity.GetSchema());
                 var columnName = property.GetColumnName(identifier);
-                property.SetColumnName(columnName.ToSnakeCase());
+                property.SetColumnName(columnName!.ToSnakeCase());
             }
 
             foreach (var key in entity.GetKeys())
-                key.SetName(key.GetName().ToSnakeCase());
+                key.SetName(key.GetName()!.ToSnakeCase());
 
             foreach (var key in entity.GetForeignKeys())
-                key.SetConstraintName(key.GetConstraintName().ToSnakeCase());
+                key.SetConstraintName(key.GetConstraintName()!.ToSnakeCase());
 
             foreach (var index in entity.GetIndexes())
                 index.SetDatabaseName(index.GetDatabaseName().ToSnakeCase());
