27548c6af8605140ded13fe34e267a7e48f3732e
Alex Yakunin <alex.yakunin@gmail.com>
2022-05-25 23:06:29 -0700

    fix: a bit better theme handling + some utils

--- src/dotnet/UI.Blazor/Services/ThemeUI.cs
+++ src/dotnet/UI.Blazor/Services/ThemeUI.cs
@@ -1,4 +1,5 @@
 using ActualChat.UI.Blazor.Module;
+using Microsoft.Extensions.Logging.Abstractions;
 using Stl.Locking;
 
 namespace ActualChat.UI.Blazor.Services;
@@ -7,34 +8,44 @@ public enum Theme { Light, Dark }
 
 public class ThemeUI : WorkerBase
 {
-    private readonly IAsyncLock _applyLock;
+    private readonly ILogger _log;
+    private readonly AppBlazorCircuitContext _circuitContext;
     private readonly IJSRuntime _js;
     private Theme _appliedTheme;
 
     public IMutableState<Theme> CurrentTheme { get; }
 
-    public ThemeUI(IJSRuntime js, IStateFactory stateFactory)
+    public ThemeUI(
+        AppBlazorCircuitContext circuitContext,
+        IJSRuntime js,
+        IStateFactory stateFactory,
+        ILogger<ThemeUI>? log = null)
     {
-        _applyLock = new AsyncLock(ReentryMode.UncheckedDeadlock);
+        _log = log ?? NullLogger<ThemeUI>.Instance;
+        _circuitContext = circuitContext;
         _js = js;
         CurrentTheme = stateFactory.NewMutable<Theme>();
         Start();
     }
 
-    protected override Task RunInternal(CancellationToken cancellationToken)
-        => CurrentTheme.When(theme => {
-            _ = ApplyTheme(theme, cancellationToken);
-            return false;
-        }, cancellationToken);
-
-    private async Task ApplyTheme(Theme theme, CancellationToken cancellationToken)
+    protected override async Task RunInternal(CancellationToken cancellationToken)
     {
-        using var _ = await _applyLock.Lock(cancellationToken).ConfigureAwait(false);
-        if (_appliedTheme == theme)
-            return;
-        await _js
-            .InvokeVoidAsync($"{BlazorUICoreModule.ImportName}.setTheme", cancellationToken, theme.ToString())
-            .ConfigureAwait(false);
-        _appliedTheme = theme;
+        await foreach (var cTheme in CurrentTheme.Changes(cancellationToken).ConfigureAwait(false))
+            _ = BackgroundTask.Run(
+                () => ApplyTheme(cTheme.Value, cancellationToken),
+                _log, "Failed to apply new theme",
+                cancellationToken);
     }
+
+    private Task ApplyTheme(Theme theme, CancellationToken cancellationToken)
+        => _circuitContext.Dispatcher.InvokeAsync(async () => {
+            if (_appliedTheme == theme)
+                return;
+            _appliedTheme = theme;
+            await _js.InvokeVoidAsync(
+                $"{BlazorUICoreModule.ImportName}.setTheme",
+                cancellationToken,
+                theme.ToString()
+            ).ConfigureAwait(false);
+        });
 }
