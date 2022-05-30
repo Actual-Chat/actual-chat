fb8c2c50d3ede18b1ef19796ec0778dbe71754f2
Frol <4425748+FrolYo@users.noreply.github.com>
2022-05-31 01:31:10 +0300

    feat: enable cdn/media from config #101

--- src/dotnet/UI.Blazor/Services/ContentUrlMapper.cs
+++ src/dotnet/UI.Blazor/Services/ContentUrlMapper.cs
@@ -1,3 +1,6 @@
+using ActualChat.Module;
+using Microsoft.Extensions.Hosting;
+
 namespace ActualChat.UI.Blazor.Services;
 
 public class ContentUrlMapper
@@ -6,12 +9,22 @@ public class ContentUrlMapper
     private readonly string _contentBaseUri;
     private readonly string _mediaBaseUri;
 
-    public ContentUrlMapper(NavigationManager nav)
+    public ContentUrlMapper(NavigationManager nav, CoreSettings settings, IHostEnvironment env)
     {
         var baseUri = new Uri(nav.BaseUri);
-        _transformUri = StringComparer.OrdinalIgnoreCase.Equals(baseUri.Host, "local.actual.chat");
-        _contentBaseUri = _transformUri ? $"{baseUri.Scheme}://cdn.{baseUri.Host}/" : "";
-        _mediaBaseUri = _transformUri ? $"{baseUri.Scheme}://media.{baseUri.Host}/" : "";
+
+        if (env.IsDevelopment()) {
+            // TODO: refactor when new certificate with subdomain is available
+            _transformUri = StringComparer.OrdinalIgnoreCase.Equals(baseUri.Host, "local.actual.chat");
+            _contentBaseUri = _transformUri ? $"{baseUri.Scheme}://cdn.{baseUri.Host}/" : "";
+            _mediaBaseUri = _transformUri ? $"{baseUri.Scheme}://media.{baseUri.Host}/" : "";
+        }
+        else {
+            _transformUri = baseUri.Host.EndsWith("actual.chat", StringComparison.OrdinalIgnoreCase);
+            // TODO: change to subdomain when new certificate is available
+            _contentBaseUri = settings.UseCdnServer ? $"{baseUri.Scheme}://cdn-{baseUri.Host}/" : "";
+            _mediaBaseUri = settings.UseMediaServer ? $"{baseUri.Scheme}://media-{baseUri.Host}/" : "";
+        }
     }
 
     public string ContentUrl(string contentId)
