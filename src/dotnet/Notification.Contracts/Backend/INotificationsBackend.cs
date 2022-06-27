79550db3afca886066b0c29290716512e737afa5
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-06-27 16:17:57 +0300

    feat: unsubscribe outdated messaging devices #283

--- src/dotnet/Notification.Contracts/Backend/INotificationsBackend.cs
+++ src/dotnet/Notification.Contracts/Backend/INotificationsBackend.cs
@@ -7,15 +7,15 @@ public interface INotificationsBackend
     [ComputeMethod(KeepAliveTime = 10)]
     Task<ImmutableArray<Symbol>> ListSubscriberIds(string chatId, CancellationToken cancellationToken);
 
-     [CommandHandler]
-     Task NotifySubscribers(NotifySubscribersCommand subscribersCommand, CancellationToken cancellationToken);
+    [CommandHandler]
+    Task NotifySubscribers(NotifySubscribersCommand subscribersCommand, CancellationToken cancellationToken);
 
-     [DataContract]
-     public sealed record NotifySubscribersCommand(
-         [property: DataMember] string ChatId,
-         [property: DataMember] long EntryId,
-         [property: DataMember] string AuthorUserId,
-         [property: DataMember] string Title,
-         [property: DataMember] string Content
-         ) : ICommand<Unit>, IBackendCommand;
+    [DataContract]
+    public sealed record NotifySubscribersCommand(
+     [property: DataMember] string ChatId,
+     [property: DataMember] long EntryId,
+     [property: DataMember] string AuthorUserId,
+     [property: DataMember] string Title,
+     [property: DataMember] string Content
+     ) : ICommand<Unit>, IBackendCommand;
 }
