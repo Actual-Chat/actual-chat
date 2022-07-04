8b04b6572b4f1b56bc6c0dc9f2aae569c1b97af4
Frol <4425748+FrolYo@users.noreply.github.com>
2022-07-04 13:56:28 +0300

    chore: cross-platform ./run-prebuilt.cmd

--- run-prebuilt.cmd
+++ run-prebuilt.cmd
@@ -1,9 +1,22 @@
-@echo off
+:<<BATCH
+    @echo off
 
-set ASPNETCORE_ENVIRONMENT=Development
-set ASPNETCORE_URLS=https://localhost:7080;https://localhost:7081
+    set ASPNETCORE_ENVIRONMENT=Development
+    set ASPNETCORE_URLS=https://localhost:7080;https://localhost:7081
 
-start cmd /C timeout 5 ^& start https://localhost:7081/"
-pushd artifacts\bin
-ActualChat.Host.exe
+    start cmd /C timeout 5 ^& start https://localhost:7081/"
+    pushd artifacts\bin
+    ActualChat.Host.exe
+    popd
+    exit /b
+BATCH
+
+#!/bin/sh
+ASPNETCORE_ENVIRONMENT=Development
+ASPNETCORE_URLS=https://localhost:7080;https://localhost:7081
+
+(sleep 5 && open https://localhost:7081 || xdbg-open https://localhost:7081) &
+
+pushd artifacts/bin
+./ActualChat.Host
 popd
