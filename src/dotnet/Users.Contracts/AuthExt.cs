da516539334423eba01e8fcd1f391545df004ed7
Alex Yakunin <alex.yakunin@gmail.com>
2022-07-11 13:44:44 -0700

    chore: update Fusion

--- src/dotnet/Users.Contracts/AuthExt.cs
+++ src/dotnet/Users.Contracts/AuthExt.cs
@@ -2,12 +2,6 @@ namespace ActualChat.Users;
 
 public static class AuthExt
 {
-    public static async ValueTask<User> RequireUser(this IAuth auth, Session session, CancellationToken cancellationToken)
-    {
-        var user = await auth.GetUser(session, cancellationToken).ConfigureAwait(false);
-        return user.Required();
-    }
-
     public static async ValueTask<UserProfile> RequireUserProfile(this IAuth auth, Session session, CancellationToken cancellationToken)
     {
         var userProfiles = auth.GetServices().GetRequiredService<IUserProfiles>();
