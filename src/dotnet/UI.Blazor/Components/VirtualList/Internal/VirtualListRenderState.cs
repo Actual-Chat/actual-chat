5acd31273b4c46a1a82597c394210df904f032e9
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-07-19 20:41:34 +0300

    chore: renames according to style guide

--- src/dotnet/UI.Blazor/Components/VirtualList/Internal/VirtualListRenderState.cs
+++ src/dotnet/UI.Blazor/Components/VirtualList/Internal/VirtualListRenderState.cs
@@ -4,7 +4,6 @@ namespace ActualChat.UI.Blazor.Components.Internal;
 public class VirtualListRenderState
 {
     public long RenderIndex { get; set; }
-//
     public VirtualListDataQuery Query { get; set; }
 
     public double SpacerSize { get; set; }
@@ -12,18 +11,10 @@ public class VirtualListRenderState
     public int StartExpansion { get; set; }
     public int EndExpansion { get; set; }
 
-//     public double? ScrollHeight { get; set; }
-//     public double? ScrollTop { get; set; }
-//     public double? ViewportHeight { get; set; }
     public bool HasVeryFirstItem { get; set; }
     public bool HasVeryLastItem { get; set; }
 
     public string? ScrollToKey { get; set; }
-    // public bool UseSmoothScroll { get; set; }
-//
-//     public Dictionary<string, double> ItemSizes { get; set; } = null!;
-//     public bool HasUnmeasuredItems { get; set; }
-//     public VirtualListStickyEdgeState? StickyEdge { get; set; }
 
-     public Dictionary<string, VirtualListRenderItem> Items { get; set; } = null!;
+    public Dictionary<string, VirtualListRenderItem> Items { get; set; } = null!;
 }
