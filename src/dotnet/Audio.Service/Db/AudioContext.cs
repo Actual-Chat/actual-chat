e4d6fe5454357ae12f14b516de2a75b837e57e48
Alex Yakunin <alex.yakunin@gmail.com>
2022-07-29 03:47:13 -0700

    refactor: remove useless DB types from Audio.Service.Db

--- src/dotnet/Audio.Service/Db/AudioContext.cs
+++ src/dotnet/Audio.Service/Db/AudioContext.cs
@@ -1,4 +1,7 @@
 namespace ActualChat.Audio.Db;
 
 public class AudioContext
-{ }
+{
+    // There is nothing here: DB isn't used by this project,
+    // but AudioContext is via RedisDb<AudioContext>
+}
