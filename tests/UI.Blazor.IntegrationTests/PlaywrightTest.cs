b3ff9f1471138ab9edf98c646a810e088a7f737e
Alex Yakunin <alex.yakunin@gmail.com>
2022-08-07 17:37:06 -0700

    fix: size of chat icon in chat title

--- tests/UI.Blazor.IntegrationTests/PlaywrightTest.cs
+++ tests/UI.Blazor.IntegrationTests/PlaywrightTest.cs
@@ -30,7 +30,7 @@ public class PlaywrightTest : AppHostTestBase
 
         await Task.Delay(2000).ConfigureAwait(false);
 
-        var chatPage = await page.QuerySelectorAsync(".chat-page").ConfigureAwait(false);
+        var chatPage = await page.QuerySelectorAsync(".chat-layout").ConfigureAwait(false);
         chatPage.Should().NotBeNull();
         var input = await page.QuerySelectorAsync("[role='textbox']").ConfigureAwait(false);
         input.Should().NotBeNull();
@@ -64,7 +64,7 @@ public class PlaywrightTest : AppHostTestBase
         }
 
         static async Task<IReadOnlyList<IElementHandle>> GetMessages(IPage page)
-            => await page.QuerySelectorAllAsync(".chat-page .content");
+            => await page.QuerySelectorAllAsync(".chat-layout .content");
 
         static async Task<string?> GetLastMessage(IEnumerable<IElementHandle> messages)
             => await messages.Last().TextContentAsync().ConfigureAwait(false);
@@ -80,7 +80,7 @@ public class PlaywrightTest : AppHostTestBase
         await Task.Delay(1000);
         user.Id.Value.Should().NotBeNullOrEmpty();
         user.Name.Should().Be("ChatPageTester");
-        var messages = await page.QuerySelectorAllAsync(".chat-page .content");
+        var messages = await page.QuerySelectorAllAsync(".chat-layout .content");
         messages.Count.Should().BeGreaterThan(0);
         await Task.Delay(200);
     }
