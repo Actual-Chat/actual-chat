2c48d9fd8c8e50b0d9a9014f2f9a8eabbd9b76ac
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-08-11 14:54:42 +0300

    fix: opening non-existing chat in WASM produces endless requests loop #357

--- src/dotnet/UI.Blazor/Components/Requirements/RequireChat.cs
+++ src/dotnet/UI.Blazor/Components/Requirements/RequireChat.cs
@@ -8,11 +8,16 @@ public class RequireChat : RequirementComponent
     [Inject] protected Session Session { get; init; } = null!;
     [Inject] protected IAccounts Accounts { get; init; } = null!;
     [Inject] protected IChats Chats { get; init; } = null!;
+    [Inject] private ILogger<RequireChat> Log { get; init; } = null!;
 
-    [Parameter] public string ChatId { get; set; } = "";
+    [Parameter]
+    [EditorRequired]
+    public string ChatId { get; set; } = "";
 
     public override async Task<Unit> Require(CancellationToken cancellationToken)
     {
+        if (ChatId.IsNullOrEmpty())
+            Log.LogWarning("ChatId is null");
         var chat = await Chats.Get(Session, ChatId, cancellationToken).ConfigureAwait(false);
         chat.Require();
         return default;
