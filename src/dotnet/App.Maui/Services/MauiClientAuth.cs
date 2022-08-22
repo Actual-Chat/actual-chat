df3a71700a1be92b696cd42b468dd82bf0b9f2d2
Alex Yakunin <alex.yakunin@gmail.com>
2022-08-21 21:40:59 -0700

    chore: misc. UI fixes

--- src/dotnet/App.Maui/Services/MauiClientAuth.cs
+++ src/dotnet/App.Maui/Services/MauiClientAuth.cs
@@ -4,51 +4,48 @@ namespace ActualChat.App.Maui.Services;
 
 internal sealed class MauiClientAuth : IClientAuth
 {
-    private readonly ClientAppSettings _clientAppSettings;
-    private readonly ILogger<MauiClientAuth> _log;
-
-    private const string GoogleSchemeName = "Google";
-    private const string FacebookSchemeName = "Facebook";
+    private ClientAppSettings ClientAppSettings { get; }
+    private ILogger<MauiClientAuth> Log { get; }
 
     public MauiClientAuth(ClientAppSettings clientAppSettings, ILogger<MauiClientAuth> log)
     {
-        _clientAppSettings = clientAppSettings;
-        _log = log;
+        ClientAppSettings = clientAppSettings;
+        Log = log;
     }
 
     public async ValueTask SignIn(string scheme)
     {
-        if (string.Equals(GoogleSchemeName, scheme, StringComparison.Ordinal)) {
-            #if ANDROID
+        if (string.Equals(IClientAuth.GoogleSchemeName, scheme, StringComparison.Ordinal)) {
+#if ANDROID
             var activity = (MainActivity)Platform.CurrentActivity!;
             await activity.SignInWithGoogle().ConfigureAwait(false);
             return;
-            #endif
+#endif
         }
 
-        var uri = $"{_clientAppSettings.BaseUri.EnsureSuffix("/")}mobileauth/signin/{_clientAppSettings.SessionId}/{scheme}";
+        var uri = $"{ClientAppSettings.BaseUri.EnsureSuffix("/")}mobileauth/signin/{ClientAppSettings.SessionId}/{scheme}";
         await OpenSystemBrowserForSignIn(uri).ConfigureAwait(true);
     }
 
     public async ValueTask SignOut()
     {
-        #if ANDROID
+#if ANDROID
         var activity = (MainActivity)Platform.CurrentActivity!;
         if (activity.IsSignedInWithGoogle()) {
             await activity.SignOutWithGoogle().ConfigureAwait(true);
             return;
         }
-        #endif
+#endif
 
-        var uri = $"{_clientAppSettings.BaseUri.EnsureSuffix("/")}mobileauth/signout/{_clientAppSettings.SessionId}";
+        var uri = $"{ClientAppSettings.BaseUri.EnsureSuffix("/")}mobileauth/signout/{ClientAppSettings.SessionId}";
         await OpenSystemBrowserForSignIn(uri).ConfigureAwait(true);
     }
 
     public ValueTask<(string Name, string DisplayName)[]> GetSchemas()
-        => ValueTask.FromResult(new[]{
-                (GoogleSchemeName, "Google"),
-                (FacebookSchemeName, "Facebook")
-            });
+        => ValueTask.FromResult(new[] {
+            (IClientAuth.GoogleSchemeName, "Google"),
+            (IClientAuth.FacebookSchemeName, "Facebook")
+        });
 
     private async Task OpenSystemBrowserForSignIn(string url)
     {
@@ -57,7 +54,7 @@ internal sealed class MauiClientAuth : IClientAuth
             await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred).ConfigureAwait(true);
         }
         catch (Exception ex) {
-            _log.LogError(ex, "Failed to authenticate");
+            Log.LogError(ex, "Failed to authenticate");
             throw;
         }
     }
