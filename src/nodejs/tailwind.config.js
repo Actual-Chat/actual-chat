fecc4b7215abf9398a762964b608724921c80b6e
iqmulator <iqmulator@gmail.com>
2022-08-26 15:31:55 +0500

    base font size is 1rem

--- src/nodejs/tailwind.config.js
+++ src/nodejs/tailwind.config.js
@@ -591,9 +591,8 @@ module.exports = {
     fontSize: {
       xxs: ['0.6rem', { lineHeight: '0.75rem' }],
       xs: ['0.75rem', { lineHeight: '1rem' }],
-      sm: ['0.8rem', { lineHeight: '1.1rem' }],
-      base: ['0.875rem', { lineHeight: '1.25rem' }],
-      md: ['1rem', { lineHeight: '1.5rem' }],
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],
+      base: ['1rem', { lineHeight: '1.5rem' }],
       lg: ['1.125rem', { lineHeight: '1.75rem' }],
       xl: ['1.25rem', { lineHeight: '1.75rem' }],
       '2xl': ['1.5rem', { lineHeight: '2rem' }],
