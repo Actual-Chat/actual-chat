ce27fb91a384672b0b302198ab12b45807855f9e
Frol <4425748+FrolYo@users.noreply.github.com>
2022-08-26 17:59:00 +0300

    feat: wait for server start before event processing

--- src/dotnet/Core/Events/EventListener.cs
+++ src/dotnet/Core/Events/EventListener.cs
@@ -1,26 +1,33 @@
+using Microsoft.Extensions.Hosting;
+
 namespace ActualChat.Events;
 
 public class EventListener<T>: WorkerBase where T: IEvent
 {
     private readonly IEventReader<T> _eventReader;
-    private readonly IEventHandler<T> _eventHandler;
+    private readonly Lazy<IEventHandler<T>> _eventHandler;
     private readonly ICommander _commander;
+    private readonly IHostLifetime _hostLifetime;
     private readonly ILogger<EventListener<T>> _log;
 
     public EventListener(
         IEventReader<T> eventReader,
-        IEventHandler<T> eventHandler,
+        Lazy<IEventHandler<T>> eventHandler,
         ICommander commander,
+        IHostLifetime hostLifetime,
         ILogger<EventListener<T>> log)
     {
         _eventReader = eventReader;
         _eventHandler = eventHandler;
         _commander = commander;
+        _hostLifetime = hostLifetime;
         _log = log;
     }
 
     protected override async Task RunInternal(CancellationToken cancellationToken)
     {
+        await _hostLifetime.WaitForStartAsync(cancellationToken).ConfigureAwait(false);
+
         const int batchSize = 10;
         var ackTasks = new List<Task>(batchSize);
         while (!cancellationToken.IsCancellationRequested)
@@ -31,11 +38,13 @@ public class EventListener<T>: WorkerBase where T: IEvent
                     continue;
 
                 foreach (var (@event, id) in batch) {
-                    var task = _eventHandler
+                    var task = _eventHandler.Value
                         .Handle(@event, _commander, cancellationToken)
                         .ContinueWith(
                             _ => _eventReader.Ack(id, cancellationToken),
-                            CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+                            CancellationToken.None,
+                            TaskContinuationOptions.ExecuteSynchronously,
+                            TaskScheduler.Default);
                     ackTasks.Add(task);
                 }
                 await Task.WhenAll(ackTasks).ConfigureAwait(false);
