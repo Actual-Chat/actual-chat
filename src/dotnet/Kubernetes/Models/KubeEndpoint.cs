0094b66926ea7072dbf11e84b9ef5995f0c3c346
Alex Yakunin <alex.yakunin@gmail.com>
2022-08-29 16:31:19 -0700

    chore: fix warnings

--- src/dotnet/Kubernetes/Models/KubeEndpoint.cs
+++ src/dotnet/Kubernetes/Models/KubeEndpoint.cs
@@ -6,6 +6,7 @@ public record KubeEndpoint(ImmutableArray<string> Addresses, bool IsReady)
 {
     protected virtual bool PrintMembers(StringBuilder builder)
     {
+ #pragma warning disable MA0011
         builder.Append("Addresses = [");
         foreach (var address in Addresses.Take(1))
             builder.Append(address);
@@ -13,6 +14,7 @@ public record KubeEndpoint(ImmutableArray<string> Addresses, bool IsReady)
             builder.Append($", {address}");
         builder.Append("], ");
         builder.Append($"IsReady = {IsReady}");
+ #pragma warning restore MA0011
         return true;
     }
 }
