d8aba93c7c2087a4a96a78f5488b78c404306733
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-08-29 11:01:41 +0300

    chore: hide analyzer error that prohibits custom cert validation

--- src/dotnet/Kubernetes/Module/KubernetesModule.cs
+++ src/dotnet/Kubernetes/Module/KubernetesModule.cs
@@ -28,6 +28,7 @@ public class KubernetesModule : HostModule<KubernetesSettings>
                 var kubeInfo = sp.GetRequiredService<KubeInfo>();
                 var log = sp.GetRequiredService<ILogger<KubeServices>>();
                 var caCert = X509Certificate2.CreateFromPemFile(kubeInfo.CACertPath);
+                #pragma error disable MA0039
                 handler.ServerCertificateCustomValidationCallback =
                     handler.ServerCertificateCustomValidationCallback =
                         (_, cert, _, policyErrors) =>
@@ -51,6 +52,7 @@ public class KubernetesModule : HostModule<KubernetesSettings>
                             }
                         };
                 return handler;
+                #pragma error restore MA0039
             })
             .SetHandlerLifetime(TimeSpan.FromMinutes(5))
             .AddPolicyHandler(GetRetryPolicy());
