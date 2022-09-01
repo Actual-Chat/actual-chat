fccfcab86cba4e20bcc4ab4a854d99989ecfdddd
Frol <4425748+FrolYo@users.noreply.github.com>
2022-09-01 20:35:38 +0300

    fix: anonymous chatting errors #457

--- src/dotnet/Users.Service/RecentEntries.cs
+++ src/dotnet/Users.Service/RecentEntries.cs
@@ -2,14 +2,12 @@ namespace ActualChat.Users;
 
 public class RecentEntries : IRecentEntries
 {
-    private IAuth Auth { get; }
     private IAccounts Accounts { get; }
     private IRecentEntriesBackend Backend { get; }
     private ICommander Commander { get; }
 
     public RecentEntries(IAuth auth, IAccounts accounts, IRecentEntriesBackend backend, ICommander commander)
     {
-        Auth = auth;
         Accounts = accounts;
         Backend = backend;
         Commander = commander;
@@ -33,11 +31,14 @@ public class RecentEntries : IRecentEntries
     public virtual async Task<RecentEntry?> Update(IRecentEntries.UpdateCommand command, CancellationToken cancellationToken)
     {
         if (Computed.IsInvalidating())
-            return default; // It just spawns other commands, so nothing to do here
+            return null; // It just spawns other commands, so nothing to do here
 
         var (session, scope, key, moment) = command;
-        var user = await Auth.GetUser(session, cancellationToken).Require().ConfigureAwait(false);
-        var cmd = new IRecentEntriesBackend.UpdateCommand(scope, user.Id, key, moment);
+        var account = await Accounts.Get(session, cancellationToken).ConfigureAwait(false);
+        if (account.IsGuest())
+            return null;
+
+        var cmd = new IRecentEntriesBackend.UpdateCommand(scope, account.Id, key, moment);
         return await Commander.Call(cmd, true, cancellationToken).ConfigureAwait(false);
     }
 }
