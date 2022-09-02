ba245d534a962006a0ffc12ac9897525bf11f147
Alex Yakunin <alex.yakunin@gmail.com>
2022-09-02 02:05:53 -0700

    chore: make SearchResult more convenient to use

--- src/dotnet/Core/Search/SearchResult.cs
+++ src/dotnet/Core/Search/SearchResult.cs
@@ -5,6 +5,8 @@ public abstract class SearchResult
 {
     [DataMember] public string Id { get; }
     [DataMember] public SearchMatch SearchMatch { get; }
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public string Text => SearchMatch.Text;
 
     protected SearchResult(string id, SearchMatch searchMatch)
     {
