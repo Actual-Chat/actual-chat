f062c2be75f7b1f5698538d6e2cc8b491e189689
Alex Yakunin <alex.yakunin@gmail.com>
2022-09-05 03:39:04 -0700

    refactor: ChatDbInitializer.cs

---
+++ src/dotnet/Chat.Service/Module/ChatDbInitializer.Verify.cs
@@ -0,0 +1,38 @@
+using ActualChat.Chat.Db;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActualChat.Chat.Module;
+
+public partial class ChatDbInitializer
+{
+    private async Task Verify(ChatDbContext dbContext, CancellationToken cancellationToken)
+    {
+        Log.LogInformation("Verifying DB...");
+
+        var chatIds = await dbContext.Chats
+            .Select(c => c.Id)
+            .ToListAsync(cancellationToken)
+            .ConfigureAwait(false);
+        foreach (var chatId in chatIds) {
+            var thisChatEntries = dbContext.ChatEntries.Where(e => e.ChatId == chatId);
+            var duplicateEntries = await (
+                from e in thisChatEntries
+                let count = thisChatEntries.Count(e1 => e1.Id == e.Id && e1.Type == e.Type)
+                where count > 1
+                select e
+                ).ToListAsync(cancellationToken)
+                .ConfigureAwait(false);
+            if (duplicateEntries.Count <= 0)
+                continue;
+
+            Log.LogCritical("Duplicate entries in Chat #{ChatId}:", chatId);
+            foreach (var e in duplicateEntries)
+                Log.LogCritical(
+                    "- Entry w/ CompositeId = {CompositeId}, Id = {Id}, Type = {Type}, '{Content}'",
+                    e.CompositeId,
+                    e.Id,
+                    e.Type,
+                    e.Content);
+        }
+    }
+}
