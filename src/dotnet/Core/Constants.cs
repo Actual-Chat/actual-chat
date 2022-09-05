f044fe072ccdfd2e669030ff92750d4dae6f5d35
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2022-09-05 11:01:21 +0200

    chore: increase picture file size to 10MB

--- src/dotnet/Core/Constants.cs
+++ src/dotnet/Core/Constants.cs
@@ -9,7 +9,7 @@ public static partial class Constants
         public static TileStack<long> IdTileStack { get; } = TileStacks.Long16To1K;
         public static TileStack<Moment> TimeTileStack { get; } = TileStacks.Moment3MTo6Y;
         public static TimeSpan MaxEntryDuration { get; } = TimeTileStack.MinTileSize.EpochOffset; // 3 minutes, though it can be any
-        public const int PictureFileSizeLimit = 1 * 1024 * 1024;
+        public const int PictureFileSizeLimit = 10 * 1024 * 1024; // 10MB
         public const int MaxRecentPeerChats = 5;
     }
 
