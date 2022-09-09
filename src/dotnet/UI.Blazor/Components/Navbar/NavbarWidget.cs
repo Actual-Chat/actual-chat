3ac173d420ad1b26e2178334f5368e83aedc7a80
Frol <4425748+FrolYo@users.noreply.github.com>
2022-09-09 16:39:29 +0300

    feat: skip render of inactive navbar widget #488

--- src/dotnet/UI.Blazor/Components/Navbar/NavbarWidget.cs
+++ src/dotnet/UI.Blazor/Components/Navbar/NavbarWidget.cs
@@ -3,6 +3,7 @@ namespace ActualChat.UI.Blazor.Components;
 public record NavbarWidget(Type ComponentType)
 {
     public double Order { get; init; }
+    public string? NavbarGroupId { get; init; }
 
     public RenderFragment Content => builder => {
         builder.OpenComponent(0, ComponentType);
