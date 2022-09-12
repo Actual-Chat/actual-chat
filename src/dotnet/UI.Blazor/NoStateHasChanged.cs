6bfad03f3bc89e728566141f36ec41816336fd3d
Alex Yakunin <alex.yakunin@gmail.com>
2022-09-12 14:01:13 -0700

    misc: add NoStateHasChanged.cs

---
+++ src/dotnet/UI.Blazor/NoStateHasChanged.cs
@@ -0,0 +1,22 @@
+namespace ActualChat.UI.Blazor;
+
+public sealed class NoStateHasChanged : IHandleEvent
+{
+    public static NoStateHasChanged Instance { get; } = new ();
+
+    private NoStateHasChanged() { }
+
+    public static EventCallback EventCallback(Action eventHandler)
+        => new(Instance, eventHandler);
+    public static EventCallback<T> EventCallback<T>(Action<T> eventHandler)
+        => new(Instance, eventHandler);
+    public static EventCallback EventCallback(Func<Task> eventHandler)
+        => new(Instance, eventHandler);
+    public static EventCallback<T> EventCallback<T>(Func<T, Task> eventHandler)
+        => new(Instance, eventHandler);
+
+    // Private methods
+
+    Task IHandleEvent.HandleEventAsync(EventCallbackWorkItem item, object? arg)
+        => item.InvokeAsync(arg);
+}
