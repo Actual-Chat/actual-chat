4cd41fadeef700329963991113e699e9f3546de6
Frol <4425748+FrolYo@users.noreply.github.com>
2022-09-22 11:09:44 +0300

    chore: optimizing recent chat list #536

--- src/dotnet/Chat.UI.Blazor/Services/RecentChats.cs
+++ src/dotnet/Chat.UI.Blazor/Services/RecentChats.cs
@@ -3,8 +3,9 @@ using ActualChat.Users.UI.Blazor.Services;
 
 namespace ActualChat.Chat.UI.Blazor.Services;
 
-public class RecentChats
+public class RecentChats : WorkerBase
 {
+    private ImmutableArray<Chat> _chatsIncludingActive = ImmutableArray<Chat>.Empty;
     private IChats Chats { get; }
     private UnreadMessagesFactory UnreadMessagesFactory { get; }
     private IRecentEntries RecentEntries { get; }
@@ -23,44 +24,13 @@ public class RecentChats
         ChatUI = chatUI;
         Session = session;
         UnreadMessagesFactory = unreadMessagesFactory;
+        Start();
     }
 
     [ComputeMethod]
-    public virtual async Task<ImmutableArray<Chat>> List(CancellationToken cancellationToken = default)
-    {
-        var chats = await Chats.List(Session, cancellationToken).ConfigureAwait(false);
-        var result = new List<Chat>(chats.Length + 1);
-        var activeChat = await GetActiveChat(cancellationToken);
-        if (activeChat != null && !chats.Contains(activeChat))
-            result.Insert(0, activeChat);
-
-        var chatIdsWithMentions = chats.Zip(
-                await chats.Select(HasMentions)
-                    .Collect()
-                    .ConfigureAwait(false))
-            .Where(x => x.Second)
-            .Select(x => x.First.Id)
-            .ToHashSet();
-        var chatsWithMentions = chats.Where(x => chatIdsWithMentions.Contains(x.Id)).ToList();
-        var chatsWithoutMentions = chats.Where(x => !chatIdsWithMentions.Contains(x.Id)).ToList();
-        result.AddRange(await OrderByRecency(chatsWithMentions).ConfigureAwait(false));
-        result.AddRange(await OrderByRecency(chatsWithoutMentions).ConfigureAwait(false));
-
-        return result.ToImmutableArray();
-
-        Task<List<Chat>> OrderByRecency(List<Chat> items)
-            => RecentEntries.OrderByRecency(Session,
-                items,
-                RecencyScope.ChatContact,
-                chats.Length,
-                cancellationToken);
-
-        async Task<bool> HasMentions(Chat chat)
-        {
-            using var unreadMessages = UnreadMessagesFactory.Get(chat.Id);
-            return await unreadMessages.HasMentions(cancellationToken).ConfigureAwait(false);
-        }
-    }
+    // optimized
+    public virtual Task<ImmutableArray<Chat>> ListIncludingActive()
+        => Task.FromResult(_chatsIncludingActive);
 
     [ComputeMethod]
     public virtual async Task<Chat?> GetActiveChat(CancellationToken cancellationToken)
@@ -80,7 +50,73 @@ public class RecentChats
         if (activeChat != null)
             return activeChat;
 
-        var chats = await List(cancellationToken).ConfigureAwait(false);
+        var chats = await ListIncludingActive().ConfigureAwait(false);
         return chats.FirstOrDefault(x => x.Id == Constants.Chat.DefaultChatId) ?? chats.FirstOrDefault();
+    }
+
+    [ComputeMethod]
+    protected virtual async Task<ImmutableArray<Chat>> List(CancellationToken cancellationToken)
+    {
+        var chats = await Chats.List(Session, cancellationToken).ConfigureAwait(false);
+        var chatIdsWithMentions = chats.Zip(
+                await chats.Select(HasMentions)
+                    .Collect()
+                    .ConfigureAwait(false))
+            .Where(x => x.Second)
+            .Select(x => x.First.Id)
+            .ToHashSet();
+        var chatsWithMentions = chats.Where(x => chatIdsWithMentions.Contains(x.Id)).ToList();
+        var chatsWithoutMentions = chats.Where(x => !chatIdsWithMentions.Contains(x.Id)).ToList();
+        chatsWithMentions = await OrderByRecency(chatsWithMentions).ConfigureAwait(false);
+        chatsWithoutMentions = await OrderByRecency(chatsWithoutMentions).ConfigureAwait(false);
+
+        return chatsWithMentions.Concat(chatsWithoutMentions).ToImmutableArray();
+
+        Task<List<Chat>> OrderByRecency(List<Chat> items)
+            => RecentEntries.OrderByRecency(Session,
+                items,
+                RecencyScope.ChatContact,
+                chats.Length,
+                cancellationToken);
+
+        async Task<bool> HasMentions(Chat chat)
+        {
+            using var unreadMessages = UnreadMessagesFactory.Get(chat.Id);
+            return await unreadMessages.HasMentions(cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    protected override Task RunInternal(CancellationToken cancellationToken)
+        => SyncListWithActiveChat(cancellationToken);
+
+    [ComputeMethod]
+    protected virtual async Task<ImmutableArray<Chat>> ListChatsIncludingActiveInternal(CancellationToken cancellationToken)
+    {
+        var activeChat = await GetActiveChat(cancellationToken).ConfigureAwait(false);
+        var chats = await List(cancellationToken).ConfigureAwait(false);
+        if (activeChat != null && !chats.Contains(activeChat))
+            chats = chats.Insert(0, activeChat);
+
+        return chats;
+    }
+
+    private async Task SyncListWithActiveChat(CancellationToken cancellationToken)
+    {
+        SetChats(await ListChatsIncludingActiveInternal(cancellationToken).ConfigureAwait(false));
+
+        var changes = (await Computed.Capture(() => ListChatsIncludingActiveInternal(cancellationToken)).ConfigureAwait(false)).Changes(cancellationToken);
+        await foreach (var cChats in changes.ConfigureAwait(false)) {
+            var newChats = cChats.Value;
+
+            if (!newChats.SequenceEqual(_chatsIncludingActive))
+                SetChats(newChats);
+        }
+
+        void SetChats(ImmutableArray<Chat> newChats)
+        {
+            _chatsIncludingActive = newChats;
+            using (Computed.Invalidate())
+                _ = ListIncludingActive();
+        }
     }
 }
