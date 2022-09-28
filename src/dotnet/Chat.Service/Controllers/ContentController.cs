fb09d1d5240ccbd09c6eeccf2dea29a22254dd51
Alex Yakunin <alex.yakunin@gmail.com>
2022-09-27 18:16:56 -0700

    fix: ContentController.cs bug

--- src/dotnet/Chat.Service/Controllers/ContentController.cs
+++ src/dotnet/Chat.Service/Controllers/ContentController.cs
@@ -14,10 +14,14 @@ public class ContentController : ControllerBase
     [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Client, VaryByQueryKeys = new[] { "blobId" })]
     public async Task<ActionResult> Download(string blobId, CancellationToken cancellationToken)
     {
+        if (blobId.IsNullOrEmpty())
+            return NotFound();
+
         var blobStorage = _blobs.GetBlobStorage(BlobScope.ContentRecord);
         var byteStream = await blobStorage.OpenReadAsync(blobId, cancellationToken).ConfigureAwait(false);
         if (byteStream == null)
             return NotFound();
+
         var blob = (await blobStorage.GetBlobsAsync(new[] {blobId}, cancellationToken).ConfigureAwait(false)).Single();
         string contentType = MediaTypeNames.Application.Octet;
         if (blob != null && blob.IsFile && blob.Metadata.TryGetValue(Constants.Headers.ContentType, out var metadataContentType))
