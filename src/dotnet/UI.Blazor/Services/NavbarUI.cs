85ba71074c72cd053bde64231d40c14a84201a13
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2022-10-01 16:39:45 +0200

    feat: toggle left panel

--- src/dotnet/UI.Blazor/Services/NavbarUI.cs
+++ src/dotnet/UI.Blazor/Services/NavbarUI.cs
@@ -2,11 +2,12 @@ namespace ActualChat.UI.Blazor.Services;
 
 public class NavbarUI
 {
-    public bool IsVisible { get; set; }
+    public bool IsVisible { get; private set; }
     public string ActiveGroupId { get; private set; } = "chats";
     public string ActiveGroupTitle { get; private set; } = "Chats";
     public Dictionary<string, Action> AddButtonAction { get; } = new (StringComparer.Ordinal);
     public event EventHandler? ActiveGroupChanged;
+    public event EventHandler? VisibilityChanged;
 
     public void ActivateGroup(string id, string title)
     {
@@ -16,5 +17,14 @@ public class NavbarUI
         ActiveGroupId = id;
         ActiveGroupTitle = title;
         ActiveGroupChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void ChangeVisibility(bool visible)
+    {
+        if (IsVisible == visible)
+            return;
+
+        IsVisible = visible;
+        VisibilityChanged?.Invoke(this, EventArgs.Empty);
     }
 }
