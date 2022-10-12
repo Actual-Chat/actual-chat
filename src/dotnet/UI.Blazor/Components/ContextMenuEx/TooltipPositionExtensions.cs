2ebf0b5f77db8d524c94acc6f2b9d921a3243f9a
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2022-10-12 13:38:50 +0200

    feat: add menu triggers

---
+++ src/dotnet/UI.Blazor/Components/ContextMenuEx/TooltipPositionExtensions.cs
@@ -0,0 +1,7 @@
+namespace ActualChat.UI.Blazor.Components;
+
+public static class MenuTriggersExtensions
+{
+    public static int ToTriggersString(this MenuTriggers triggers)
+        => (int)triggers;
+}
