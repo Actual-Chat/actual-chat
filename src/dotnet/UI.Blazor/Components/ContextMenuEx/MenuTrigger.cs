09b2430208354fe67670ae96515ff8025940e4ce
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2022-10-18 10:57:34 +0200

    feat: migrate chat message menu

--- src/dotnet/UI.Blazor/Components/ContextMenuEx/MenuTrigger.cs
+++ src/dotnet/UI.Blazor/Components/ContextMenuEx/MenuTrigger.cs
@@ -2,12 +2,12 @@ namespace ActualChat.UI.Blazor.Components;
 
 public static class MenuTrigger
 {
-    public static string Format<TMenu>(string? argument = null)
-        => $"{typeof(TMenu).Name}:{argument}";
+    public static string Format<TMenu>(params string[] arguments)
+        => string.Join(":", new[] { typeof(TMenu).Name }.Concat(arguments));
 
-    public static (string menu, string? argument) Parse(string trigger)
+    public static (string, ImmutableArray<string>) Parse(string trigger)
     {
         var parts = trigger.Split(":");
-        return (parts[0], parts.ElementAtOrDefault(1));
+        return (parts[0], parts.Skip(1).ToImmutableArray());
     }
 }
