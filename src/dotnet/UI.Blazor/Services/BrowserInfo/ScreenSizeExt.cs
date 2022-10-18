edf961e80a0c2efd9163da9715b571c4748efca5
Alex Yakunin <alex.yakunin@gmail.com>
2022-10-17 19:08:08 -0700

    refactor: rename ScreenSize.IsMobile() to IsNarrow, and IsDesktop to IsWide.

--- src/dotnet/UI.Blazor/Services/BrowserInfo/ScreenSizeExt.cs
+++ src/dotnet/UI.Blazor/Services/BrowserInfo/ScreenSizeExt.cs
@@ -5,9 +5,9 @@ public static class ScreenSizeExt
     public static bool IsUnknown(this ScreenSize screenSize)
         => screenSize is ScreenSize.Unknown;
 
-    public static bool IsMobile(this ScreenSize screenSize)
+    public static bool IsNarrow(this ScreenSize screenSize)
         => screenSize is ScreenSize.Small;
 
-    public static bool IsDesktop(this ScreenSize screenSize)
-        => !screenSize.IsMobile();
+    public static bool IsWide(this ScreenSize screenSize)
+        => !screenSize.IsNarrow();
 }
