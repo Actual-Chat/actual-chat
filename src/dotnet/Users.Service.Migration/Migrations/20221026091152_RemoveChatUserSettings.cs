13f6711d082a134d30132faa4c3353e0454349db
Frol <4425748+FrolYo@users.noreply.github.com>
2022-10-26 14:04:56 +0300

    fix: contatcs and kvas migrations

---
+++ src/dotnet/Users.Service.Migration/Migrations/20221026091152_RemoveChatUserSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Users.Migrations
+{
+    public partial class RemoveChatUserSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "chat_user_settings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chat_user_settings",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    avatar_id = table.Column<string>(type: "text", nullable: false),
+                    chat_id = table.Column<string>(type: "text", nullable: false),
+                    language = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<string>(type: "text", nullable: false),
+                    version = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_chat_user_settings", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chat_user_settings_chat_id_user_id",
+                table: "chat_user_settings",
+                columns: new[] { "chat_id", "user_id" });
+        }
+    }
+}
