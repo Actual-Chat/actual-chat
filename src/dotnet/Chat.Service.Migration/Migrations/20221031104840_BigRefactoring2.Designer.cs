d17774fc6ae66c5e95e1dffa5222846a78314f47
Alex Yakunin <alex.yakunin@gmail.com>
2022-10-31 03:50:31 -0700

    wip: add BigRefactoring2 migration

--- src/dotnet/Chat.Service.Migration/Migrations/20221031104840_BigRefactoring2.Designer.cs
+++ src/dotnet/Chat.Service.Migration/Migrations/20221031104840_BigRefactoring2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ActualChat.Chat.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ActualChat.Chat.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221031104840_BigRefactoring2")]
+    partial class BigRefactoring2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
