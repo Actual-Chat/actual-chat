e559d8b729ff8bddccf1674e0c4fb635fc0de571
Alex Yakunin <alex.yakunin@gmail.com>
2022-10-31 01:36:51 -0700

    wip: more changes

--- src/dotnet/Chat.Service/Db/DbTextEntryAttachment.cs
+++ src/dotnet/Chat.Service/Db/DbTextEntryAttachment.cs
@@ -5,12 +5,13 @@ using Stl.Versioning;
 namespace ActualChat.Chat.Db;
 
 [Table("TextEntryAttachments")]
-public class DbTextEntryAttachment : IHasVersion<long>, IRequirementTarget
+public class DbTextEntryAttachment : IHasId<string>, IHasVersion<long>, IRequirementTarget
 {
     public DbTextEntryAttachment() { }
     public DbTextEntryAttachment(TextEntryAttachment model) => UpdateFrom(model);
 
     // (ChatId, EntryId, Index)
+    string IHasId<string>.Id => CompositeId;
     [Key] public string CompositeId { get; set; } = "";
     public string ChatEntryId { get; set; } = "";
     public int Index { get; set; }
