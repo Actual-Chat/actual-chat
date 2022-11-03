1f708f8ba9469949016fd482cbbe56b1db36b51c
Frol <4425748+FrolYo@users.noreply.github.com>
2022-11-03 16:40:19 +0300

    chore: cleanup - MarkupHub #651

--- src/dotnet/Chat.UI.Blazor/Services/MarkupHub.cs
+++ src/dotnet/Chat.UI.Blazor/Services/MarkupHub.cs
@@ -10,7 +10,6 @@ public class MarkupHub : IHasServices
     private MarkupEditorHtmlConverter? _markupEditorHtmlConverter;
 
     public IServiceProvider Services { get; }
-    public Session Session { get; }
     public Symbol ChatId { get; set; } = Symbol.Empty;
 
     public MarkupEditorHtmlConverter MarkupEditorHtmlConverter
@@ -51,26 +50,13 @@ public class MarkupHub : IHasServices
         }
     }
 
-    public MarkupHub(IServiceProvider services, Session session)
-    {
-        Services = services;
-        Session = session;
-    }
-
-    public Markup Parse(string markupText)
-        => MarkupParser.Parse(markupText);
+    public MarkupHub(IServiceProvider services)
+        => Services = services;
 
     public async ValueTask<Markup> ParseAndNameMentions(string markupText, CancellationToken cancellationToken)
     {
         var markup = MarkupParser.Parse(markupText);
         markup = await MentionNamer.Rewrite(markup, cancellationToken).ConfigureAwait(false);
-        return markup;
-    }
-
-    public Markup Trim(Markup markup, int maxLength)
-    {
-        var markupTrimmer = new MarkupTrimmer(maxLength);
-        markup = markupTrimmer.Rewrite(markup);
         return markup;
     }
 }
