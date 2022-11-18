eecee0ef044f2d130e5e03391738878a01777dbe
iqmulator <iqmulator@gmail.com>
2022-11-18 19:22:04 +0600

    CreateModal refactoring, add selected contacts, rewrite computed model

--- src/dotnet/Users/UserConstants.cs
+++ src/dotnet/Users/UserConstants.cs
@@ -4,8 +4,8 @@ public static class UserConstants
 {
     public static class Admin
     {
-        public static string UserId { get; } = "admin";
-        public static string Name { get; } =  "Admin";
+        public static string UserId { get; } = "actualadmin";
+        public static string Name { get; } =  "Actual Admin";
         public static string Picture { get; } = "https://avatars.dicebear.com/api/avataaars/12333323132.svg";
         public static Session Session { get; set; } = Session.Null;
     }
