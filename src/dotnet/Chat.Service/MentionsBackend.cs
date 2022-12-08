d3de09fe3cb878785adac0576c7c57fd79346f86
Alex Yakunin <alex.yakunin@gmail.com>
2022-12-08 11:49:41 -0800

    fix: more fixes

--- src/dotnet/Chat.Service/MentionsBackend.cs
+++ src/dotnet/Chat.Service/MentionsBackend.cs
@@ -23,7 +23,7 @@ internal class MentionsBackend : DbServiceBase<ChatDbContext>, IMentionsBackend
         await using var __ = dbContext.ConfigureAwait(false);
 
         var dbMention = await dbContext.Mentions
-            .Where(x => x.ChatId == chatId.Value && x.MentionId == mentionId)
+            .Where(x => x.ChatId == chatId.Value && x.MentionId == mentionId.Value)
             .OrderByDescending(x => x.EntryId)
             .FirstOrDefaultAsync(cancellationToken)
             .ConfigureAwait(false);
