0818a6dba941588597ee23f57dfc51b2cc34e543
Alex Yakunin <alex.yakunin@gmail.com>
2022-12-13 14:51:59 -0800

    migration: rename ServiceEntry -> SystemEntry

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20221213225142_RenameServiceEntry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameServiceEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_service_entry",
+                table: "chat_entries",
+                newName: "is_system_entry");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_system_entry",
+                table: "chat_entries",
+                newName: "is_service_entry");
+        }
+    }
+}
