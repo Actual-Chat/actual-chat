59ecea2fc96167c60524f1bb8cf2e68a04643b81
Alex Yakunin <alex.yakunin@gmail.com>
2022-12-13 14:50:48 -0800

    migration: add DbActivationKey

--- src/dotnet/Invite.Service.Migration/Migrations/20221213225014_AddDbActivationKey.Designer.cs
+++ src/dotnet/Invite.Service.Migration/Migrations/20221213225014_AddDbActivationKey.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ActualChat.Invite.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ActualChat.Invite.Migrations
 {
     [DbContext(typeof(InviteDbContext))]
-    partial class InviteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221213225014_AddDbActivationKey")]
+    partial class AddDbActivationKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
