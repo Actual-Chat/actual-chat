5285d36f502074dc36762a86ebfca3eca7333fc2
Alex Yakunin <alex.yakunin@gmail.com>
2022-12-20 19:50:09 -0800

    migration: avatars

---
+++ src/dotnet/Users.Service.Migration/Migrations/20221221034931_RefactorAuthorsAndAvatars.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Users.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorAuthorsAndAvatars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "principal_id",
+                table: "avatars",
+                newName: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "user_id",
+                table: "avatars",
+                newName: "principal_id");
+        }
+    }
+}
