1034497181f1d369c1148307fc368fb5ba392e52
Alexey Kochetov <alexis.kochetov@gmail.com>
2022-12-28 19:36:22 +0300

    chore: resolve pr issues

--- src/dotnet/App.Maui/Services/NativeHttpClientFactory.Android.cs
+++ src/dotnet/App.Maui/Services/NativeHttpClientFactory.Android.cs
@@ -11,29 +11,4 @@ public partial class NativeHttpClientFactory
 {
     private partial HttpMessageHandler? CreatePlatformMessageHandler()
         => new CronetMessageHandler(Services.GetRequiredService<IExecutorService>());
-    // => new AndroidMessageHandler {
-    //     SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13,
-    //     AutomaticDecompression = DecompressionMethods.All,
-    //     UseCookies = true,
-    //     MaxConnectionsPerServer = 200,
-    // };
-
-
-    // this code can be used for debugging purposes
-    // private class NativeMessageHandler : AndroidMessageHandler
-    // {
-    //     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    //     {
-    //         try {
-    //             var response = await base.SendAsync(request, cancellationToken);
-    //             var content = await response.Content.ReadAsStringAsync();
-    //             Console.WriteLine(content);
-    //             return response;
-    //         }
-    //         catch (Exception e) {
-    //             Console.WriteLine(e.ToString());
-    //             throw;
-    //         }
-    //     }
-    // }
 }
