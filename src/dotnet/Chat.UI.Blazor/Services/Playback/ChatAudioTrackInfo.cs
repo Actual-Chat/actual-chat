951bd992331fd9596619e57136e343d630323da6
Dmitry Filippov <crui3er@gmail.com>
2022-12-29 11:57:48 +0400

    fix: History playback text highlighting no longer works #788

--- src/dotnet/Chat.UI.Blazor/Services/Playback/ChatAudioTrackInfo.cs
+++ src/dotnet/Chat.UI.Blazor/Services/Playback/ChatAudioTrackInfo.cs
@@ -7,7 +7,7 @@ public record ChatAudioTrackInfo(ChatEntry AudioEntry) : TrackInfo(ComposeTrackI
 {
     public static Symbol ComposeTrackId(ChatEntry entry)
         => entry.Kind == ChatEntryKind.Audio
-            ? ZString.Concat("audio:", entry.ChatId, ":", entry.Id)
+            ? ComposeTrackId(entry.ChatId, entry.LocalId)
             : ComposeTrackId(entry.ChatId, entry.AudioEntryId ?? 0);
 
     public static Symbol ComposeTrackId(ChatId chatId, long audioEntryId)
