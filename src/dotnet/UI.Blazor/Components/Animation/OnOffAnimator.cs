01dde62f71aaed6e5934647833ef3ddf2f47a4f7
Alex Yakunin <alex.yakunin@gmail.com>
2023-01-10 03:15:18 -0800

    feat: add OnOffAnimator.cs

--- src/dotnet/UI.Blazor/Components/Animation/OnOffAnimator.cs
+++ src/dotnet/UI.Blazor/Components/Animation/OnOffAnimator.cs
@@ -1,20 +1,17 @@
 namespace ActualChat.UI.Blazor.Components;
 
-public class ShowHideAnimator : ComponentAnimator
+public class OnOffAnimator : ComponentAnimator
 {
     private bool _state;
 
-    public TimeSpan MinDuration { get; init; } = TimeSpan.FromMicroseconds(50);
-
     public bool State { get => _state; set => Transition(value); }
     public string Class { get; private set; }
-    public bool MustHideComponent => OrdinalEquals(Class, "hidden");
 
-    public ShowHideAnimator(ComponentBase component, TimeSpan duration, IMomentClock clock, bool state = false)
+    public OnOffAnimator(ComponentBase component, TimeSpan duration, IMomentClock clock, bool state = false)
         : base(component, duration, clock)
     {
         State = state;
-        Class = state ? "" : "hidden";
+        Class = state ? "on" : "off";
     }
 
     private void Transition(bool newState)
@@ -24,19 +21,17 @@ public class ShowHideAnimator : ComponentAnimator
         var skipAnimation = TimeSpan.Zero;
         var (newClass, duration) = newState
             ? Class switch {
-                "hidden" => ("off", MinDuration),
                 "off" => ("off-to-on", Duration),
-                "off-to-on" => (isAnimating ? Class : "", skipAnimation),
-                "" => (Class, skipAnimation),
+                "off-to-on" => (isAnimating ? Class : "on", skipAnimation),
                 "on-to-off" => ("off-to-on", Duration),
+                "on" => (Class, skipAnimation),
                 _ => throw StandardError.Internal($"Invalid Class: '{Class}'."),
             }
             : Class switch {
-                "hidden" => (Class, skipAnimation),
-                "off" => ("hidden", skipAnimation),
+                "off" => (Class, skipAnimation),
                 "off-to-on" => ("on-to-off", Duration),
-                "" => ("on-to-off", Duration),
-                "on-to-off" => (isAnimating ? Class : "hidden", skipAnimation),
+                "on-to-off" => (isAnimating ? Class : "off", skipAnimation),
+                "on" => ("on-to-off", Duration),
                 _ => throw StandardError.Internal($"Invalid Class: '{Class}'."),
             };
 
