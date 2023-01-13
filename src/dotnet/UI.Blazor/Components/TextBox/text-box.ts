413d93b742cf0cffa0c19d3ee8f7e70db7942382
Alex Yakunin <alex.yakunin@gmail.com>
2023-01-13 12:36:17 -0800

    chore: coding style

--- src/dotnet/UI.Blazor/Components/TextBox/text-box.ts
+++ src/dotnet/UI.Blazor/Components/TextBox/text-box.ts
@@ -18,16 +18,14 @@ export class TextBox implements Disposable {
     constructor(input: HTMLInputElement) {
         this.input = input;
         fromEvent(input, 'input')
-            .pipe(takeUntil(this.disposed$))
-            .pipe(debounceTime(800))
+            .pipe(
+                takeUntil(this.disposed$),
+                debounceTime(800),
+            )
             .subscribe(() => {
                 debugLog?.log(`input handler, value:`, input.value);
                 input.dispatchEvent(new Event('change'));
             });
-    }
-
-    public focus() {
-        this.input.focus();
     }
 
     public dispose() {
@@ -36,5 +34,9 @@ export class TextBox implements Disposable {
 
         this.disposed$.next();
         this.disposed$.complete();
+    }
+
+    public focus() {
+        this.input.focus();
     }
 }
