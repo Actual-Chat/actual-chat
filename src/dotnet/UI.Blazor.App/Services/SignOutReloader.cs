9034eceb120ad05fbb43d79fe74ca5a34c87105e
Frol <4425748+FrolYo@users.noreply.github.com>
2023-01-19 16:33:11 +0300

    Revert "chore: get rid of SignoutReloader #814"
    This reverts commit 4a5d716831c71bb7806a9eae10f1f10305cd943f.

---
+++ src/dotnet/UI.Blazor.App/Services/SignOutReloader.cs
@@ -0,0 +1,56 @@
+namespace ActualChat.UI.Blazor.App.Services;
+
+public class SignOutReloader : WorkerBase
+{
+    private IServiceProvider Services { get; }
+    private UICommander UICommander { get; }
+    private MomentClockSet Clocks { get; }
+    private ILogger Log { get; }
+
+    public SignOutReloader(IServiceProvider services)
+    {
+        Services = services;
+        Log = services.LogFor(GetType());
+        UICommander = Services.UICommander();
+        Clocks = services.Clocks();
+    }
+
+    protected override async Task RunInternal(CancellationToken cancellationToken)
+    {
+        var session = Services.GetRequiredService<Session>();
+        var auth = Services.GetRequiredService<IAuth>();
+
+        var updateDelayer = FixedDelayer.Instant;
+        while (true) {
+            try {
+                var cAuthInfo0 = await Computed
+                    .Capture(() => auth.GetAuthInfo(session, cancellationToken))
+                    .ConfigureAwait(false);
+
+                // Wait for sign-in unless already signed in
+                if (cAuthInfo0.ValueOrDefault?.IsAuthenticated() != true) {
+                    await cAuthInfo0.When(i => i?.IsAuthenticated() ?? false, updateDelayer, cancellationToken).ConfigureAwait(false);
+                    await UICommander.RunNothing().ConfigureAwait(false); // Reset all update delays
+                }
+
+                // Wait for sign-out
+                await cAuthInfo0.When(i => !(i?.IsAuthenticated() ?? false), updateDelayer, cancellationToken).ConfigureAwait(false);
+                await UICommander.RunNothing().ConfigureAwait(false); // Reset all update delays
+
+                // Wait 0.5 seconds before we force page refresh
+                await Clocks.CpuClock.Delay(TimeSpan.FromSeconds(0.5), cancellationToken).ConfigureAwait(false);
+                break;
+            }
+            catch (OperationCanceledException) {
+                throw;
+            }
+            catch {
+                // Intended
+            }
+        }
+
+        Log.LogInformation("Forcing reload on sign-out");
+        var nav = Services.GetRequiredService<NavigationManager>();
+        nav.NavigateTo(nav.Uri, true);
+    }
+}
