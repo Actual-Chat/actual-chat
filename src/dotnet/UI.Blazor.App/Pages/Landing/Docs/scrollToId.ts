4fb1c546d856710b2d2de8575ccecfeb9793a004
iqmulator <iqmulator@gmail.com>
2023-01-20 22:11:28 +0600

    add anchors for docs pages, update ui, refactor css

---
+++ src/dotnet/UI.Blazor.App/Pages/Landing/Docs/scrollToId.ts
@@ -0,0 +1,10 @@
+export function scrollToId(id: string) {
+    const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+           behavior: "smooth",
+           block: "start",
+           inline: "nearest"
+       });
+    }
+}
