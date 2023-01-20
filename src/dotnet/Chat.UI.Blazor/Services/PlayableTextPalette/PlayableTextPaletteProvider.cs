8c1c34d408c0556cccb81df0bf440ef291157619
Dmitry Filippov <crui3er@gmail.com>
2023-01-20 20:30:28 +0400

    feat: Simplify PlayableTextPaletteProvider with using LRU cache #846

--- src/dotnet/Chat.UI.Blazor/Services/PlayableTextPalette/PlayableTextPaletteProvider.cs
+++ src/dotnet/Chat.UI.Blazor/Services/PlayableTextPalette/PlayableTextPaletteProvider.cs
@@ -3,43 +3,10 @@ namespace ActualChat.Chat.UI.Blazor.Services;
 public class PlayableTextPaletteProvider
 {
     private const int MaxPalettesNumber = 5;
-    private readonly LinkedList<(ChatId ChatId, PlayableTextPalette Palette)> _cache = new ();
+    private ThreadSafeLruCache<ChatId, PlayableTextPalette> _cache = new (MaxPalettesNumber);
 
     public PlayableTextPalette GetPalette(ChatId chatId)
-    {
-        PlayableTextPalette palette;
-        lock (_cache) {
-            if (_cache.Count == 0) {
-                palette = NewPalette();
-                _cache.AddFirst((chatId, palette));
-            }
-            else {
-                var first = _cache.First!;
-                if (first.Value.ChatId == chatId)
-                    palette = first.Value.Palette;
-                else {
-                    var node = first.Next;
-                    while (node != null) {
-                        if (node.Value.ChatId == chatId)
-                            break;
-                        node = node.Next;
-                    }
-                    if (node != null) {
-                        palette = node.Value.Palette;
-                        _cache.Remove(node);
-                        _cache.AddFirst(node.Value);
-                    }
-                    else {
-                        while (_cache.Count >= MaxPalettesNumber)
-                            _cache.RemoveLast();
-                        palette = NewPalette();
-                        _cache.AddFirst((chatId, palette));
-                    }
-                }
-            }
-        }
-        return palette;
-    }
+        => _cache.GetOrCreate(chatId, _ => NewPalette());
 
     private static PlayableTextPalette NewPalette() => new ();
 }
