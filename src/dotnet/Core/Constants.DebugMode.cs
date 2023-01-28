cc0f5dc93a26cf8262f79ed8760ab98650c0b8db
Alex Yakunin <alex.yakunin@gmail.com>
2023-01-28 09:29:48 -0800

    fix: ChatUI: ChatPosition write bug + debug logging there

--- src/dotnet/Core/Constants.DebugMode.cs
+++ src/dotnet/Core/Constants.DebugMode.cs
@@ -8,8 +8,18 @@ public static partial class Constants
 
     public static class DebugMode
     {
-        public static bool VirtualList { get; } = true;
+        // Core components
+        public static bool SignalR { get; } = false;
+        public static bool StoredState { get; } = false;
+        public static bool SyncedState { get; } = false;
+        public static bool MarkupParser { get; } = false;
 
+        // UI
+        public static bool VirtualList { get; } = true;
+        public static bool ChatUI { get; } = false;
+
+        // Audio
+        public static bool WebMReader { get; } = false;
         public static bool AudioSource { get; } = false;
         public static bool AudioProcessor { get; } = true;
         public static bool AudioPlayback { get; } = false;
@@ -18,19 +28,13 @@ public static partial class Constants
         public static bool AudioRecordingStream { get; } = false;
         public static bool AudioStreamProxy { get; } = true;
 
+        // Transcription
         public static bool Transcription { get; } = false;
         public static bool TranscriberAny { get; } = false;
         public static bool TranscriberGoogle { get; } = true;
         public static bool TranscriptStreamProxy { get; } = false;
 
-        public static bool WebMReader { get; } = false;
-        public static bool MarkupParser { get; } = false;
-
-        public static bool StoredState { get; } = false;
-        public static bool SyncedState { get; } = false;
-
+        // Misc.
         public static bool KubeEmulation { get; } = false;
-
-        public static bool SignalR { get; } = false;
     }
 }
