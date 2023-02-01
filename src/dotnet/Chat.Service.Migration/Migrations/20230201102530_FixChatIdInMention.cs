153a7056bc9d88b61fcd62ec0d2f0c690e25d44e
Frol <4425748+FrolYo@users.noreply.github.com>
2023-02-01 13:33:33 +0300

    fix: mentions badge is not displayed #893

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20230201102530_FixChatIdInMention.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixChatIdInMention : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+update mentions
+set chat_id = split_part(id, ':', 1)
+where (chat_id is null OR chat_id = '')
+  and exists (select 1 from chats where id = split_part(id, ':', 1));
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
