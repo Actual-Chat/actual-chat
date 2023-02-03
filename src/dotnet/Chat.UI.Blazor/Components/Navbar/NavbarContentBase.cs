82a1ea184e19de0bce213010fb7e2168b573e57e
Dmitry Filippov <crui3er@gmail.com>
2023-02-03 15:48:33 +0400

    refactor: use AccountUI.OwnAccount for initial state in ComputedStateComponent's

--- src/dotnet/Chat.UI.Blazor/Components/Navbar/NavbarContentBase.cs
+++ src/dotnet/Chat.UI.Blazor/Components/Navbar/NavbarContentBase.cs
@@ -1,20 +1,20 @@
 using ActualChat.Hosting;
+using ActualChat.UI.Blazor.Services;
 using ActualChat.Users;
 
 namespace ActualChat.Chat.UI.Blazor.Components;
 
 public abstract class NavbarContentBase : ComputedStateComponent<AccountFull>
 {
-    [Inject] protected Session Session { get; init; } = null!;
-    [Inject] protected IAccounts Accounts { get; init; } = null!;
+    [Inject] protected AccountUI AccountUI { get; init; } = null!;
     [Inject] protected HostInfo HostInfo { get; init; } = null!;
 
     protected override ComputedState<AccountFull>.Options GetStateOptions()
         => new() {
-            InitialValue = AccountFull.Loading,
+            InitialValue = AccountUI.OwnAccount.Value,
             Category = GetStateCategory(),
         };
 
-    protected override Task<AccountFull> ComputeState(CancellationToken cancellationToken)
-        => Accounts.GetOwn(Session, cancellationToken);
+    protected override async Task<AccountFull> ComputeState(CancellationToken cancellationToken)
+        => await AccountUI.OwnAccount.Use(cancellationToken);
 }
