6d81f9436e409b0af4f1735c94bf6b93d0732cb2
Dmitry Filippov <crui3er@gmail.com>
2023-02-03 09:56:04 +0400

    fix: when user is signed in auto they should be navigated to chats when app is started from home page

--- src/dotnet/UI.Blazor/Services/AutoNavigationUI.cs
+++ src/dotnet/UI.Blazor/Services/AutoNavigationUI.cs
@@ -1,28 +1,21 @@
-using ActualChat.Users;
-
 namespace ActualChat.UI.Blazor.Services;
 
 public sealed class AutoNavigationUI
 {
-    private Session Session { get; }
-    private IAccounts Accounts { get; }
     private NavigationManager Nav { get; }
 
     public bool MustNavigateToChatsOnSignIn { get; set; } = true;
 
     public AutoNavigationUI(IServiceProvider services)
-    {
-        Session = services.GetRequiredService<Session>();
-        Accounts = services.GetRequiredService<IAccounts>();
-        Nav = services.GetRequiredService<NavigationManager>();
-    }
+        => Nav = services.GetRequiredService<NavigationManager>();
 
-    public void TryNavigateToChatsOnSignIn()
+    public bool TryNavigateToChatsOnSignIn()
     {
         if (!MustNavigateToChatsOnSignIn)
-            return;
+            return false;
 
         MustNavigateToChatsOnSignIn = false;
         Nav.NavigateTo(Links.Chat(default));
+        return true;
     }
 }
