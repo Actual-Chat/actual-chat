17fc3ff4cf869393461ba9e60da472d85630914d
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-02-08 08:37:10 +0100

    feat: use account full name instead of user name

--- src/dotnet/Core/DefaultUserPicture.cs
+++ src/dotnet/Core/DefaultUserPicture.cs
@@ -4,8 +4,8 @@ public static class DefaultUserPicture
 {
     public const int DefaultSize = 160;
 
-    public static string Get(User user, int size = DefaultSize)
-        => GetBoringAvatar(user.Name.GetSHA1HashCode(), size);
+    public static string Get(string name, int size = DefaultSize)
+        => GetBoringAvatar(name.GetSHA1HashCode(), size);
 
     public static string GetBoringAvatar(string hash, int size = DefaultSize)
         => $"https://source.boringavatars.com/beam/{size}/{hash.UrlEncode()}?colors=FFDBA0,BBBEFF,9294E1,FF9BC0,0F2FE8";
