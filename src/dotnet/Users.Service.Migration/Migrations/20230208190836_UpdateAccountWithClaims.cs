5f2b325a02a1b575f9eebef5e6dbad3a3f3bdb53
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-02-09 07:16:51 +0100

    fix: null error in migration

--- src/dotnet/Users.Service.Migration/Migrations/20230208190836_UpdateAccountWithClaims.cs
+++ src/dotnet/Users.Service.Migration/Migrations/20230208190836_UpdateAccountWithClaims.cs
@@ -19,9 +19,9 @@ namespace ActualChat.Users.Migrations
                 from (
                     select
                         id,
-                        claims_json::json->>'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname' AS first_name,
-                        claims_json::json->>'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname' AS last_name,
-                        claims_json::json->>'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' AS email
+                        coalesce(claims_json::json->>'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname', '') AS first_name,
+                        coalesce(claims_json::json->>'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname', '') AS last_name,
+                        coalesce(claims_json::json->>'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress', '') AS email
                     from users
                     where claims_json <> '{}' and claims_json <> ''
                 ) as subquery
