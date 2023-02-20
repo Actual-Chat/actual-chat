9af97430e82c0ed22cb748ae10cae56c2aa45f95
Frol <4425748+FrolYo@users.noreply.github.com>
2023-02-20 16:48:08 +0300

    fix: experimental - clipboard.write instead of clipboard.writeText #397

--- src/dotnet/UI.Blazor/Components/Clipboard/clipboard.ts
+++ src/dotnet/UI.Blazor/Components/Clipboard/clipboard.ts
@@ -1,6 +1,13 @@
-﻿export function selectAndCopy(inputRef: HTMLInputElement, text: string | null) {
+﻿import { Log, LogLevel, LogScope } from 'logging';
+
+const LogScope: LogScope = 'Clipboard';
+const errorLog = Log.get(LogScope, LogLevel.Error);
+
+export function selectAndCopy(inputRef: HTMLInputElement, text: string | null) {
     inputRef.select();
-    void navigator.clipboard.writeText(text ?? inputRef.value);
+    const blob = new Blob([text ?? inputRef.value], { type: 'text/plain' });
+    const clipboardItem = new ClipboardItem({ ['text/plain']: blob });
+    return navigator.clipboard.write([clipboardItem]).catch(e => errorLog?.log('selectAndCopy: failed to write to clipboard', e));
 }
 
 export function selectAndGet(inputRef: HTMLInputElement) {
