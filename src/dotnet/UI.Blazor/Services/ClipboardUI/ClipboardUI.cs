1707a69a5df552a39027057795ed47d3587fb6c7
Frol <4425748+FrolYo@users.noreply.github.com>
2023-02-21 01:18:56 +0300

    fix: ios - copy to clipboard #397

--- src/dotnet/UI.Blazor/Services/ClipboardUI/ClipboardUI.cs
+++ src/dotnet/UI.Blazor/Services/ClipboardUI/ClipboardUI.cs
@@ -12,9 +12,6 @@ public class ClipboardUI
     public virtual ValueTask<string> ReadText()
         => _js.InvokeAsync<string>("navigator.clipboard.readText");
 
-    public virtual ValueTask WriteText(string text)
-        => _js.InvokeVoidAsync("navigator.clipboard.writeText", text);
-
     protected virtual ValueTask<string> GetTextFrom(ElementReference inputRef)
         => _js.InvokeAsync<string>($"{BlazorUICoreModule.ImportName}.selectAndGet", inputRef);
 }
--- src/dotnet/UI.Blazor/Services/ClipboardUI/clipboard.ts
+++ src/dotnet/UI.Blazor/Services/ClipboardUI/clipboard.ts
@@ -1,9 +1,35 @@
 ﻿import { Log, LogLevel, LogScope } from 'logging';
+import { DocumentEvents } from 'event-handling';
+import { getOrInheritData } from 'dom-helpers';
+import { exhaustMap, filter, map, tap, catchError } from 'rxjs';
 
 const LogScope: LogScope = 'Clipboard';
+const debugLog = Log.get(LogScope, LogLevel.Debug);
 const errorLog = Log.get(LogScope, LogLevel.Error);
 
 export function selectAndGet(inputRef: HTMLInputElement) {
     inputRef.select();
     return inputRef.value;
 }
+
+// we intentionally do it on client side since iOS safari requires event handler stack for clipboard write access
+function subscribeOnCopy() {
+    DocumentEvents.active.click$.pipe().subscribe();
+    debugLog?.log(`subscribeOnCopy`);
+    DocumentEvents.active.click$
+        .pipe(
+            map(ev => {
+                const [triggerElement, text] = getOrInheritData(ev.target, 'clipboardText');
+                return text;
+            }),
+            filter(text => !!text),
+            tap(() => debugLog?.log(`subscribeOnCopy: writing to clipboard`)),
+            exhaustMap(text => navigator.clipboard.writeText(text)),
+            catchError((err, caught) => {
+                errorLog?.log(`subscribeOnCopy: failed to copy: `, err);
+                return caught;
+            }),
+        ).subscribe();
+}
+
+subscribeOnCopy();
