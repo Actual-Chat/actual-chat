1f92c93ce24f721ee32685a167f351221e042299
Dmitry Filippov <crui3er@gmail.com>
2023-02-28 16:33:43 +0400

    feat: Decorate complex objects before logging to console in js #1008

--- src/nodejs/src/logging.ts
+++ src/nodejs/src/logging.ts
@@ -9,9 +9,15 @@ export interface LogRef {
     id : number;
 }
 
+interface QueueItem
+{
+    ref : LogRef;
+    touchedAt : number;
+}
+
 class LogRefQueue
 {
-    items : LogRef[];
+    items : QueueItem[];
     capacity : number;
     idSeed : number;
 
@@ -22,31 +28,36 @@ class LogRefQueue
     }
 
     public ref(data: unknown) : LogRef {
-        const itemIndex = this.items.findIndex(el => el.target === data);
+        const itemIndex = this.items.findIndex(el => el.ref.target === data);
         if (itemIndex >= 0) {
             const existentItem = this.items[itemIndex];
-            if (this.items.length > 1 && itemIndex < (this.items.length - 1)) {
-                // move item to the beginning of the queue
-                this.items.splice(itemIndex, 1);
-                this.items.push(existentItem);
-            }
-            return existentItem;
+            existentItem.touchedAt = Date.now();
+            return existentItem.ref;
         }
         else {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
             const id = data['__logRefId'] as number ?? this.idSeed++;
-            const newItem = { target: data, id: id };
+            const newRef = { target: data, id: id };
             data['__logRefId'] = id;
             if (this.items.length >= this.capacity) {
-                const deletedItems = this.items.splice(0, this.items.length - this.capacity + 1);
-                for (const item of deletedItems) {
-                    // clear log ref target to prevent memory leaks
-                    // and keep string representation of the target for tracing
-                    item.target = item.target.toString();
+                let indexToEliminate = 0;
+                let itemToEliminate = this.items[0];
+                for (let i = 1; i < this.items.length; i++) {
+                    const item = this.items[i];
+                    if (item.touchedAt < itemToEliminate.touchedAt) {
+                        itemToEliminate = item;
+                        indexToEliminate = i;
+                    }
                 }
+                this.items.splice(indexToEliminate, 1);
+                // clear log ref target to prevent memory leaks
+                // and keep string representation of the target for tracing
+                const ref = itemToEliminate.ref;
+                ref.target = ref.target.toString();
             }
+            const newItem = { ref : newRef, touchedAt : Date.now() };
             this.items.push(newItem);
-            return newItem;
+            return newRef;
         }
     }
 }
@@ -55,7 +66,7 @@ export class Log {
     public static readonly minLevels: Map<LogScope, LogLevel> = new Map<LogScope, LogLevel>();
     public static defaultMinLevel = LogLevel.Info;
     private static isInitialized = false;
-    private static logRefs : LogRefQueue = new LogRefQueue(20);
+    private static logRefs : LogRefQueue = new LogRefQueue(10);
     public log: (...data: unknown[]) => void;
 
     constructor(
@@ -96,7 +107,11 @@ export class Log {
         return level >= minLevel ? this.loggerFactory(scope, level) : null;
     }
 
-    public static ref(data: unknown) : LogRef {
+    public static ref(data: unknown) : LogRef | null | undefined {
+        if (data === null)
+            return null;
+        if (typeof data === 'undefined')
+            return undefined;
         return this.logRefs.ref(data);
     }
 
