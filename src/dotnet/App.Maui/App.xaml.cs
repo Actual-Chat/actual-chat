a9b0d6a88616a040664c518cefb65f041e5b9721
Dmitry Filippov <crui3er@gmail.com>
2023-03-01 19:34:47 +0400

    feat: Open actual chat links with android app #1017

--- src/dotnet/App.Maui/App.xaml.cs
+++ src/dotnet/App.Maui/App.xaml.cs
@@ -2,8 +2,17 @@ namespace ActualChat.App.Maui
 {
     public partial class App : Application
     {
-        public App(MainPage mainPage)
+        private ILogger Log { get; }
+
+        protected override void OnAppLinkRequestReceived(Uri uri)
         {
+            Log.LogDebug("OnAppLinkRequestReceived: '{Uri}'", uri);
+            Services.AppLinks.OnAppLinkRequestReceived(uri);
+        }
+
+        public App(MainPage mainPage, ILogger<App> log)
+        {
+            Log = log;
             InitializeComponent();
             MainPage = mainPage;
         }
