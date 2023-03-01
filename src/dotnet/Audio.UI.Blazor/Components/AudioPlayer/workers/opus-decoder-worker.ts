afa7055dab584bfe9d07f3487987315369ef816e
Alexey Kochetov <alexis.kochetov@gmail.com>
2023-03-01 21:52:19 +0300

    fix: build issues

--- src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/workers/opus-decoder-worker.ts
+++ src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/workers/opus-decoder-worker.ts
@@ -8,7 +8,7 @@ import codec, { Codec, Decoder } from '@actual-chat/codec/codec.debug';
 import codecWasm from '@actual-chat/codec/codec.debug.wasm';
 import codecWasmMap from '@actual-chat/codec/codec.debug.wasm.map';
 /// #else
-/// #code import codec, { Encoder, Codec } from '@actual-chat/codec';
+/// #code import codec, { Decoder, Codec } from '@actual-chat/codec';
 /// #code import codecWasm from '@actual-chat/codec/codec.wasm';
 /// #endif
 
