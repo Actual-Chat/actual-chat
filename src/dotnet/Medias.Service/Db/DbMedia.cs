3fd1f47cbc83aae1dab86508a2062bad901864f8
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-03-09 06:48:52 +0100

    Replace fields with metadata

--- src/dotnet/Medias.Service/Db/DbMedia.cs
+++ src/dotnet/Medias.Service/Db/DbMedia.cs
@@ -1,62 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Stl.Versioning;
 
 namespace ActualChat.Medias.Db;
 
 [Table("Medias")]
-public class DbMedia : IHasId<string>, IHasVersion<long>, IRequirementTarget
+public class DbMedia : IHasId<string>, IRequirementTarget
 {
-    private DateTime _createdAt = CoarseSystemClock.Now;
-
     public DbMedia() { }
     public DbMedia(Media model) => UpdateFrom(model);
 
     [Key] public string Id { get; set; } = "";
-    [ConcurrencyCheck] public long Version { get; set; }
 
-    public string FileName { get; set; } = "";
     public string ContentId { get; set; } = "";
-    public string ContentType { get; set; } = "";
-    public long Length { get; set; }
-    public bool IsRemoved { get; set; }
-
-    public DateTime CreatedAt {
-        get => _createdAt.DefaultKind(DateTimeKind.Utc);
-        set => _createdAt = value.DefaultKind(DateTimeKind.Utc);
-    }
+    public string MetadataJson { get; set; } = "";
 
     public Media ToModel()
-        => new (new MediaId(Id), Version) {
-            FileName = FileName,
+        => new (new MediaId(Id)) {
             ContentId = ContentId,
-            ContentType = ContentType,
-            Length = Length,
-            IsRemoved = IsRemoved,
+            MetadataJson = MetadataJson,
         };
 
     public void UpdateFrom(Media model)
     {
         this.RequireSameOrEmptyId(model.Id);
-        model.RequireSomeVersion();
 
         Id = model.Id;
-        Version = model.Version;
         ContentId = model.ContentId;
-        FileName = model.FileName;
-        ContentType = model.ContentType;
-        Length = model.Length;
-        IsRemoved = model.IsRemoved;
-    }
-
-    internal class EntityConfiguration : IEntityTypeConfiguration<DbMedia>
-    {
-        public void Configure(EntityTypeBuilder<DbMedia> builder) {
-            builder.Property(a => a.FileName).IsRequired();
-            builder.Property(a => a.ContentType).IsRequired();
-            builder.Property(a => a.ContentId).IsRequired();
-        }
+        MetadataJson = model.MetadataJson;
     }
 }
--- src/dotnet/Medias/Media.cs
+++ src/dotnet/Medias/Media.cs
@@ -1,5 +1,4 @@
 ﻿using Stl.Fusion.Blazor;
-using Stl.Versioning;
 
 namespace ActualChat.Medias;
 
@@ -8,11 +7,73 @@ namespace ActualChat.Medias;
 public sealed record Media(
     [property: DataMember] MediaId Id,
     [property: DataMember] long Version = 0
-    ) : IHasId<MediaId>, IHasVersion<long>, IRequirementTarget
+    ) : IHasId<MediaId>, IRequirementTarget
 {
+    private readonly NewtonsoftJsonSerialized<ImmutableOptionSet> _metadata =
+        NewtonsoftJsonSerialized.New(ImmutableOptionSet.Empty);
+
     [DataMember] public string ContentId { get; init; } = "";
-    [DataMember] public string FileName { get; init; } = "";
-    [DataMember] public string ContentType { get; set; } = "";
-    [DataMember] public long Length { get; set; }
-    [DataMember] public bool IsRemoved { get; set; }
+
+    [DataMember] public string MetadataJson {
+        get => _metadata.Data;
+        init => _metadata.Data = value;
+    }
+
+    // Computed properties
+
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public ImmutableOptionSet Metadata {
+        get => _metadata.Value;
+        init => _metadata.Value = value;
+    }
+
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public long Length {
+        get => GetMetadataValue(0L);
+        init => SetMetadataValue(value);
+    }
+
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public string FileName {
+        get => GetMetadataValue("");
+        init => SetMetadataValue(value);
+    }
+
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public string ContentType {
+        get => GetMetadataValue("");
+        init => SetMetadataValue(value);
+    }
+
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public int Width {
+        get => GetMetadataValue<int>();
+        init => SetMetadataValue(value);
+    }
+
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore]
+    public int Height {
+        get => GetMetadataValue<int>();
+        init => SetMetadataValue(value);
+    }
+
+    public Media() : this(MediaId.None) { }
+
+    // Private methods
+
+    private T GetMetadataValue<T>(T @default = default!, [CallerMemberName] string symbol = "")
+    {
+        var value = Metadata[symbol];
+        if (value == null)
+            return @default;
+
+        // TODO: remove this workaround when int is not deserialized as long
+        if (typeof(T) == typeof(int))
+            value = Convert.ToInt32(value, CultureInfo.InvariantCulture);
+
+        return (T)value;
+    }
+
+    private void SetMetadataValue<T>(T value, [CallerMemberName] string symbol = "")
+        => _metadata.Value = Metadata.Set(symbol, value);
 }
