529d49beb18d2fa780fc668a2462dc14716a0950
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-03-20 07:18:04 +0100

    Add Operations to Media module

--- src/dotnet/Media.Service/Db/MediaDbContext.cs
+++ src/dotnet/Media.Service/Db/MediaDbContext.cs
@@ -1,12 +1,16 @@
 using ActualChat.Db;
 using Microsoft.EntityFrameworkCore;
 using Stl.Fusion.EntityFramework;
+using Stl.Fusion.EntityFramework.Operations;
 
 namespace ActualChat.Media.Db;
 
 public class MediaDbContext : DbContextBase
 {
     public DbSet<DbMedia> Media { get; protected set; } = null!;
+
+    // Stl.Fusion.EntityFramework tables
+    public DbSet<DbOperation> Operations { get; protected set; } = null!;
 
     public MediaDbContext(DbContextOptions options) : base(options) { }
 
