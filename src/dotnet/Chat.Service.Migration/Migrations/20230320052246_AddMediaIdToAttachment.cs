c1d252f6a95155f26df9fdaacd5e7b69e25d13e2
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-03-20 06:23:18 +0100

    Add MediaId to Attachment

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20230320052246_AddMediaIdToAttachment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaIdToAttachment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "media_id",
+                table: "text_entry_attachments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "media_id",
+                table: "text_entry_attachments");
+        }
+    }
+}
