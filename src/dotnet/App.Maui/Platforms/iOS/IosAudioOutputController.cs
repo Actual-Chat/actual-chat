699b313c6e96e5b626c5692a971a5fe2bf123978
Frol <4425748+FrolYo@users.noreply.github.com>
2023-03-29 13:54:11 +0300

    feat: concurrent safe configure ios audio session #1076

--- src/dotnet/App.Maui/Platforms/iOS/IosAudioOutputController.cs
+++ src/dotnet/App.Maui/Platforms/iOS/IosAudioOutputController.cs
@@ -5,6 +5,7 @@ namespace ActualChat.App.Maui;
 
 public class IosAudioOutputController : IAudioOutputController
 {
+    private readonly object _lock = new ();
     private readonly IMutableState<bool> _isAudioOn;
     private readonly IMutableState<bool> _isSpeakerphoneOn;
 
@@ -28,29 +29,42 @@ public class IosAudioOutputController : IAudioOutputController
 
     public ValueTask SetAudioEnabled(bool mustEnable)
     {
-        _isAudioOn.Value = mustEnable;
-        // TODO: concurrency
+        lock (_lock) {
+            if (_isAudioOn.Value == mustEnable)
+                return ValueTask.CompletedTask;
 
-        try {
-            var avAudioSession = AVAudioSession.SharedInstance();
-            if (mustEnable) {
-                avAudioSession
-                    .SetCategory(AVAudioSessionCategory.PlayAndRecord,
-                        AVAudioSessionCategoryOptions.AllowBluetooth
-                        // | AVAudioSessionCategoryOptions.AllowBluetoothA2DP TODO: only for audio playback
-                        | AVAudioSessionCategoryOptions.DuckOthers
-                        | AVAudioSessionCategoryOptions.DefaultToSpeaker)
-                    .Assert();
-                if (!avAudioSession.OverrideOutputAudioPort(AVAudioSessionPortOverride.Speaker, out var error))
-                    error.Assert();
-            }
-            avAudioSession.SetActive(mustEnable, AVAudioSessionSetActiveOptions.NotifyOthersOnDeactivation).Assert();
-        }
-        catch (Exception e) {
-            DefaultLog.LogError(e, "Failed to configure audio session");
+            _isAudioOn.Value = mustEnable;
+            ConfigureAudioSession();
         }
 
         return ValueTask.CompletedTask;
+
+        void ConfigureAudioSession()
+        {
+            try
+            {
+                var avAudioSession = AVAudioSession.SharedInstance();
+                if (mustEnable)
+                {
+                    avAudioSession
+                        .SetCategory(AVAudioSessionCategory.PlayAndRecord,
+                            AVAudioSessionCategoryOptions.AllowBluetooth
+                            // | AVAudioSessionCategoryOptions.AllowBluetoothA2DP TODO: only for audio playback
+                            | AVAudioSessionCategoryOptions.DuckOthers
+                            | AVAudioSessionCategoryOptions.DefaultToSpeaker
+                        )
+                        .Assert();
+                    if (!avAudioSession.OverrideOutputAudioPort(AVAudioSessionPortOverride.Speaker, out var error))
+                        error.Assert();
+                }
+
+                avAudioSession.SetActive(mustEnable, AVAudioSessionSetActiveOptions.NotifyOthersOnDeactivation).Assert();
+            }
+            catch (Exception e)
+            {
+                DefaultLog.LogError(e, "Failed to configure audio session");
+            }
+        }
     }
 
     public ValueTask SetSpeakerphoneEnabled(bool mustEnable)
