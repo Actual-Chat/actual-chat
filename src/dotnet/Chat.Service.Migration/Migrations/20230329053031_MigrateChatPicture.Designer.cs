8c1786d24065044db4e9cef1ebf9a0be36ecf908
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-03-29 08:57:51 +0200

    Migrate chat pictures

--- src/dotnet/Chat.Service.Migration/Migrations/20230329053031_MigrateChatPicture.Designer.cs
+++ src/dotnet/Chat.Service.Migration/Migrations/20230329053031_MigrateChatPicture.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ActualChat.Chat.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ActualChat.Chat.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230329053031_MigrateChatPicture")]
+    partial class MigrateChatPicture
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
