0f673285ddd474dcc552c1c931a6111683506de9
Alex Yakunin <alex.yakunin@gmail.com>
2023-03-31 05:15:32 -0700

    perf: increase initial flush delay for FlushingKeyValueStore.cs

--- src/dotnet/Core/Collections/FlushingKeyValueStore.cs
+++ src/dotnet/Core/Collections/FlushingKeyValueStore.cs
@@ -5,6 +5,7 @@ public abstract class FlushingKeyValueStore : WorkerBase
     protected readonly ConcurrentDictionary<HashedString, string?> WriteCache = new();
 
     public ILogger? Log { get; init; }
+    public TimeSpan InitialFlushDelay { get; init; } = TimeSpan.FromSeconds(3);
     public RandomTimeSpan FlushPeriod { get; init; } = TimeSpan.FromSeconds(1).ToRandom(0.1);
     public RetryDelaySeq FlushRetryDelays { get; init; } = new(0.25, 1);
 
@@ -52,7 +53,7 @@ public abstract class FlushingKeyValueStore : WorkerBase
                 .RetryForever(FlushRetryDelays, Log)
                 .AppendDelay(FlushPeriod)
                 .CycleForever()
-                .PrependDelay(FlushPeriod)
+                .PrependDelay(InitialFlushDelay)
                 .RunIsolated(cancellationToken)
                 .ConfigureAwait(false);
         }
