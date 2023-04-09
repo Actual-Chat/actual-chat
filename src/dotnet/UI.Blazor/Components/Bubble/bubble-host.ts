dbf90477863400673681d2f2c38d547137307e2f
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-04-09 13:44:38 +0200

    Remove hover support

--- src/dotnet/UI.Blazor/Components/Bubble/bubble-host.ts
+++ src/dotnet/UI.Blazor/Components/Bubble/bubble-host.ts
@@ -36,7 +36,6 @@ interface Bubble {
     id: string;
     bubbleRef: string;
     triggerElement: HTMLElement;
-    isHoverBubble: boolean;
     placement: Placement;
     position: Vector2D | null;
     historyStepId: string | null;
@@ -54,25 +53,15 @@ export class BubbleHost implements Disposable {
 
     constructor(private readonly blazorRef: DotNet.DotNetObject) {
         debugLog?.log('constructor');
-        merge(
-            DocumentEvents.active.click$,
-            DocumentEvents.active.contextbubble$,
-            )
-            .pipe(takeUntil(this.disposed$))
-            .subscribe((event: MouseEvent) => this.onClick(event));
 
-        DocumentEvents.passive.pointerOver$
-            .pipe(takeUntil(this.disposed$))
-            .subscribe((event: PointerEvent) => this.onPointerOver(event));
-
-        Escapist.event$
-            .pipe(takeUntil(this.disposed$))
-            .subscribe((event: KeyboardEvent) => {
-                if (this.bubble != null) {
-                    stopEvent(event);
-                    this.hide();
-                }
-            });
+        // Escapist.event$
+        //     .pipe(takeUntil(this.disposed$))
+        //     .subscribe((event: KeyboardEvent) => {
+        //         if (this.bubble != null) {
+        //             stopEvent(event);
+        //             this.hide();
+        //         }
+        //     });
     }
 
     public dispose(): void {
@@ -89,12 +78,11 @@ export class BubbleHost implements Disposable {
 
     public showOrPosition(
         bubbleRef: string,
-        isHoverBubble: boolean,
         triggerElement: HTMLElement | string,
         placement?: Placement | null,
         position?: Vector2D | null,
     ): void {
-        let bubble = this.create(bubbleRef, isHoverBubble, triggerElement, placement, position);
+        let bubble = this.create(bubbleRef, triggerElement, placement, position);
         if (this.isShown(bubble))
             void this.position(this.bubble, bubble);
         else
@@ -118,7 +106,7 @@ export class BubbleHost implements Disposable {
             return;
         }
 
-        if (bubble.isHoverBubble && !bubble.bubbleElement) {
+        if (!bubble.bubbleElement) {
             // This is the very first render of hover bubble
             await delayAsync(this.hoverBubbleDelayMs);
         }
@@ -131,7 +119,6 @@ export class BubbleHost implements Disposable {
 
     private create(
         bubbleRef: string,
-        isHoverBubble: boolean,
         triggerElement: HTMLElement | SVGElement | string,
         placement: Placement | null,
         position: Vector2D | null,
@@ -145,7 +132,6 @@ export class BubbleHost implements Disposable {
             id: nextId(),
             bubbleRef: bubbleRef,
             triggerElement: triggerElement,
-            isHoverBubble: isHoverBubble,
             placement: placement,
             position: position,
             historyStepId: null,
@@ -157,8 +143,7 @@ export class BubbleHost implements Disposable {
         let m = this.bubble;
         return m
             && m.bubbleRef === bubble.bubbleRef
-            && m.triggerElement === bubble.triggerElement
-            && m.isHoverBubble === bubble.isHoverBubble;
+            && m.triggerElement === bubble.triggerElement;
     }
 
     private show(bubble: Bubble): void {
@@ -167,14 +152,13 @@ export class BubbleHost implements Disposable {
             throw new Error(`${logScope}.show: bubble == null.`);
 
         this.bubble = bubble;
-        this.blazorRef.invokeMethodAsync('OnShowRequest', bubble.id, bubble.bubbleRef, bubble.isHoverBubble);
+        this.blazorRef.invokeMethodAsync('OnShowRequest', bubble.id, bubble.bubbleRef);
         if (ScreenSize.isNarrow())
             VibrationUI.vibrate();
     }
 
     private hide(options?: {
         id?: string,
-        isHoverBubble?: boolean,
     }): void {
         debugLog?.log('hide, options:', options);
         const bubble = this.bubble;
@@ -183,8 +167,6 @@ export class BubbleHost implements Disposable {
 
         if (options) {
             if (options.id !== undefined && bubble.id !== options.id)
-                return;
-            if (options.isHoverBubble !== undefined && bubble.isHoverBubble !== options.isHoverBubble)
                 return;
         }
 
@@ -214,12 +196,7 @@ export class BubbleHost implements Disposable {
         let referenceElement: ReferenceElement;
         const middleware: Middleware[] = [];
         const position = bubble.position;
-        if (bubble.isHoverBubble) {
-            // Hover bubble positioning
-            referenceElement = bubble.triggerElement;
-            middleware.push(offset({ mainAxis: -15, crossAxis: -10 }));
-            middleware.push(flip());
-        } else if (position && !isButtonTrigger(bubble.triggerElement)) {
+        if (position && !isButtonTrigger(bubble.triggerElement)) {
             // Pointer relative positioning
             referenceElement = {
                 getBoundingClientRect() {
@@ -255,99 +232,6 @@ export class BubbleHost implements Disposable {
             left: `${x}px`,
             top: `${y}px`,
         });
-    }
-
-    // Event handlers
-
-    private onClick(event: Event): void {
-        let trigger = BubbleTrigger.None
-        if (event.type == 'click')
-            trigger = BubbleTrigger.Primary;
-        if (event.type == 'contextbubble')
-            trigger = BubbleTrigger.Secondary;
-        debugLog?.log('onClick, event:', event, ', trigger:', trigger);
-
-        let isDesktopMode = this.isDesktopMode;
-
-        // Ignore clicks which definitely aren't "ours"
-        if (trigger == BubbleTrigger.None)
-            return;
-        if (!(event.target instanceof Element))
-            return;
-
-        let [triggerElement, bubbleRef] = getOrInheritData(event.target, 'bubble');
-        if (triggerElement && bubbleRef) {
-            const bubbleTrigger = BubbleTrigger[triggerElement.dataset['bubbleTrigger'] ?? 'Secondary'];
-            if (trigger !== bubbleTrigger) {
-                const altBubbleTrigger = bubbleTrigger == BubbleTrigger.Primary ? BubbleTrigger.Secondary : BubbleTrigger.None;
-                if (!isDesktopMode || trigger != altBubbleTrigger)
-                    bubbleRef = null;
-            }
-        }
-
-        if (!bubbleRef) {
-            // We couldn't find any bubble to activate on click
-            const isClickInsideBubble = event.target.closest('.ac-bubble, .ac-bubble-hover') != null;
-            if (isClickInsideBubble) {
-                // The bubble will process the action, but we can schedule bubble hiding here
-                nextTick(() => this.hide({ id: this.bubble.id }));
-                return;
-            }
-
-            // It's a click outside of any bubble which doesn't trigger another bubble
-            if (!this.bubble || this.bubble.isHoverBubble)
-                return; // There is no visible bubble or visible bubble is a hover bubble
-
-            // Non-hover bubble is visible, so we need to hide it on this click
-            this.hide();
-            return stopEvent(event);
-        }
-
-        const position = isDesktopMode && event instanceof PointerEvent
-            ? new Vector2D(event.clientX, event.clientY)
-            : null;
-        const bubble = this.create(bubbleRef, false, triggerElement, null, position);
-        if (this.isShown(bubble)) {
-            // Is it the second click on the same button that triggered the bubble?
-            if (triggerElement.nodeName == 'BUTTON')
-                this.hide();
-            else
-                void this.position(this.bubble, bubble)
-        }
-        else
-            this.show(bubble);
-
-        stopEvent(event);
-    }
-
-    private async onPointerOver(event: Event): Promise<void> {
-        // Hover bubbles work only in desktop mode
-        if (!this.isDesktopMode)
-            return;
-
-        // Hover bubbles shouldn't be shown when non-hover bubble is shown
-        if (this.bubble?.isHoverBubble === false)
-            return;
-
-        // Ignore hovers which definitely aren't "ours"
-        if (!(event.target instanceof Element)) {
-            this.hide({ isHoverBubble: true });
-            return;
-        }
-
-        const [triggerElement, bubbleRef] = getOrInheritData(event.target, 'hoverBubble');
-        if (!bubbleRef) {
-            const isInsideHoverBubble = event.target.closest('.ac-bubble-hover') != null;
-            if (!isInsideHoverBubble)
-                this.hide({ isHoverBubble: true });
-            return;
-        }
-
-        const bubble = this.create(bubbleRef, true, triggerElement, "top-end", null);
-        if (this.isShown(bubble))
-            return;
-
-        this.show(bubble);
     }
 }
 
