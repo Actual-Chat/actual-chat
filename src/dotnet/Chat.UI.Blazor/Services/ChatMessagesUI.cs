921484a3635629d056717cdbbb29ec3212da294c
Frol <4425748+FrolYo@users.noreply.github.com>
2023-04-11 19:03:16 +0300

    wip: attachments #1075

--- src/dotnet/Chat.UI.Blazor/Services/ChatMessagesUI.cs
+++ src/dotnet/Chat.UI.Blazor/Services/ChatMessagesUI.cs
@@ -1,18 +1,12 @@
-﻿using ActualChat.Kvas;
-using ActualChat.UI.Blazor.Services;
-using ActualChat.Users;
-
-namespace ActualChat.Chat.UI.Blazor.Services;
+﻿namespace ActualChat.Chat.UI.Blazor.Services;
 
 public class ChatMessagesUI : WorkerBase, IComputeService
 {
     private readonly ConcurrentQueue<EntryChange> _queue = new ();
     private readonly SemaphoreSlim _semaphore = new (0, 1);
-    private SyncedStateLease<ChatPosition>? ReadPositionState { get; set; } = null!;
     private Session Session { get; }
     private ChatEditorUI ChatEditorUI { get; }
     private ChatAttachmentsUI ChatAttachmentsUI { get; }
-    private ErrorUI ErrorUI { get; }
     private UICommander UICommander { get; }
     private ILogger Log { get; }
     public ChatMessagesUI(IServiceProvider services)
@@ -20,7 +14,6 @@ public class ChatMessagesUI : WorkerBase, IComputeService
         Session = services.GetRequiredService<Session>();
         ChatEditorUI = services.GetRequiredService<ChatEditorUI>();
         ChatAttachmentsUI = services.GetRequiredService<ChatAttachmentsUI>();
-        ErrorUI = services.GetRequiredService<ErrorUI>();
         UICommander = services.UICommander();
         Log = services.LogFor<ChatMessagesUI>();
     }
@@ -33,7 +26,7 @@ public class ChatMessagesUI : WorkerBase, IComputeService
             return;
 
         try {
-            _queue.Enqueue(new (EntryChangeKind.Post, chatId, text, attachments, relatedChatEntry));
+            _queue.Enqueue(new (chatId, text, attachments, relatedChatEntry));
             _semaphore.Release();
         }
         catch (Exception) {
@@ -69,50 +62,20 @@ public class ChatMessagesUI : WorkerBase, IComputeService
         await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
         // TODO: continue if some entry fails
         while (_queue.TryPeek(out var change)) {
-            switch (change.Kind) {
-            case EntryChangeKind.Post:
-                await Post(change.ChatId, change.Text, change.Attachments, change.RelatedChatEntry, cancellationToken).ConfigureAwait(false);
-                break;
-            case EntryChangeKind.Remove:
-                // TODO: support message remove queue
-                throw new NotImplementedException();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(change.Kind), change.Kind, null);
-            }
+            var (chatId, text, attachments, relatedEntry) = change;
+            IChats.UpsertTextEntryCommand cmd;
+            cmd = relatedEntry is { Kind: RelatedEntryKind.Edit } vRelated
+                ? new IChats.UpsertTextEntryCommand(Session, vRelated.Id.ChatId, vRelated.Id.LocalId, text)
+                : new IChats.UpsertTextEntryCommand(Session, chatId, null, text) {
+                    RepliedChatEntryId = relatedEntry?.Id.LocalId,
+                };
+            var entry = await UICommander.Run(cmd, cancellationToken).ConfigureAwait(false);
+            // TODO: update LastReadEntry if there were no incoming messages
+            // if (ReadPositionState != null && ReadPositionState.Value.EntryLid < readEntryLid)
+            //     ReadPositionState.Value = new ChatPosition(readEntryLid);
             _queue.TryDequeue(out _);
         }
     }
 
-    private async Task Post(
-        ChatId chatId,
-        string text,
-        ImmutableArray<Attachment> attachments,
-        RelatedChatEntry? relatedEntry,
-        CancellationToken cancellationToken)
-    {
-        IChats.UpsertTextEntryCommand cmd;
-        if (relatedEntry is { Kind: RelatedEntryKind.Edit } vRelated) {
-            if (vRelated.Id.IsNone)
-                throw StandardError.Constraint("Invalid ChatUI.RelatedChatEntry value.");
-
-            cmd = new IChats.UpsertTextEntryCommand(Session, vRelated.Id.ChatId, vRelated.Id.LocalId, text);
-        }
-        else {
-            cmd = new IChats.UpsertTextEntryCommand(Session, chatId,  null, text) {
-                RepliedChatEntryId = relatedEntry?.Id.LocalId,
-            };
-        }
-        var entry = await UICommander.Run(cmd, cancellationToken).ConfigureAwait(false);
-
-        if (ReadPositionState != null && ReadPositionState.Value.EntryLid < readEntryLid)
-            ReadPositionState.Value = new ChatPosition(readEntryLid);
-    }
-
-    private sealed record EntryChange(EntryChangeKind Kind, ChatId ChatId, string Text, ImmutableArray<Attachment> Attachments, RelatedChatEntry? RelatedChatEntry);
-
-    private enum EntryChangeKind {
-        Post,
-        Remove,
-    }
+    private sealed record EntryChange(ChatId ChatId, string Text, ImmutableArray<Attachment> Attachments, RelatedChatEntry? RelatedChatEntry);
 }
