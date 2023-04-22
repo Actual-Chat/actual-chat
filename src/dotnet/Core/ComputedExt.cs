7315cbd81a8c84e8d1d8e5bd8b45a20094d93818
Alex Yakunin <alex.yakunin@gmail.com>
2023-04-21 19:23:38 -0700

    feat: ComputedExt.cs - DebugDump

--- src/dotnet/Core/ComputedExt.cs
+++ src/dotnet/Core/ComputedExt.cs
@@ -1,3 +1,7 @@
+using Cysharp.Text;
+using Stl.Fusion.Internal;
+using Stl.Fusion.Operations.Internal;
+
 namespace ActualChat;
 
 public static class ComputedExt
@@ -68,4 +72,56 @@ public static class ComputedExt
             hasNextChangeTask = enumerator.MoveNextAsync();
         }
     }
+
+    // Debug dump
+
+    private static readonly ConcurrentDictionary<(Type, string), PropertyInfo?> _propertyCache = new();
+    private static readonly ConcurrentDictionary<(Type, string), FieldInfo?> _fieldCache = new();
+
+    public static string DebugDump(this IComputed computed)
+    {
+        var type = computed.GetType();
+        var pFlags = GetProperty(type, "Flags")!;
+
+        using var sb = ZString.CreateStringBuilder();
+        // var flags = pFlags.GetGetter<ComputedFlags>().Invoke(computed);
+        var flags = (ComputedFlags)pFlags.GetMethod!.Invoke(computed, Array.Empty<object?>())!;
+        sb.Append("Computed: ");
+        sb.AppendLine(computed.ToString()!);
+        sb.Append("- Flags: ");
+        sb.AppendLine(flags.ToString());
+        sb.AppendLine("- Dependencies:");
+        var impl = (IComputedImpl)computed;
+        foreach (var d in impl.Used) {
+            sb.Append("  - ");
+            sb.AppendLine(d.ToString()!);
+        }
+        return sb.ToString();
+    }
+
+    private static PropertyInfo? GetProperty(Type type, string name)
+        => _propertyCache.GetOrAdd((type, name), static state => {
+            var (type1, name1) = state;
+            while (type1 != null) {
+                var result = type1.GetProperty(name1, BindingFlags.Instance | BindingFlags.NonPublic);
+                if (result != null)
+                    return result;
+
+                type1 = type1.BaseType;
+            }
+            return null;
+        });
+
+    private static FieldInfo? GetField(Type type, string name)
+        => _fieldCache.GetOrAdd((type, name), static state => {
+            var (type1, name1) = state;
+            while (type1 != null) {
+                var result = type1.GetField(name1, BindingFlags.Instance | BindingFlags.NonPublic);
+                if (result != null)
+                    return result;
+
+                type1 = type1.BaseType;
+            }
+            return null;
+        });
 }
