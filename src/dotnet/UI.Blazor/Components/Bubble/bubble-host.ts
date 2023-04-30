50814bba104fa6b8c8f789142d6d13212e8add6c
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-04-30 23:22:46 +0200

    Rewrite

--- src/dotnet/UI.Blazor/Components/Bubble/bubble-host.ts
+++ src/dotnet/UI.Blazor/Components/Bubble/bubble-host.ts
@@ -1,53 +1,74 @@
-import {
-    Placement,
-    computePosition,
-    flip,
-    shift,
-    offset,
-    arrow,
-} from '@floating-ui/dom';
+import { arrow, computePosition, flip, offset, Placement, shift } from '@floating-ui/dom';
+import { Subject, debounceTime, startWith } from 'rxjs';
 import { Log } from 'logging';
 
-interface BubbleInfo {
+interface BubbleModel {
     bubbleRef: string;
-    order: number;
+    triggerElement: HTMLElement;
+    bubbleElement?: HTMLElement;
+    isInViewport: boolean;
+    isTopElement: boolean;
+    isRead: boolean;
+    isShown: boolean;
 }
 
 const { debugLog } = Log.get('BubbleHost');
 
 export class BubbleHost {
+    private readonly _bubbles: BubbleModel[] = [];
+
     public static create(blazorRef: DotNet.DotNetObject): BubbleHost {
         return new BubbleHost(blazorRef);
     }
 
     constructor(private readonly blazorRef: DotNet.DotNetObject) {
         debugLog?.log('constructor');
+
+        const domChanged$ = new Subject();
+        domChanged$
+            .pipe(
+                startWith(undefined),
+                debounceTime(1000),
+            )
+            .subscribe(() => {
+                this.updateBubbles();
+                this.showNextBubble();
+            });
+
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach(mutation => {
+                if (mutation.addedNodes.length || mutation.removedNodes.length) {
+                    domChanged$.next(undefined);
+                }
+            });
+        });
+        observer.observe(document.getElementById('app'), { subtree: true, childList: true });
     }
 
-    public getBubblesByGroup(group: string): BubbleInfo[] {
-        debugLog?.log(`getBubblesByGroup:`, group);
+    public async readBubble(bubbleRef: string): Promise<void> {
+        debugLog?.log(`readBubble:`, bubbleRef);
 
-        const elements = [...document.querySelectorAll(`[data-bubble-group="${group}"]`)];
-        return elements
-            .map((el: HTMLElement) => {
-                const bubbleInfo: BubbleInfo = {
-                    bubbleRef: el.dataset['bubble'],
-                    order: Number(el.dataset['bubbleOrder']),
-                };
-                return bubbleInfo;
-            });
+        const bubble = this._bubbles.find(x => x.bubbleRef === bubbleRef);
+        bubble.isRead = true;
+
+        this.showNextBubble();
     }
 
     public async showBubble(id: string, bubbleRef: string): Promise<void> {
         debugLog?.log(`showBubble:`, id, bubbleRef);
 
-        const triggerElement: HTMLElement = document.querySelector(`[data-bubble="${bubbleRef}"]`);
+        const bubble = this._bubbles.find(x => x.bubbleRef === bubbleRef);
+        bubble.isShown = true;
+
+        const triggerElement = bubble.triggerElement;
         const position = triggerElement.dataset['bubblePlacement'] as Placement;
         const bubbleElement = document.getElementById(id);
+        bubble.bubbleElement = bubbleElement;
+
         const arrowElement = document.getElementsByClassName('ac-bubble-arrow')[0] as HTMLElement;
 
         if (bubbleElement.style.display != 'block')
-            bubbleElement.style.display = 'block'
+            bubbleElement.style.display = 'block';
 
         const { x, y, placement, middlewareData } = await computePosition(
             triggerElement,
@@ -82,5 +103,85 @@ export class BubbleHost {
             bottom: '',
             [staticSide]: '-4px',
         });
+    }
+
+    private updateBubbles(): void {
+        debugLog?.log(`updateBubbles`);
+
+        const elements = this.getBubbleElements();
+        elements.forEach(el => {
+            const bubbleRef = el.dataset['bubble'];
+            const bubble = this._bubbles.find(x => x.bubbleRef === bubbleRef);
+            if (bubble) {
+                if (bubble.isRead)
+                    return;
+
+                bubble.isInViewport = this.isElementInViewport(el);
+                bubble.isTopElement = this.isTopElement(el);
+
+                return;
+            }
+
+            const newBubble: BubbleModel = {
+                bubbleRef: bubbleRef,
+                triggerElement: el,
+                isInViewport: this.isElementInViewport(el),
+                isTopElement: this.isTopElement(el),
+                isRead: false,
+                isShown: false,
+            };
+            this._bubbles.push(newBubble);
+        });
+    }
+
+    private showNextBubble(): void {
+        debugLog?.log(`showNextBubble`);
+
+        const notReadBubbles = this._bubbles.filter(x => !x.isRead);
+        const shownBubble = notReadBubbles.find(x => x.isShown);
+        console.log('shown', shownBubble?.bubbleRef);
+        if (shownBubble) {
+            if (!shownBubble.isInViewport || !shownBubble.isTopElement) {
+                shownBubble.isShown = false;
+                shownBubble.bubbleElement.style.display = 'none';
+            }
+
+            return;
+        }
+
+        const bubbleToShow = notReadBubbles.find(x => x.isInViewport && x.isTopElement);
+        if (!bubbleToShow)
+            return;
+
+        this.show(bubbleToShow);
+    }
+
+    private show(bubble: BubbleModel): void {
+        debugLog?.log(`show:`, bubble.bubbleRef);
+
+        void this.blazorRef.invokeMethodAsync('OnShow', bubble.bubbleRef);
+    }
+
+    private isElementInViewport(element: HTMLElement): boolean {
+        const rect = element.getBoundingClientRect();
+        const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+        return rect.bottom >= 0 && rect.top - viewHeight < 0;
+    }
+
+    private isTopElement(element: HTMLElement): boolean {
+        const rect = element.getBoundingClientRect();
+        const topElement = document.elementFromPoint(rect.left, rect.top);
+        if (element.isSameNode(topElement))
+            return true;
+
+        if (topElement == null)
+            return false;
+
+        return topElement.contains(element);
+    }
+
+    private getBubbleElements(): HTMLElement[] {
+        const elements = [...document.querySelectorAll('[data-bubble]')];
+        return elements as HTMLElement[];
     }
 }
