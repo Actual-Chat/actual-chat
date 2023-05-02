0a1491ed0e899b2b8a128162e96f32bd2a8e44c3
Dmitry Filippov <crui3er@gmail.com>
2023-05-02 18:01:56 +0400

    feat: Join to chat anonymously. Add migrations

---
+++ src/dotnet/Users.Service.Migration/Migrations/20230502135455_JoinAnonymously.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Users.Migrations
+{
+    /// <inheritdoc />
+    public partial class JoinAnonymously : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_anonymous",
+                table: "avatars",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_anonymous",
+                table: "avatars");
+        }
+    }
+}
