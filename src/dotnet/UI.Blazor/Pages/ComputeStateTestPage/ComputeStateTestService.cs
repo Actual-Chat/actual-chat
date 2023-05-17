d3b904297c7db5e3267186d25ac5013450bfc4c7
Alexey Kochetov <alexis.kochetov@gmail.com>
2023-05-17 18:00:36 +0300

    chore: fix warnings

--- src/dotnet/UI.Blazor/Pages/ComputeStateTestPage/ComputeStateTestService.cs
+++ src/dotnet/UI.Blazor/Pages/ComputeStateTestPage/ComputeStateTestService.cs
@@ -19,18 +19,16 @@ public class ComputeStateTestService : IHasServices, IComputeService, INotifyIni
     public void Initialized()
     { }
 
-    public async Task<string> GetValue(CancellationToken cancellationToken)
-    {
+    public Task<string> GetValue(CancellationToken cancellationToken)
         // var latestValue = await _state.Use(cancellationToken).ConfigureAwait(false);
         // return latestValue;
-        return _state.Value;
-    }
+        => Task.FromResult(_state.Value);
 
     public async Task MutateAndInvalidate(string finalValue, CancellationToken cancellationToken)
     {
         for (int i = 0; i < 100; i++) {
             if (i % 10 == 0)
-                await Task.Delay(100, cancellationToken);
+                await Task.Delay(100, cancellationToken).ConfigureAwait(false);
 
             _state.Value = i.ToString(CultureInfo.InvariantCulture);
         }
