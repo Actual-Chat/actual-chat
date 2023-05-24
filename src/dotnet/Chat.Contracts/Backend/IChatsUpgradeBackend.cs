afb358cc9d668baeb9d6aaf2d554dee282f99248
Alexey Kochetov <alexis.kochetov@gmail.com>
2023-05-24 16:00:31 +0300

    feat: create feedback template chat

--- src/dotnet/Chat.Contracts/Backend/IChatsUpgradeBackend.cs
+++ src/dotnet/Chat.Contracts/Backend/IChatsUpgradeBackend.cs
@@ -7,6 +7,8 @@ public interface IChatsUpgradeBackend : ICommandService
     [CommandHandler]
     Task<Chat> CreateDefaultChat(CreateDefaultChatCommand command, CancellationToken cancellationToken);
     [CommandHandler]
+    Task<Chat> CreateFeedbackTemplateChat(CreateFeedbackTemplateChatCommand command, CancellationToken cancellationToken);
+    [CommandHandler]
     Task UpgradeChat(UpgradeChatCommand command, CancellationToken cancellationToken);
     [CommandHandler]
     Task FixCorruptedReadPositions(FixCorruptedReadPositionsCommand command, CancellationToken cancellationToken);
@@ -18,6 +20,11 @@ public interface IChatsUpgradeBackend : ICommandService
     [DataContract]
     public sealed record CreateDefaultChatCommand(
     ) : ICommand<Chat>, IBackendCommand;
+
+    [DataContract]
+    public sealed record CreateFeedbackTemplateChatCommand(
+    ) : ICommand<Chat>, IBackendCommand;
+
 
     public sealed record UpgradeChatCommand(
         [property: DataMember] ChatId ChatId
