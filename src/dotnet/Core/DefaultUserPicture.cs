abd9df6b470fc9e0195fde7a30ad1b75360c02b3
Frol <4425748+FrolYo@users.noreply.github.com>
2023-06-29 22:14:24 +0300

    feat: boring avatars cache #1277

--- src/dotnet/Core/DefaultUserPicture.cs
+++ src/dotnet/Core/DefaultUserPicture.cs
@@ -3,10 +3,11 @@ namespace ActualChat;
 public static class DefaultUserPicture
 {
     public const int DefaultSize = 160;
+    public const string BoringAvatarsBaseUrl = "https://source.boringavatars.com/";
 
     public static string Get(string name, int size = DefaultSize)
         => GetBoringAvatar(name.GetSHA1HashCode(), size);
 
     public static string GetBoringAvatar(string hash, int size = DefaultSize)
-        => $"https://source.boringavatars.com/beam/{size}/{hash.UrlEncode()}?colors=FFDBA0,BBBEFF,9294E1,FF9BC0,0F2FE8";
+        => $"{BoringAvatarsBaseUrl}beam/{size}/{hash.UrlEncode()}?colors=FFDBA0,BBBEFF,9294E1,FF9BC0,0F2FE8";
 }
