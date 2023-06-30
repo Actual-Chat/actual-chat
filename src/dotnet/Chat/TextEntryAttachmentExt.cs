4c91d1df0934da54da2a2298832ba6fdd67958ac
Frol <4425748+FrolYo@users.noreply.github.com>
2023-06-30 22:37:45 +0300

    fix: skip image resize for gifs due to image proxy issues #1284

--- src/dotnet/Chat/TextEntryAttachmentExt.cs
+++ src/dotnet/Chat/TextEntryAttachmentExt.cs
@@ -4,6 +4,8 @@ public static class TextEntryAttachmentExt
 {
     public static bool IsImage(this TextEntryAttachment attachment)
         => attachment.Media.ContentType.OrdinalIgnoreCaseStartsWith("image");
+    public static bool IsGif(this TextEntryAttachment attachment)
+        => OrdinalIgnoreCaseEquals(attachment.Media.ContentType, "image/gif");
     public static bool IsVideo(this TextEntryAttachment attachment)
         => attachment.Media.ContentType.OrdinalIgnoreCaseStartsWith("video");
     public static bool IsVisualMedia(this TextEntryAttachment attachment)
