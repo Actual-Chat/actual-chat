ac83af250bb7f2b23a5d0ca67180373d017d69e9
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-07-04 08:17:21 +0200

    fix: modals close in the wrong order

--- src/nodejs/src/keyboard.ts
+++ src/nodejs/src/keyboard.ts
@@ -1,3 +1,7 @@
 export function hasModifierKey(event: KeyboardEvent | MouseEvent | WheelEvent): boolean {
     return event.altKey || event.shiftKey || event.ctrlKey || event.metaKey;
 }
+
+export function isEscapeKey(event: KeyboardEvent): boolean {
+    return event.keyCode === 27 || event.key === 'Escape' || event.key === 'Esc';
+}
