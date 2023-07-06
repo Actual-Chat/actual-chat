5aa4a44306125499e1959bf4da7afcd62c6ff1d4
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-07-06 23:17:11 +0200

    fix: AppLink does not work

--- src/dotnet/App.Maui/Platforms/iOS/AppDelegate.cs
+++ src/dotnet/App.Maui/Platforms/iOS/AppDelegate.cs
@@ -1,4 +1,6 @@
+using CoreSpotlight;
 using Foundation;
+using UIKit;
 
 namespace ActualChat.App.Maui;
 
@@ -9,5 +11,37 @@ public class AppDelegate : MauiUIApplicationDelegate
     {
         NSHttpCookieStorage.SharedStorage.AcceptPolicy = NSHttpCookieAcceptPolicy.Always;
         return MauiProgram.CreateMauiApp();
+    }
+
+    public override bool ContinueUserActivity(
+        UIApplication application,
+        NSUserActivity userActivity,
+        UIApplicationRestorationHandler completionHandler)
+    {
+        CheckForAppLink(userActivity);
+        return base.ContinueUserActivity(application, userActivity, completionHandler);
+    }
+
+    private void CheckForAppLink(NSUserActivity userActivity)
+    {
+        var strLink = string.Empty;
+
+        switch (userActivity.ActivityType)
+        {
+            case "NSUserActivityTypeBrowsingWeb":
+                strLink = userActivity.WebPageUrl.AbsoluteString;
+                break;
+            case "com.apple.corespotlightitem":
+                if (userActivity.UserInfo?.ContainsKey(CSSearchableItem.ActivityIdentifier) == true)
+                    strLink = userActivity.UserInfo.ObjectForKey(CSSearchableItem.ActivityIdentifier).ToString();
+                break;
+            default:
+                if (userActivity.UserInfo?.ContainsKey(new NSString("link")) == true)
+                    strLink = userActivity.UserInfo[new NSString("link")].ToString();
+                break;
+        }
+
+        if (!string.IsNullOrEmpty(strLink))
+            App.Current.SendOnAppLinkRequestReceived(new Uri(strLink));
     }
 }
