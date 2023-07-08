88f0c0a7036313df0675fb04711825a00863bac9
Frol <4425748+FrolYo@users.noreply.github.com>
2023-07-08 17:14:13 +0300

    feat: win-publish - version from nbgv

--- build/AppxManifestGenerator.cs
+++ build/AppxManifestGenerator.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -6,11 +7,34 @@ namespace Build;
 
 public static class AppxManifestGenerator
 {
-    public static Task Generate(bool isProduction, CancellationToken cancellationToken)
-        => Generate("",
+    public static async Task Generate(bool isProduction, CancellationToken cancellationToken)
+    {
+        var version= await GetVersion(cancellationToken);
+        await Generate($"{version.ToString(3)}.0",
             isProduction ? "" : ".Dev",
             isProduction ? "" : " Dev",
             cancellationToken);
+    }
+
+    private static async Task<Version> GetVersion(CancellationToken cancellationToken)
+    {
+        var process = Process.Start(new ProcessStartInfo("nbgv", "get-version -v NuGetPackageVersion") {
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+        });
+        if (process?.HasExited != false)
+            throw new InvalidOperationException("Failed to start run nbgv command.");
+
+        await process.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
+        var output = await process.StandardOutput.ReadToEndAsync(cancellationToken).ConfigureAwait(false);
+        var error = await process.StandardError.ReadToEndAsync(cancellationToken).ConfigureAwait(false);
+        if (process.ExitCode != 0)
+            throw new InvalidOperationException($"Failed to get version via nbgv command: {error}. {output}");
+
+        return Version.Parse(output);
+    }
 
     public static async Task Generate(
         string version,
@@ -22,8 +46,8 @@ public static class AppxManifestGenerator
         var (doc, xns) = await Read(manifestPath, cancellationToken);
 
         if (!string.IsNullOrEmpty(version))
-            UpdateAttr("//Package/Identity", "Version",version);
-        UpdateAttr("//Package/Identity", "Name",$"ActualChatInc.ActualChat{packageIdentityNameSuffix}");
+            UpdateAttr("//default:Package/default:Identity", "Version",version);
+        UpdateAttr("//default:Package/default:Identity", "Name",$"ActualChatInc.ActualChat{packageIdentityNameSuffix}");
         UpdateAttr("//uap5:StartupTask", "DisplayName", $"Actual Chat{startupTaskDisplayNameSuffix}");
 
         await Write(manifestPath, doc, cancellationToken);
@@ -31,7 +55,7 @@ public static class AppxManifestGenerator
         void UpdateAttr(string xpath, string attrName, string attrValue)
         {
             var element = doc.XPathSelectElement(xpath, xns);
-            element?.SetAttributeValue(attrName, attrValue);
+            element!.SetAttributeValue(attrName, attrValue);
         }
     }
 
@@ -48,6 +72,7 @@ public static class AppxManifestGenerator
         var xDocument = await XDocument.LoadAsync(input, LoadOptions.PreserveWhitespace, cancellationToken);
         var nsManager = new XmlNamespaceManager(new NameTable());
         nsManager.AddNamespace("", "http://schemas.microsoft.com/appx/manifest/foundation/windows10");
+        nsManager.AddNamespace("default", "http://schemas.microsoft.com/appx/manifest/foundation/windows10");
         nsManager.AddNamespace("uap5", "http://schemas.microsoft.com/appx/manifest/uap/windows10/5");
         return (xDocument, nsManager);
     }
