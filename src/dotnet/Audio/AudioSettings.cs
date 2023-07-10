c92fdd5d34164a971221ee0d9c8553eb2ddf45a3
Frol <4425748+FrolYo@users.noreply.github.com>
2023-07-10 09:34:14 +0300

    feat: increased idle recording/listening timeouts #1313

--- src/dotnet/Audio/AudioSettings.cs
+++ src/dotnet/Audio/AudioSettings.cs
@@ -5,9 +5,9 @@ public sealed class AudioSettings
     public string Redis { get; set; } = "";
     public string ServiceName { get; set; } = "actual-chat-app-service";
     public string Namespace { get; set; } = "default";
-    public TimeSpan IdleRecordingTimeout { get; set; } = TimeSpan.FromMinutes(1.5);
-    public TimeSpan IdleRecordingTimeoutBeforeCountdown { get; set; } = TimeSpan.FromMinutes(1);
+    public TimeSpan IdleRecordingTimeout { get; set; } = TimeSpan.FromMinutes(3);
+    public TimeSpan IdleRecordingTimeoutBeforeCountdown { get; set; } = TimeSpan.FromMinutes(2.5);
     public TimeSpan IdleRecordingCheckInterval { get; set; } = TimeSpan.FromSeconds(1);
-    public TimeSpan IdleListeningTimeout { get; set; } = TimeSpan.FromMinutes(3);
+    public TimeSpan IdleListeningTimeout { get; set; } = TimeSpan.FromMinutes(5);
     public TimeSpan IdleListeningCheckInterval { get; set; } = TimeSpan.FromSeconds(3); // not critical to stop at exact time for playback
 }
