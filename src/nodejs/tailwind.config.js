ed3ca8ae01e2347bdc294816f8b4cabb41f51e78
Alex Yakunin <alex.yakunin@gmail.com>
2023-07-12 02:21:41 -0700

    docs: improve landing

--- src/nodejs/tailwind.config.js
+++ src/nodejs/tailwind.config.js
@@ -676,6 +676,7 @@ module.exports = {
       xl: ['1.25rem', { lineHeight: '1.75rem' }],
       '2xl': ['1.5rem', { lineHeight: '2rem' }],
       '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '3.5xl': ['2rem', { lineHeight: '2.5rem' }],
       '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
       '4.5xl': ['2.5rem', { lineHeight: '3rem' }],
       '5xl': ['3rem', { lineHeight: '1' }],
