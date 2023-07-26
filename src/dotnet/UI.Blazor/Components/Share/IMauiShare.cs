c3abb510ca213a2ea0fa6cda0de5bdf3943c4818
Dmitry Filippov <crui3er@gmail.com>
2023-07-26 17:32:27 +0400

    chore: update Share ui with comments from AlexY

--- src/dotnet/UI.Blazor/Components/Share/IMauiShare.cs
+++ src/dotnet/UI.Blazor/Components/Share/IMauiShare.cs
@@ -2,6 +2,6 @@
 
 public interface IMauiShare
 {
-    public Task ShareLink(string title, string link);
+    public Task ShareLink(string title, string link, string text = "");
     public Task ShareText(string title, string text);
 }
