0a5647d892a011ac7f70589ed7cdb634e7dc46f5
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-08-04 07:10:44 +0200

    feat: add ability to subscribe once to escape events

--- src/dotnet/UI.Blazor/Services/Escapist/EscapistSubscription.cs
+++ src/dotnet/UI.Blazor/Services/Escapist/EscapistSubscription.cs
@@ -5,16 +5,19 @@ namespace ActualChat.UI.Blazor.Services;
 public class EscapistSubscription : IAsyncDisposable
 {
     private Action? _action;
+    private bool _once;
     private DotNetObjectReference<EscapistSubscription>? _blazorRef;
     private IJSObjectReference? _jsRef;
 
     public static async ValueTask<IAsyncDisposable> Create(
         IJSRuntime js,
-        Action? action,
+        Action action,
+        bool once,
         CancellationToken cancellationToken)
     {
-        var subscription = new EscapistSubscription() {
-            _action = action
+        var subscription = new EscapistSubscription {
+            _action = action,
+            _once = once,
         };
         subscription._blazorRef = DotNetObjectReference.Create(subscription);
         subscription._jsRef = await js.InvokeAsync<IJSObjectReference>(
@@ -42,5 +45,10 @@ public class EscapistSubscription : IAsyncDisposable
     }
 
     [JSInvokable]
-    public void OnEscape() => _action?.Invoke();
+    public void OnEscape() {
+        _action?.Invoke();
+
+        if (_once)
+            _ = DisposeAsync().ConfigureAwait(false);
+    }
 }
