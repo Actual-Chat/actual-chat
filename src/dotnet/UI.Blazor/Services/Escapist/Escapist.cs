99a801b6d138cac8491dbe20889a8b0cc600ce6c
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-08-04 07:10:44 +0200

    feat: add ability to subscribe once to escape events

--- src/dotnet/UI.Blazor/Services/Escapist/Escapist.cs
+++ src/dotnet/UI.Blazor/Services/Escapist/Escapist.cs
@@ -7,6 +7,9 @@ public sealed class Escapist
     public Escapist(IJSRuntime js)
         => JS = js;
 
-    public ValueTask<IAsyncDisposable> Subscribe(Action? action, CancellationToken cancellationToken = default)
-        => EscapistSubscription.Create(JS, action, cancellationToken);
+    public ValueTask<IAsyncDisposable> Subscribe(Action action, CancellationToken cancellationToken = default)
+        => EscapistSubscription.Create(JS, action, once: false, cancellationToken);
+
+    public ValueTask<IAsyncDisposable> SubscribeOnce(Action action, CancellationToken cancellationToken = default)
+        => EscapistSubscription.Create(JS, action, once: true, cancellationToken);
 }
