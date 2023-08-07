a6e793a7af6150ae356691989692e529b3c38d72
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-08-07 07:50:35 +0200

    feat: add ability to select message by click

---
+++ src/dotnet/Chat.UI.Blazor/Components/Selection/selection-host.ts
@@ -0,0 +1,73 @@
+import { Subject, takeUntil } from 'rxjs';
+import { Log } from 'logging';
+import { Disposable } from 'disposable';
+import { DocumentEvents, preventDefaultForEvent } from 'event-handling';
+import { getOrInheritData } from 'dom-helpers';
+
+const { debugLog } = Log.get('SelectionHost');
+
+export class SelectionHost implements Disposable {
+    private readonly disposed$ = new Subject<void>();
+    private readonly selection: Set<string> = new Set<string>();
+
+    public static create(
+        blazorRef: DotNet.DotNetObject,
+        chatEntryId: string): SelectionHost {
+        return new SelectionHost(blazorRef, chatEntryId);
+    }
+
+    constructor(
+        private readonly blazorRef: DotNet.DotNetObject,
+        private readonly chatEntryId: string,
+    ) {
+        debugLog?.log('constructor');
+        this.selection.add(chatEntryId);
+
+        DocumentEvents.capturedActive.click$
+            .pipe(takeUntil(this.disposed$))
+            .subscribe(async (x: MouseEvent) => {
+                await this.onClick(x);
+            });
+    }
+
+    public dispose() {
+        if (this.disposed$.isStopped)
+            return;
+
+        this.disposed$.next();
+        this.disposed$.complete();
+    }
+
+    private async onClick(event: MouseEvent): Promise<void> {
+        debugLog?.log('onClick, event:', event);
+
+        if (!(event.target instanceof Element))
+            return;
+
+        let [triggerElement, chatEntryId] = getOrInheritData(event.target, 'chatEntryId');
+        if (!triggerElement || !chatEntryId)
+            return;
+
+        preventDefaultForEvent(event);
+
+        if (this.selection.has(chatEntryId)) {
+            debugLog?.log('onUnselect, chatEntryId:', chatEntryId);
+
+            if (triggerElement.classList.contains('replied-message')) {
+                triggerElement.classList.remove('replied-message');
+            }
+
+            this.selection.delete(chatEntryId);
+            await this.blazorRef.invokeMethodAsync('OnUnselect', chatEntryId);
+            return;
+        }
+
+        debugLog?.log('onSelect, chatEntryId:', chatEntryId);
+
+        this.selection.add(chatEntryId);
+        if (!triggerElement.classList.contains('replied-message')) {
+            triggerElement.classList.remove('replied-message');
+        }
+        await this.blazorRef.invokeMethodAsync('OnSelect', chatEntryId);
+    }
+}
--- src/dotnet/Chat.UI.Blazor/exports.ts
+++ src/dotnet/Chat.UI.Blazor/exports.ts
@@ -3,5 +3,5 @@ export * from './Components/ChatMessageEditor/chat-message-editor';
 export * from './Components/MarkupEditor/markup-editor';
 export * from './Components/MarkupParts/CodeBlockMarkupView/code-block-markup-view';
 export * from './Components/MentionList/mention-list';
+export * from './Components/Selection/selection-host';
 export * from './Services/LanguageUI/language-ui';
-
