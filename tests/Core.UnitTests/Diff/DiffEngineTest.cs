6507bb33c66155c22c21a46213fd84c56e18ad7e
Frol <4425748+FrolYo@users.noreply.github.com>
2023-08-18 15:35:14 +0300

    chore: DiffEngineTest #1355

---
+++ tests/Core.UnitTests/Diff/DiffEngineTest.cs
@@ -0,0 +1,53 @@
+﻿using ActualChat.Diff;
+
+namespace ActualChat.Core.UnitTests.Diff;
+
+public class DiffEngineTest
+{
+    [Fact]
+    public void ShouldDetectNoChange()
+    {
+        var source = new Data("1", "Jack");
+        var target = new Data("1", "Jack");
+        var diff = DiffEngine.Default.Diff<Data, DataDiff>(source, target);
+        diff.Should().BeEquivalentTo(DataDiff.Empty);
+    }
+
+    [Fact]
+    public void ShouldDetectNameChange()
+    {
+        // given
+        var source = new Data("1", "Jack");
+        var target = new Data("1", "John");
+
+        // when
+        var diff = DiffEngine.Default.Diff<Data, DataDiff>(source, target);
+
+        // then
+        diff.Should().BeEquivalentTo(new DataDiff { Name = "John" });
+    }
+
+    [Fact]
+    public void ShouldDetectAllChanges()
+    {
+        // given
+        var source = new Data("1", "Jack");
+        var target = new Data("2", "John");
+
+        // when
+        var diff = DiffEngine.Default.Diff<Data, DataDiff>(source, target);
+
+        // then
+        diff.Should().BeEquivalentTo(new DataDiff { Id = "2", Name = "John" });
+    }
+
+    private record Data(Symbol Id, string Name);
+
+    private sealed record DataDiff : RecordDiff
+    {
+        public static readonly DataDiff Empty = new ();
+
+        public Symbol? Id { get; init; }
+        public string? Name { get; init; }
+    }
+}
