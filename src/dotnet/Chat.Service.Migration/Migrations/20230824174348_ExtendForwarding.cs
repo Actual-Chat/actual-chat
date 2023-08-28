86e415a8d887b2d0f0fc8b559cf6ad2a335fe731
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-08-28 11:42:38 +0200

    feat: extended forwarding migration

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20230824174348_ExtendForwarding.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendForwarding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "forwarded_author_name",
+                table: "chat_entries",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "forwarded_chat_entry_begins_at",
+                table: "chat_entries",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "forwarded_chat_title",
+                table: "chat_entries",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "forwarded_author_name",
+                table: "chat_entries");
+
+            migrationBuilder.DropColumn(
+                name: "forwarded_chat_entry_begins_at",
+                table: "chat_entries");
+
+            migrationBuilder.DropColumn(
+                name: "forwarded_chat_title",
+                table: "chat_entries");
+        }
+    }
+}
