f4d14dc7ab90617658cc0ac6a6f65034d6538561
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-08-29 22:46:25 +0200

    feat: add ability to write text to ClipboardUI

--- src/dotnet/UI.Blazor/Services/ClipboardUI/ClipboardUI.cs
+++ src/dotnet/UI.Blazor/Services/ClipboardUI/ClipboardUI.cs
@@ -14,6 +14,9 @@ public class ClipboardUI
     public virtual ValueTask<string> ReadText()
         => JS.InvokeAsync<string>("navigator.clipboard.readText");
 
+    public virtual ValueTask WriteText(string text)
+        => JS.InvokeVoidAsync("navigator.clipboard.writeText", text);
+
     protected virtual ValueTask<string> GetTextFrom(ElementReference inputRef)
         => JS.InvokeAsync<string>(JSSelectAndGetMethod, inputRef);
 }
