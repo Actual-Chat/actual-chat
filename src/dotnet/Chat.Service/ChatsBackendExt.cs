367777759c45c5c29b5167498b3a934d358f85f9
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-08-31 22:18:55 +0200

    feat: bulk message deletion

--- src/dotnet/Chat.Service/ChatsBackendExt.cs
+++ src/dotnet/Chat.Service/ChatsBackendExt.cs
@@ -19,4 +19,22 @@ public static class ChatsBackendExt
             .ConfigureAwait(false);
         return tile.Entries.SingleOrDefault(e => e.LocalId == entryId.LocalId);
     }
+
+    public static async ValueTask<ChatEntry?> GetRemovedEntry(
+        this IChatsBackend chatsBackend,
+        ChatEntryId entryId,
+        CancellationToken cancellationToken = default)
+    {
+        if (entryId.IsNone)
+            return null;
+
+        var idTile = Constants.Chat.IdTileStack.FirstLayer.GetTile(entryId.LocalId);
+        var tile = await chatsBackend.GetTile(entryId.ChatId,
+                entryId.Kind,
+                idTile.Range,
+                true,
+                cancellationToken)
+            .ConfigureAwait(false);
+        return tile.Entries.SingleOrDefault(e => e.LocalId == entryId.LocalId);
+    }
 }
