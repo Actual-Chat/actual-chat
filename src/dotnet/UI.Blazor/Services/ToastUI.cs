b258817a60641a8b0fc4ee9e55e98d3d79e05020
Dmitry Filippov <crui3er@gmail.com>
2023-09-05 15:53:50 +0400

    feat: use predefined values for toast auto dismiss delay.

--- src/dotnet/UI.Blazor/Services/ToastUI.cs
+++ src/dotnet/UI.Blazor/Services/ToastUI.cs
@@ -1,25 +1,36 @@
 ﻿namespace ActualChat.UI.Blazor.Services;
 
+public enum ToastDismissDelay { Short, Long }
+
 public class ToastUI
 {
     private readonly MutableList<InfoToastModel> _items = new ();
 
     public IReadOnlyMutableList<InfoToastModel> Items => _items;
 
-    public void Show(string info, string icon = "", double? autoDismissDelay = null)
+    public void Show(string info, ToastDismissDelay autoDismissDelay)
+        => ShowInternal(info, "", null, "", autoDismissDelay);
+
+    public void Show(string info, string icon, ToastDismissDelay autoDismissDelay)
         => ShowInternal(info, icon, null, "", autoDismissDelay);
 
-    public void Show(string info, Action action, string actionText, double? autoDismissDelay = null)
+    public void Show(string info, Action action, string actionText, ToastDismissDelay autoDismissDelay)
         => ShowInternal(info, "", action, actionText, autoDismissDelay);
 
-    public void Show(string info, string icon, Action action, string actionText, double? autoDismissDelay = null)
+    public void Show(string info, string icon, Action action, string actionText, ToastDismissDelay autoDismissDelay)
         => ShowInternal(info, icon, action, actionText, autoDismissDelay);
 
     public bool Dismiss(InfoToastModel toastInfo)
         => _items.Remove(toastInfo);
 
-    private void ShowInternal(string info, string icon, Action? action, string actionText, double? autoDismissDelay)
-        => _items.Add(new InfoToastModel(info, icon, action, actionText, autoDismissDelay ?? 60));
+    private void ShowInternal(string info, string icon, Action? action, string actionText, ToastDismissDelay autoDismissDelay)
+        => _items.Add(new InfoToastModel(info, icon, action, actionText, GetDelay(autoDismissDelay)));
+
+    private double? GetDelay(ToastDismissDelay autoDismissDelay)
+        => autoDismissDelay switch {
+            ToastDismissDelay.Short => 3,
+            _ => 5
+        };
 }
 
 // Must be class, not record, to avoid issue with blazor @key comparison.
