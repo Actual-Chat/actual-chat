5a6efe510c9e85c9210d1f32ecbfe2bca80ffd64
Alexey Kochetov <alexis.kochetov@gmail.com>
2023-09-08 20:26:54 +0300

    fix: more stable fallback player for ios and AEC workaround for chromium android

--- src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/fallback-playback.ts
+++ src/dotnet/Audio.UI.Blazor/Components/AudioPlayer/fallback-playback.ts
@@ -1,7 +1,6 @@
 import { Log } from 'logging';
 import { FeederAudioWorkletNode } from './worklets/feeder-audio-worklet-node';
 import { DeviceInfo } from 'device-info';
-import { audioContextSource } from '../../Services/audio-context-source';
 import { PromiseSource } from 'promises';
 import { createWebRtcAecStream, isWebRtcAecRequired } from './web-rtc-aec';
 import { Disposable } from 'disposable';
@@ -12,7 +11,6 @@ export class FallbackPlayback {
     private readonly audio: HTMLAudioElement;
     private destinationNode: MediaStreamAudioDestinationNode = null;
     private aecStream: MediaStream & Disposable = null;
-    private attachedCount = 0;
     private whenReady: PromiseSource<void> = new PromiseSource<void>();
 
     private get audioStream() { return this.aecStream ?? this.destinationNode.stream; }
@@ -27,10 +25,8 @@ export class FallbackPlayback {
         this.audio.preload = "none";
         this.audio.loop = false;
         this.audio.hidden = true;
-        this.audio.muted = true;
+        this.audio.muted = false;
         document.body.append(this.audio);
-        audioContextSource.contextCreated$.subscribe(x => this.onContextCreated(x));
-        audioContextSource.contextClosing$.subscribe(() => this.onContextClosing());
 
         document.body.addEventListener(
             'click',
@@ -43,21 +39,23 @@ export class FallbackPlayback {
             { capture: true, passive: false, once: true });
     }
 
-    public async attach(feederNode: FeederAudioWorkletNode, context: AudioContext) {
+    public async attach(context: AudioContext): Promise<void> {
         if (!this.isRequired)
             return;
 
-        debugLog?.log('-> attach(): attachedCount:', this.attachedCount, Log.ref(context), Log.ref(feederNode));
+        debugLog?.log('-> attach(): ', Log.ref(context));
 
         try {
-            if (this.attachedCount++ <= 0) {
+            if (!this.destinationNode || this.destinationNode.context !== context) {
+                this.destinationNode = context.createMediaStreamDestination();
+                this.destinationNode.channelCountMode = 'max';
+                this.destinationNode.channelCount = 2;
+                this.destinationNode.channelInterpretation = 'speakers';
+                if (isWebRtcAecRequired)
+                    this.aecStream = await createWebRtcAecStream(this.destinationNode.stream);
                 this.audio.srcObject = this.audioStream;
-                this.audio.muted = false;
-                await this.play();
-                debugLog?.log('attach(): success')
             }
-
-            feederNode.connect(this.destinationNode);
+            debugLog?.log('attach(): success')
         } catch (e) {
             errorLog?.log('attach(): failed to connect feeder node to fallback output:', e);
         }
@@ -70,11 +68,16 @@ export class FallbackPlayback {
 
         debugLog?.log('-> detach()');
         try {
-            if (--this.attachedCount <= 0) {
-                debugLog?.log('detach(): removing audio.srcObject');
-                this.audio.muted = true;
-                this.audio.pause();
-                this.audio.srcObject = null;
+            debugLog?.log('detach(): removing audio.srcObject');
+            this.audio.pause();
+            this.audio.srcObject = null;
+            this.destinationNode.stream.getAudioTracks().forEach(x => x.stop());
+            this.destinationNode.stream.getVideoTracks().forEach(x => x.stop());
+            this.destinationNode.disconnect();
+            this.destinationNode = null;
+            if (this.aecStream) {
+                this.aecStream.dispose();
+                this.aecStream = null;
             }
         } catch (e) {
             errorLog?.log('detach(): failed to disconnect feeder node from fallback output:', e);
@@ -82,75 +85,17 @@ export class FallbackPlayback {
         debugLog?.log('<- detach()');
     }
 
-    private async play() {
+    public async play(feederNode: FeederAudioWorkletNode): Promise<void> {
         try {
-            if (this.audio.paused) {
-                await this.whenReady;
-                await this.audio.play();
-                debugLog?.log('play(): successfully resumed');
-            } else
-                debugLog?.log('play(): already playing');
+            feederNode.connect(this.destinationNode);
+            this.audio.muted = false;
+            await this.audio.play();
         } catch (e) {
             errorLog?.log('play(): failed to resume:', e);
         }
     }
 
-    private async onContextCreated(context: AudioContext): Promise<void> {
-        if (!this.isRequired)
-            return;
-
-        debugLog?.log('-> onContextCreated()');
-        try {
-            this.destinationNode = null;
-            this.aecStream = null;
-
-            const destinationNode = this.destinationNode = context.createMediaStreamDestination();
-            this.destinationNode.channelCountMode = 'max';
-            this.destinationNode.channelCount = 2;
-            this.destinationNode.channelInterpretation = 'speakers';
-            if (isWebRtcAecRequired) {
-                const aecStream = await createWebRtcAecStream(this.destinationNode.stream);
-                if (destinationNode !== this.destinationNode) {
-                    // Concurrent onContextCreated is already creating aecStream for a newer destinationNode
-                    aecStream?.dispose();
-                    return;
-                }
-                this.aecStream = aecStream;
-            }
-            if (this.attachedCount > 0) {
-                debugLog?.log('onContextCreated: replacing audio.srcObject with:', this.audioStream);
-                this.audio.srcObject = this.audioStream;
-            }
-        } catch (e) {
-            errorLog?.log('onContextCreated(): failed to create destination node:', e)
-        }
-        debugLog?.log('<- onContextCreated()');
-    }
-
-    private onContextClosing() {
-        if (!this.isRequired)
-            return;
-
-        debugLog?.log('-> onContextClosing()');
-        try {
-            this.audio.muted = true;
-            this.audio.pause();
-            this.audio.srcObject = null;
-            this.aecStream = null;
-            this.destinationNode.stream.getAudioTracks().forEach(x => x.stop());
-            this.destinationNode.stream.getVideoTracks().forEach(x => x.stop());
-            this.destinationNode = null;
-            if (this.aecStream) {
-                this.aecStream.dispose();
-                this.aecStream = null;
-            }
-        } catch (e) {
-            errorLog?.log('onContextClosing: failed to cleanup:', e)
-        }
-        debugLog?.log('<- onContextClosing()');
-    }
-
-    private async warmup() {
+    private async warmup(): Promise<void> {
         debugLog?.log('-> warmup()');
         try {
             const warmupTask = this.audio.play().then(() => this.audio.pause());
