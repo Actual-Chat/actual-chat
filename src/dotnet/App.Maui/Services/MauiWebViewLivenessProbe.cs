589af4f7dcd2c8363c3c75b815efbdf2741cb662
Dmitry Filippov <crui3er@gmail.com>
2023-09-11 14:32:55 +0400

    feat: improve MauiWebViewLivenessProbe to reduce false WebView deadness detections

--- src/dotnet/App.Maui/Services/MauiWebViewLivenessProbe.cs
+++ src/dotnet/App.Maui/Services/MauiWebViewLivenessProbe.cs
@@ -33,12 +33,18 @@ public class MauiWebViewLivenessProbe
 
     private async Task<bool> IsAlive(CancellationToken cancellationToken)
     {
-        var cts = cancellationToken.CreateLinkedTokenSource();
-        cts.CancelAfter(TimeSpan.FromMilliseconds(300));
+        // We give only 2000ms for one attempt to check aliveness, but not more than 300ms after
+        // we ensures that main thread is free to pump up js invoke messages to WebView.
+        // This optimization is need to reduce number of false WebView deadness detections.
+        var cts1 = new CancellationTokenSource(TimeSpan.FromMilliseconds(2000));
+        var cts2 = cancellationToken.LinkWith(cts1.Token);
         try {
-            var services = await ScopedServicesTask.WaitAsync(cts.Token).ConfigureAwait(false);
+            var services = await ScopedServicesTask.WaitAsync(cts2.Token).ConfigureAwait(false);
             var jsRuntime = services.GetRequiredService<IJSRuntime>();
-            var isAlive = await jsRuntime.Eval<bool>("true", cts.Token).ConfigureAwait(false);
+            _ = MainThread.InvokeOnMainThreadAsync(() => {
+                cts2.CancelAfter(TimeSpan.FromMilliseconds(300));
+            });
+            var isAlive = await jsRuntime.Eval<bool>("true", cts2.Token).ConfigureAwait(false);
             return isAlive;
         }
         catch (Exception e) {
