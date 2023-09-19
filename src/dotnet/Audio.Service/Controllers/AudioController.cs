be1fec0ca37f69c9f1158ae09b1c54036cad2284
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-09-19 10:00:28 +0200

    fix: historical playback does not work

--- src/dotnet/Audio.Service/Controllers/AudioController.cs
+++ src/dotnet/Audio.Service/Controllers/AudioController.cs
@@ -5,7 +5,7 @@ namespace ActualChat.Audio.Controllers;
 [ApiController, Route("api/audio")]
 public sealed class AudioController(IBlobStorageProvider blobs) : ControllerBase
 {
-    [HttpGet("{**blobId}")]
+    [HttpGet("download/{**blobId}")]
     [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Client, VaryByQueryKeys = new[] { "blobId" })]
     public async Task<ActionResult > Download(string blobId, CancellationToken cancellationToken)
     {
