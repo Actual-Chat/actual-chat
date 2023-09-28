78dc7cd9835e3248f771a0aab7be2117a1402ea4
Alex Yakunin <alex.yakunin@gmail.com>
2023-09-28 11:39:38 -0700

    chore: BatchingKvasTest.cs

--- tests/Core.UnitTests/Kvas/BatchingKvasTest.cs
+++ tests/Core.UnitTests/Kvas/BatchingKvasTest.cs
@@ -105,6 +105,7 @@ public class BatchingKvasTest : TestBase
 
         var s2a = stateFactory.NewKvasStored<string>(new(prefixedKvas, "s2"));
         await s2a.WhenRead;
+        await Task.Yield();
         s2a.Value.Should().Be("b");
         s2a.Value = "c";
         s2a.Value.Should().Be("c");
