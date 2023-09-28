78e8be37697db1ac44a057fb4ef85c2bbe9f047e
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-09-28 16:50:07 +0300

    feat: update Notes chat icon for existing users

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20230928133831_SetNotesChatIcon.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetNotesChatIcon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                update chats
+                set media_id = 'system-icons:notes'
+                where system_tag = 'notes'
+                  and media_id <> 'system-icons:notes'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
