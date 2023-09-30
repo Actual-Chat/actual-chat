a82196e4ed13abb542bd8643bf304b0146c5cbab
Alex Yakunin <alex.yakunin@gmail.com>
2023-09-30 11:31:35 -0700

    chore: fix UserStatusTest.cs

--- tests/Users.IntegrationTests/UserStatusTest.cs
+++ tests/Users.IntegrationTests/UserStatusTest.cs
@@ -54,6 +54,7 @@ public class UserStatusTest : AppHostTestBase
         foreach (var newStatus in newStatuses) {
             var newAccount = account with { Status = newStatus };
             await _tester.Commander.Call(new Accounts_Update(_adminSession, newAccount, account.Version));
+            await Task.Delay(100);
 
             // assert
             account = await GetOwnAccount();
