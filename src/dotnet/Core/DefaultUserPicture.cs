c2d182db1772c4d325d06338cda8b551c4d3fa64
Frol <4425748+FrolYo@users.noreply.github.com>
2023-10-07 15:19:53 +0300

    fix: boring avatars for local.actual.chat

--- src/dotnet/Core/DefaultUserPicture.cs
+++ src/dotnet/Core/DefaultUserPicture.cs
@@ -6,7 +6,7 @@ public static class DefaultUserPicture
     public const string BoringAvatarsBaseUrl = "https://source.boringavatars.com/";
 
     public static string Get(string name, int size = DefaultSize)
-        => GetBoringAvatar(name.GetSHA1HashCode(), size);
+        => GetBoringAvatar(name.GetSHA1HashCode(HashEncoding.AlphaNumeric), size);
 
     public static string GetBoringAvatar(string hash, int size = DefaultSize)
         => $"{BoringAvatarsBaseUrl}beam/{size}/{hash.UrlEncode()}?colors=FFDBA0,BBBEFF,9294E1,FF9BC0,0F2FE8";
