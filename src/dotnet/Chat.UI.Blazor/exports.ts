687d69e8a63415654d53a3efbd58a5fd6c0f215d
iqmulator <iqmulator@gmail.com>
2023-10-11 02:46:27 +0500

    user interface settings, font-size

--- src/dotnet/Chat.UI.Blazor/exports.ts
+++ src/dotnet/Chat.UI.Blazor/exports.ts
@@ -7,4 +7,5 @@ export * from './Components/MarkupEditor/markup-editor';
 export * from './Components/MarkupParts/CodeBlockMarkupView/code-block-markup-view';
 export * from './Components/MentionList/mention-list';
 export * from './Components/Selection/selection-host';
+export * from './Components/Settings/user-interface';
 export * from './Services/LanguageUI/language-ui';
