61e5a581e139ea62dbd28f69d2fa46fb712f0050
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-10-10 09:57:31 +0300

    fix: force show the phone step

--- src/dotnet/Users/UserOnboardingSettings.cs
+++ src/dotnet/Users/UserOnboardingSettings.cs
@@ -8,12 +8,12 @@ public sealed partial record UserOnboardingSettings : IHasOrigin
 {
     public const string KvasKey = nameof(UserOnboardingSettings);
 
-    [DataMember, MemoryPackOrder(0)] public bool IsVerifyPhoneStepCompleted { get; init; }
     [DataMember, MemoryPackOrder(1)] public bool IsAvatarStepCompleted { get; init; }
     [DataMember, MemoryPackOrder(2)] public Moment LastShownAt { get; init; }
     [DataMember, MemoryPackOrder(3)] public string Origin { get; init; } = "";
     [DataMember, MemoryPackOrder(4)] public bool IsSetChatsStepCompleted { get; init; }
     [DataMember, MemoryPackOrder(5)] public bool IsEmailStepCompleted { get; init; }
+    [DataMember, MemoryPackOrder(6)] public bool IsVerifyPhoneStepCompleted { get; init; }
 
     [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
     public bool HasUncompletedSteps
