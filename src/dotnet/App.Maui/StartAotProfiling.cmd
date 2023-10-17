f02b6e56bcc56fdcda55e3cedb8342eb3a110afa
Alexey Kochetov <alexis.kochetov@gmail.com>
2023-10-17 14:30:52 +0300

    chore: custom aot profile (not used)

--- src/dotnet/App.Maui/StartAotProfiling.cmd
+++ src/dotnet/App.Maui/StartAotProfiling.cmd
@@ -1,2 +1,2 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Android\android-sdk\platform-tools"
-dotnet build -f:net7.0-android -c:Debug -t:BuildAndStartAotProfiling
+dotnet build -f:net8.0-android -c:Debug -t:BuildAndStartAotProfiling
