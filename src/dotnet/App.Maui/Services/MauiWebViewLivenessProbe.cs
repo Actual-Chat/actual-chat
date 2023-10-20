32af2597eef265a85417369cbc3c134587751a36
Dmitry Filippov <crui3er@gmail.com>
2023-10-20 16:26:22 +0500

    feat: improve MauiWebViewLivenessProbe to reduce false WebView deadness detections

--- src/dotnet/App.Maui/Services/MauiWebViewLivenessProbe.cs
+++ src/dotnet/App.Maui/Services/MauiWebViewLivenessProbe.cs
@@ -7,12 +7,17 @@ namespace ActualChat.App.Maui.Services;
 public class MauiWebViewLivenessProbe(IServiceProvider services)
 {
     private readonly CancellationTokenSource _cancellationTokenSource = new ();
+    private IDispatcher? _dispatcher;
 
     private CancellationToken CancellationToken => _cancellationTokenSource.Token;
     private IServiceProvider Services { get; } = services;
     private ILogger Log { get; } = services.LogFor<MauiWebViewLivenessProbe>();
+    private IDispatcher Dispatcher => _dispatcher ??= Services.GetRequiredService<IDispatcher>();
 
-    public async Task StartCheck()
+    public Task StartCheck()
+        => BackgroundTask.Run(StartCheckInternal, CancellationToken);
+
+    public async Task StartCheckInternal()
     {
         Log.LogDebug("Starting check");
         try {
@@ -39,24 +44,33 @@ public class MauiWebViewLivenessProbe(IServiceProvider services)
 
     private async Task<bool> IsAlive(CancellationToken cancellationToken)
     {
-        // We give only 2000ms for one attempt to check aliveness, but not more than 300ms after
-        // we ensures that main thread is free to pump up js invoke messages to WebView.
+        // We give only 2000ms for one attempt to check aliveness, but not more than 500ms after
+        // we have ensured that main thread is free after dispatching js invoke message to WebView.
         // This optimization is need to reduce number of false WebView deadness detections.
         var cts1 = new CancellationTokenSource(TimeSpan.FromMilliseconds(2000));
         var cts2 = cancellationToken.LinkWith(cts1.Token);
         try {
             var scopedServices = await WhenScopedServicesReady(cts2.Token).ConfigureAwait(false);
             var jsRuntime = scopedServices.GetRequiredService<IJSRuntime>();
-            _ = MainThread.InvokeOnMainThreadAsync(() => {
-                cts2.CancelAfter(TimeSpan.FromMilliseconds(300));
+            var isAliveTask = jsRuntime.Eval<bool>("true", cts2.Token);
+            _ = Dispatcher.DispatchAsync(() => {
+                // Will cancel check in 500ms after BeginInvokeJS message is dispatched.
+                try {
+                    cts2.CancelAfter(TimeSpan.FromMilliseconds(500));
+                }
+                catch(ObjectDisposedException){}
             });
-            var isAlive = await jsRuntime.Eval<bool>("true", cts2.Token).ConfigureAwait(false);
+            var isAlive = await isAliveTask.ConfigureAwait(false);
             return isAlive;
         }
         catch (Exception e) {
             var isSilent = e is OperationCanceledException or TimeoutException or JSDisconnectedException;
             if (!isSilent)
                 Log.LogWarning(e, "An exception occurred during aliveness check");
+        }
+        finally {
+            cts2.DisposeSilently();
+            cts1.DisposeSilently();
         }
         return false;
     }
