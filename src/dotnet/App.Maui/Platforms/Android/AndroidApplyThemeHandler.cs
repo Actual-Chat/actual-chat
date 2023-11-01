ec6201e336c3753559247966e4f4cb07923a60c5
Frol <4425748+FrolYo@users.noreply.github.com>
2023-11-01 18:54:04 +0300

    feat: ios - apply safe area color respecting the theme #1623

--- src/dotnet/App.Maui/Platforms/Android/AndroidApplyThemeHandler.cs
+++ src/dotnet/App.Maui/Platforms/Android/AndroidApplyThemeHandler.cs
@@ -1,81 +1,35 @@
-﻿using ActualChat.UI.Blazor.Services;
 using AndroidX.Core.View;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 
 namespace ActualChat.App.Maui;
 
-public class AndroidApplyThemeHandler
+public class AndroidApplyThemeHandler : MauiApplyThemeHandler
 {
-    private const string BarColors = "Theme_BarColors";
-    private string _lastAppliedColors = "";
-
     private Android.Views.Window? Window => (Platform.CurrentActivity as MainActivity)?.Window;
 
-    public static readonly AndroidApplyThemeHandler Instance = new ();
+    public static new readonly AndroidApplyThemeHandler Instance = new ();
 
     private AndroidApplyThemeHandler()
+    { }
+
+    protected override bool ApplyBarColors(string sTopBarColor, string sBottomBarColor)
     {
-    }
+        var topBarColor = Android.Graphics.Color.ParseColor(sTopBarColor);
+        var bottomBarColor = Android.Graphics.Color.ParseColor(sBottomBarColor);
+        var window = Window;
+        if (window == null)
+            return false;
 
-    public void TryRestoreLastTheme()
-    {
-        var barsColor = Preferences.Default.Get<string>(BarColors, "");
-        ApplyBarColors(barsColor);
-    }
-
-    public void OnApplyTheme(Theme theme)
-        => _ = OnApplyThemeAsync();
-
-    private async Task OnApplyThemeAsync()
-    {
-        try {
-            var services = await WhenScopedServicesReady().ConfigureAwait(true);
-            var themeUI = services.GetRequiredService<ThemeUI>();
-            var themeBarColors = await themeUI.GetBarColors().ConfigureAwait(true);
-            Preferences.Default.Set(BarColors, themeBarColors);
-            ApplyBarColors(themeBarColors);
-        }
-        catch {
-            // Ignore
-        }
-    }
-
-    private void ApplyBarColors(string sColors)
-    {
-        if (string.IsNullOrEmpty(sColors))
-            return;
-
-        _ = MainThread.InvokeOnMainThreadAsync(() => {
-            if (OrdinalEquals(sColors, _lastAppliedColors))
-                return;
-
-            try {
-                var items = sColors.Split(";");
-                var sTopBarColor = sColors;
-                var sBottomBarColor = sColors;
-                if (items.Length >= 2) {
-                    sTopBarColor = items[0];
-                    sBottomBarColor = items[1];
-                }
-                var topBarColor = Android.Graphics.Color.ParseColor(sTopBarColor);
-                var bottomBarColor = Android.Graphics.Color.ParseColor(sBottomBarColor);
-                var window = Window;
-                if (window != null) {
-                    // Set System bars colors
-                    // See https://developer.android.com/design/ui/mobile/guides/layout-and-content/layout-basics
-                    // I do it from here because I can not modify theme 'Maui.MainTheme'
-                    // which is applied after calling base.OnCreate.
-                    window.SetStatusBarColor(topBarColor);
-                    var wic = new WindowInsetsControllerCompat(window, window.DecorView);
-                    var isDarkStatusBar = IsColorDark(topBarColor);
-                    wic.AppearanceLightStatusBars = !isDarkStatusBar;
-                    window.SetNavigationBarColor(bottomBarColor);
-                    _lastAppliedColors = sColors;
-                }
-            }
-            catch {
-                // Ignore
-            }
-        });
+        // Set System bars colors
+        // See https://developer.android.com/design/ui/mobile/guides/layout-and-content/layout-basics
+        // I do it from here because I can not modify theme 'Maui.MainTheme'
+        // which is applied after calling base.OnCreate.
+        window.SetStatusBarColor(topBarColor);
+        var wic = new WindowInsetsControllerCompat(window, window.DecorView);
+        var isDarkStatusBar = IsColorDark(topBarColor);
+        wic.AppearanceLightStatusBars = !isDarkStatusBar;
+        window.SetNavigationBarColor(bottomBarColor);
+        return true;
 
         bool IsColorDark(Android.Graphics.Color color) {
             var darkness = 1 - (0.299 * color.R + 0.587 * color.G + 0.114 * color.B) / 255;
