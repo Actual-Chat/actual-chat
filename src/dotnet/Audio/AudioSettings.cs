07e1c9bf589851558aaa311824a575a7b4ffec45
Alex Yakunin <alex.yakunin@gmail.com>
2023-11-07 09:53:50 -0800

    fix: set recording timeout to 1 min, countdown to 15 sec, listening to 1 min.

--- src/dotnet/Audio/AudioSettings.cs
+++ src/dotnet/Audio/AudioSettings.cs
@@ -7,10 +7,10 @@ public sealed class AudioSettings
     public string Namespace { get; set; } = "default";
 
     public TimeSpan IdleRecordingCheckPeriod { get; set; } = TimeSpan.FromSeconds(1);
-    public TimeSpan IdleRecordingTimeout { get; set; } = TimeSpan.FromSeconds(180);
-    public TimeSpan IdleRecordingPreCountdownTimeout { get; set; } = TimeSpan.FromSeconds(150);
+    public TimeSpan IdleRecordingTimeout { get; set; } = TimeSpan.FromSeconds(60);
+    public TimeSpan IdleRecordingPreCountdownTimeout { get; set; } = TimeSpan.FromSeconds(45);
     public TimeSpan IdleListeningCheckPeriod { get; set; } = TimeSpan.FromSeconds(3); // Not critical to stop it at the exact time
-    public TimeSpan IdleListeningTimeout { get; set; } = TimeSpan.FromSeconds(30);
+    public TimeSpan IdleListeningTimeout { get; set; } = TimeSpan.FromSeconds(60);
     public TimeSpan IdleListeningPreCountdownTimeout
         // There is no actual countdown, all we need here is to make sure it starts to make the next check fail
         => IdleListeningTimeout - IdleListeningCheckPeriod + TimeSpan.FromSeconds(1);
