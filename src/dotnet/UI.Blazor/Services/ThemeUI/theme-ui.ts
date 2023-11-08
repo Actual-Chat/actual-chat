68fd385f5cd7b1b661ba48078e6bbe11c8790757
Dmitry Filippov <crui3er@gmail.com>
2023-11-08 11:03:19 +0400

    fix: Can not apply theme color to bars on Android #1629

--- src/dotnet/UI.Blazor/Services/ThemeUI/theme-ui.ts
+++ src/dotnet/UI.Blazor/Services/ThemeUI/theme-ui.ts
@@ -8,6 +8,12 @@
         const style = getComputedStyle(document.body);
         const headerColor = style.getPropertyValue('--background-01');
         const postPanelColor = style.getPropertyValue('--post-panel');
-        return headerColor + ";" + postPanelColor;
+        return ThemeUI.normalizeColor(headerColor) + ";" + ThemeUI.normalizeColor(postPanelColor);
+    }
+
+    private static normalizeColor(hex : string) : string {
+        if (hex && hex.length === 4)
+            return '#' + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3];
+        return hex;
     }
 }
