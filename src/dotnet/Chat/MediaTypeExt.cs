40840dc4fa912a6e2f5bb03565ebba9f4b13691a
Frol <4425748+FrolYo@users.noreply.github.com>
2023-11-08 23:09:42 +0300

    fix: some image formats led to errors in chat view #1634

---
+++ src/dotnet/Chat/MediaTypeExt.cs
@@ -0,0 +1,16 @@
+﻿namespace ActualChat.Chat;
+
+public static class MediaTypeExt
+{
+    private static readonly HashSet<string> _supportedImageTypes = ["image/png", "image/jpeg", "image/svg+xml", "image/webp", "image/gif", "image/bmp", "image/heif", "image/heic", "image/heif", "image/avif"];
+    private static readonly HashSet<string> _supportedVideoTypes = ["video/mp4", "video/vp8", "video/vp9", "video/av1"];
+
+    public static bool IsSupportedImage(string? contentType)
+        => !contentType.IsNullOrEmpty() && _supportedImageTypes.Contains(contentType.ToLowerInvariant());
+    public static bool IsGif(string? contentType)
+        => OrdinalIgnoreCaseEquals(contentType, "image/gif");
+    public static bool IsSvg(string? contentType)
+        => OrdinalIgnoreCaseEquals(contentType, "image/svg+xml");
+    public static bool IsSupportedVideo(string? contentType)
+        => !contentType.IsNullOrEmpty() && _supportedVideoTypes.Contains(contentType.ToLowerInvariant());
+}
