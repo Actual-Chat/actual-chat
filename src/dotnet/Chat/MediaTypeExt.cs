32a3e3e1e2b5134463c68d30694f9ed7cc6a0475
Frol <4425748+FrolYo@users.noreply.github.com>
2023-11-14 18:53:57 +0300

    fix: image/video type support

--- src/dotnet/Chat/MediaTypeExt.cs
+++ src/dotnet/Chat/MediaTypeExt.cs
@@ -2,33 +2,39 @@
 
 public static class MediaTypeExt
 {
-    private static readonly HashSet<string> _supportedImageTypes = ["image/png", "image/jpeg", "image/svg+xml", "image/webp", "image/gif", "image/bmp", "image/heif", "image/heic", "image/heif", "image/avif"];
-    private static readonly HashSet<string> _supportedVideoTypes = ["video/mp4", "video/vp8", "video/vp9", "video/av1"];
-    private static readonly Dictionary<string, string> ExtensionByContentType =
+    private static readonly Dictionary<string, string> ImageExtensionByContentType =
         new (StringComparer.OrdinalIgnoreCase) {
-            // images
             ["image/bmp"] = ".bmp",
             ["image/jpeg"] = ".jpg",
+            ["image/gif"] = ".gif",
             ["image/vnd.microsoft.icon"] = ".ico",
             ["image/png"] = ".png",
             ["image/svg+xml"] = ".svg",
             ["image/webp"] = ".webp",
-            // videos
-            ["video/mp4"] = ".mp4",
-            ["video/vp8"] = ".webm",
-            ["video/vp9"] = ".webm",
-            ["video/av1"] = ".webm",
-            ["video/webm"] = ".webm",
+            ["image/heif"] = ".heif",
+            ["image/heic"] = ".heic",
+            ["image/avif"] = ".avif",
         };
+    private static readonly Dictionary<string, string> VideoExtensionByContentType =
+        new (StringComparer.OrdinalIgnoreCase) {
+            ["video/mp4"] = ".mp4",
+            // webm seems not working on iOS. Probably supported types must be platform-specific
+            // ["video/vp8"] = ".webm",
+            // ["video/vp9"] = ".webm",
+            // ["video/av1"] = ".webm",
+            // ["video/webm"] = ".webm",
+        };
+    private static readonly Dictionary<string, string> ExtensionByContentType =
+        new (ImageExtensionByContentType.Concat(VideoExtensionByContentType), StringComparer.OrdinalIgnoreCase);
 
     public static bool IsSupportedImage(string? contentType)
-        => !contentType.IsNullOrEmpty() && _supportedImageTypes.Contains(contentType.ToLowerInvariant());
+        => !contentType.IsNullOrEmpty() && ImageExtensionByContentType.ContainsKey(contentType.ToLowerInvariant());
     public static bool IsGif(string? contentType)
         => OrdinalIgnoreCaseEquals(contentType, "image/gif");
     public static bool IsSvg(string? contentType)
         => OrdinalIgnoreCaseEquals(contentType, "image/svg+xml");
     public static bool IsSupportedVideo(string? contentType)
-        => !contentType.IsNullOrEmpty() && _supportedVideoTypes.Contains(contentType.ToLowerInvariant());
+        => !contentType.IsNullOrEmpty() && VideoExtensionByContentType.ContainsKey(contentType.ToLowerInvariant());
     public static bool IsSupportedVisualMedia(string? contentType)
         => IsSupportedImage(contentType) || IsSupportedVideo(contentType);
     public static string? GetFileExtension(string? contentType)
