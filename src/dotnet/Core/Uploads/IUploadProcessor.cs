8ab9758bb2fe26808664923d87aa1eddfd8e95d7
Frol <4425748+FrolYo@users.noreply.github.com>
2023-11-14 23:49:57 +0300

    feat: convert video on upload #1650

--- src/dotnet/Core/Uploads/IUploadProcessor.cs
+++ src/dotnet/Core/Uploads/IUploadProcessor.cs
@@ -1,11 +1,31 @@
+using Stl.IO;
+
 namespace ActualChat.Uploads;
 
 public interface IUploadProcessor
 {
-    bool Supports(FileInfo file);
-    Task<ProcessedFileInfo> Process(FileInfo file, CancellationToken cancellationToken);
+    bool Supports(UploadedFile file);
+    Task<ProcessedFile> Process(UploadedFile file, CancellationToken cancellationToken);
 }
 
-public sealed record ProcessedFileInfo(FileInfo File, Size? Size);
+public sealed record ProcessedFile(UploadedFile File, Size? Size) : IDisposable
+{
+    public void Dispose()
+        => File.Delete();
+}
 
-public sealed record FileInfo(string FileName, string ContentType, long Length, byte[] Content); // TODO: calc length from bytes?
+public sealed record UploadedFile(string FileName, string ContentType, long Length, FilePath TempFilePath)
+{
+    public Stream Open()
+        => File.OpenRead(TempFilePath);
+
+    public void Delete()
+        => File.Delete(TempFilePath);
+
+    public async Task<string> GetSHA256HashCode(HashEncoding hashEncoding)
+    {
+        var stream = Open();
+        await using var _ = stream.ConfigureAwait(false);
+        return await stream.GetSHA256HashCode(hashEncoding).ConfigureAwait(false);
+    }
+}
