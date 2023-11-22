c79a31a934bfccc44bf96a39f9f8f85c688808b4
Alex Yakunin <alex.yakunin@gmail.com>
2023-11-22 07:42:59 -0800

    fix: show loading overlay on iOS + remove it faster

--- src/dotnet/App.Maui/wwwroot/_overlay.js
+++ src/dotnet/App.Maui/wwwroot/_overlay.js
@@ -2,17 +2,20 @@
 (function() {
     const overlay = document.getElementById('until-ui-is-ready');
     if (overlay) {
+        /*
         const userAgent = (navigator?.userAgent ?? '').toLowerCase();
         const isChromium = userAgent.indexOf('chrome') >= 0;
         const isWebKit = !isChromium && userAgent.indexOf('webkit') >= 0;
-        if (isWebKit)
+        if (isWebKit) {
             overlay.remove(); // No loading overlay on iOS
-        else
-            overlay.innerHTML = `
-                        <div class="c-box">
-                            <img draggable="false" src="/dist/images/landing/ac-icon-light.svg" alt="Loading...">
-                        </div>
-                        <div class="c-rotating-bg"></div>
-                `;
+            return;
+        }
+        */
+        overlay.innerHTML = `
+            <div class="c-box">
+                <img draggable="false" src="/dist/images/landing/ac-icon-light.svg" alt="Loading...">
+            </div>
+            <div class="c-rotating-bg"></div>
+        `;
     }
 })();
