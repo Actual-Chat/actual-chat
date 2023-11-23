56dfb9dd0ade4d4142a7c0ba58b2248a3ea0076c
Alex Yakunin <alex.yakunin@gmail.com>
2023-11-23 06:23:18 -0800

    fix: full trimming of MAUI app

--- src/dotnet/App.Maui/MauiAppStartup.cs
+++ src/dotnet/App.Maui/MauiAppStartup.cs
@@ -1,46 +1,16 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Net.Sockets;
-using System.Net.WebSockets;
-using ActualChat.App.Maui.Services;
 using ActualChat.Hosting;
 using ActualChat.UI.Blazor.App;
-#if ANDROID
-using Android.Net.Http;
-using Xamarin.Android.Net;
-#endif
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace ActualChat.App.Maui;
 
 public static class MauiAppStartup
 {
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MauiAppBuilderExt))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MainPage))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MauiBlazorApp))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MauiWebView))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MauiThemeHandler))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(NativeHttpClientFactory))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(SafeJSRuntime))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(SafeJSObjectReference))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Editor))] // Triggers VTable setup crash
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ILoggerFactory))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Serilog.LoggerConfiguration))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Serilog.ILogger))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(System.Net.WebRequest))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(System.Net.WebResponse))]
-#if ANDROID
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(AndroidWebChromeClient))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(AndroidWebViewClientOverride))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(FirebaseMessagingService))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(FirebaseMessagingUtils))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MainActivity))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MainApplication))]
-    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(NativeGoogleAuth))]
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(AndroidHttpClient))]
- #pragma warning disable CS0618 // Type or member is obsolete
-    // [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(AndroidClientHandler))]
- #pragma warning restore CS0618 // Type or member is obsolete
-#elif IOS
-#endif
+    // The rest is in ILLink.Descriptors.Maui.xml
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(DotNetObjectReference<>))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(EventCallback<>))]
     public static void ConfigureServices(IServiceCollection services)
         => AppStartup.ConfigureServices(services, AppKind.MauiApp, c => new HostModule[] {
             new Module.MauiAppModule(c),
