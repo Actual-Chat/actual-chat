4a99abdd8137ad596c46a227e0936b862c150d11
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-11-23 21:59:12 +0100

    feat: update cats svgs

---
+++ src/dotnet/Chat.UI.Blazor/Components/ChatNotFound/chat-not-found-svg.lit.ts
@@ -0,0 +1,49 @@
+import {customElement, property} from "lit/decorators.js";
+import {css, html, LitElement} from "lit";
+
+@customElement('chat-not-found-svg')
+class ChatNotFoundSvg extends LitElement {
+    protected render(): unknown {
+        return html`
+            <svg width="307" height="188" viewBox="0 0 307 188" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_13104_265247)">
+                    <path d="M283.602 136.7C283.602 61.2 222.402 0 146.902 0C71.4016 0 10.1016 61.2 10.1016 136.7C10.1016 153.7 13.2016 169.9 18.8016 184.9H274.802C280.502 169.9 283.602 153.7 283.602 136.7Z" fill="var(--not-found-cat-body)"/>
+                    <path d="M157 182H66V157L70.5 150L81.5 141.5L97.5 138.5L109 134L120 121L127 107L137 99.5L147 97.5L154 115L153 130L160.5 144.5V160L157 182Z" fill="var(--not-found-cat-face)"/>
+                    <path d="M145.501 180.6L136.601 188.3L123.801 195.5L109.901 198L94.4012 195.7L82.9012 190L76.2012 184.1C76.2012 184.1 76.4012 179.4 82.1012 176.1C90.6012 171.1 102.501 167 110.901 174L129.301 169.1L134.801 167L144.301 171.4L145.501 180.6Z" fill="var(--not-found-cat-bg)"/>
+                    <path d="M105.801 180.1L92.6008 179.8L92.8008 167.6L86.7008 152C86.7008 152 83.9008 140.4 94.2008 139.2C104.501 137.9 106.001 151.3 106.001 151.3L105.201 162.9C105.101 162.9 103.101 175.2 105.801 180.1Z" fill="var(--not-found-cat-bg)"/>
+                    <path d="M126.402 112.3C139.102 115.9 150.102 124.9 155.902 137.8C166.302 161.2 155.702 188.7 132.102 199.2C108.502 209.7 80.902 199.3 70.502 176C69.502 173.8 68.802 171.7 68.102 169.5L67.002 165.7" stroke="var(--not-found-cat-lines)" stroke-width="5.3942" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M144.502 181.7C147.802 165.3 130.502 166.5 130.502 166.5C130.502 166.5 119.302 167.4 110.602 174.7" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M66.1016 166.2C66.9016 164.7 67.6016 163.7 67.6016 163.7C74.0016 157.6 83.0016 159.1 90.3016 164.1C95.7016 167.8 92.2016 179.6 92.2016 179.6C97.2016 181.8 103.202 181.5 108.002 178.1" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M101.201 189.8L100.201 180.8" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M94.002 143.9C94.302 151.3 96.502 159 98.602 164" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M93.1014 167C93.1014 167 86.6014 158.2 86.8014 150" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M160.001 151.9C160.001 151.9 157.001 187 183.501 187" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M276.901 159.6C248.301 158.2 234.201 146.7 230.701 132C220.701 89.9 198.101 66.5 155.501 66.5C127.701 66.5 103.301 81.2 89.7008 103.3C89.7008 103.3 80.9008 117.4 71.8008 125.5" stroke="var(--not-found-cat-lines)" stroke-width="5.5687" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M119 123.5L126.2 108.4L135.1 100.9L143.8 99.2L149.5 105.9L151.7 122.2L149.1 135.6L119 123.5Z" fill="var(--not-found-cat-face)"/>
+                    <path d="M133.102 125.7C133.102 125.7 144.602 125.1 149.602 134.6C149.602 134.6 157.102 112.4 145.002 101.7C144.902 101.7 133.102 105.2 133.102 125.7Z" fill="var(--not-found-cat-bg)"/>
+                    <path d="M64.9004 164.8C64.9004 148.3 82.1004 143.2 82.1004 143.2C111.7 143.2 119.5 120.4 119.5 120.4C119.5 120.4 127.4 97.2 144.9 99.4C144.9 99.4 160.3 115.5 149.2 134.6" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M45.8008 184.9C56.3008 184.9 64.9008 181.3 64.9008 164.8" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M144.5 100.1C131.7 106.6 131 123.4 131 123.4C131 123.4 139.9 128.8 143.7 122.2" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M77.4004 177.5C77.4004 177.5 83.6004 170.3 93.0004 172.7" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M3 185H304" stroke="var(--not-found-cat-lines)" stroke-width="6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M19.7012 174.8C31.0012 174.8 42.5012 177.1 49.1012 141.7C55.7012 106.5 76.5012 81.5 101.101 68.7" stroke="var(--not-found-cat-lines)" stroke-width="6.3271" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M60.5 184.9H78.1L66.1 168.2L60.5 184.9Z" fill="var(--not-found-cat-lines)"/>
+                    <path d="M172.8 184.5H147.5L160.2 166L172.8 184.5Z" fill="var(--not-found-cat-lines)"/>
+                    <path d="M106.701 171.9C109.901 163.1 120.001 160.4 126.801 162.5" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linejoin="round"/>
+                    <path d="M105.5 169C105.5 169 102.9 158 106.1 150.5" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M156.801 140.6C156.801 140.6 155.101 154.5 140.701 160.3" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M159.4 150.8C159.4 150.8 157.4 160.9 146.5 166.2" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M109.102 142.3C109.102 142.3 116.802 129.2 131.002 130.6C131.002 130.6 144.502 130.5 148.702 144" stroke="var(--not-found-cat-lines)" stroke-width="5.0933" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M133.1 77.5C133.1 77.5 192.4 56.1 206 137.2C215 190.6 256.3 184.9 256.3 184.9" stroke="var(--not-found-cat-lines)" stroke-width="5.5687" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M150.9 108.9C150.9 108.9 169.1 113 178 139.1C187.5 166.9 186.2 178.9 206.1 184.9" stroke="var(--not-found-cat-lines)" stroke-width="5.5687" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M125.3 108.3C125.3 108.3 106.1 124.4 96.0996 122.8" stroke="var(--not-found-cat-lines)" stroke-width="6.3271" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                <defs>
+                    <clipPath id="clip0_13104_265247">
+                        <rect width="307" height="188" fill="white"/>
+                    </clipPath>
+                </defs>
+            </svg>
+        `;
+    }
+}
