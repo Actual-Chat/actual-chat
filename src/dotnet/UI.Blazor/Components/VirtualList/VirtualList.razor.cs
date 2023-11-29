6fb1408ce8634efea1b754285a3f17dfa8406342
Alex Yakunin <alex.yakunin@gmail.com>
2023-11-29 02:03:42 -0800

    fix: update Fusion + revert back State == null checks

--- src/dotnet/UI.Blazor/Components/VirtualList/VirtualList.razor.cs
+++ src/dotnet/UI.Blazor/Components/VirtualList/VirtualList.razor.cs
@@ -23,8 +23,7 @@ public sealed partial class VirtualList<TItem> : ComputedStateComponent<VirtualL
     private DotNetObjectReference<IVirtualListBackend> BlazorRef { get; set; } = null!;
 
     private VirtualListDataQuery Query { get; set; } = VirtualListDataQuery.None;
-    // State can be null when ComputeState runs synchronously
-    private VirtualListData<TItem> Data => State?.LastNonErrorValue ?? VirtualListData<TItem>.None;
+    private VirtualListData<TItem> Data => State.LastNonErrorValue;
     private VirtualListData<TItem> LastData { get; set; } = VirtualListData<TItem>.None;
     private VirtualListItemVisibility LastReportedItemVisibility { get; set; } = VirtualListItemVisibility.Empty;
 
