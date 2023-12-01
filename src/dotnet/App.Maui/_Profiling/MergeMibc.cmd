809ab0716e717c3007f712fd6498d0f453da7492
Alex Yakunin <alex.yakunin@gmail.com>
2023-12-01 00:51:28 -0800

    perf: AOT

--- src/dotnet/App.Maui/_Profiling/MergeMibc.cmd
+++ src/dotnet/App.Maui/_Profiling/MergeMibc.cmd
@@ -1,1 +1,1 @@
-dotnet-pgo merge -i %* -o android-merged.mibc --compressed
+dotnet-pgo merge -i android-v1.mibc -i android-v2.mibc -i android-v3.mibc -i android-v4.mibc -i android-v5.mibc -o android-merged.mibc --compressed
