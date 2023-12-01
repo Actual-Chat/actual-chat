28112a881a53f14a95ae93163d6662bf0ade5a80
Alex Yakunin <alex.yakunin@gmail.com>
2023-11-30 23:01:05 -0800

    wip: AOT profile

--- src/dotnet/App.Maui/InstallForProfiling.cmd
+++ src/dotnet/App.Maui/InstallForProfiling.cmd
@@ -1,2 +1,3 @@
 dotnet publish -f:net8.0-android /p:IsProfilingEnabled=true -c:Debug
 adb install -r ..\..\..\artifacts\publish\App.Maui\debug_net8.0-android\chat.actual.dev.app-Signed.apk
+call _StartDevApp.cmd
---
+++ src/dotnet/App.Maui/_Profiling/EnableMonoLogs.cmd
@@ -0,0 +1,1 @@
+adb shell setprop debug.mono.log default,assembly,mono_log_level=debug,mono_log_mask=all
