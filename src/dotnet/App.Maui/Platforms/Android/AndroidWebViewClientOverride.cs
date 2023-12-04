66b455c47b94587467c19dd7f773c8316ca520a1
Alex Yakunin <alex.yakunin@gmail.com>
2023-12-03 19:01:18 -0800

    chore: disable recreate Android WebView in OnRenderProcessGone

--- src/dotnet/App.Maui/Platforms/Android/AndroidWebViewClientOverride.cs
+++ src/dotnet/App.Maui/Platforms/Android/AndroidWebViewClientOverride.cs
@@ -30,6 +30,7 @@ public class AndroidWebViewClientOverride(
         var didCrash = detail?.DidCrash() == true;
         var details = $"DidCrash: {didCrash}, RendererPriorityAtExit: {detail?.RendererPriorityAtExit()}, {detail}";
         log.LogWarning("OnRenderProcessGone: {Details}", details);
+#if false // This seems to cause unnecessary WebView recreations
         var markedDead = view.IsNotNull()
             && MauiWebView.Current is { } mauiWebView
             && mauiWebView.AndroidWebView.IfNotNull() is { } androidWebView
@@ -39,6 +40,7 @@ public class AndroidWebViewClientOverride(
             MainThread.BeginInvokeOnMainThread(() => MainPage.Current.RecreateWebView());
             return true; // Indicates that we've handled this gracefully
         }
+#endif
         return base.OnRenderProcessGone(view, detail);
     }
 
