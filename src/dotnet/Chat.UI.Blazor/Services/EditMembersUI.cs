9c1505fab5859f2ab013f7c4539397d73b18f57d
Dmitry Filippov <crui3er@gmail.com>
2023-12-07 17:06:23 +0400

    fix: fix build after rebase

--- src/dotnet/Chat.UI.Blazor/Services/EditMembersUI.cs
+++ src/dotnet/Chat.UI.Blazor/Services/EditMembersUI.cs
@@ -8,14 +8,14 @@ public class EditMembersUI(ChatHub chatHub)
     {
         bool canAddContacts;
         if (!chat.Id.IsPlaceChat) {
-            var peopleContacts = await ChatHub.ChatListUI.ListPeopleContacts();
+            var peopleContacts = await ChatHub.ChatListUI.ListPeopleContacts().ConfigureAwait(false);
             canAddContacts = peopleContacts.Count > 0;
         } else {
             if (chat.IsPublic)
                 canAddContacts = false;
             else {
-                var chatMembers = await ChatHub.Authors.ListUserIds(ChatHub.Session, chat.Id, default);
-                var placeMembers = await ChatHub.Authors.ListUserIds(ChatHub.Session, chat.Id.PlaceId.ToRootChatId(), default);
+                var chatMembers = await ChatHub.Authors.ListUserIds(ChatHub.Session, chat.Id, default).ConfigureAwait(false);
+                var placeMembers = await ChatHub.Authors.ListUserIds(ChatHub.Session, chat.Id.PlaceId.ToRootChatId(), default).ConfigureAwait(false);
                 canAddContacts = placeMembers.Except(chatMembers).Any();
             }
         }
