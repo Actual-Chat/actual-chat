17c76a602a4e054409479a0bd3c1fa246750cebd
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2023-12-17 10:08:39 +0100

    fix: UpgradeContacts2 fails on a fresh DB

--- src/dotnet/Contacts.Service.Migration/Migrations/20221209064152_UpgradeContacts2.cs
+++ src/dotnet/Contacts.Service.Migration/Migrations/20221209064152_UpgradeContacts2.cs
@@ -21,59 +21,60 @@ namespace ActualChat.Contacts.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            UpAsync(migrationBuilder).Wait();
+            // NOTE(EK): Obsolete: applied to all of our DBs. Now it causes migration in tests to fail.
+            //UpAsync(migrationBuilder).Wait();
         }
 
-        private async Task UpAsync(MigrationBuilder migrationBuilder)
-        {
-            var dbInitializer = DbInitializer.GetCurrent<ContactsDbInitializer>();
-            var log = dbInitializer.Services.LogFor(GetType());
-
-            var clocks = dbInitializer.Services.Clocks();
-
-            using var dbContext = dbInitializer.CreateDbContext(true);
-
-            var dbContacts = await dbContext.Contacts.OrderBy(c => c.Id).ToListAsync().ConfigureAwait(false);
-            log.LogInformation("Upgrading {Count} contacts", dbContacts.Count);
-            var changeCount = 0;
-            foreach (var contact in dbContacts) {
-                var id = contact.Id;
-                if (id.Split(' ') is not [ var sOwnerId, var tail ])
-                    goto skip;
-                var ownerId = new UserId(sOwnerId);
-                if (tail.Split(':') is not [ var type, var otherId])
-                    goto skip;
-
-                var newId = id;
-                switch (type) {
-                case "c":
-                    var chatId = new ChatId(otherId);
-                    newId = new ContactId(ownerId, chatId);
-                    break;
-                case "u":
-                    var userId = new UserId(otherId);
-                    var peerChatId = new PeerChatId(ownerId, userId);
-                    newId = new ContactId(ownerId, peerChatId);
-                    break;
-                default:
-                    goto skip;
-                }
-
-                var newContact = MemberwiseCloner.Invoke(contact);
-                newContact.Id = newId;
-                dbContext.Contacts.Add(newContact);
-                dbContext.Contacts.Remove(contact);
-                changeCount++;
-                log.LogInformation("- '{Id}': new Id = '{NewId}'", id, newId);
-                continue;
-            skip:
-                continue;
-                // log.LogInformation("- '{Id}': skipped", id);
-            }
-            log.LogInformation("- Saving changes");
-            await dbContext.SaveChangesAsync().ConfigureAwait(false);
-            log.LogInformation("Upgrading contacts: {ChangeCount} / {Count} upgraded", changeCount, dbContacts.Count);
-        }
+        // private async Task UpAsync(MigrationBuilder migrationBuilder)
+        // {
+        //     var dbInitializer = DbInitializer.GetCurrent<ContactsDbInitializer>();
+        //     var log = dbInitializer.Services.LogFor(GetType());
+        //
+        //     var clocks = dbInitializer.Services.Clocks();
+        //
+        //     using var dbContext = dbInitializer.CreateDbContext(true);
+        //
+        //     var dbContacts = await dbContext.Contacts.OrderBy(c => c.Id).ToListAsync().ConfigureAwait(false);
+        //     log.LogInformation("Upgrading {Count} contacts", dbContacts.Count);
+        //     var changeCount = 0;
+        //     foreach (var contact in dbContacts) {
+        //         var id = contact.Id;
+        //         if (id.Split(' ') is not [ var sOwnerId, var tail ])
+        //             goto skip;
+        //         var ownerId = new UserId(sOwnerId);
+        //         if (tail.Split(':') is not [ var type, var otherId])
+        //             goto skip;
+        //
+        //         var newId = id;
+        //         switch (type) {
+        //         case "c":
+        //             var chatId = new ChatId(otherId);
+        //             newId = new ContactId(ownerId, chatId);
+        //             break;
+        //         case "u":
+        //             var userId = new UserId(otherId);
+        //             var peerChatId = new PeerChatId(ownerId, userId);
+        //             newId = new ContactId(ownerId, peerChatId);
+        //             break;
+        //         default:
+        //             goto skip;
+        //         }
+        //
+        //         var newContact = MemberwiseCloner.Invoke(contact);
+        //         newContact.Id = newId;
+        //         dbContext.Contacts.Add(newContact);
+        //         dbContext.Contacts.Remove(contact);
+        //         changeCount++;
+        //         log.LogInformation("- '{Id}': new Id = '{NewId}'", id, newId);
+        //         continue;
+        //     skip:
+        //         continue;
+        //         // log.LogInformation("- '{Id}': skipped", id);
+        //     }
+        //     log.LogInformation("- Saving changes");
+        //     await dbContext.SaveChangesAsync().ConfigureAwait(false);
+        //     log.LogInformation("Upgrading contacts: {ChangeCount} / {Count} upgraded", changeCount, dbContacts.Count);
+        // }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
