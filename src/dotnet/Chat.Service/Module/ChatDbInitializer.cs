1f2cd2413ca5ca03968abae38326d6b5598beee7
Alexey Kochetov <alexis.kochetov@gmail.com>
2023-12-23 09:41:29 +0300

    chore: comment out outdated chat db initializers as they are failed due to timeout

--- src/dotnet/Chat.Service/Module/ChatDbInitializer.cs
+++ src/dotnet/Chat.Service/Module/ChatDbInitializer.cs
@@ -14,14 +14,14 @@ public class ChatDbInitializer(IServiceProvider services) : DbInitializer<ChatDb
         await chatDbInitializer.WaitForOtherInitializers(_ => true).ConfigureAwait(false);
         var options = Services.GetService<InitializeDataOptions>() ?? new InitializeDataOptions();
 
-        if (options.AddAnnouncementsChat)
-            await EnsureAnnouncementsChatExists(cancellationToken).ConfigureAwait(false);
-        if (options.AddFeedbackTemplateChat)
-            await EnsureFeedbackTemplateChatExists(cancellationToken).ConfigureAwait(false);
+        // if (options.AddAnnouncementsChat)
+        //     await EnsureAnnouncementsChatExists(cancellationToken).ConfigureAwait(false);
+        // if (options.AddFeedbackTemplateChat)
+        //     await EnsureFeedbackTemplateChatExists(cancellationToken).ConfigureAwait(false);
         if (options.AddDefaultChat && HostInfo.IsDevelopmentInstance)
             await EnsureDefaultChatExists(cancellationToken).ConfigureAwait(false);
-        if (options.AddNotesChat)
-            await EnsureNotesChatsExist(cancellationToken).ConfigureAwait(false);
+        // if (options.AddNotesChat)
+        //     await EnsureNotesChatsExist(cancellationToken).ConfigureAwait(false);
     }
 
     public override async Task RepairData(CancellationToken cancellationToken)
