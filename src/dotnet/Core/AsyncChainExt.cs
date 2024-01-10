17b08691f152761ef92ceb9fcd0801d819a66a50
Alex Yakunin <alex.yakunin@gmail.com>
2024-01-10 09:20:29 -0800

    chore: update Fusion + fix TerminalError.Detector

--- src/dotnet/Core/AsyncChainExt.cs
+++ src/dotnet/Core/AsyncChainExt.cs
@@ -2,24 +2,9 @@ namespace ActualChat;
 
 public static class AsyncChainExt
 {
-    private static TerminalErrorDetector Detector { get; }
-        = static (e, ct) => e.IsCancellationOf(ct) || e.Any(IsServiceProviderDisposedException);
-
     public static AsyncChain From(
         Func<CancellationToken, Task> start,
         [CallerArgumentExpression(nameof(start))]
         string name = "")
-        => new (name, start, Detector);
-
-    private static bool IsServiceProviderDisposedException(Exception error)
-    {
-        if (Equals(error.GetType().Name, "JSDisconnectedException"))
-            return true; // This is specific to Blazor Server, it also indicates the scope is going to die soon
-
-        if (error is not ObjectDisposedException ode)
-            return false;
-
-        return ode.ObjectName.Contains("IServiceProvider", StringComparison.Ordinal)
-            || ode.Message.Contains("'IServiceProvider'", StringComparison.Ordinal);
-    }
+        => new(name, start);
 }
