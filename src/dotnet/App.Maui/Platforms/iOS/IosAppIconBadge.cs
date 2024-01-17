d590d172b8fd16a4044dfc67013e48c391cf7ad0
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-01-17 21:47:10 +0300

    feat: iOS badge counter

---
+++ src/dotnet/App.Maui/Platforms/iOS/IosAppIconBadge.cs
@@ -0,0 +1,10 @@
+﻿using ActualChat.UI.Blazor.App.Services;
+using UIKit;
+
+namespace ActualChat.App.Maui;
+
+public class IosAppIconBadge : IAppIconBadge
+{
+    public void SetUnreadChatCount(int count)
+        => UIApplication.SharedApplication.ApplicationIconBadgeNumber = count;
+}
