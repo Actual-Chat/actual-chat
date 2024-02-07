0ba43b9174324d683b37c2fc192b6d75396798c5
Alex Yakunin <alex.yakunin@gmail.com>
2024-02-06 18:49:16 -0800

    fix: RecorderToggle.razor & phone sign-in

--- src/dotnet/UI.Blazor/Components/Stepper/Step.cs
+++ src/dotnet/UI.Blazor/Components/Stepper/Step.cs
@@ -3,7 +3,7 @@ namespace ActualChat.UI.Blazor.Components;
 public abstract class Step : ComponentBase
 {
     public virtual bool CanSkip => false;
-    public abstract bool IsCompleted { get; }
+    public virtual bool IsCompleted => false;
     public Step? CurrentStep => Stepper.CurrentStep;
 
     [CascadingParameter] public Stepper Stepper { get; set; } = null!;
@@ -19,7 +19,10 @@ public abstract class Step : ComponentBase
 
     protected abstract Task<bool> Validate();
     protected abstract Task<bool> Save();
-    protected abstract void MarkCompleted();
+
+    protected virtual void MarkCompleted()
+    { }
+
     protected virtual ValueTask OnSkip()
         => ValueTask.CompletedTask;
 
