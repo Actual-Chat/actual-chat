fd5a6291653fa26e7b6e17d688a2e20ada65b424
Alex Yakunin <alex.yakunin@gmail.com>
2024-02-11 23:52:34 -0800

    fix: NodeRefTest

--- tests/Core.UnitTests/Identifiers/NodeRefTest.cs
+++ tests/Core.UnitTests/Identifiers/NodeRefTest.cs
@@ -3,5 +3,5 @@ namespace ActualChat.Core.UnitTests.Identifiers;
 public class NodeRefTest(ITestOutputHelper @out) : SymbolIdentifierTestBase<NodeRef>(@out)
 {
     public override Symbol[] ValidIdentifiers => [ "1234abcd", "1234abcde" ];
-    public override Symbol[] InvalidIdentifiers => [ "x", "some:node", "peer-no", "~wrong~ne" ];
+    public override Symbol[] InvalidIdentifiers => [ "x", "some:node", "x-no", "~wrong~ne" ];
 }
