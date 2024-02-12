8c97e2f1cebb9e01cdef7f3caa6ad0c4238eba99
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-02-12 14:45:34 +0300

    fix: contacts tests are divided across collections

--- tests/Contacts.IntegrationTests/Collections/ExternalContactCollection.cs
+++ tests/Contacts.IntegrationTests/Collections/ExternalContactCollection.cs
@@ -3,14 +3,14 @@ using ActualChat.Users;
 
 namespace ActualChat.Contacts.IntegrationTests;
 
-[CollectionDefinition(nameof(ContactCollection))]
-public class ContactCollection : ICollectionFixture<AppHostFixture>;
+[CollectionDefinition(nameof(ExternalContactCollection))]
+public class ExternalContactCollection : ICollectionFixture<ExternalAppHostFixture>;
 
-public class AppHostFixture(IMessageSink messageSink) : ActualChat.Testing.Host.AppHostFixture(messageSink)
+public class ExternalAppHostFixture(IMessageSink messageSink) : ActualChat.Testing.Host.AppHostFixture(messageSink)
 {
     private const AccountStatus NewAccountStatus = AccountStatus.Active;
 
-    public override string DbInstanceName => "contacts";
+    public override string DbInstanceName => "external-contacts";
 
     public override async Task InitializeAsync()
         => Host = await TestAppHostFactory.NewAppHost(MessageSink, DbInstanceName, TestAppHostOptions.Default with {
---
+++ tests/Contacts.IntegrationTests/ExternalContactStressTest.cs
@@ -0,0 +1,199 @@
+using System.Globalization;
+using System.Security.Claims;
+using ActualChat.Performance;
+using ActualChat.Testing.Assertion;
+using ActualChat.Testing.Host;
+using ActualChat.Users;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.Toolkit.HighPerformance;
+
+namespace ActualChat.Contacts.IntegrationTests;
+
+[Collection(nameof(ExternalContactStressCollection)), Trait("Category", nameof(ExternalContactStressCollection))]
+public class ExternalContactStressTest(ExternalStressAppHostFixture fixture, ITestOutputHelper @out): IAsyncLifetime
+{
+    private TestAppHost Host => fixture.Host;
+    private ITestOutputHelper Out { get; } = fixture.Host.UseOutput(@out);
+
+    private WebClientTester _tester = null!;
+    private ICommander _commander = null!;
+    private IAccounts _accounts = null!;
+    private IContacts _contacts = null!;
+
+    private static string BobEmail => "bob@actual.chat";
+    private static Phone BobPhone => new ("1-2345678901");
+    private static User Bob { get; } = new User("", $"Bob-{nameof(ExternalContactsTest)}")
+        .WithIdentity(new UserIdentity(GoogleDefaults.AuthenticationScheme, "111"))
+        .WithPhone(BobPhone)
+        .WithClaim(ClaimTypes.Email, BobEmail);
+
+    private static string JackEmail => "jack@actual.chat";
+    private static Phone JackPhone => new ("1-3456789012");
+    private static User Jack { get; } = new User("", $"JackAdmin-{nameof(ExternalContactsTest)}")
+        .WithIdentity(new UserIdentity(GoogleDefaults.AuthenticationScheme, "222"))
+        .WithPhone(JackPhone)
+        .WithClaim(ClaimTypes.Email, JackEmail);
+
+    public Task InitializeAsync()
+    {
+        Tracer.Default = Out.NewTracer();
+        _tester = Host.NewWebClientTester(Out);
+        _accounts = Host.Services.GetRequiredService<IAccounts>();
+        _contacts = Host.Services.GetRequiredService<IContacts>();
+        _commander = Host.Services.Commander();
+
+        FluentAssertions.Formatting.Formatter.AddFormatter(new UserFormatter());
+        return Task.CompletedTask;
+    }
+
+    public async Task DisposeAsync()
+    {
+        Tracer.Default = Tracer.None;
+        foreach (var formatter in FluentAssertions.Formatting.Formatter.Formatters.OfType<UserFormatter>().ToList())
+            FluentAssertions.Formatting.Formatter.RemoveFormatter(formatter);
+        await _tester.DisposeAsync().AsTask();
+    }
+
+
+
+    [Theory]
+    [InlineData("small", 5)]
+    [InlineData("medium", 37)]
+    public async Task StressTest_AllUsersExist_AllAreConnected(string prefix, int count)
+    {
+        // arrange
+        var tracer = Tracer.Default;
+        using var __ = tracer.Region();
+        var deviceIds = Enumerable.Repeat(0, count).Select(_ => NewDeviceId()).ToList();
+        var accounts = new AccountFull[count];
+        for (int i = 0; i < accounts.Length; i++) {
+            using var _1 = tracer.Region($"Sign in as user #{i + 1} {prefix}");
+            // TODO: find the way of fast user creation to perform real stress test 🙂
+            accounts[i] = await _tester.SignIn(BuildUser(prefix, i + 1));
+        }
+
+        // act
+        for (var i = 0; i < accounts.Length; i++) {
+            var account = accounts[i];
+            using var _2 = tracer.Region("Create external contacts " + account.User.Name);
+            await _tester.SignIn(account.User);
+            var externalContacts = Enumerable.Range(1, count)
+                .Select(idx => NewExternalContact(account, deviceIds[i], prefix, idx))
+                .ToArray();
+            await Add(externalContacts);
+        }
+
+        // assert
+        using var _3 = tracer.Region($"Assert {count} accounts");
+        foreach (var account in accounts) {
+            using var _4 = tracer.Region($"Assert contacts of {account.User.Name} #({account.User.Id})");
+            await _tester.SignIn(account.User);
+            await AssertConnectedUsers(account, accounts);
+        }
+    }
+
+    [Theory(Skip = "Flaky")]
+    [InlineData("small", 5)]
+    [InlineData("medium", 37)]
+    public async Task StressTest_UsersCreatedSequentially_AllAreConnected(string prefix, int count)
+    {
+        // arrange
+        var accounts = new AccountFull[count];
+        var deviceIds = Enumerable.Repeat(0, count).Select(_ => NewDeviceId()).ToList();
+
+        // act
+        for (int i = 0; i < accounts.Length; i++) {
+            var account = accounts[i] = await _tester.SignIn(BuildUser(prefix, i + 1));
+            var externalContacts = Enumerable.Range(1, count)
+                .Select(idx => NewExternalContact(account, deviceIds[i], prefix, idx))
+                .ToArray();
+            await Add(externalContacts);
+        }
+
+        foreach (var u in accounts.Select(x => x.User)) {
+            // act
+            var account = await _tester.SignIn(u);
+
+            // assert
+            await TestExt.WhenMetAsync(async () => {
+                    var acc = await _accounts.GetOwn(_tester.Session, CancellationToken.None);
+                    acc.IsGreetingCompleted.Should().BeTrue();
+                },
+                TimeSpan.FromSeconds(5));
+            await AssertConnectedUsers(account, accounts);
+        }
+    }
+
+    private async Task Add(params ExternalContact[] externalContacts)
+    {
+        var changes = externalContacts.Select(x => new ExternalContactChange(x.Id, null, Change.Create(x)));
+        var results = await _commander.Call(new ExternalContacts_BulkChange(_tester.Session, changes.ToApiArray()));
+        results.Select(x => x.Value).Should().NotContainNulls();
+        var errors = results.Select(x => x.Error).SkipNullItems().ToList();
+        if (errors.Count > 0)
+            throw new AggregateException("Failed to create external contacts", errors);
+    }
+
+    private async Task AssertConnectedUsers(AccountFull account, AccountFull[] allAccounts)
+    {
+        var userMap = allAccounts.ToDictionary(x => x.Id, x => x.User);
+        var contactIds = await ListContactIds(allAccounts.Length - 1);
+        var connectedUsers = contactIds.ConvertAll(GetUser).OrderBy(x => x.Name);
+        var otherUsers = allAccounts.Where(x => x.Id != account.Id).Select(x => x.User).OrderBy(x => x.Name);
+        connectedUsers.Should().BeEquivalentTo(otherUsers);
+        return;
+
+        User GetUser(ContactId x)
+            => userMap[x.ChatId.IsPeerChat(out var peerChatId)
+                ? peerChatId.UserIds.OtherThan(account.Id)
+                : throw new Exception("Peer chat contact was expected")];
+    }
+
+    private async Task<List<ContactId>> ListContactIds(int expectedCount)
+    {
+        await TestExt.WhenMetAsync(async () => {
+                var peerContactIds = await ListContactIds();
+                peerContactIds.Should().HaveCountGreaterOrEqualTo(expectedCount);
+            },
+            TimeSpan.FromSeconds(10));
+
+        return await ListContactIds();
+    }
+
+    private async Task<List<ContactId>> ListContactIds()
+    {
+        var ids = await _contacts.ListIds(_tester.Session, CancellationToken.None);
+        return ids.Where(x => x.ChatId.Kind == ChatKind.Peer && !Constants.Chat.SystemChatIds.Contains(x.ChatId)).ToList();
+    }
+
+
+    private static ExternalContact NewExternalContact(AccountFull owner, Symbol ownerDeviceId)
+        => new (new ExternalContactId(owner.Id, ownerDeviceId, NewDeviceContactId()));
+
+    private static ExternalContact NewExternalContact(AccountFull owner, Symbol deviceId, string prefix, int i)
+        => NewExternalContact(owner, deviceId).WithPhone(BuildPhone(prefix, i)).WithEmail(BuildEmail(prefix, i));
+
+    private static Symbol NewDeviceId()
+        => new (Guid.NewGuid().ToString());
+
+    private static Symbol NewDeviceContactId()
+        => new (Guid.NewGuid().ToString());
+
+    private static ContactId BuildContactId(AccountFull owner, AccountFull friendAccount)
+        => ContactId.Peer(owner.Id, friendAccount.Id);
+
+    private static User BuildUser(string prefix, int i)
+        => new User("", BuildUserName(i))
+            .WithIdentity(new UserIdentity(GoogleDefaults.AuthenticationScheme,  $"{prefix}-{i.ToString("00000", CultureInfo.InvariantCulture)}"))
+            .WithPhone(BuildPhone(prefix, i))
+            .WithClaim(ClaimTypes.Email, BuildEmail(prefix, i));
+
+    private static string BuildUserName(int i)
+        => $"user{i:00000}";
+
+    private static Phone BuildPhone(string prefix, int i)
+        => new ($"1-{Math.Abs(prefix.GetDjb2HashCode() % 100000):00000}{i:00000}");
+
+    private static string BuildEmail(string prefix, int i)
+        => $"{prefix}.user.{i:00000}@actual.chat";
+}
