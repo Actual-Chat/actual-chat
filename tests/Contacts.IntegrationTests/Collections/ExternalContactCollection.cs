8cbdc83aebeb7a371a3906c0c2abbabad7178b63
Alex Yakunin <alex.yakunin@gmail.com>
2024-02-12 04:51:53 -0800

    fix: post-merge fixes

--- tests/Contacts.IntegrationTests/Collections/ExternalContactCollection.cs
+++ tests/Contacts.IntegrationTests/Collections/ExternalContactCollection.cs
@@ -6,15 +6,15 @@ namespace ActualChat.Contacts.IntegrationTests;
 [CollectionDefinition(nameof(ExternalContactCollection))]
 public class ExternalContactCollection : ICollectionFixture<ExternalAppHostFixture>;
 
-public class ExternalAppHostFixture(IMessageSink messageSink) : ActualChat.Testing.Host.AppHostFixture(messageSink)
+public class ExternalAppHostFixture(IMessageSink messageSink)
+    : ActualChat.Testing.Host.AppHostFixture("ext-contacts", messageSink)
 {
     private const AccountStatus NewAccountStatus = AccountStatus.Active;
 
-    public override string DbInstanceName => "external-contacts";
-
-    public override async Task InitializeAsync()
-        => Host = await TestAppHostFactory.NewAppHost(MessageSink, DbInstanceName, TestAppHostOptions.Default with {
+    protected override TestAppHostOptions CreateHostOptions()
+        => base.CreateHostOptions() with {
             AppConfigurationExtender = cfg => {
                 cfg.AddInMemory(("UsersSettings:NewAccountStatus", NewAccountStatus.ToString()));
-            }});
+            },
+        };
 }
--- tests/Contacts.IntegrationTests/Collections/ExternalContactStressCollection.cs
+++ tests/Contacts.IntegrationTests/Collections/ExternalContactStressCollection.cs
@@ -6,15 +6,15 @@ namespace ActualChat.Contacts.IntegrationTests;
 [CollectionDefinition(nameof(ExternalContactStressCollection))]
 public class ExternalContactStressCollection : ICollectionFixture<ExternalStressAppHostFixture>;
 
-public class ExternalStressAppHostFixture(IMessageSink messageSink) : ActualChat.Testing.Host.AppHostFixture(messageSink)
+public class ExternalStressAppHostFixture(IMessageSink messageSink)
+    : ActualChat.Testing.Host.AppHostFixture("ext-contacts-stress", messageSink)
 {
     private const AccountStatus NewAccountStatus = AccountStatus.Active;
 
-    public override string DbInstanceName => "external-contacts-stress";
-
-    public override async Task InitializeAsync()
-        => Host = await TestAppHostFactory.NewAppHost(MessageSink, DbInstanceName, TestAppHostOptions.Default with {
+    protected override TestAppHostOptions CreateHostOptions()
+        => base.CreateHostOptions() with {
             AppConfigurationExtender = cfg => {
                 cfg.AddInMemory(("UsersSettings:NewAccountStatus", NewAccountStatus.ToString()));
-            }});
+            },
+        };
 }
