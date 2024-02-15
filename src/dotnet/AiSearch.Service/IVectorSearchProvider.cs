5a1f1fc4e135e2ddd3d1599ac5c35ddaa364c9ad
Alex Ustinov <alex.ustinov@actual.chat>
2024-02-14 20:37:28 -0800

    Declare IVectorSearchProvider.Find method

---
+++ src/dotnet/AiSearch.Service/IVectorSearchProvider.cs
@@ -0,0 +1,34 @@
+
+namespace ActualChat.AiSearch;
+
+internal interface IVectorSearchProvider
+{
+    Task<VectorSearchResult> Find(VectorSearchQuery query, CancellationToken cancellationToken);
+}
+
+internal class VectorSearchQuery
+{
+    public MetadataFilter MetadataFilter;
+    public string FreeTextFilter;
+}
+
+public class MetadataFilter
+{
+}
+
+internal class VectorSearchResult
+{
+    public VectorSearchRankedDocument[] Documents;
+}
+
+internal class VectorSearchRankedDocument
+{
+    public double Rank;
+    public IndexedDocument Document;
+}
+
+internal class IndexedDocument
+{
+    public string Uri;
+    public string Text;
+}
