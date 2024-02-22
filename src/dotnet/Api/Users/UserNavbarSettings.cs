b4504a9840b10be8e221be131f5a330c88cc7ac4
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-02-22 10:50:43 +0300

    feat: add Navbar settings

---
+++ src/dotnet/Api/Users/UserNavbarSettings.cs
@@ -0,0 +1,13 @@
+using ActualChat.Kvas;
+using MemoryPack;
+
+namespace ActualChat.Users;
+
+[DataContract, MemoryPackable(GenerateType.VersionTolerant)]
+public sealed partial record UserNavbarSettings : IHasOrigin
+{
+    public const string KvasKey = nameof(UserNavbarSettings);
+
+    [DataMember, MemoryPackOrder(0)] public string Origin { get; init; } = "";
+    [DataMember, MemoryPackOrder(1)] public ApiArray<ChatId> PinnedChats { get; init; }
+}
