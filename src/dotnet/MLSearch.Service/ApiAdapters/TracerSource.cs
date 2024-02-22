c90130e542f11041432aaf04817c720aeab0f568
Andrew Kurochkin <andrew.kurochkin@actual.chat>
2024-02-22 15:21:04 +0100

    feat: tracing. add extension method to support no-tracing setup

--- src/dotnet/MLSearch.Service/ApiAdapters/TracerSource.cs
+++ src/dotnet/MLSearch.Service/ApiAdapters/TracerSource.cs
@@ -8,3 +8,14 @@ internal class TracerSource : ITracerSource
 {
     public Tracer GetTracer() => Tracer.Default;
 }
+
+internal static class TracerSourceExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TraceRegion? TraceRegion(this ITracerSource? tracing)
+    {
+        // This method is made as an extension to handle nulls
+        var tracer = tracing?.GetTracer();
+        return tracer?.Region();
+    }
+}
