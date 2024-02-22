faae413975c378c068ad385c912c1469b353c8dc
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-02-22 13:58:06 +0300

    feat: add ability to reorder pinned chats

--- src/dotnet/Chat.UI.Blazor/Components/Navbar/navbar-chat-buttons.ts
+++ src/dotnet/Chat.UI.Blazor/Components/Navbar/navbar-chat-buttons.ts
@@ -1,17 +1,28 @@
 import { Subject } from 'rxjs';
+import Sortable, { SortableEvent } from 'sortablejs';
 
 export class NavbarChatButtons {
     private readonly disposed$: Subject<void> = new Subject<void>();
     private blazorRef: DotNet.DotNetObject;
-    private readonly chats: HTMLElement;
+    private readonly list: HTMLElement;
+    private readonly sortable: Sortable;
 
-    static create(places: HTMLElement, blazorRef: DotNet.DotNetObject): NavbarChatButtons {
-        return new NavbarChatButtons(places, blazorRef);
+    static create(list: HTMLElement, blazorRef: DotNet.DotNetObject): NavbarChatButtons {
+        return new NavbarChatButtons(list, blazorRef);
     }
 
-    constructor(chats: HTMLElement, blazorRef: DotNet.DotNetObject) {
-        this.chats = chats;
+    constructor(list: HTMLElement, blazorRef: DotNet.DotNetObject) {
+        this.list = list;
         this.blazorRef = blazorRef;
+        this.sortable = Sortable.create(
+            this.list,
+            {
+                animation: 150,
+                onUpdate: (_: SortableEvent) => {
+                    const chats = Array.from(this.list.children).map((x: HTMLElement) => x.dataset['chatId']);
+                    void this.blazorRef.invokeMethodAsync('OnOrderChanged', chats);
+                }
+            });
     }
 
     public dispose() {
@@ -20,5 +31,6 @@ export class NavbarChatButtons {
 
         this.disposed$.next();
         this.disposed$.complete();
+        this.sortable.destroy();
     }
 }
