c7b2c1f530c531d500fdf2a0462ab78fe04a8c52
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-02-22 14:26:43 +0300

    feat: force verify email step

--- src/dotnet/Api/Users/UserOnboardingSettings.cs
+++ src/dotnet/Api/Users/UserOnboardingSettings.cs
@@ -15,10 +15,12 @@ public sealed partial record UserOnboardingSettings : IHasOrigin
     [DataMember, MemoryPackOrder(2)] public Moment LastShownAt { get; init; }
     [DataMember, MemoryPackOrder(3)] public string Origin { get; init; } = "";
     [DataMember, MemoryPackOrder(4)] public bool IsCreateChatsStepCompleted { get; init; }
+    [Obsolete("Use IsVerifyEmailStepCompleted")]
     [DataMember, MemoryPackOrder(5)] public bool IsEmailStepCompleted { get; init; }
     [DataMember, MemoryPackOrder(6)] public bool IsVerifyPhoneStepCompleted { get; init; }
     [Obsolete("Use LocalOnboardingSettings.IsPermissionsStepCompleted.")]
     [DataMember, MemoryPackOrder(7)] public bool IsPermissionsStepCompleted { get; init; }
+    [DataMember, MemoryPackOrder(8)] public bool IsVerifyEmailStepCompleted { get; init; }
 
     [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
     public bool HasUncompletedSteps
@@ -26,7 +28,7 @@ public sealed partial record UserOnboardingSettings : IHasOrigin
             IsAvatarStepCompleted: true,
             IsVerifyPhoneStepCompleted: true,
             IsCreateChatsStepCompleted: true,
-            IsEmailStepCompleted: true,
+            IsVerifyEmailStepCompleted: true,
             // IsPermissionsStepCompleted: true,
         };
 }
