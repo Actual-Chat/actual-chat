3fd1b0d1f505e68a35a44b87113442e15247f24a
Alex Yakunin <alex.yakunin@gmail.com>
2024-02-25 19:53:26 -0800

    refactor: ServiceModeAttribute.cs

--- src/dotnet/Core/Attributes/ServiceModeAttribute.cs
+++ src/dotnet/Core/Attributes/ServiceModeAttribute.cs
@@ -3,9 +3,9 @@ using ActualChat.Hosting;
 namespace ActualChat.Attributes;
 
 [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class, AllowMultiple = true)]
-public sealed class ServiceModeAttribute(string hostRole, ServiceMode serviceMode, double priority = 0) : Attribute
+public sealed class ServiceModeAttribute(string hostRole, ServiceMode serviceMode) : Attribute
 {
     public string HostRole { get; } = hostRole;
     public ServiceMode ServiceMode { get; } = serviceMode;
-    public double Priority { get; } = priority;
+    public double Priority { get; init; }
 }
--- src/dotnet/Streaming.Contracts/AssemblyAttributes.cs
+++ src/dotnet/Streaming.Contracts/AssemblyAttributes.cs
@@ -1,5 +1,5 @@
 using ActualChat.Attributes;
 using ActualChat.Hosting;
 
-[assembly: ServiceMode(nameof(HostRole.SingleServer), ServiceMode.Mixed, 1)]
+[assembly: ServiceMode(nameof(HostRole.SingleServer), ServiceMode.Mixed, Priority = 1)]
 [assembly: ServiceMode(nameof(HostRole.AudioBackend), ServiceMode.Server)]
