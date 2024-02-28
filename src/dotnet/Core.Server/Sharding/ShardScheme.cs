e7ca568f3bd19d2ad2f8cf43a18e95f493167687
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-02-28 17:03:12 +0300

    feat: add BackendService attributes for backend contract assemblies

--- src/dotnet/Core.Server/Sharding/ShardScheme.cs
+++ src/dotnet/Core.Server/Sharding/ShardScheme.cs
@@ -14,6 +14,13 @@ public sealed class ShardScheme(Symbol id, int shardCount, HostRole hostRole) : 
     public static readonly ShardScheme AnyServer = new(nameof(AnyServer), 10, HostRole.AnyServer); // Mostly for testing
     public static readonly ShardScheme MediaBackend = new(nameof(MediaBackend), 10, HostRole.MediaBackend);
     public static readonly ShardScheme AudioBackend = new(nameof(AudioBackend), 10, HostRole.AudioBackend);
+    public static readonly ShardScheme ChatBackend = new(nameof(ChatBackend), 30, HostRole.ChatBackend);
+    public static readonly ShardScheme ContactsBackend = new(nameof(ContactsBackend), 10, HostRole.ContactsBackend);
+    public static readonly ShardScheme InviteBackend = new(nameof(InviteBackend), 1, HostRole.InviteBackend);
+    public static readonly ShardScheme NotificationBackend = new(nameof(NotificationBackend), 10, HostRole.NotificationBackend);
+    public static readonly ShardScheme SearchBackend = new(nameof(SearchBackend), 10, HostRole.SearchBackend);
+    public static readonly ShardScheme TranscriptionBackend = new(nameof(TranscriptionBackend), 10, HostRole.TranscriptionBackend);
+    public static readonly ShardScheme UsersBackend = new(nameof(UsersBackend), 10, HostRole.UsersBackend);
     public static readonly ShardScheme ContactIndexingWorker = new(nameof(ContactIndexingWorker), 1, HostRole.ContactIndexingWorker);
     public static readonly ShardScheme DefaultQueue = new(nameof(DefaultQueue), 1, HostRole.DefaultQueue);
     public static readonly ShardScheme EventQueue = new(nameof(EventQueue), 1, HostRole.EventQueue);
@@ -24,6 +31,13 @@ public sealed class ShardScheme(Symbol id, int shardCount, HostRole hostRole) : 
         { AnyServer.Id, AnyServer },
         { MediaBackend.Id, MediaBackend },
         { AudioBackend.Id, AudioBackend },
+        { ChatBackend.Id, ChatBackend },
+        { ContactsBackend.Id, ContactsBackend },
+        { InviteBackend.Id, InviteBackend },
+        { NotificationBackend.Id, NotificationBackend },
+        { SearchBackend.Id, SearchBackend },
+        { TranscriptionBackend.Id, TranscriptionBackend },
+        { UsersBackend.Id, UsersBackend },
         { ContactIndexingWorker.Id, ContactIndexingWorker },
         // Queues
         { DefaultQueue.Id, DefaultQueue },
