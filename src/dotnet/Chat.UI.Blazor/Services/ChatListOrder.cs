926b97f90f177456593d69c0559c8a398d24e178
Dmitry Filippov <crui3er@gmail.com>
2024-03-05 14:32:27 +0400

    feat: keep ChatListSettings individually by place, save ChatListSettings to LocalSettings

--- src/dotnet/Chat.UI.Blazor/Services/ChatListOrder.cs
+++ src/dotnet/Chat.UI.Blazor/Services/ChatListOrder.cs
@@ -20,8 +20,8 @@ public static class ChatListOrderExt
     public static string GetIcon(this ChatListOrder order)
         => order switch {
             ChatListOrder.ByLastEventTime => "icon-sort-by-recent",
-            ChatListOrder.ByOwnUpdateTime => "icon-sort-by-message",
-            ChatListOrder.ByUnreadCount => "icon-sort-by-recent",
+            ChatListOrder.ByOwnUpdateTime => "icon-sort-by-me",
+            ChatListOrder.ByUnreadCount => "icon-sort-by-message",
             ChatListOrder.ByAlphabet => "icon-sort-by-alphabet",
             _ => throw new ArgumentOutOfRangeException(nameof(order)),
         };
