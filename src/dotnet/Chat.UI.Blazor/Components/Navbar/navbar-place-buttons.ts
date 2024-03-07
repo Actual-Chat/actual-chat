514a05b174f71469270fda71a0b3a08924a1ef98
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-03-07 13:26:44 +0300

    feat: use sortablejs to reorder places

--- src/dotnet/Chat.UI.Blazor/Components/Navbar/navbar-place-buttons.ts
+++ src/dotnet/Chat.UI.Blazor/Components/Navbar/navbar-place-buttons.ts
@@ -1,30 +1,42 @@
 import { Subject } from 'rxjs';
-import { clearTimeout, setTimeout } from 'timerQueue';
+import Sortable, { SortableEvent } from 'sortablejs';
+import { DeviceInfo } from 'device-info';
 
 export class NavbarPlaceButtons {
     private readonly disposed$: Subject<void> = new Subject<void>();
     private blazorRef: DotNet.DotNetObject;
-    private readonly places: HTMLElement;
-    private buttons: HTMLElement[] = [];
-    private placeIds: string[] = [];
-    private shiftY: number;
-    private currentBtn: HTMLElement;
-    private ghostBtn: HTMLElement;
-    private topLimit: number;
-    private bottomLimit: number;
-    private readonly longTap: number = 500;
-    private delay: number;
-    private placeListObserver: MutationObserver;
-    private isScrolling: boolean;
+    private readonly list: HTMLElement;
+    private readonly sortable: Sortable;
 
-    static create(places: HTMLElement, blazorRef: DotNet.DotNetObject): NavbarPlaceButtons {
-        return new NavbarPlaceButtons(places, blazorRef);
+    static create(list: HTMLElement, blazorRef: DotNet.DotNetObject): NavbarPlaceButtons {
+        return new NavbarPlaceButtons(list, blazorRef);
     }
 
-    constructor(places: HTMLElement, blazorRef: DotNet.DotNetObject) {
-        this.places = places;
+    constructor(list: HTMLElement, blazorRef: DotNet.DotNetObject) {
+        this.list = list;
         this.blazorRef = blazorRef;
-        this.updatePlaceState(false);
+
+        const options: Sortable.Options = DeviceInfo.isMobile
+            ? {
+                animation: 150,
+                delay: 500,
+                dragClass: 'sortable-target',
+                chosenClass: 'sortable-target',
+                delayOnTouchOnly: true,
+                onUpdate: (_: SortableEvent) => {
+                    const places = Array.from(this.list.children).map((x: HTMLElement) => x.dataset['placeId']);
+                    void this.blazorRef.invokeMethodAsync('OnOrderChanged', places);
+                },
+            }
+            : {
+                animation: 150,
+                handle: '.c-dots',
+                onUpdate: (_: SortableEvent) => {
+                    const places = Array.from(this.list.children).map((x: HTMLElement) => x.dataset['placeId']);
+                    void this.blazorRef.invokeMethodAsync('OnOrderChanged', places);
+                },
+            };
+        this.sortable = Sortable.create(this.list, options);
     }
 
     public dispose() {
@@ -33,199 +45,6 @@ export class NavbarPlaceButtons {
 
         this.disposed$.next();
         this.disposed$.complete();
-        this.places.removeEventListener('pointerdown', this.onPointerDown);
-        window.removeEventListener('pointermove', this.onPointerMove);
-        window.removeEventListener('pointerup', this.onPointerUp);
-        window.removeEventListener('pointercancel', this.onPointerUp);
-        if (this.currentBtn && this.currentBtn.classList.contains('drag-btn'))
-            this.currentBtn.classList.remove('drag-btn');
-        this.placeListObserver.disconnect();
-    }
-
-    private updatePlaceList = (mutationList, observer) => {
-        mutationList.forEach(m => {
-            m.addedNodes.forEach(element => {
-                if (element
-                    && element.classList
-                    && element.classList.contains('navbar-button')
-                    && !element.classList.contains('ghost-btn')
-                    && this.buttons.length != this.places.children.length) {
-                    this.updatePlaceState(true);
-                }
-            });
-            m.removedNodes.forEach(element => {
-                if (element
-                    && element.classList
-                    && element.classList.contains('navbar-button')
-                    && !element.classList.contains('ghost-btn')
-                    && this.buttons.length != this.places.children.length) {
-                    this.updatePlaceState(true);
-                }
-            });
-        });
-    }
-
-    private updatePlaceState(dispose: boolean) {
-        if (dispose) {
-            this.dispose();
-        }
-        this.updatePlaceOrder();
-        for (const btn of this.buttons) {
-            btn.style.touchAction = 'none';
-            btn.ondragstart = () => false;
-        }
-        this.places.addEventListener('pointerdown', this.onPointerDown);
-        this.placeListObserver = new MutationObserver(this.updatePlaceList);
-        this.placeListObserver.observe(this.places, {
-            attributes: true,
-            childList: true,
-            subtree: true,
-        });
-    }
-
-    private scrollUp() {
-        let delta = Math.round(this.places.scrollTop);
-        if (this.isScrolling && delta > 2) {
-            setTimeout(() => {
-                this.places.scrollBy(0, -10);
-                this.scrollUp();
-            }, 25);
-        } else {
-            return;
-        }
-    }
-
-    private scrollDown() {
-        let delta = Math.round(this.places.scrollHeight - this.places.scrollTop - this.places.clientHeight);
-        if (this.isScrolling && delta > 2) {
-            setTimeout(() => {
-                this.places.scrollBy(0, 10);
-                this.scrollDown();
-            }, 25);
-        } else {
-            return;
-        }
-    }
-
-    private updatePlaceOrder() {
-        this.buttons = [];
-        let navbarButtons = this.places.querySelectorAll('.navbar-button');
-        navbarButtons.forEach(b => {
-            let btn = b as HTMLElement;
-            this.buttons.push(btn);
-        });
-        this.placeIds = this.getPlacesOrdering();
-    }
-
-    private onPointerDown = (event: PointerEvent) => {
-        event.preventDefault();
-        for (const btn of this.buttons) {
-            if (btn.contains(event.target as HTMLElement)) {
-                this.currentBtn = btn;
-                window.addEventListener('pointerup', this.onPointerUp);
-                window.addEventListener('pointercancel', this.onPointerUp);
-                break;
-            }
-        }
-        if (this.currentBtn == null)
-            return;
-        this.delay = setTimeout(() => {
-            this.onLongTap(event);
-        }, this.longTap);
-    }
-
-    private onLongTap = (event: PointerEvent) => {
-        event.preventDefault();
-        this.addGhostBtn(event);
-        window.addEventListener('pointermove', this.onPointerMove);
-    }
-
-    private onPointerUp = (event: PointerEvent) => {
-        event.preventDefault();
-        if (this.delay)
-            clearTimeout(this.delay);
-        if (this.ghostBtn) {
-            this.ghostBtn.remove();
-        }
-        this.currentBtn.classList.remove('drag-btn');
-        window.removeEventListener('pointermove', this.onPointerMove);
-        window.removeEventListener('pointerup', this.onPointerUp);
-        window.removeEventListener('pointercancel', this.onPointerUp);
-
-        void this.saveOnBackend();
-    }
-
-    private getPlacesOrdering() : string[] {
-        let placeIds = [];
-        this.buttons.forEach(b => {
-            let btnId = b.getAttribute('data-place-id');
-            placeIds.push(btnId);
-        });
-        return placeIds;
-    }
-
-    private async saveOnBackend() : Promise<void> {
-        await this.blazorRef.invokeMethodAsync("StorePlacesOrder", this.placeIds);
-    }
-
-    private onPointerMove = (event: PointerEvent) => {
-        event.preventDefault();
-        let ghostRect = this.ghostBtn.getBoundingClientRect();
-        let top = event.y - this.shiftY;
-        if (top <= this.topLimit) {
-            this.ghostBtn.style.top = this.topLimit + 'px';
-            if (!this.isScrolling) {
-                this.isScrolling = true;
-                this.scrollUp();
-            }
-        } else if (top >= this.bottomLimit - ghostRect.height) {
-            this.ghostBtn.style.top = this.bottomLimit - ghostRect.height + 'px';
-            if (!this.isScrolling) {
-                this.isScrolling = true;
-                this.scrollDown();
-            }
-        } else {
-            this.ghostBtn.style.top = top + 'px';
-            this.isScrolling = false;
-        }
-        this.replaceButton(ghostRect);
-    }
-
-    private replaceButton = (ghostRect: DOMRect) => {
-        for (const btn of this.buttons) {
-            let rect = btn.getBoundingClientRect();
-            let topDelta = Math.abs(rect.top - ghostRect.top);
-            let bottomDelta = Math.abs(rect.bottom - ghostRect.bottom);
-            if (topDelta < 10 && bottomDelta < 10 && !btn.classList.contains('drag-btn')) {
-                let currentBtnIndex = this.buttons.indexOf(this.currentBtn);
-                let btnIndex = this.buttons.indexOf(btn);
-                if (currentBtnIndex < btnIndex) {
-                    this.places.insertBefore(btn, this.currentBtn);
-                } else {
-                    this.places.insertBefore(this.currentBtn, btn);
-                }
-                this.updatePlaceOrder();
-            }
-        }
-    }
-
-    private addGhostBtn = (event: PointerEvent) => {
-        this.ghostBtn = document.createElement('div');
-        this.ghostBtn.classList.add('ghost-btn');
-
-        let btn = this.currentBtn.cloneNode(true) as HTMLElement;
-        let rect = this.currentBtn.getBoundingClientRect();
-        this.shiftY = (event.y - rect.top) / 2;
-        this.ghostBtn.appendChild(btn);
-        document.body.appendChild(this.ghostBtn);
-
-        this.currentBtn.classList.add('drag-btn');
-        this.ghostBtn.style.left = rect.left + 'px';
-        this.ghostBtn.style.top = rect.top + 'px';
-
-        let placesRect = this.places.getBoundingClientRect();
-        this.topLimit = placesRect.top - 5;
-        this.bottomLimit = placesRect.bottom + 10;
+        this.sortable.destroy();
     }
 }
-
