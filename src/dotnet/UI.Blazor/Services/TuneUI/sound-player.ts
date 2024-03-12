78eeac1ca652619bd0b6498cfef68e9731bff6f3
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-03-12 20:36:09 +0300

    feat: do not play same sounds too often

--- src/dotnet/UI.Blazor/Services/TuneUI/sound-player.ts
+++ src/dotnet/UI.Blazor/Services/TuneUI/sound-player.ts
@@ -3,13 +3,21 @@ import { audioContextSource } from '../../../Streaming.UI.Blazor/Services/audio-
 import { Log } from 'logging';
 
 const { debugLog, warnLog } = Log.get('SoundsPlayer');
+const PLAYBACK_COOLDOWN = 30 * 1000; // 30s
 
 export class SoundPlayer {
     private readonly buffers = new Map<string, AudioBuffer>();
+    private readonly recentlyPlayedMap = new Map<string, number>;
     private context?: AudioContext;
 
     public async play(url: string): Promise<void>{
         debugLog?.log('-> play', url);
+        const lastPlayedAt = this.recentlyPlayedMap.get(url) ?? 0;
+        const playedAt = Date.now();
+        this.recentlyPlayedMap.set(url, playedAt);
+        if (playedAt - lastPlayedAt <= PLAYBACK_COOLDOWN)
+            return; // do not play same sound too often
+
         const audioContextRef = audioContextSource.getRef('play-tunes', {
             attach: context => this.onAttach(context),
             detach: context => this.onDetach(context),
