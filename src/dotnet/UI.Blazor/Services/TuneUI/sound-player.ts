f4d3bde726a0223d3e5122d312b0fc39e9a2d973
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-03-13 15:09:08 +0300

    feat: tune-specific cooldown

--- src/dotnet/UI.Blazor/Services/TuneUI/sound-player.ts
+++ src/dotnet/UI.Blazor/Services/TuneUI/sound-player.ts
@@ -3,19 +3,19 @@ import { audioContextSource } from '../../../Streaming.UI.Blazor/Services/audio-
 import { Log } from 'logging';
 
 const { debugLog, warnLog } = Log.get('SoundsPlayer');
-const PLAYBACK_COOLDOWN = 30 * 1000; // 30s
+const DEFAULT_COOLDOWN = 3; // 3s
 
 export class SoundPlayer {
     private readonly buffers = new Map<string, AudioBuffer>();
     private readonly recentlyPlayedMap = new Map<string, number>;
     private context?: AudioContext;
 
-    public async play(url: string): Promise<void>{
+    public async play(url: string, cooldown?: number): Promise<void> {
         debugLog?.log('-> play', url);
         const lastPlayedAt = this.recentlyPlayedMap.get(url) ?? 0;
         const playedAt = Date.now();
         this.recentlyPlayedMap.set(url, playedAt);
-        if (playedAt - lastPlayedAt <= PLAYBACK_COOLDOWN)
+        if (playedAt - lastPlayedAt <= (cooldown ?? DEFAULT_COOLDOWN) * 1000)
             return; // do not play same sound too often
 
         const audioContextRef = audioContextSource.getRef('play-tunes', {
