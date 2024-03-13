f66d8f3dc912ac3fb2637c68b1f1fe3ad1721eee
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-03-13 18:43:04 +0300

    feat: shared resource pool is async disposable now

--- src/dotnet/Core/Pooling/SharedResourcePool.cs
+++ src/dotnet/Core/Pooling/SharedResourcePool.cs
@@ -1,13 +1,14 @@
 namespace ActualChat.Pooling;
 
-// TODO(AY): Make it IAsyncDisposable
 public partial class SharedResourcePool<TKey, TResource>(
     Func<TKey, CancellationToken, Task<TResource>> resourceFactory,
-    Func<TKey, TResource, ValueTask>? resourceDisposer = null)
+    Func<TKey, TResource, ValueTask>? resourceDisposer = null) : IAsyncDisposable
     where TKey : notnull
     where TResource : class
 {
     private readonly ConcurrentDictionary<TKey, Lease> _leases = new ();
+    private volatile bool _isDisposed;
+
     private Func<TKey, CancellationToken, Task<TResource>> ResourceFactory { get; } = resourceFactory;
     private Func<TKey, TResource, ValueTask> ResourceDisposer { get; } = resourceDisposer ?? DefaultResourceDisposer;
 
@@ -16,6 +17,9 @@ public partial class SharedResourcePool<TKey, TResource>(
 
     public async ValueTask<Lease> Rent(TKey key, CancellationToken cancellationToken = default)
     {
+        if (_isDisposed)
+            throw new ObjectDisposedException(nameof(SharedResourcePool<TKey, TResource>));
+
         while (true) {
             var lease = _leases.GetOrAdd(key, static (key1, state) => {
                 var (self, cancellationToken1) = state;
@@ -27,6 +31,30 @@ public partial class SharedResourcePool<TKey, TResource>(
 
             await endRentTask.WaitAsync(cancellationToken).ConfigureAwait(false);
         }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        _isDisposed = true;
+        Thread.MemoryBarrier();
+
+        while (_leases.Count > 0)
+            try {
+                List<TKey> keys = [.._leases.Keys];
+                foreach (var key in keys) {
+                    if (!_leases.TryRemove(key, out var lease))
+                        continue;
+
+                    await ResourceDisposer.Invoke(key, lease.Resource).ConfigureAwait(false);
+                }
+            }
+            catch (Exception e) {
+                var log = Log == NullLogger.Instance
+                    ? DefaultLog
+                    : Log;
+
+                log.LogError(e, $"Error disposing '{nameof(SharedResourcePool<TKey, TResource>)}'");
+            }
     }
 
     private static async ValueTask DefaultResourceDisposer(TKey key, TResource resource)
