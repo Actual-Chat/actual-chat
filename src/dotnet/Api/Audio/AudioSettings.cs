4dc80bb04391e5877a0a7a380675bf0a22c52a6f
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-03-20 18:17:00 +0300

    feat: tune on new audio message after long delay

--- src/dotnet/Api/Audio/AudioSettings.cs
+++ src/dotnet/Api/Audio/AudioSettings.cs
@@ -7,6 +7,7 @@ public sealed class AudioSettings
     public TimeSpan IdleRecordingPreCountdownTimeout { get; init; } = TimeSpan.FromSeconds(20); // 10s to count
     public TimeSpan IdleListeningCheckPeriod { get; init; } = TimeSpan.FromSeconds(3); // Not critical to stop it at the exact time
     public TimeSpan IdleListeningTimeout { get; init; } = TimeSpan.FromSeconds(60);
+    public TimeSpan IdleListeningNewMessageTrigger { get; init; } = TimeSpan.FromMinutes(5);
     public TimeSpan IdleListeningPreCountdownTimeout
         // There is no actual countdown, all we need here is to make sure it starts to make the next check fail
         => IdleListeningTimeout - IdleListeningCheckPeriod + TimeSpan.FromSeconds(1);
