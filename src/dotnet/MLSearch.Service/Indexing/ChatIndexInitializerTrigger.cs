f197ca086f019a8109c763a0a4fb1a0c19851355
Alex Ustinov <alex.ustinov@actual.chat>
2024-03-25 21:41:44 -0700

    fix: OnCommand method is not virtual

--- src/dotnet/MLSearch.Service/Indexing/ChatIndexInitializerTrigger.cs
+++ src/dotnet/MLSearch.Service/Indexing/ChatIndexInitializerTrigger.cs
@@ -4,6 +4,6 @@ namespace ActualChat.MLSearch.Indexing;
 internal class ChatIndexInitializerTrigger(IChatIndexInitializer indexInitializer)
     : IChatIndexInitializerTrigger, IComputeService
 {
-    public async Task OnCommand(MLSearch_TriggerChatIndexingCompletion e, CancellationToken cancellationToken)
+    public virtual async Task OnCommand(MLSearch_TriggerChatIndexingCompletion e, CancellationToken cancellationToken)
         => await indexInitializer.PostAsync(e, cancellationToken).ConfigureAwait(false);
 }
