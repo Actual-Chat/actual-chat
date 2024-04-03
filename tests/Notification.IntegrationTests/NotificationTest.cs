68532129790d50afa1aab6df25b905d33bf51cbb
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-04-03 18:47:36 +0300

    fix: handle version mismatch for concurrent notifications

--- tests/Notification.IntegrationTests/NotificationTest.cs
+++ tests/Notification.IntegrationTests/NotificationTest.cs
@@ -3,7 +3,7 @@ using ActualChat.Testing.Host;
 namespace ActualChat.Notification.IntegrationTests;
 
 [Collection(nameof(NotificationCollection))]
-public class NotificationUpsertTest(AppHostFixture fixture, ITestOutputHelper @out)
+public class NotificationTest(AppHostFixture fixture, ITestOutputHelper @out)
     : SharedAppHostTestBase<AppHostFixture>(fixture, @out)
 {
     [Fact]
@@ -31,5 +31,29 @@ public class NotificationUpsertTest(AppHostFixture fixture, ITestOutputHelper @o
         Out.WriteLine(string.Join(", ", results));
 
         results.Should().ContainSingle(r => r);
+    }
+
+    [Fact]
+    public async Task ParallelNotificationsAreSafe()
+    {
+        var appHost = AppHost;
+        await using var tester = appHost.NewBlazorTester();
+        var account = await tester.SignInAsBob();
+
+        var notificationId = new NotificationId(account.Id, NotificationKind.Message, Constants.Chat.DefaultChatId);
+        var notification = new Notification(notificationId) {
+            Title = "Notify",
+            Content = "Hello",
+        };
+
+        var tasks = new List<Task>();
+        for (int i = 0; i < 20; i++) {
+            var upsert = new NotificationsBackend_Notify(notification);
+            // ReSharper disable once AccessToDisposedClosure
+            var task = Task.Run(() => tester.Commander.Call(upsert, true));
+            tasks.Add(task);
+        }
+
+        await Task.WhenAll(tasks);
     }
 }
