7ab11ac691ce5bb925f45be80d49226a291ed3db
Alex Ustinov <alex.ustinov@actual.chat>
2024-04-08 15:12:49 -0700

    chore: get rid of NotImplemented warnings in MLSearchChatId

--- src/dotnet/MLSearch/MLSearchChatId.cs
+++ src/dotnet/MLSearch/MLSearchChatId.cs
@@ -2,21 +2,32 @@ using MemoryPack;
 
 namespace ActualChat.MLSearch;
 
+// TODO: this is a stub, remove or implement properly
 [DataContract, MemoryPackable(GenerateType.VersionTolerant)]
 public readonly partial struct MLSearchChatId : ISymbolIdentifier<MLSearchChatId>
 {
-    public static MLSearchChatId None => throw new NotImplementedException();
+    public static MLSearchChatId None => default;
 
     [DataMember(Order = 0), MemoryPackOrder(0)]
-    public Symbol Id => throw new NotImplementedException();
+    public Symbol Id { get; }
 
     [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
-    public string Value => throw new NotImplementedException();
+    public string Value => Id.Value;
     [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
-    public bool IsNone => throw new NotImplementedException();
+    public bool IsNone => Id.IsEmpty;
 
-    public static MLSearchChatId Parse(string? s) => throw new NotImplementedException();
-    public static MLSearchChatId ParseOrNone(string s) => throw new NotImplementedException();
-    public static bool TryParse(string? s, out MLSearchChatId result) => throw new NotImplementedException();
-    public bool Equals(MLSearchChatId other) => throw new NotImplementedException();
-}
+    public static MLSearchChatId Parse(string? s)
+        => TryParse(s, out var result) ? result : throw StandardError.Format<MLSearchChatId>(s);
+    public static MLSearchChatId ParseOrNone(string s)
+        => TryParse(s, out var result) ? result : None;
+    public static bool TryParse(string? s, out MLSearchChatId result)
+    {
+        result = default;
+        return false;
+    }
+
+    public bool Equals(MLSearchChatId other) => Id == other.Id;
+    public override bool Equals(object? obj) => obj is MLSearchChatId other && Equals(other);
+    public override int GetHashCode() => Id.GetHashCode();
+    public static bool operator ==(MLSearchChatId left, MLSearchChatId right) => left.Equals(right);
+    public static bool operator !=(MLSearchChatId left, MLSearchChatId right) => !left.Equals(right);}
