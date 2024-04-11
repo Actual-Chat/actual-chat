50630309c64c4ed9c60565919a52192219343915
Alex Yakunin <alex.yakunin@gmail.com>
2024-04-11 03:27:59 -0700

    fix: session update on check-in

--- src/dotnet/Users.Service/UserPresences.cs
+++ src/dotnet/Users.Service/UserPresences.cs
@@ -2,7 +2,10 @@ namespace ActualChat.Users;
 
 public class UserPresences(IServiceProvider services) : IUserPresences
 {
+    private static readonly TimeSpan SessionUpdatePeriod = TimeSpan.FromHours(1);
+
     private IUserPresencesBackend Backend { get; } = services.GetRequiredService<IUserPresencesBackend>();
+    private IAuth Auth { get; } = services.GetRequiredService<IAuth>();
     private IAccounts Accounts { get; } = services.GetRequiredService<IAccounts>();
     private ICommander Commander { get; } = services.Commander();
     private MomentClockSet Clocks { get; } = services.Clocks();
@@ -23,6 +26,12 @@ public class UserPresences(IServiceProvider services) : IUserPresences
             return;
 
         var (session, isActive) = command;
+
+        var sessionInfo = await Auth.GetSessionInfo(session, cancellationToken).ConfigureAwait(false);
+        if (sessionInfo != null && SystemNow - sessionInfo.LastSeenAt > SessionUpdatePeriod) {
+            var setupSessionCommand = new AuthBackend_SetupSession(session);
+            await Commander.Call(setupSessionCommand, true, cancellationToken).ConfigureAwait(false);
+        }
 
         var account = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
         if (!account.IsActive())
