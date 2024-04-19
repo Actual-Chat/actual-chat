70ca404b240ef6b9d4c9da6b972562d4d0933b12
Alex Ustinov <alex.ustinov@actual.chat>
2024-04-18 21:44:39 -0700

    wip: switch chat bot to new chat update extractor

--- src/dotnet/MLSearch.Service/Bot/SampleChatBot.cs
+++ src/dotnet/MLSearch.Service/Bot/SampleChatBot.cs
@@ -12,8 +12,8 @@ internal class SampleChatBot(ICommander commander, IChatsBackend chats, ISearchE
     protected virtual string NotFoundMessage()
         => "Was not able to find anything";
 
-    public async Task ExecuteAsync(IEnumerable<ChatEntry>? updatedDocuments, IEnumerable<ChatEntryId>? deletedDocuments, CancellationToken cancellationToken) {
-
+    public async Task ExecuteAsync(IEnumerable<ChatEntry>? updatedDocuments, IEnumerable<ChatEntryId>? deletedDocuments, CancellationToken cancellationToken)
+    {
         if (updatedDocuments == null) {
             return;
         }
