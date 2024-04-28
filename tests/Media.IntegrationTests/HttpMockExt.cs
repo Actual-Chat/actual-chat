63b3fd8333a452c51caa347c7944c12fa831f7a2
Frol <4425748+FrolYo@users.noreply.github.com>
2024-04-28 13:51:37 +0300

    chore: fix/extend crawler tests #2019

--- tests/Media.IntegrationTests/HttpMockExt.cs
+++ tests/Media.IntegrationTests/HttpMockExt.cs
@@ -5,7 +5,10 @@ namespace ActualChat.Media.IntegrationTests;
 
 public static class HttpMockExt
 {
-    public static HttpHandlerMock SetupHtmlResponse(this HttpHandlerMock mock, string url, Action<OpenGrapHtmlBuilder> htmlBuilder)
+    public static HttpHandlerMock SetupHtml(
+        this HttpHandlerMock mock,
+        string url,
+        Action<OpenGrapHtmlBuilder> htmlBuilder)
     {
         var builder = new OpenGrapHtmlBuilder();
         htmlBuilder(builder);
@@ -16,13 +19,54 @@ public static class HttpMockExt
             });
     }
 
-    public static HttpHandlerMock SetupImageResponse(this HttpHandlerMock mock, string url, string resourceName = "default.jpg", string contentType = "image/jpeg")
-        => mock.Setup(url, req => new (HttpStatusCode.OK) {
-            RequestMessage = req,
-            Content = new StreamContent(GetImgStream(resourceName)) {
-                Headers = { ContentType = MediaTypeHeaderValue.Parse(contentType) },
-            },
-        });
+    public static HttpHandlerMock SetupImage(
+        this HttpHandlerMock mock,
+        string url,
+        string resourceName = "default.jpg",
+        string contentType = "image/jpeg")
+        => mock.Setup(url,
+            req => new (HttpStatusCode.OK) {
+                RequestMessage = req,
+                Content = new StreamContent(GetImgStream(resourceName)) {
+                    Headers = { ContentType = MediaTypeHeaderValue.Parse(contentType) },
+                },
+            });
+
+    public static HttpHandlerMock SetupRobotsNotFound(this HttpHandlerMock mock, string url)
+        => mock.Setup(GetRobotsUrl(url),
+            req => new (HttpStatusCode.NotFound) {
+                RequestMessage = req,
+            });
+
+    public static HttpHandlerMock SetupEmptyRobots(this HttpHandlerMock mock, string url)
+        => mock.Setup(GetRobotsUrl(url),
+            req => new (HttpStatusCode.OK) {
+                RequestMessage = req,
+                Content = new StringContent("", MediaTypeHeaderValue.Parse("text/plain")),
+            });
+
+    public static HttpHandlerMock SetupAllAllowedRobots(this HttpHandlerMock mock, string url)
+        => mock.Setup(GetRobotsUrl(url),
+            req => new (HttpStatusCode.OK) {
+                RequestMessage = req,
+                Content = new StringContent("""
+                                            User-agent: *
+                                            """, MediaTypeHeaderValue.Parse("text/plain")),
+            });
+
+    public static HttpHandlerMock SetupAllDisallowedRobots(this HttpHandlerMock mock, string url)
+        => mock.Setup(GetRobotsUrl(url),
+            req => new (HttpStatusCode.OK) {
+                RequestMessage = req,
+                Content = new StringContent("""
+                                            User-agent: *
+                                            Disallow: /
+                                            """,
+                    MediaTypeHeaderValue.Parse("text/plain")),
+            });
+
+    private static string GetRobotsUrl(string url)
+        => new Uri(new Uri(url), "/robots.txt").AbsoluteUri;
 
     private static Stream GetImgStream(string name)
     {
