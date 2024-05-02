e1588298aa039f792decd8854591ccb0cfb46023
Alex Yakunin <alex.yakunin@gmail.com>
2024-05-02 06:51:31 -0700

    feat: add UseAppPack option (false by default)

--- clean.cmd
+++ clean.cmd
@@ -3,6 +3,7 @@
     rmdir /S /Q artifacts\bin
     rmdir /S /Q artifacts\obj
     rmdir /S /Q artifacts\out
+    rmdir /S /Q artifacts\repacked
     rmdir /S /Q artifacts\publish
     rmdir /S /Q artifacts\tests
     dotnet restore ActualChat.sln
@@ -15,7 +16,8 @@ dotnet build-server shutdown
 rmdir artifacts/bin
 rmdir artifacts/obj
 rmdir artifacts/out
-rmdir artifacts/tests
+rmdir artifacts/repacked
 rmdir artifacts/publish
+rmdir artifacts/tests
 dotnet restore ActualChat.sln
 echo "Clean completed."
