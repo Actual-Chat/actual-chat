19c6fb97b3dbe559d3349b861c66be13d06d3799
Alex Yakunin <alex.yakunin@gmail.com>
2024-05-01 17:04:28 -0700

    chore: tracing-related .cmd changes

--- src/dotnet/App.Maui/Build-Tracing-AR.cmd
+++ src/dotnet/App.Maui/Build-Tracing-AR.cmd
@@ -1,3 +1,3 @@
-dotnet publish -f:net8.0-android -p:IsTracingEnabled=true -p:EmbedAssembliesIntoApk=true -c:Debug
+dotnet publish -f:net8.0-android -p:IsTracingEnabled=true -p:EmbedAssembliesIntoApk=true -c:Release
 adb install -r ..\..\..\artifacts\publish\App.Maui\debug_net8.0-android\chat.actual.dev.app-Signed.apk
 
---
+++ src/dotnet/App.Maui/_Profiling/ConvertNettrace-A.cmd
@@ -0,0 +1,1 @@
+dotnet-trace convert --format Speedscope android.nettrace
