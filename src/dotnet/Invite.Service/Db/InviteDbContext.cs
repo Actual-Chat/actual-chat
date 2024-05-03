f5f447af13dde083e7707a19e17dca4179e9a48b
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-05-03 09:11:45 +0300

    feat: collation C for invite Ids

--- src/dotnet/Invite.Service/Db/InviteDbContext.cs
+++ src/dotnet/Invite.Service/Db/InviteDbContext.cs
@@ -15,5 +15,22 @@ public class InviteDbContext(DbContextOptions<InviteDbContext> options) : DbCont
     public DbSet<DbEvent> Events { get; protected set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder model)
-        => model.ApplyConfigurationsFromAssembly(typeof(InviteDbContext).Assembly).UseSnakeCaseNaming();
+    {
+        model.ApplyConfigurationsFromAssembly(typeof(InviteDbContext).Assembly).UseSnakeCaseNaming();
+
+        var invite = model.Entity<DbInvite>();
+        invite.Property(e => e.Id).UseCollation("C");
+        invite.Property(e => e.SearchKey).UseCollation("C");
+        invite.Property(e => e.CreatedBy).UseCollation("C");
+
+        var activationKey = model.Entity<DbActivationKey>();
+        activationKey.Property(e => e.Id).UseCollation("C");
+
+        var operation = model.Entity<DbOperation>();
+        operation.Property(e => e.Uuid).UseCollation("C");
+        operation.Property(e => e.HostId).UseCollation("C");
+
+        var events = model.Entity<DbEvent>();
+        events.Property(e => e.Uuid).UseCollation("C");
+    }
 }
