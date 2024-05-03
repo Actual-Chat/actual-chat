e7f4c45b0879d08547a482d669fedf051624bbb7
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-05-03 10:10:19 +0300

    feat: collation C for Notification Ids

---
+++ src/dotnet/Notification.Service.Migration/Migrations/20240503070522_AddCollationToIds.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Notification.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollationToIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "notifications",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "similarity_key",
+                table: "notifications",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "chat_id",
+                table: "notifications",
+                type: "text",
+                nullable: true,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "author_id",
+                table: "notifications",
+                type: "text",
+                nullable: true,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "notifications",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "devices",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "devices",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "host_id",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_events",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "notifications",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "similarity_key",
+                table: "notifications",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "chat_id",
+                table: "notifications",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "author_id",
+                table: "notifications",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "notifications",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "devices",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "devices",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "host_id",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_events",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+        }
+    }
+}
--- src/dotnet/Notification.Service/Db/NotificationDbContext.cs
+++ src/dotnet/Notification.Service/Db/NotificationDbContext.cs
@@ -15,5 +15,25 @@ public class NotificationDbContext(DbContextOptions<NotificationDbContext> optio
     public DbSet<DbEvent> Events { get; protected set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder model)
-        => model.ApplyConfigurationsFromAssembly(typeof(NotificationDbContext).Assembly).UseSnakeCaseNaming();
+    {
+        model.ApplyConfigurationsFromAssembly(typeof(NotificationDbContext).Assembly).UseSnakeCaseNaming();
+
+        var device = model.Entity<DbDevice>();
+        device.Property(e => e.Id).UseCollation("C");
+        device.Property(e => e.UserId).UseCollation("C");
+
+        var notification = model.Entity<DbNotification>();
+        notification.Property(e => e.Id).UseCollation("C");
+        notification.Property(e => e.UserId).UseCollation("C");
+        notification.Property(e => e.ChatId).UseCollation("C");
+        notification.Property(e => e.AuthorId).UseCollation("C");
+        notification.Property(e => e.SimilarityKey).UseCollation("C");
+
+        var operation = model.Entity<DbOperation>();
+        operation.Property(e => e.Uuid).UseCollation("C");
+        operation.Property(e => e.HostId).UseCollation("C");
+
+        var events = model.Entity<DbEvent>();
+        events.Property(e => e.Uuid).UseCollation("C");
+    }
 }
