eef8538f9dfa44120a42e0f8959976399e9ef09d
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-05-03 09:11:45 +0300

    feat: collation C for invite Ids

--- src/dotnet/Invite.Service.Migration/Migrations/20240503055008_AddCollationToIds.Designer.cs
+++ src/dotnet/Invite.Service.Migration/Migrations/20240503055008_AddCollationToIds.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ActualChat.Invite.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ActualChat.Invite.Migrations
 {
     [DbContext(typeof(InviteDbContext))]
-    partial class InviteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240503055008_AddCollationToIds")]
+    partial class AddCollationToIds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
