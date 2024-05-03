6e6eb0094f817531ed1bd19b90548fc4d260ec73
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-05-03 09:30:47 +0300

    feat: collation C for media Ids

---
+++ src/dotnet/Media.Service.Migration/Migrations/20240503062646_AddCollationToIds.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Media.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCollationToIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "scope",
+                table: "media",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "local_id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "content_id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "thumbnail_media_id",
+                table: "link_previews",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "link_previews",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "host_id",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_events",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "scope",
+                table: "media",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "local_id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "content_id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "thumbnail_media_id",
+                table: "link_previews",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id",
+                table: "link_previews",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "host_id",
+                table: "_operations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "uuid",
+                table: "_events",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+        }
+    }
+}
