a74c9beac94e4e94e29a243101cb5f235c9a19e8
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-05-14 08:25:58 +0300

    fix: header on iOS Safari goes off the screen

--- src/nodejs/src/init.ts
+++ src/nodejs/src/init.ts
@@ -7,3 +7,10 @@ DeviceInfo.init();
 Interactive.init();
 Gestures.init();
 void ServiceWorker.init();
+
+if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', () => {
+        const vh = window.visualViewport.height * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+}
