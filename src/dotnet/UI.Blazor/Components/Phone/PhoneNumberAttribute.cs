a8c4a36f5ae803d723a7125d8ce73ba9d5829db5
iqmulator <iqmulator@gmail.com>
2024-05-14 21:10:51 +0500

    fix: phone field, name field, remove ValidationSummary, add validation messages under form field, fix forms with contact list, update input Clear btn, small fixes

--- src/dotnet/UI.Blazor/Components/Phone/PhoneNumberAttribute.cs
+++ src/dotnet/UI.Blazor/Components/Phone/PhoneNumberAttribute.cs
@@ -6,10 +6,15 @@ namespace ActualChat.UI.Blazor.Components;
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
 public sealed class PhoneNumberAttribute : AsyncValidationAttribute
 {
+    public bool IsOptional { get; set; }
+
     public override async Task<ValidationResult?> IsValidAsync(object? value, ValidationContext validationContext, CancellationToken cancellationToken)
     {
         if (value is not string sValue)
             return new ValidationResult(ErrorMessage ?? "Your phone number is incorrect");
+
+        if (IsOptional && sValue.IsNullOrEmpty())
+            return ValidationResult.Success;
 
         var phones = validationContext.GetRequiredService<IPhones>();
         var phone = await phones.Parse(sValue, cancellationToken);
