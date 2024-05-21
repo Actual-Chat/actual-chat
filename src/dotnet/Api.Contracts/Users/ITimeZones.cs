ebc3caff56446a5f03ced782047e44a16b72bf9d
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-05-21 06:45:41 +0300

    fix: time zone is not detected on windows

--- src/dotnet/Api.Contracts/Users/ITimeZones.cs
+++ src/dotnet/Api.Contracts/Users/ITimeZones.cs
@@ -5,4 +5,7 @@ public interface ITimeZones : IComputeService
     // NOTE(AY): Should it really be a compute method? Let's discuss this.
     [ComputeMethod]
     Task<ApiArray<TimeZone>> List(string languageCode, CancellationToken cancellationToken);
+
+    [ComputeMethod]
+    Task<string> ConvertWindowsToIana(string windowsTimeZone, CancellationToken cancellationToken);
 }
--- src/dotnet/Users.Service/TimeZones.cs
+++ src/dotnet/Users.Service/TimeZones.cs
@@ -1,3 +1,4 @@
+using TimeZoneConverter;
 using TimeZoneNames;
 
 namespace ActualChat.Users;
@@ -7,7 +8,16 @@ public class TimeZones : ITimeZones
     // [ComputeMethod]
     public virtual Task<ApiArray<TimeZone>> List(string languageCode, CancellationToken cancellationToken)
     {
-        var zones = TZNames.GetDisplayNames(languageCode, true).Select(x => new TimeZone(x.Key) { IanaName = x.Value }).ToApiArray();
+        var zones = TZNames.GetDisplayNames(languageCode, true)
+            .Select(x => new TimeZone(x.Key) { IanaName = x.Value })
+            .ToApiArray();
         return Task.FromResult(zones);
+    }
+
+    // [ComputeMethod]
+    public virtual Task<string> ConvertWindowsToIana(string windowsTimeZone, CancellationToken cancellationToken)
+    {
+        var ianaTimeZone = TZConvert.WindowsToIana(windowsTimeZone);
+        return Task.FromResult(ianaTimeZone);
     }
 }
