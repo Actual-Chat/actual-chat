23b41aa10efdc04bfad46fd4193e86cd554d67c6
Alex Ustinov <alex.ustinov@actual.chat>
2024-05-22 19:55:10 -0700

    chore: cleanup code

--- tests/MLSearch.UnitTests/Engine/OpenSearch/Setup/ClusterSetupActionsTest.cs
+++ tests/MLSearch.UnitTests/Engine/OpenSearch/Setup/ClusterSetupActionsTest.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using ActualChat.MLSearch.Engine.OpenSearch.Configuration;
 using ActualChat.MLSearch.Engine.OpenSearch.Setup;
 using ActualChat.Performance;
@@ -8,117 +7,6 @@ using OpenSearch.Net;
 using HttpMethod = OpenSearch.Net.HttpMethod;
 
 namespace ActualChat.MLSearch.UnitTests.Engine.OpenSearch.Setup;
-
-// Response to model Id request
-// {
-//   "took": 3,
-//   "timed_out": false,
-//   "_shards": {
-//     "total": 1,
-//     "successful": 1,
-//     "skipped": 0,
-//     "failed": 0
-//   },
-//   "hits": {
-//     "total": {
-//       "value": 1,
-//       "relation": "eq"
-//     },
-//     "max_score": null,
-//     "hits": [
-//       {
-//         "_index": ".plugins-ml-model-group",
-//         "_id": "sQslDo8BjOZ4dQx_Hke0",
-//         "_version": 2,
-//         "_seq_no": 1,
-//         "_primary_term": 1,
-//         "_score": null,
-//         "_source": {
-//           "created_time": 1713929264559,
-//           "access": "public",
-//           "latest_version": 1,
-//           "last_updated_time": 1713929265790,
-//           "name": "NLP_model_group",
-//           "description": "A model group for NLP models"
-//         },
-//         "sort": [
-//           1
-//         ]
-//       }
-//     ]
-//   }
-// }
-
-// Response to model parameters request
-// {
-//   "took": 3,
-//   "timed_out": false,
-//   "_shards": {
-//     "total": 1,
-//     "successful": 1,
-//     "skipped": 0,
-//     "failed": 0
-//   },
-//   "hits": {
-//     "total": {
-//       "value": 9,
-//       "relation": "eq"
-//     },
-//     "max_score": null,
-//     "hits": [
-//       {
-//         "_index": ".plugins-ml-model",
-//         "_id": "sgslDo8BjOZ4dQx_I0eB",
-//         "_version": 22,
-//         "_seq_no": 29,
-//         "_primary_term": 7,
-//         "_score": null,
-//         "_source": {
-//           "last_deployed_time": 1715924158689,
-//           "model_version": "1",
-//           "created_time": 1713929266047,
-//           "deploy_to_all_nodes": true,
-//           "model_format": "TORCH_SCRIPT",
-//           "model_state": "DEPLOYED",
-//           "planning_worker_node_count": 1,
-//           "total_chunks": 8,
-//           "model_content_hash_value": "2ffa1f98dc511ec793232a44c116aaac9455912b65a0a7299fd41e4a8f9f77b9",
-//           "model_config": {
-//             "all_config": """{"_name_or_path":"old_models/paraphrase-MiniLM-L3-v2/0_Transformer","architectures":["BertModel"],"attention_probs_dropout_prob":0.1,"gradient_checkpointing":false,"hidden_act":"gelu","hidden_dropout_prob":0.1,"hidden_size":384,"initializer_range":0.02,"intermediate_size":1536,"layer_norm_eps":1e-12,"max_position_embeddings":512,"model_type":"bert","num_attention_heads":12,"num_hidden_layers":3,"pad_token_id":0,"position_embedding_type":"absolute","transformers_version":"4.7.0","type_vocab_size":2,"use_cache":true,"vocab_size":30522}""",
-//             "model_type": "bert",
-//             "embedding_dimension": 384,
-//             "framework_type": "SENTENCE_TRANSFORMERS"
-//           },
-//           "auto_redeploy_retry_times": 0,
-//           "last_updated_time": 1715924158690,
-//           "name": "sentence-transformers/paraphrase-MiniLM-L3-v2",
-//           "current_worker_node_count": 1,
-//           "model_group_id": "sQslDo8BjOZ4dQx_Hke0",
-//           "model_content_size_in_bytes": 70401552,
-//           "planning_worker_nodes": [
-//             "tcvZRDqbQSuDz0vk7Dxf4A"
-//           ],
-//           "algorithm": "TEXT_EMBEDDING"
-//         },
-//         "sort": [
-//           29
-//         ]
-//       }
-//     ]
-//   }
-// }
-
-
-// var createResponse = new CreateIndexResponse { Acknowledged = true, Index = "testing", ShardsAcknowledged = true };
-// var mockedResponse = TestableResponseFactory.CreateSuccessfulResponse(createResponse, 201);
-
-// var mockedClient = Mock.Of<ElasticsearchClient>(e =>
-//     e.Indices.Create<It.IsAnyType>() == mockedResponse);
-
-// var testResponse = mockedClient.Indices.Create<Person>();
-
-// if (testResponse.IsValidResponse)
-//     Console.WriteLine("SUCCESS");
 
 public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
 {
@@ -630,7 +518,7 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
             }
         }
         """;
-        var connection = new TestableInMemoryConnection(a => { }, [ (200, response) ]);
+        var connection = new TestableInMemoryOpenSearchConnection(a => { }, [ (200, response) ]);
         var actions = CreateActions(connection);
 
         await actions.EnsureTemplateAsync(name, pattern, numReplicas, CancellationToken.None);
@@ -653,7 +541,7 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
 
         void AssertRequest(RequestData requestData)
             => Assert.True(expectedRequests.Remove((requestData.Method, requestData.PathAndQuery)));
-        var connection = new TestableInMemoryConnection(AssertRequest, responses);
+        var connection = new TestableInMemoryOpenSearchConnection(AssertRequest, responses);
         var actions = CreateActions(connection);
 
         await actions.EnsureTemplateAsync(name, pattern, numReplicas, CancellationToken.None);
@@ -675,7 +563,7 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
             }
         }
         """;
-        var connection = new TestableInMemoryConnection(a => { }, [ (200, response) ]);
+        var connection = new TestableInMemoryOpenSearchConnection(a => { }, [ (200, response) ]);
         var actions = CreateActions(connection);
 
         await actions.EnsureEmbeddingIngestPipelineAsync(PipelineName, ModelId, TextField, CancellationToken.None);
@@ -700,7 +588,7 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
 
         void AssertRequest(RequestData requestData)
             => Assert.True(expectedRequests.Remove((requestData.Method, requestData.PathAndQuery.Trim('/'))));
-        var connection = new TestableInMemoryConnection(AssertRequest, responses);
+        var connection = new TestableInMemoryOpenSearchConnection(AssertRequest, responses);
         var actions = CreateActions(connection);
 
         await actions.EnsureEmbeddingIngestPipelineAsync(PipelineName, ModelId, TextField, CancellationToken.None);
@@ -724,7 +612,7 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
             _ => throw new NotSupportedException($"Action '{actionName}' is not supported.")
         };
 
-        var connection = new TestableInMemoryConnection(a => { }, [ (200, string.Empty) ]);
+        var connection = new TestableInMemoryOpenSearchConnection(a => { }, [ (200, string.Empty) ]);
         var actions = CreateActions(connection);
 
         await callAction(actions);
@@ -761,7 +649,7 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
         void AssertRequest(RequestData requestData)
             => Assert.True(expectedRequests.Remove((requestData.Method, requestData.PathAndQuery.Trim('/'))));
 
-        var connection = new TestableInMemoryConnection(AssertRequest, responses);
+        var connection = new TestableInMemoryOpenSearchConnection(AssertRequest, responses);
         var actions = CreateActions(connection);
 
         await callAction(actions);
@@ -770,114 +658,14 @@ public class ClusterSetupActionsTest(ITestOutputHelper @out) : TestBase(@out)
     }
 
     private ClusterSetupActions CreateActions(List<(int, string)> responses)
-        => CreateActions(new TestableInMemoryConnection(a => { }, responses));
+        => CreateActions(new TestableInMemoryOpenSearchConnection(a => { }, responses));
 
-    private ClusterSetupActions CreateActions(TestableInMemoryConnection connection)
+    private ClusterSetupActions CreateActions(TestableInMemoryOpenSearchConnection connection)
     {
         var client = new OpenSearchClient(
             new ConnectionSettings(_fakeConnectionPool, connection)
         );
 
         return new ClusterSetupActions(client, _openSearchNamingPolicy, Tracer.None);
-    }
-}
-
-internal sealed class TestableInMemoryConnection(Action<RequestData> perRequestAssertion, List<(int, string)> responses) : IConnection
-{
-    internal static readonly byte[] EmptyBody = Encoding.UTF8.GetBytes("");
-    private int _requestCounter = -1;
-
-    public void AssertExpectedCallCount() => _requestCounter.Should().Be(responses.Count - 1);
-
-    async Task<TResponse> IConnection.RequestAsync<TResponse>(RequestData requestData, CancellationToken cancellationToken)
-    {
-        Interlocked.Increment(ref _requestCounter);
-
-        perRequestAssertion(requestData);
-
-        await Task.Yield(); // avoids test deadlocks
-
-        int statusCode;
-        string response;
-
-        (statusCode, response) = (responses.Count > _requestCounter)
-            ? responses[_requestCounter]
-            : (500, string.Empty);
-
-        var stream = !string.IsNullOrEmpty(response)
-            ? requestData.MemoryStreamFactory.Create(Encoding.UTF8.GetBytes(response))
-            : requestData.MemoryStreamFactory.Create(EmptyBody);
-
-        return await ResponseBuilder
-            .ToResponseAsync<TResponse>(requestData, null, statusCode, null, stream, RequestData.MimeType, cancellationToken)
-            .ConfigureAwait(false);
-    }
-
-    TResponse IConnection.Request<TResponse>(RequestData requestData)
-    {
-        Interlocked.Increment(ref _requestCounter);
-
-        perRequestAssertion(requestData);
-
-        int statusCode;
-        string response;
-
-        (statusCode, response) = (responses.Count > _requestCounter)
-            ? responses[_requestCounter]
-            : (500, string.Empty);
-
-        var stream = !string.IsNullOrEmpty(response)
-            ? requestData.MemoryStreamFactory.Create(Encoding.UTF8.GetBytes(response))
-            : requestData.MemoryStreamFactory.Create(EmptyBody);
-
-        return ResponseBuilder.ToResponse<TResponse>(requestData, null, statusCode, null, stream, RequestData.MimeType);
-    }
-
-    public void Dispose() { }
-}
-
-
-        // var openSearchLowLevelMock = new Mock<IOpenSearchLowLevelClient>();
-        // openSearchLowLevelMock
-        //     .Setup(os => os.DoRequestAsync<DynamicResponse>(
-        //         It.Is<HttpMethod>(method => method == HttpMethod.POST),
-        //         It.Is<string>(s => s.StartsWith("/_plugins/_ml/model_groups/_search", StringComparison.Ordinal)),
-        //         It.IsAny<CancellationToken>(),
-        //         It.IsAny<PostData>(),
-        //         It.IsAny<IRequestParameters>()
-        //     ))
-        //     .Returns(() => {
-        //         var responseItems = DynamicDictionary.Create(new Dictionary<string, object>() {
-        //             "hits.hits"
-        //         });
-        //         var response = new DynamicResponse(responseItems);
-        //         return Task.FromResult(TestableResponseFactory.CreateSuccessfulResponse(response, 200));
-        //     });
-        // var openSearchMock = new Mock<IOpenSearchClient>();
-        // openSearchMock.SetupGet(os => os.LowLevel).Returns(openSearchLowLevelMock.Object);
-
-        // var actions = new ClusterSetupActions(openSearchMock.Object, _openSearchNamingPolicy, Tracer.None);
-
-
-public static class TestableResponseFactory
-{
-    public static T CreateSuccessfulResponse<T>(T response, int httpStatusCode) where T : IOpenSearchResponse
-        => CreateResponse(response, httpStatusCode, true);
-
-    public static T CreateResponse<T>(T response, int httpStatusCode, bool statusCodeRepresentsSuccess) where T : IOpenSearchResponse
-    {
-        var apiCallDetails = new ApiCallDetails
-        {
-            HttpStatusCode = httpStatusCode,
-            Success = statusCodeRepresentsSuccess,
-        };
-
-        return CreateResponse<T>(response, apiCallDetails);
-    }
-
-    internal static T CreateResponse<T>(T response, ApiCallDetails apiCallDetails) where T : IOpenSearchResponse
-    {
-        response.ApiCall = apiCallDetails;
-        return response;
     }
 }
---
+++ tests/MLSearch.UnitTests/Engine/OpenSearch/TestableInMemoryOpenSearchConnection.cs
@@ -0,0 +1,58 @@
+using System.Text;
+using OpenSearch.Net;
+
+namespace ActualChat.MLSearch.UnitTests.Engine.OpenSearch;
+
+internal sealed class TestableInMemoryOpenSearchConnection(Action<RequestData> perRequestAssertion, List<(int, string)> responses) : IConnection
+{
+    internal static readonly byte[] EmptyBody = Encoding.UTF8.GetBytes("");
+    private int _requestCounter = -1;
+
+    public void AssertExpectedCallCount() => _requestCounter.Should().Be(responses.Count - 1);
+
+    async Task<TResponse> IConnection.RequestAsync<TResponse>(RequestData requestData, CancellationToken cancellationToken)
+    {
+        Interlocked.Increment(ref _requestCounter);
+
+        perRequestAssertion(requestData);
+
+        await Task.Yield(); // avoids test deadlocks
+
+        int statusCode;
+        string response;
+
+        (statusCode, response) = (responses.Count > _requestCounter)
+            ? responses[_requestCounter]
+            : (500, string.Empty);
+
+        var stream = !string.IsNullOrEmpty(response)
+            ? requestData.MemoryStreamFactory.Create(Encoding.UTF8.GetBytes(response))
+            : requestData.MemoryStreamFactory.Create(EmptyBody);
+
+        return await ResponseBuilder
+            .ToResponseAsync<TResponse>(requestData, null, statusCode, null, stream, RequestData.MimeType, cancellationToken)
+            .ConfigureAwait(false);
+    }
+
+    TResponse IConnection.Request<TResponse>(RequestData requestData)
+    {
+        Interlocked.Increment(ref _requestCounter);
+
+        perRequestAssertion(requestData);
+
+        int statusCode;
+        string response;
+
+        (statusCode, response) = (responses.Count > _requestCounter)
+            ? responses[_requestCounter]
+            : (500, string.Empty);
+
+        var stream = !string.IsNullOrEmpty(response)
+            ? requestData.MemoryStreamFactory.Create(Encoding.UTF8.GetBytes(response))
+            : requestData.MemoryStreamFactory.Create(EmptyBody);
+
+        return ResponseBuilder.ToResponse<TResponse>(requestData, null, statusCode, null, stream, RequestData.MimeType);
+    }
+
+    public void Dispose() { }
+}
