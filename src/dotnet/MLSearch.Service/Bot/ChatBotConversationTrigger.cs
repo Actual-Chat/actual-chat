137a887cad9beb77a5563e82854a6f078ec279e7
Andrew Kurochkin <andrew.kurochkin@actual.chat>
2024-06-04 07:39:10 +0200

    fix: changes in decision on what is a conversation with a bot

--- src/dotnet/MLSearch.Service/Bot/ChatBotConversationTrigger.cs
+++ src/dotnet/MLSearch.Service/Bot/ChatBotConversationTrigger.cs
@@ -1,10 +1,11 @@
 
+using ActualChat.Chat;
 using ActualChat.Chat.Events;
 using ActualChat.MLSearch.ApiAdapters.ShardWorker;
 
 namespace ActualChat.MLSearch.Bot;
 
-internal class ChatBotConversationTrigger(ICommander commander, IWorkerPool<MLSearch_TriggerContinueConversationWithBot, ChatId, ChatId> workerPool)
+internal class ChatBotConversationTrigger(ICommander commander, IChatsBackend chats, IWorkerPool<MLSearch_TriggerContinueConversationWithBot, ChatId, ChatId> workerPool)
     : IChatBotConversationTrigger, IComputeService
 {
     // ReSharper disable once UnusedMember.Global
@@ -16,8 +17,11 @@ internal class ChatBotConversationTrigger(ICommander commander, IWorkerPool<MLSe
     // [EventHandler]
     public virtual async Task OnTextEntryChangedEvent(TextEntryChangedEvent eventCommand, CancellationToken cancellationToken)
     {
-        var peerChatId = eventCommand.Entry.ChatId.PeerChatId;
-        if (!peerChatId.IsNone && peerChatId.HasUser(Constants.User.MLSearchBot.UserId)) {
+        //var peerChatId = eventCommand.Entry.ChatId.PeerChatId;
+        //if (!peerChatId.IsNone && peerChatId.HasUser(Constants.User.MLSearchBot.UserId)) {
+
+        var chat = await chats.Get(eventCommand.Entry.ChatId, cancellationToken).ConfigureAwait(false);
+        if (chat != null && OrdinalEquals(Constants.Chat.SystemTags.Bot, chat.SystemTag)) {
             // User is writing of changing something.
             var e = new MLSearch_TriggerContinueConversationWithBot(eventCommand.Entry.ChatId);
             await commander.Call(e, cancellationToken).ConfigureAwait(false);
