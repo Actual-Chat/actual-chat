2ff8cec085804ff7fd727a6f050ccaefaabe847b
Andrew Kurochkin <andrew.kurochkin@actual.chat>
2024-06-06 00:51:58 +0200

    feat: integration test added

--- src/dotnet/Api.Contracts/MLSearch/IMLSearch.cs
+++ src/dotnet/Api.Contracts/MLSearch/IMLSearch.cs
@@ -7,9 +7,10 @@ namespace ActualChat.MLSearch;
 [DataContract, MemoryPackable(GenerateType.VersionTolerant)]
 // ReSharper disable once InconsistentNaming
 public sealed partial record MLSearch_CreateChat(
-    [property: DataMember, MemoryPackOrder(0)] Session Session,
-    [property: DataMember, MemoryPackOrder(1)] MLSearchChatId MLSearchChatId,
-    [property: DataMember, MemoryPackOrder(2)] long? ExpectedVersion
+    // TODO: [Andrew K] Ensure we have some form of form token 
+    // to prevent replay on accidental double clicks.
+    // Fix if it is not generally available.
+    [property: DataMember, MemoryPackOrder(0)] Session Session
 ) : ISessionCommand<MLSearchChat>, IApiCommand;
 
 public interface IMLSearch : IComputeService
