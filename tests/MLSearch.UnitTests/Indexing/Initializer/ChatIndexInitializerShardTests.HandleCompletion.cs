6dde0a8fe7119828af52ea672bcb32d1f50ef1c6
Alex Ustinov <alex.ustinov@actual.chat>
2024-06-13 16:24:59 -0700

    test: implement job completion tests

--- tests/MLSearch.UnitTests/Indexing/Initializer/ChatIndexInitializerShardTests.HandleCompletion.cs
+++ tests/MLSearch.UnitTests/Indexing/Initializer/ChatIndexInitializerShardTests.HandleCompletion.cs
@@ -1,27 +1,82 @@
 using ActualChat.MLSearch.Indexing.Initializer;
-using ActualLab.Resilience;
 
 namespace ActualChat.MLSearch.UnitTests.Indexing.Initializer;
 
 public partial class ChatIndexInitializerShardTests
 {
+    private readonly ChatIndexInitializerShard.Cursor _fakeCursor = new (333);
+
     [Fact]
-    public async Task HandleCompletionEventMethodIncrementsEventCounter()
+    public void HandleCompletionEventMethodIncrementsEventCounter()
     {
+        var evt = new MLSearch_TriggerChatIndexingCompletion(ChatId.None);
+        var state = new ChatIndexInitializerShard.SharedState(_fakeCursor, 1);
+
+        var expected = state.EventCount + 1;
+        ChatIndexInitializerShard.HandleCompletionEvent(evt, state);
+        Assert.Equal(expected, state.EventCount);
     }
 
     [Fact]
-    public async Task HandleCompletionEventMethodRemovesJobInfoFromState()
+    public void HandleCompletionEventMethodRemovesJobInfoFromState()
     {
+        var chatId1 = new ChatId(Generate.Option);
+        var chatId2 = new ChatId(Generate.Option);
+        var state = new ChatIndexInitializerShard.SharedState(_fakeCursor, 2);
+
+        // Emulate two running jobs
+        state.ScheduledJobs[chatId1] = (1, Moment.MinValue);
+        state.ScheduledJobs[chatId2] = (1, Moment.MinValue);
+        state.Semaphore.Wait();
+        state.Semaphore.Wait();
+
+        var evt = new MLSearch_TriggerChatIndexingCompletion(chatId1);
+        ChatIndexInitializerShard.HandleCompletionEvent(evt, state);
+
+        Assert.False(state.ScheduledJobs.ContainsKey(chatId1));
+        Assert.True(state.ScheduledJobs.ContainsKey(chatId2));
     }
 
     [Fact]
-    public async Task HandleCompletionEventMethodReleasesSemaphoreSlotIfJobPresent()
+    public void HandleCompletionEventMethodReleasesSemaphoreSlotIfJobPresent()
     {
+        var chatId1 = new ChatId(Generate.Option);
+        var chatId2 = new ChatId(Generate.Option);
+        var state = new ChatIndexInitializerShard.SharedState(_fakeCursor, 5);
+
+        // Emulate two running jobs
+        state.ScheduledJobs[chatId1] = (1, Moment.MinValue);
+        state.ScheduledJobs[chatId2] = (1, Moment.MinValue);
+        state.Semaphore.Wait();
+        state.Semaphore.Wait();
+
+        var expectedSlots = state.Semaphore.CurrentCount + 1;
+
+        foreach (var _ in Enumerable.Range(0, 2)) {
+            // Handle completion event for the same chat twice
+            var evt = new MLSearch_TriggerChatIndexingCompletion(chatId1);
+            ChatIndexInitializerShard.HandleCompletionEvent(evt, state);
+        }
+        // Verify only one semaphore slot is released
+        Assert.Equal(expectedSlots, state.Semaphore.CurrentCount);
     }
 
     [Fact]
-    public async Task HandleCompletionEventMethodUpdatesMaxChatVersion()
+    public void HandleCompletionEventMethodUpdatesMaxChatVersion()
     {
+        long[] versions = [100, 300, 200, 500, 400];
+        var state = new ChatIndexInitializerShard.SharedState(_fakeCursor, 5);
+        foreach (var version in versions) {
+            var chatId = new ChatId(Generate.Option);
+            state.ScheduledJobs[chatId] = (version, Moment.MinValue);
+            state.Semaphore.Wait();
+        }
+        state.MaxVersion = 0;
+        while (!state.ScheduledJobs.IsEmpty) {
+            var chatId = state.ScheduledJobs.Keys.First();
+            var evt = new MLSearch_TriggerChatIndexingCompletion(chatId);
+            ChatIndexInitializerShard.HandleCompletionEvent(evt, state);
+        }
+        Assert.Equal(versions.Max(), state.MaxVersion);
     }
 }
