c6acd08e5616eea3b6e0304addf3453054a5bcd4
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-06-21 20:27:45 +0300

    fix: use nullable default setting for Analytics

--- src/dotnet/Api/Users/UserAppSettings.cs
+++ src/dotnet/Api/Users/UserAppSettings.cs
@@ -8,6 +8,6 @@ public sealed partial record UserAppSettings : IHasOrigin
 {
     public const string KvasKey = nameof(UserAppSettings);
 
-    [DataMember, MemoryPackOrder(0)] public bool IsAnalyticsEnabled{ get; init; }
+    [DataMember, MemoryPackOrder(0)] public bool? IsAnalyticsEnabled{ get; init; }
     [DataMember, MemoryPackOrder(1)] public string Origin { get; init; } = "";
 }
