d4bbbb43cab981574482c2ba2c2bdcb7a3e45d48
Alex Ustinov <alex.ustinov@actual.chat>
2024-06-20 19:46:19 -0700

    chore: separate init tests to theier own file

---
+++ tests/MLSearch.UnitTests/Indexing/ChatContent/ChatContentIndexerInitTests.cs
@@ -0,0 +1,66 @@
+using ActualChat.Chat;
+using ActualChat.MLSearch.Documents;
+using ActualChat.MLSearch.Indexing;
+using ActualChat.MLSearch.Indexing.ChatContent;
+
+namespace ActualChat.MLSearch.UnitTests.Indexing.ChatContent;
+
+public class ChatContentIndexerInitTests(ITestOutputHelper @out) : TestBase(@out)
+{
+    [Fact]
+    public async Task InitMethodLoadsTailDocuments()
+    {
+        const int maxTailSetSize = 333;
+        var tailDocuments = ContentHelpers.CreateDocuments();
+        var chats = Mock.Of<IChatsBackend>();
+        var docLoader = new Mock<IChatContentDocumentLoader>();
+        docLoader
+            .Setup(x => x.LoadTailAsync(
+                It.IsAny<ChatContentCursor>(),
+                It.IsAny<int>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(tailDocuments);
+        var docMapper = Mock.Of<IChatContentMapper>();
+        var sink = Mock.Of<ISink<ChatSlice, string>>();
+
+        var contentIndexer = new ChatContentIndexer(chats, docLoader.Object, docMapper, sink) {
+            MaxTailSetSize = maxTailSetSize,
+        };
+
+        var cancellationSource = new CancellationTokenSource();
+        var cursor = new ChatContentCursor(0, 0);
+        await contentIndexer.InitAsync(cursor, cancellationSource.Token);
+
+        docLoader.Verify(x => x.LoadTailAsync(
+            It.Is<ChatContentCursor>(x => x == cursor),
+            It.Is<int>(x => x == maxTailSetSize),
+            It.Is<CancellationToken>(x => x == cancellationSource.Token)
+        ), Times.Once);
+
+        Assert.Equal(cursor, contentIndexer.Cursor);
+        Assert.Equal(tailDocuments.Length, contentIndexer.TailDocs.Count);
+        foreach (var doc in tailDocuments) {
+            Assert.True(contentIndexer.TailDocs.TryGetValue(doc.Id, out var tailDoc) && ReferenceEquals(doc, tailDoc));
+        }
+    }
+
+    [Fact]
+    public async Task InitMethodDoesNotSwallowExceptions()
+    {
+        var chats = Mock.Of<IChatsBackend>();
+        var docLoader = new Mock<IChatContentDocumentLoader>();
+        docLoader
+            .Setup(x => x.LoadTailAsync(
+                It.IsAny<ChatContentCursor>(),
+                It.IsAny<int>(),
+                It.IsAny<CancellationToken>()))
+            .Returns(Task.FromException<IReadOnlyCollection<ChatSlice>>(new UniqueException()));
+        var docMapper = Mock.Of<IChatContentMapper>();
+        var sink = Mock.Of<ISink<ChatSlice, string>>();
+
+        var contentIndexer = new ChatContentIndexer(chats, docLoader.Object, docMapper, sink);
+
+        var cursor = new ChatContentCursor(0, 0);
+        await Assert.ThrowsAsync<UniqueException>(() => contentIndexer.InitAsync(cursor, CancellationToken.None));
+    }
+}
