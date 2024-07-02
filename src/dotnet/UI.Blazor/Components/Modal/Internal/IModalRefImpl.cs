c4b03ee561e719d5de19e3be53d233a44b0e8900
Dmitry Filippov <crui3er@gmail.com>
2024-07-02 20:23:48 +0400

    chore: modal animation refactoring

--- src/dotnet/UI.Blazor/Components/Modal/Internal/IModalRefImpl.cs
+++ src/dotnet/UI.Blazor/Components/Modal/Internal/IModalRefImpl.cs
@@ -2,10 +2,6 @@ namespace ActualChat.UI.Blazor.Components.Internal;
 
 public interface IModalRefImpl
 {
-    RenderFragment View { get; }
-
-    void SetView(RenderFragment view);
-    void SetModal(Modal modal);
     void MarkClosed();
     void CloseSteps();
 }
--- src/dotnet/UI.Blazor/Components/Modal/ModalRef.cs
+++ src/dotnet/UI.Blazor/Components/Modal/ModalRef.cs
@@ -7,38 +7,36 @@ namespace ActualChat.UI.Blazor.Components;
 public sealed class ModalRef : IHasId<Symbol>, IModalRefImpl
 {
     private static long _lastId;
-    private readonly TaskCompletionSource _whenShownSource = TaskCompletionSourceExt.New();
     private readonly TaskCompletionSource _whenClosedSource = TaskCompletionSourceExt.New();
-    private RenderFragment _view = null!;
     private ModalStepRefImpl? _modalStepRef;
 
     public Symbol Id { get; }
     public ModalOptions Options { get; }
     public ModalHost Host { get; }
     public object Model { get; }
-    public Modal? Modal { get; private set; }
     public ModalStepRef? StepRef => _modalStepRef;
+    public RenderFragment ChildContent { get; }
 
-    public Task WhenShown => _whenShownSource.Task;
     public Task WhenClosed => _whenClosedSource.Task;
 
-    public ModalRef(ModalOptions options, object model, ModalHost host)
+    public ModalRef(ModalOptions options, object model, ModalHost host, RenderFragment childContent)
     {
         Id = $"Modal-{model.GetType().GetName()}-{Interlocked.Increment(ref _lastId)}";
         Options = options;
         Model = model;
         Host = host;
+        ChildContent = childContent;
     }
 
-    public Task<bool> Close(bool force = false)
-        => Host.Close(Id, force);
+    public bool Close(bool force = false)
+    {
+        if (WhenClosed.IsCompleted)
+            return false;
+
+        return Host.Close(Id, force);
+    }
 
     // IModalRefImpl implementation
-
-    RenderFragment IModalRefImpl.View => _view;
-
-    public void SetView(RenderFragment view)
-        => _view = view;
 
     public ModalStepRef StepIn(string name)
     {
@@ -55,12 +53,6 @@ public sealed class ModalRef : IHasId<Symbol>, IModalRefImpl
             return false;
         _ = CloseStep(_modalStepRef, false).ConfigureAwait(false);
         return true;
-    }
-
-    void IModalRefImpl.SetModal(Modal modal)
-    {
-        Modal = modal;
-        _whenShownSource.TrySetResult();
     }
 
     void IModalRefImpl.MarkClosed()
