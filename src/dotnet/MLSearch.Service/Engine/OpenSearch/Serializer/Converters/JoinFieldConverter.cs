896aca1d184c3b52e8fa0a946d9bc68502ef01eb
Alex Ustinov <alex.ustinov@actual.chat>
2024-07-03 22:12:07 -0700

    wip: implement json serializer converters

---
+++ src/dotnet/MLSearch.Service/Engine/OpenSearch/Serializer/Converters/JoinFieldConverter.cs
@@ -0,0 +1,17 @@
+using OpenSearch.Net;
+using OpenSearch.Client;
+
+namespace ActualChat.MLSearch.Engine.OpenSearch.Serializer.Converters
+{
+    public class JoinFieldConverter(IOpenSearchSerializer builtInSerializer, IMemoryStreamFactory memoryStreamFactory)
+        : OscTypeConverter<JoinField>(builtInSerializer, memoryStreamFactory)
+    {
+        public override JoinField? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var jsonDoc = JsonDocument.ParseValue(ref reader);
+            return jsonDoc.RootElement.ValueKind == JsonValueKind.String
+                ? JoinField.Root(jsonDoc.Deserialize<string>())
+                : ReadWithBuiltInSerializer(typeToConvert, jsonDoc);
+        }
+    }
+}
---
+++ src/dotnet/MLSearch.Service/Engine/OpenSearch/Serializer/Converters/OscTypeConverter.cs
@@ -0,0 +1,54 @@
+using OpenSearch.Net;
+
+namespace ActualChat.MLSearch.Engine.OpenSearch.Serializer.Converters;
+
+public class OscTypeConverter<T>(IOpenSearchSerializer builtInSerializer, IMemoryStreamFactory memoryStreamFactory)
+    : JsonConverter<T>
+{
+    private static readonly Type targetType = typeof(T);
+    // private static readonly HashSet<Type> OscTypesThatCanAppearInSource = new HashSet<Type>
+    // {
+    //     typeof(JoinField),
+    //     typeof(QueryContainer),
+    //     typeof(CompletionField),
+    //     typeof(Attachment),
+    //     typeof(ILazyDocument),
+    //     typeof(LazyDocument),
+    //     typeof(GeoCoordinate),
+    //     typeof(GeoLocation),
+    //     typeof(CartesianPoint),
+    // };
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        var formatting = options.WriteIndented
+            ? SerializationFormatting.Indented
+            : SerializationFormatting.None;
+
+        using var ms = memoryStreamFactory.Create();
+        builtInSerializer.Serialize(value, ms, formatting);
+        ms.Position = 0;
+
+        var docOptions = new JsonDocumentOptions {
+            AllowTrailingCommas = true,
+            CommentHandling = JsonCommentHandling.Allow,
+        };
+        using var jsonDoc = JsonDocument.Parse(ms, docOptions);
+        jsonDoc.WriteTo(writer);
+    }
+
+    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using var jsonDoc = JsonDocument.ParseValue(ref reader);
+        return ReadWithBuiltInSerializer(typeToConvert, jsonDoc);
+    }
+
+    protected T? ReadWithBuiltInSerializer(Type typeToConvert, JsonDocument jsonDoc)
+    {
+        using var ms = jsonDoc.ToStream(memoryStreamFactory);
+        return (T?)builtInSerializer.Deserialize(typeToConvert, ms);
+    }
+
+    public override bool CanConvert(Type objectType) => targetType == objectType
+        || (targetType.IsInterface && targetType.IsAssignableFrom(objectType));
+}
