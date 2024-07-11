6b266c0ae8e756f6f088cc9770303b6b5d03a863
Dmitry Filippov <crui3er@gmail.com>
2024-07-11 15:47:25 +0400

    fix: chat attention is initialized with long delay

--- src/dotnet/App.Maui/Platforms/Android/AlarmReceiver.cs
+++ src/dotnet/App.Maui/Platforms/Android/AlarmReceiver.cs
@@ -5,9 +5,21 @@ namespace ActualChat.App.Maui;
 [BroadcastReceiver(Exported = false)]
 public class AlarmReceiver : BroadcastReceiver
 {
+    private ILogger? _log;
+    private ILogger Log => _log ??= StaticLog.For<AlarmReceiver>();
+
     public override void OnReceive(Context? context, Intent? intent)
     {
-        if (intent!.Action.OrdinalStartsWith(ChatAttentionService.AlarmActionPrefix))
-            ChatAttentionService.Instance.OnHandleIntent(intent);
+        try {
+            Log.LogInformation("-> OnReceive");
+            if (intent!.Action.OrdinalStartsWith(ChatAttentionService.AlarmActionPrefix))
+                ChatAttentionService.Instance.OnHandleIntent(intent);
+        }
+        catch (Exception e) {
+            Log.LogError(e, "Failed to process broadcast");
+        }
+        finally {
+            Log.LogInformation("<- OnReceive");
+        }
     }
 }
