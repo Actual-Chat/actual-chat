7bd1ee38bd6a714059d08375587a4e7fcf19e49b
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-07-30 11:34:04 +0300

    feat: add backend for emails

---
+++ src/dotnet/Users.Contracts/IEmailsBackend.cs
@@ -0,0 +1,20 @@
+using ActualLab.Rpc;
+using MemoryPack;
+
+namespace ActualChat.Users;
+
+public interface IEmailsBackend : IComputeService, IBackendService
+{
+    [CommandHandler]
+    Task<Unit> OnSendDigest(Emails_SendDigest command, CancellationToken cancellationToken);
+}
+
+[DataContract, MemoryPackable(GenerateType.VersionTolerant)]
+// ReSharper disable once InconsistentNaming
+public sealed partial record Emails_SendDigest(
+    [property: DataMember, MemoryPackOrder(0)] UserId UserId
+) : ICommand<Unit>, IBackendCommand, IHasShardKey<UserId>
+{
+    [IgnoreDataMember, MemoryPackIgnore]
+    public UserId ShardKey => UserId;
+}
