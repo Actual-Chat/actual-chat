37faa00d0ba7861f81e6352b2fea1ccc8596e74e
Andrew Kurochkin <andrew.kurochkin@actual.chat>
2024-07-31 16:45:52 +0200

    wip: adding global chat search

--- services/chatbot/app/tools/__init__.py
+++ services/chatbot/app/tools/__init__.py
@@ -1,3 +1,4 @@
+from typing import List, Any
 from langchain_core.tools import tool
 from langchain_core.runnables.config import RunnableConfig
 from datetime import date as _date
@@ -8,6 +9,7 @@ TOOLS_AUTH_FORWARD_CONTEXT = "forward-auth-context"
 
 class _Tools(object):
     REPLY = "https://local.actual.chat/api/bot/conversation/reply"
+    SEARCH_PUBLIC_CHATS = "https://local.actual.chat/api/bot/search/public-chats"
 
 @tool(parse_docstring=True)
 def reply(
@@ -42,6 +44,40 @@ def reply(
     )
     return
 
+@tool(parse_docstring=True)
+def search_in_public_chats(
+    text: str,
+    config: RunnableConfig
+) -> List[Any]:
+    """Search in all public chats.
+
+    Args:
+        text: Text to search for.
+
+    Returns:
+        List: ranked search results.
+    """
+    if (config is None):
+        config = {}
+    config = config.get("configurable", {})
+    auth_context = config.get(TOOLS_AUTH_FORWARD_CONTEXT, None)
+    headers = {
+        "Authorization": auth_context
+    }
+
+    result = requests.post(
+        _Tools.SEARCH_PUBLIC_CHATS,
+        json = {
+            "text": text
+        },
+        headers = headers,
+        verify = False # TODO: think again if needed.
+    )
+    return result.content
+
 
 def all():
-    return [reply]
+    return [
+        reply,
+        search_in_public_chats
+    ]
