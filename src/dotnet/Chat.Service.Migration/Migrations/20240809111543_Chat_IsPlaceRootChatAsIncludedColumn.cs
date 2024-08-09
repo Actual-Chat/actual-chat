7fc84b2704c45e97f9238693182c4dd6443531de
Frol <4425748+FrolYo@users.noreply.github.com>
2024-08-09 14:17:33 +0300

    chore: cr fixes - chat index included columns #2343

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20240809111543_Chat_IsPlaceRootChatAsIncludedColumn.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class Chat_IsPlaceRootChatAsIncludedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_chats_version_id_is_place_root_chat",
+                table: "chats");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chats_version",
+                table: "chats",
+                column: "version")
+                .Annotation("Npgsql:IndexInclude", new[] { "id", "is_place_root_chat" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_chats_version",
+                table: "chats");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chats_version_id_is_place_root_chat",
+                table: "chats",
+                columns: new[] { "version", "id", "is_place_root_chat" });
+        }
+    }
+}
