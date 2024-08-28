a6136f125c4144a5e9fa4667b545e9ec54469256
Alex Ustinov <alex.ustinov@actual.chat>
2024-08-28 15:23:41 -0700

    tmp: opensearch external model connector

--- services/embeddings/src/run/start-torchserve.sh
+++ services/embeddings/src/run/start-torchserve.sh
@@ -4,6 +4,7 @@ echo "server is starting..."
 
 model_file_path=$(find ./model_store -type f | head -n 1)
 model_file_name=$(basename $model_file_path)
+model_name="${model_file_name%.*}"
 
 # --ncs means the snapshot feature is disabled.
-torchserve --foreground --disable-token-auth --model-store ./model_store --models "my_model=${model_file_name}" --ncs
+torchserve --foreground --disable-token-auth --model-store ./model_store --models "${model_name}=${model_file_name}" --ncs
