944f8b92cc60548e7aa721257c44a2a8c717c245
Evgeniy Kulakov <evgeniy.kulakov@actual.chat>
2024-08-30 22:52:54 +0300

    chore: add tests for master flow

---
+++ tests/Users.IntegrationTests/Flows/MasterFlowTest.cs
@@ -0,0 +1,38 @@
+using ActualChat.Flows;
+using ActualChat.Testing.Host;
+using ActualChat.Users.Flows;
+
+namespace ActualChat.Users.IntegrationTests.Flows;
+
+[Collection(nameof(UserCollection))]
+public class MasterFlowTest(ITestOutputHelper @out)
+    : AppHostTestBase($"x-{nameof(MasterFlowTest)}", TestAppHostOptions.Default, @out)
+{
+    [Fact]
+    public async Task ShouldStartDigestFlow()
+    {
+        using var h = await NewAppHost();
+
+        var flows = h.Services.GetRequiredService<IFlows>();
+        await flows.GetOrStart<MasterFlow>("");
+
+        await ComputedTest.When(async ct => {
+            var flow = await flows.Get<DigestFlow>("actual-admin", ct);
+            flow.Should().NotBeNull();
+        }, TimeSpan.FromSeconds(30));
+    }
+
+    [Fact]
+    public async Task ShouldBeRemoved()
+    {
+        using var h = await NewAppHost();
+
+        var flows = h.Services.GetRequiredService<IFlows>();
+        await flows.GetOrStart<MasterFlow>("");
+
+        await ComputedTest.When(async ct => {
+            var flow = await flows.Get<MasterFlow>("", ct);
+            flow.Should().BeNull();
+        }, TimeSpan.FromSeconds(30));
+    }
+}
