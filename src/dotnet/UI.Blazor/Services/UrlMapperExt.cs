049d004ee2a6f83d1662087c4b51b0a325cea773
iqmulator <iqmulator@gmail.com>
2024-08-30 20:25:08 +0500

    fix: blurred place background

--- src/dotnet/UI.Blazor/Services/UrlMapperExt.cs
+++ src/dotnet/UI.Blazor/Services/UrlMapperExt.cs
@@ -14,6 +14,18 @@ public static class UrlMapperExt
         return mapper.ImagePreview128Url(pictureUrl);
     }
 
+    public static string PicturePreviewUrl(this UrlMapper mapper, Picture? picture)
+    {
+        if (picture is null)
+            return "";
+
+        var pictureUrl = mapper.PictureUrl(picture);
+        if (pictureUrl.IsNullOrEmpty())
+            return "";
+
+        return mapper.ImagePreviewUrl(pictureUrl, (int?)Constants.Attachments.MaxResolution.X, (int?)Constants.Attachments.MaxResolution.Y);
+    }
+
     private static string PictureUrl(this UrlMapper mapper, Picture picture)
         => picture.MediaContent != null
             ? mapper.ContentUrl(picture.MediaContent.ContentId)
