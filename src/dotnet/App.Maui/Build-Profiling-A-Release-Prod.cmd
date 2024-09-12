904a7992ad09bc0520a4d46345e5f8a1ea1ccbc6
Alex Yakunin <alex.yakunin@gmail.com>
2024-09-12 01:07:51 -0700

    feat: .NET 9 - initial migration

--- src/dotnet/App.Maui/Build-Profiling-A-Release-Prod.cmd
+++ src/dotnet/App.Maui/Build-Profiling-A-Release-Prod.cmd
@@ -1,2 +1,2 @@
-dotnet publish -f:net8.0-android /p:IsProfilingEnabled=true -p:IsDevMaui=false -c:Release
-adb install -r ..\..\..\artifacts\publish\App.Maui\release_net8.0-android\chat.actual.app-Signed.apk
+dotnet publish -f:net9.0-android /p:IsProfilingEnabled=true -p:IsDevMaui=false -c:Release
+adb install -r ..\..\..\artifacts\publish\App.Maui\release_net9.0-android\chat.actual.app-Signed.apk
--- src/dotnet/App.Maui/Build-Tracing-A.cmd
+++ src/dotnet/App.Maui/Build-Tracing-A.cmd
@@ -1,3 +1,2 @@
-dotnet publish -f:net8.0-android -p:IsTracingEnabled=true -p:EmbedAssembliesIntoApk=true -c:Debug
-adb install -r ..\..\..\artifacts\publish\App.Maui\debug_net8.0-android\chat.actual.dev.app-Signed.apk
-
+dotnet publish -f:net9.0-android -p:IsTracingEnabled=true -p:EmbedAssembliesIntoApk=true -c:Debug
+adb install -r ..\..\..\artifacts\publish\App.Maui\debug_net9.0-android\chat.actual.dev.app-Signed.apk
