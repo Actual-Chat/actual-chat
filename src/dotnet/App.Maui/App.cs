0f1b9ea176d33e083840a0eec2a53fc74ebfe63c
Alex Yakunin <alex.yakunin@gmail.com>
2024-09-12 09:52:55 -0700

    fix: MAUI CreateWindow impl. bug

--- src/dotnet/App.Maui/App.cs
+++ src/dotnet/App.Maui/App.cs
@@ -29,7 +29,7 @@ public class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        var window = base.CreateWindow(activationState);
+		var window = new Window(new MainPage());
         window.Destroying += (_, _) => FlushSentryData();
         window.Title = MauiSettings.IsDevApp ? "Actual Chat (Dev)" : "Actual Chat";
         return window;
