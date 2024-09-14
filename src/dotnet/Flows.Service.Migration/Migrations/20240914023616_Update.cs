01419ab8b8973c4f0f68b5deabe650458ff90975
Alex Yakunin <alex.yakunin@gmail.com>
2024-09-13 19:36:49 -0700

    db: Migrations in Flows

---
+++ src/dotnet/Flows.Service.Migration/Migrations/20240914023616_Update.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Flows.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_events_uuid",
+                table: "_events");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "can_resume",
+                table: "_flows",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_flows_can_resume",
+                table: "_flows",
+                column: "can_resume");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_flows_can_resume",
+                table: "_flows");
+
+            migrationBuilder.DropColumn(
+                name: "can_resume",
+                table: "_flows");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_events_uuid",
+                table: "_events",
+                column: "uuid",
+                unique: true);
+        }
+    }
+}
