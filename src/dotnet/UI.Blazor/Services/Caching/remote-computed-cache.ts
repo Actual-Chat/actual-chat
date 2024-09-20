00367b92f16b021223a583ceef23f2fa7485d058
Alex Yakunin <alex.yakunin@gmail.com>
2024-09-20 10:55:26 -0700

    chore: remoteComputedCache - explicit version bump

--- src/dotnet/UI.Blazor/Services/Caching/remote-computed-cache.ts
+++ src/dotnet/UI.Blazor/Services/Caching/remote-computed-cache.ts
@@ -1,4 +1,4 @@
 import { Kvas } from 'kvas';
 import { WebKvasBackend } from '../Kvas/web-kvas-backend';
 
-export const remoteComputedCache = new WebKvasBackend(new Kvas('ccc', true), "2.0", true, true);
+export const remoteComputedCache = new WebKvasBackend(new Kvas('ccc', true), "3.0", true, true);
