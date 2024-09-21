851ea63ab2aaa33ff7485b403dc5b308fff8f922
Alex Yakunin <alex.yakunin@gmail.com>
2024-09-20 20:28:51 -0700

    fix: AccountOperations.cs + update Fusion

--- src/dotnet/Api.Contracts/ApiConstants.cs
+++ src/dotnet/Api.Contracts/ApiConstants.cs
@@ -5,7 +5,7 @@ public static class ApiConstants
     public static class Concurrency
     {
         public static readonly int Unlimited = 0;
-        public static readonly int High = 512;
+        public static readonly int High = 0; // Seemingly a better option than e.g. 256 or so
         public static readonly int Low = 128;
     }
 }
