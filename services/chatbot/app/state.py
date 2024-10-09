04ea6579a83802dbe8f469524f518f60fcb728ac
Alex Ustinov <alex.ustinov@actual.chat>
2024-10-09 00:03:17 -0700

    fix: use custom reducer to kill empty AI assistant message

--- services/chatbot/app/state.py
+++ services/chatbot/app/state.py
@@ -1,10 +1,24 @@
 from pydantic import BaseModel
-from typing import Annotated, Optional
-from langchain_core.messages import AnyMessage
+from typing import Annotated, Optional, Union
+from langchain_core.messages import (
+    AnyMessage,
+    RemoveMessage,
+    MessageLikeRepresentation
+)
 from langgraph.graph import add_messages
 
+Messages = Union[list[MessageLikeRepresentation], MessageLikeRepresentation]
+
+def reduce_list(left: Messages, right: Messages) -> Messages:
+    if isinstance(left, list) and len(left) > 0 and not left[-1].content:
+        # Remove empty final message by AI agent from it previous response
+        remove_message = RemoveMessage(id=left[-1].id)
+        right = right + [remove_message] if isinstance(right, list) else [right, remove_message]
+
+    return add_messages(left, right)
+
 class State(BaseModel):
-    messages: Annotated[list[AnyMessage], add_messages]
+    messages: Annotated[list[AnyMessage], reduce_list]
 
     summary: Optional[str] = None
     search_type: Optional[str] = None
