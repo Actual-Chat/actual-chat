0907a4e7e1f4a7f3a2da01066c69c162a0b6bd6c
Alex Ustinov <alex.ustinov@actual.chat>
2024-10-21 21:18:59 -0700

    wip: draft implementation of search type detector

--- src/dotnet/MLSearch.Service/Module/MLSearchServiceModule.cs
+++ src/dotnet/MLSearch.Service/Module/MLSearchServiceModule.cs
@@ -231,6 +231,7 @@ public sealed class MLSearchServiceModule(IServiceProvider moduleServices) : Hos
                 });
                 services.AddSingleton<IFilters, Filters>();
                 services.AddSingleton<IBotConversationHandler, ChatBotConversationHandler>();
+                services.AddSingleton<ISearchTypeDetector, SearchTypeDetector>();
                 services.AddSingleton<IChatBotWorker>(
                     static c => c.CreateInstance<ChatBotWorker>());
                 services.AddWorkerPool<IChatBotWorker, MLSearch_TriggerContinueConversationWithBot, ChatId, ChatId>(
