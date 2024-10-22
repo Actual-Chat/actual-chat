665fee09b173eb14c25c9074202f28361231a04d
Alex Ustinov <alex.ustinov@actual.chat>
2024-10-21 21:18:59 -0700

    wip: draft implementation of search type detector

--- src/dotnet/MLSearch.Service/Bot/External/ExternalChatBotConversationHandler.cs
+++ src/dotnet/MLSearch.Service/Bot/External/ExternalChatBotConversationHandler.cs
@@ -1,6 +1,6 @@
+using System.Collections.Frozen;
 using ActualChat.Chat;
 using ActualChat.MLSearch.Bot.Tools.Context;
-using Microsoft.Extensions.Options;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents;
 using Microsoft.SemanticKernel.Agents.History;
@@ -23,32 +23,33 @@ namespace ActualChat.MLSearch.Bot;
 **/
 internal class ChatBotConversationHandler(
     Kernel kernel,
+    ISearchTypeDetector searchTypeDetector,
     IAuthorsBackend authors,
     IBotToolsContextHandler botToolsContextHandler)
     : IBotConversationHandler
 {
     private const string AgentInstructions =
     """
-        For the given objective, come up with a simple step by step plan.
-        Use tools provided if neccessary.
-        You have the following list of tools:
-        {tools}
+    For the given objective, come up with a simple step by step plan.
+    Use tools provided if neccessary.
+    You have the following list of tools:
+    {tools}
 
-        In order to use a tool, you can use <tool></tool> and <tool_input></tool_input> tags.
-        You will then get back a response in the form <observation></observation>
+    In order to use a tool, you can use <tool></tool> and <tool_input></tool_input> tags.
+    You will then get back a response in the form <observation></observation>
 
-        This plan should involve individual tasks, that if executed correctly
-        will yield the correct answer.
-        Do not add any superfluous steps.
-        When you are done, respond with a final answer between <final_answer></final_answer>
-        Make sure that each step has all the information needed - do not skip steps.
+    This plan should involve individual tasks, that if executed correctly
+    will yield the correct answer.
+    Do not add any superfluous steps.
+    When you are done, respond with a final answer between <final_answer></final_answer>
+    Make sure that each step has all the information needed - do not skip steps.
 
-        Previous conversation was:
-        {chat_history}
+    Previous conversation was:
+    {chat_history}
 
-        Objective: {input}
+    Objective: {input}
 
-        Thoughts: {agent_scratchpad}
+    Thoughts: {agent_scratchpad}
     """;
 
     private const int reducerMessageCount = 10;
@@ -136,5 +137,68 @@ internal sealed class SearchToolPlugin
 
 }
 
+
+[Flags]
+public enum SearchType
+{
+    None = 0,
+    Public = 1,
+    Private = 2,
+    General = Public | Private,
+}
+
+internal interface ISearchTypeDetector
+{
+    Task<SearchType> Detect(ChatMessageContent message, CancellationToken cancellationToken = default);
+}
+
+internal class SearchTypeDetector(Kernel kernel): ISearchTypeDetector
+{
+    private const string DetectSearchTypePrompt =
+    """
+    As an expert in searching for information in chats, you follow a clear process to identify the target search area.
+    Depending on your answer, the search process runs through different subsets of chats, so the answer is critical.
+    There are three possible search areas:
+    * PUBLIC - search in the publicly available chats
+    * PRIVATE - search in the chats where the user is a member or owner
+    * GENERAL - search in all chats, both PUBLIC and PRIVATE
+    There is also one special value UNCERTAIN, when it is unclear from the user's message where to run next search.
+    Instructions:
+    * If the user says "search all chats" or "search everywhere," the search area is GENERAL
+    * If the user requested to reset or start the search over, the search area is GENERAL
+    * If the user explicitly mentions "public chats" or similar, the search area is PUBLIC
+    * If the user refers to "private chats" or "my chats" or similar, the search area is PRIVATE
+    * In all other cases when user's message is unrelated to chats the search area is UNCERTAIN
+    Important:
+    * Every user message in the list redefines search area unless search area is UNCERTAIN.
+    * Return only one word in the output (PUBLIC, PRIVATE, GENERAL or UNCERTAIN).
+    """;
+
+    private static readonly FrozenDictionary<string, SearchType> ResponseMap = new Dictionary<string, SearchType>() {
+        { "PUBLIC", SearchType.Public },
+        { "PRIVATE", SearchType.Private },
+        { "GENERAL", SearchType.General },
+    }.ToFrozenDictionary();
+
+    public async Task<SearchType> Detect(ChatMessageContent message, CancellationToken cancellationToken = default)
+    {
+        var agent = new ChatCompletionAgent() {
+            Name = Constants.User.Sherlock.Name,
+            Instructions = DetectSearchTypePrompt,
+            Kernel = kernel,
+        };
+
+        var response = await agent.InvokeAsync([message], cancellationToken: cancellationToken)
+            .FirstAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+
+        return response != null && ResponseMap.TryGetValue(response.Content ?? string.Empty, out var searchType)
+            ? searchType
+            : SearchType.None;
+    }
+}
+
 #pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 #pragma warning restore SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
---
+++ tests/MLSearch.IntegrationTests/Bot/SearchTypeDetectorTest.cs
@@ -0,0 +1,61 @@
+
+using ActualChat.MLSearch.Bot;
+using ActualChat.MLSearch.Module;
+using ActualLab.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace ActualChat.MLSearch.IntegrationTests.Bot;
+
+public class SearchTypeDetectorTest(ITestOutputHelper @out): TestBase(@out)
+{
+    private static Kernel CreateKernel()
+    {
+        var configuration = GetConfiguration();
+
+        var openAISettings = configuration.GetSection("MLSearchSettings:Bot:OpenAI").Get<OpenAISettings>();
+
+        return Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(
+                apiKey: openAISettings!.ApiKey,
+                modelId: openAISettings!.ChatModel)
+            .Build();
+
+        static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(GetTestsBaseDirectory())
+                .AddJsonFile("testsettings.json", false, false);
+            if (EnvExt.IsRunningInContainer())
+                builder.AddJsonFile("testsettings.docker.json", false, false);
+            builder.AddJsonFile("testsettings.local.json", true, false);
+            builder.AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+            return configuration;
+
+            static FilePath GetTestsBaseDirectory()
+                => FilePath.New(typeof(DefaultStartup).Assembly.Location ?? Environment.CurrentDirectory).DirectoryPath;
+        }
+    }
+
+    public static TheoryData<string, SearchType> ExpectedPairs => new() {
+        { "Search in public chats", SearchType.Public },
+        { "search in all chats", SearchType.General },
+        { "London is the capital of the Great Britain", SearchType.None },
+        { "search in my chats", SearchType.Private },
+        { "search in my private chats", SearchType.Private },
+        { "search in my public chats", SearchType.Public },
+    };
+
+    private readonly ISearchTypeDetector _searchTypeDetector = new SearchTypeDetector(CreateKernel());
+
+    [Theory]
+    [MemberData(nameof(ExpectedPairs))]
+    public async Task SearchTypeDetectorProvidesExpectedOutput(string userInput, SearchType expectedSearchType)
+    {
+        var searchType = await _searchTypeDetector.Detect(new ChatMessageContent(AuthorRole.User, userInput));
+        Assert.Equal(expectedSearchType, searchType);
+    }
+}
