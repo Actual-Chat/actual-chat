2eef467d70a4980c814ec44188cd39dcd5986aa3
Dmitry Filippov <crui3er@gmail.com>
2024-10-29 01:00:54 +0400

    chore: refactor AutoNavigationUI.DispatchNavigateTo

--- src/dotnet/UI.Blazor/Services/INotificationUI.cs
+++ src/dotnet/UI.Blazor/Services/INotificationUI.cs
@@ -4,5 +4,4 @@ public interface INotificationUI
 {
     Task DeregisterDevice(CancellationToken cancellationToken = default);
     Task EnsureDeviceRegistered(CancellationToken cancellationToken = default);
-    Task NavigateToNotificationUrl(string url);
 }
