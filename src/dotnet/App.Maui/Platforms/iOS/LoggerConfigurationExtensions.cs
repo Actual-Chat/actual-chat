2bed6dcfef8a05cf7f41c709efc7625c6e8e215d
Frol <4425748+FrolYo@users.noreply.github.com>
2024-10-28 20:06:46 +0300

    feat: apple unified logging + remove serilog.sinks.xamarin #2627

---
+++ src/dotnet/App.Maui/Platforms/iOS/LoggerConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+
+namespace ActualChat.App.Maui;
+
+// Adds WriteTo.AppleLog() to the logger configuration
+public static class LoggerConfigurationExtensions
+{
+    public const string DefaultAppleLogOutputTemplate =
+        "{Level:u3} [{SourceContext}] {Message:l{NewLine:l}{Exception:l}";
+
+    public static LoggerConfiguration AppleLog(
+        this LoggerSinkConfiguration sinkConfiguration,
+        LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose,
+        string outputTemplate = DefaultAppleLogOutputTemplate,
+        IFormatProvider? formatProvider = null)
+    {
+        if (sinkConfiguration == null)
+            throw new ArgumentNullException(nameof(sinkConfiguration));
+
+        var templateTextFormatter = outputTemplate != null
+            ? new MessageTemplateTextFormatter(outputTemplate, formatProvider)
+            : throw new ArgumentNullException(nameof(outputTemplate));
+
+        return sinkConfiguration.Sink(new AppleUnifiedLogSink(templateTextFormatter), restrictedToMinimumLevel);
+    }
+}
