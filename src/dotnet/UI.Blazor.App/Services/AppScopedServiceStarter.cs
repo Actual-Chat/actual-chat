28b0162dc64386d07719c37c6f729c231ce202fd
Alexey Kochetov <alexis.kochetov@gmail.com>
2024-11-06 17:48:38 +0300

    chore: start hosted services sequentially

--- src/dotnet/UI.Blazor.App/Services/AppScopedServiceStarter.cs
+++ src/dotnet/UI.Blazor.App/Services/AppScopedServiceStarter.cs
@@ -146,12 +146,11 @@ public class AppScopedServiceStarter
     private async Task StartHostedServices()
     {
         using var _ = Tracer.Region();
-        var tasks = new List<Task>();
         var tracePrefix = nameof(StartHostedServices) + ": starting ";
         foreach (var hostedService in Hub.HostedServices()) {
             Tracer.Point(tracePrefix + hostedService.GetType().Name);
-            tasks.Add(hostedService.StartAsync(default));
+            await hostedService.StartAsync(default);
+            await Task.Yield();
         }
-        await Task.WhenAll(tasks).ConfigureAwait(false);
     }
 }
