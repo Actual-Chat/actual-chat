218392267b774710f1641f943a9a2e562e44ddc1
Frol <4425748+FrolYo@users.noreply.github.com>
2024-11-11 22:55:09 +0300

    feat: env prefix in open search names #2658

--- src/dotnet/MLSearch.Service/Engine/OpenSearchNames.cs
+++ src/dotnet/MLSearch.Service/Engine/OpenSearchNames.cs
@@ -4,25 +4,26 @@ namespace ActualChat.MLSearch.Engine;
 
 internal sealed class OpenSearchNames
 {
-    public const string MLPrefix = "ml";
-    public const string IngestPipelineNameSuffix = "ingest-pipeline";
-    public const string IndexNameSuffix = "index";
-    public const string TemplateNameSuffix = "template";
+    private const string MLPrefix = "ml";
+    private const string IngestPipelineNameSuffix = "ingest-pipeline";
+    private const string IndexNameSuffix = "index";
+    private const string TemplateNameSuffix = "template";
     public const string EntryIndexVersion = "v4";
     public const string UserIndexVersion = "v5";
     public const string GroupIndexVersion = "v5";
     public const string PlaceIndexVersion = "v3";
 
-    public const string TestPrefix = "test";
+    private const string TestPrefix = "test";
     public const string ChatContent = "chat-content";
     public const string ChatContentCursor = "chat-content-cursor";
     public const string ChatCursor = "v2-chat-cursor";
     public string UniquePart { get; init; } = ""; // for testing purpose only
+    public string Env { get; init; } = "";
     public static string MLIndexPattern => $"{MLPrefix}-*";
     public static string MLTestIndexPattern => $"{MLPrefix}-{TestPrefix}-*";
     public static string MLTemplateName => $"{MLPrefix}-{TemplateNameSuffix}";
-    private string MLFullPrefix => UniquePart.IsNullOrEmpty() ? MLPrefix : $"{MLPrefix}-{UniquePart}";
-    private string Prefix => UniquePart.IsNullOrEmpty() ? "sm-" : $"sm-{UniquePart}-"; // sm == "Search Module"
+    private string MLFullPrefix => ToPrefix(Env, MLPrefix, UniquePart);
+    private string Prefix => ToPrefix(Env, "sm", UniquePart); // sm == "Search Module"
     public string CommonIndexTemplateName => $"{Prefix}common";
     public string CommonIndexPattern => $"{Prefix}*";
     public string UserIndexName => $"{Prefix}users-{UserIndexVersion}";
@@ -31,16 +32,20 @@ internal sealed class OpenSearchNames
     public string EntryIndexName => $"{Prefix}entries-{EntryIndexVersion}";
 
     internal string GetFullName(string id, EmbeddingModelProps modelProps)
-        => string.Join('-',
-            MLFullPrefix,
-            id,
-            IndexNameSuffix,
-            modelProps.UniqueKey);
+        => GetFull(MLFullPrefix, id, IndexNameSuffix, modelProps.UniqueKey);
 
     internal string GetFullIngestPipelineName(string id, EmbeddingModelProps modelProps)
-        => string.Join('-',
-            MLFullPrefix,
-            id,
-            IngestPipelineNameSuffix,
-            modelProps.UniqueKey);
+        => GetFull(MLFullPrefix, id, IngestPipelineNameSuffix, modelProps.UniqueKey);
+
+    private static string GetFull(params IEnumerable<string> parts)
+        => string.Join("", parts.Select(ToPrefix)).TrimEnd('-');
+
+    private static string ToPrefix(params IEnumerable<string> parts)
+        => string.Join("", parts.Select(ToPrefix));
+
+    private static string ToPrefix(string s)
+    {
+        s = s.Trim('-');
+        return s.IsNullOrEmpty() ? "" : $"{s}-";
+    }
 }
