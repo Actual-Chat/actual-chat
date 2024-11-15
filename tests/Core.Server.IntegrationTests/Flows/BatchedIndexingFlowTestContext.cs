8f793437e0926e58b9e6511c3b9e023e601b37ac
Frol <4425748+FrolYo@users.noreply.github.com>
2024-11-15 19:12:30 +0300

    feat: indexing flow fixes #2663

--- tests/Core.Server.IntegrationTests/Flows/BatchedIndexingFlowTestContext.cs
+++ tests/Core.Server.IntegrationTests/Flows/BatchedIndexingFlowTestContext.cs
@@ -2,14 +2,14 @@ using ActualChat.Flows;
 
 namespace ActualChat.Core.Server.IntegrationTests.Flows;
 
-public sealed class BatchedIndexingFlowTestContext<TItem>
+public sealed class BatchedIndexingFlowTestContext<TItem>(MomentClockSet clocks)
 {
     private readonly Dictionary<Symbol, Queue<IReadOnlyList<TItem>>> _batches = new();
     private readonly Dictionary<Symbol, List<IReadOnlyList<TItem>>> _processedBatches = new();
     private readonly Dictionary<Symbol, List<FlowTransition>> _appliedTransitions = new();
     private readonly Dictionary<Symbol, Queue<Func<Task<bool>>>> _tailHandlers = new();
 
-    public void Add(Symbol id, params IReadOnlyList<TItem>[] batches)
+    public void Add(Symbol id, params IEnumerable<IReadOnlyList<TItem>> batches)
     {
         var queue = _batches.GetOrAdd(id);
         foreach (var result in batches)
@@ -17,7 +17,7 @@ public sealed class BatchedIndexingFlowTestContext<TItem>
     }
 
     public IReadOnlyList<TItem> Next(Symbol id)
-        => _batches[id].Dequeue();
+        => _batches[id].TryDequeue(out var batch) ? batch : [];
 
     public Queue<IReadOnlyList<TItem>> ListRemaining(Symbol id)
         => _batches[id];
@@ -25,14 +25,22 @@ public sealed class BatchedIndexingFlowTestContext<TItem>
     public void OnTransition(Symbol id, FlowTransition transition)
         => _appliedTransitions.GetOrAdd(id).Add(transition);
 
-    public IReadOnlyList<FlowTransition> ListTransitions(Symbol id)
-        => _appliedTransitions.GetValueOrDefault(id, []);
+    public List<(string Step, TimeSpan? HardResumeIn)> ListTransitions(Symbol id)
+        => _appliedTransitions.GetValueOrDefault(id, [])
+            .Select(transition => (transition.Step.Value,
+                transition.HardResumeAt == Flow.InfiniteHardResumeAt
+                    ? TimeSpan.MaxValue
+                    : transition.HardResumeAt - clocks.SystemClock.Now))
+            .ToList();
 
     public void OnProcessed(Symbol id, IReadOnlyList<TItem> batch)
         => _processedBatches.GetOrAdd(id).Add(batch);
 
-    public List<IReadOnlyList<TItem>> ListProcessed(Symbol id)
-        => _processedBatches.GetValueOrDefault(id, []);
+    public List<IReadOnlyList<TItem>> ListProcessed(Symbol id, bool skipEmpty = true)
+    {
+        var list = _processedBatches.GetValueOrDefault(id, []);
+        return !skipEmpty ? list : list.Where(x => x.Count > 0).ToList();
+    }
 
     public void AddTailHandler(Symbol id, Func<Task<bool>> handler)
         => _tailHandlers.GetOrAdd(id).Enqueue(handler);
