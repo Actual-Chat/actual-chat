7e987ac63271ccce0ef375bfaa6ebdb6d782d0c4
Dmitry Filippov <crui3er@gmail.com>
2024-11-21 13:40:23 +0400

    chore: remove IsAvailable methods from IUserLinks

--- src/dotnet/Chat.Service/UserLinks.cs
+++ src/dotnet/Chat.Service/UserLinks.cs
@@ -8,25 +8,16 @@ public class UserLinks(IServiceProvider services) : IUserLinks
     private IChatsBackend ChatsBackend { get; } = services.GetRequiredService<IChatsBackend>();
     private IAccountsBackend AccountsBackend { get; } = services.GetRequiredService<IAccountsBackend>();
 
-    public virtual async Task<ResolvedUserLinkResult> ResolveUserLink(UserLinkId userLinkId, CancellationToken cancellationToken = default)
+    public virtual async Task<UserLinkRef> GetUserLinkRef(UserLinkId userLinkId, CancellationToken cancellationToken = default)
     {
         if (userLinkId.IsNone)
             throw new ArgumentOutOfRangeException(nameof(userLinkId));
 
         var userLink = await Backend.Get(userLinkId, cancellationToken).ConfigureAwait(false);
         if (userLink is null)
-            return ResolvedUserLinkResult.None;
+            return UserLinkRef.None;
 
-        return new ResolvedUserLinkResult(userLink.Kind, userLink.TargetId);
-    }
-
-    public virtual async Task<bool> IsUserLinkAvailable(UserLinkId userLinkId, CancellationToken cancellationToken)
-    {
-        if (userLinkId.IsNone)
-            throw new ArgumentOutOfRangeException(nameof(userLinkId));
-
-        var userLink = await Backend.Get(userLinkId, cancellationToken).ConfigureAwait(false);
-        return userLink is null;
+        return new UserLinkRef(userLink.Kind, userLink.TargetId);
     }
 
     public virtual Task<PlaceChatId> GetPlaceChatIdByUserLink(PlaceId placeId, UserLinkId userLinkId, CancellationToken cancellationToken)
@@ -39,31 +30,11 @@ public class UserLinks(IServiceProvider services) : IUserLinks
         return ChatsBackend.GetPlaceChatIdByUserLink(placeId, userLinkId.ToLower(), cancellationToken);
     }
 
-    public virtual async Task<bool> IsPlaceChatUserLinkAvailable(PlaceId placeId, UserLinkId userLinkId, CancellationToken cancellationToken)
-    {
-        if (placeId.IsNone)
-            throw new ArgumentOutOfRangeException(nameof(placeId));
-        if (userLinkId.IsNone)
-            throw new ArgumentOutOfRangeException(nameof(userLinkId));
-
-        var chatId = await GetPlaceChatIdByUserLink(placeId, userLinkId, cancellationToken).ConfigureAwait(false);
-        return chatId.IsNone;
-    }
-
-    public virtual Task<UserId> ResolveAccountUserLink(UserLinkId userLinkId, CancellationToken cancellationToken)
+    public virtual Task<UserId> GetUserIdByUserLink(UserLinkId userLinkId, CancellationToken cancellationToken)
     {
         if (userLinkId.IsNone)
             throw new ArgumentOutOfRangeException(nameof(userLinkId));
 
         return AccountsBackend.GetIdByUserLink(userLinkId, cancellationToken);
-    }
-
-    public virtual async Task<bool> IsUserLinkAvailableForAccount(UserLinkId userLinkId, CancellationToken cancellationToken)
-    {
-        if (userLinkId.IsNone)
-            throw new ArgumentOutOfRangeException(nameof(userLinkId));
-
-        var userId = await ResolveAccountUserLink(userLinkId, cancellationToken).ConfigureAwait(false);
-        return userId.IsNone;
     }
 }
