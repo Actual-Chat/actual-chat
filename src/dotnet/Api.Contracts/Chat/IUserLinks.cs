53dc676987837c92580af71a590d76ff3f5762aa
Dmitry Filippov <crui3er@gmail.com>
2024-11-21 13:40:23 +0400

    chore: remove IsAvailable methods from IUserLinks

--- src/dotnet/Api.Contracts/Chat/IUserLinks.cs
+++ src/dotnet/Api.Contracts/Chat/IUserLinks.cs
@@ -3,28 +3,19 @@ namespace ActualChat.Chat;
 public interface IUserLinks : IComputeService
 {
     [ComputeMethod(MinCacheDuration = 60), RemoteComputeMethod(CacheMode = RemoteComputedCacheMode.Cache, MinCacheDuration = 600)]
-    Task<ResolvedUserLinkResult> ResolveUserLink(UserLinkId userLinkId, CancellationToken cancellationToken = default);
-
-    [ComputeMethod(MinCacheDuration = 60)]
-    Task<bool> IsUserLinkAvailable(UserLinkId userLinkId, CancellationToken cancellationToken = default);
+    Task<UserLinkRef> GetUserLinkRef(UserLinkId userLinkId, CancellationToken cancellationToken = default);
 
     [ComputeMethod(MinCacheDuration = 60), RemoteComputeMethod(CacheMode = RemoteComputedCacheMode.Cache, MinCacheDuration = 600)]
     Task<PlaceChatId> GetPlaceChatIdByUserLink(PlaceId placeId, UserLinkId userLinkId, CancellationToken cancellationToken = default);
 
-    [ComputeMethod(MinCacheDuration = 60)]
-    Task<bool> IsPlaceChatUserLinkAvailable(PlaceId placeId, UserLinkId userLinkId, CancellationToken cancellationToken = default);
-
     [ComputeMethod(MinCacheDuration = 60), RemoteComputeMethod(CacheMode = RemoteComputedCacheMode.Cache, MinCacheDuration = 600)]
-    Task<UserId> ResolveAccountUserLink(UserLinkId userLinkId, CancellationToken cancellationToken = default);
-
-    [ComputeMethod(MinCacheDuration = 60)]
-    Task<bool> IsUserLinkAvailableForAccount(UserLinkId userLinkId, CancellationToken cancellationToken = default);
+    Task<UserId> GetUserIdByUserLink(UserLinkId userLinkId, CancellationToken cancellationToken = default);
 }
 
 public enum UserLinkKind { Chat, Place }
 
-public record ResolvedUserLinkResult(UserLinkKind Kind, string TargetId)
+public record UserLinkRef(UserLinkKind Kind, string TargetId)
 {
-    public static readonly ResolvedUserLinkResult None = new (UserLinkKind.Chat, "");
+    public static readonly UserLinkRef None = new (UserLinkKind.Chat, "");
     public bool IsNone => TargetId.IsNullOrEmpty();
 }
