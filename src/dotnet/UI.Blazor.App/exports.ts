14ae68b50eab6bb661ec9190e81da36ae52b1ca6
iqmulator <iqmulator@gmail.com>
2024-12-10 20:29:55 +0500

    update NoResults component, add no-search-result svg, fix ui bugs

--- src/dotnet/UI.Blazor.App/exports.ts
+++ src/dotnet/UI.Blazor.App/exports.ts
@@ -27,6 +27,7 @@ export * from './Components/MarkupParts/CodeBlockMarkupView/code-block-markup-vi
 export * from './Components/MentionList/mention-list';
 export * from './Components/Navbar/sortable-list';
 export * from './Components/NoResults/no-results-svg.lit';
+export * from './Components/NoResults/no-search-results-svg.lit';
 export * from './Components/Onboarding/phone-verification-cat-svg.lit';
 export * from './Components/SearchPanel/search-panel';
 export * from './Components/Selection/selection-host';
