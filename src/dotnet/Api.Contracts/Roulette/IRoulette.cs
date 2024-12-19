97c04e4aab432f06c6e94c5739ef6ea6bdf4b14b
Dmitry Filippov <crui3er@gmail.com>
2024-12-19 14:08:35 +0400

    feat: Chat Roulette banner

--- src/dotnet/Api.Contracts/Roulette/IRoulette.cs
+++ src/dotnet/Api.Contracts/Roulette/IRoulette.cs
@@ -1,3 +1,5 @@
+using MemoryPack;
+
 namespace ActualChat.Roulette;
 
 public interface IRoulette : IComputeService
@@ -12,4 +14,16 @@ public interface IRoulette : IComputeService
 
     [ComputeMethod]
     Task<ChatId> GetOrCreateChat(Session session, Symbol ownProfileId, Symbol peerProfileId, CancellationToken cancellationToken);
+
+    // Commands
+
+    [CommandHandler]
+    Task OnDeclineChatRoulette(Roulette_DeclineChatRoulette command, CancellationToken cancellationToken);
 }
+
+[DataContract, MemoryPackable(GenerateType.VersionTolerant)]
+// ReSharper disable once InconsistentNaming
+public sealed partial record Roulette_DeclineChatRoulette(
+    [property: DataMember, MemoryPackOrder(0)] Session Session,
+    [property: DataMember, MemoryPackOrder(1)] ChatId ChatId
+) : IApiCommand;
