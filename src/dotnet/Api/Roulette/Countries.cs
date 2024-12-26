caeb8f5bebb376e1ef4c11314deee633ed4d4a0a
Dmitry Filippov <crui3er@gmail.com>
2024-12-26 15:49:19 +0400

    feat: extend country list for actual chat

--- src/dotnet/Api/Roulette/Countries.cs
+++ src/dotnet/Api/Roulette/Countries.cs
@@ -1,17 +1,8 @@
 namespace ActualChat.Roulette;
 
-public static class Countries
+public static partial class Countries
 {
     public static readonly Country NotSpecified = Country.NotSpecified;
-    public static readonly Country USA = new ("US", "United States", AssumeValid.Option);
-    public static readonly Country Russia = new ("RU", "Russia", AssumeValid.Option);
-    public static readonly Country Armenia = new ("AM", "Armenia", AssumeValid.Option);
-
-    public static readonly ApiArray<Country> All = [
-        Armenia,
-        Russia,
-        USA
-    ];
 
     public static readonly Dictionary<string, Country> CodeToCountry =
         All.Select(x => new KeyValuePair<string, Country>(x.Code, x))
