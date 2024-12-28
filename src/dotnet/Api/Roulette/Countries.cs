29c7953309de9587d13f69f59833254f562df834
Dmitry Filippov <crui3er@gmail.com>
2024-12-28 11:43:25 +0400

    fix: profile country saving does not work

--- src/dotnet/Api/Roulette/Countries.cs
+++ src/dotnet/Api/Roulette/Countries.cs
@@ -4,9 +4,12 @@ public static partial class Countries
 {
     public static readonly Country NotSpecified = Country.NotSpecified;
 
-    public static readonly Dictionary<string, Country> CodeToCountry =
-        All.Select(x => new KeyValuePair<string, Country>(x.Code, x))
-            .Concat(All.Select(x => new KeyValuePair<string, Country>(x.Code.ToLowerInvariant(), x)))
-            .DistinctBy(kv => kv.Key)
-            .ToDictionary(kv => kv.Key, kv => kv.Value);
+    public static readonly Dictionary<string, Country> CodeToCountry;
+
+    static Countries()
+        => CodeToCountry =
+            All.Select(x => new KeyValuePair<string, Country>(x.Code, x))
+                .Concat(All.Select(x => new KeyValuePair<string, Country>(x.Code.ToLowerInvariant(), x)))
+                .DistinctBy(kv => kv.Key)
+                .ToDictionary(kv => kv.Key, kv => kv.Value);
 }
