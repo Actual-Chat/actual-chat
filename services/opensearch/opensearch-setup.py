55fff788c0fa25a713c17dfbe3c318d6a3878685
Alex Ustinov <alex.ustinov@actual.chat>
2024-12-30 19:53:13 -0800

    wip: do not deploy built-in model on configuration

--- services/opensearch/opensearch-setup.py
+++ services/opensearch/opensearch-setup.py
@@ -5,7 +5,6 @@ import time
 from opensearchpy import OpenSearch
 from opensearch_py_ml.ml_commons import MLCommonClient
 
-
 class API:
     def __init__(self, cluster_url, username=None, password=None, client_cert_path=None, client_key_path=None, ca_cert_path=None):
         self._cluster_url = cluster_url
@@ -32,44 +31,6 @@ class API:
         print(result)
         print(result.json())
         return result
-    
-    def configure(self):
-        data = {
-            "persistent": {
-                "plugins": {
-                    "ml_commons": {
-                        "only_run_on_ml_node": "false",
-                        "allow_registering_model_via_url": "true",
-                        "allow_registering_model_via_local_file": "true",
-                        "model_access_control_enabled": "true",
-                        "native_memory_threshold": "95"
-                    }
-                }
-            }
-        }
-
-        # Check current settings
-        current_settings = self.call_opensearch("/_cluster/settings").json()
-        current_persistent_settings = current_settings.get('persistent', {}).get('plugins', {}).get('ml_commons', {})
-
-        # If settings are already applied, return current settings
-        if current_persistent_settings == data['persistent']['plugins']['ml_commons']:
-            print("Cluster settings are already applied.")
-            return current_settings
-
-        # Apply new settings
-        result = self.call_opensearch(
-            "/_cluster/settings",
-            method=requests.put,
-            data=data
-        )
-
-        # Throw an exception if the configuration update fails
-        if result.status_code != 200:
-            raise Exception(f"Failed to update OpenSearch configuration. Status code: {result.status_code}\nDetails: {result.text}")
-
-        return result.json()
-
 
     def register_model_group(self, model_group_name, *, description=""):
         # Notes:
@@ -101,18 +62,24 @@ class API:
         ).json()['model_group_id']
 
     def get_model_group_model_id(self, model_group_id):
-        # Notes:
-        # For some reason current opensearch_py_ml client
-        # does not have methods to register a model group
         try:
-            # Return an existing model group id if it already exists.
+            # Return an existing model group id if any.
             response = self.call_opensearch(
                 "/_plugins/_ml/models/_search",
                 method = requests.post,
                 data = {
                     "query": {
-                        "match": {
-                            "model_group_id": model_group_id
+                        "bool": {
+                            "must": [{
+                                "exists": {
+                                    "field": "model_state"
+                                }
+                            }],
+                            "should": [{
+                                "match": {
+                                    "model_group_id": model_group_id
+                                }
+                            }]
                         }
                     },
                     "sort": [{
@@ -129,18 +96,14 @@ class API:
             # Python EAFP principle
             return None
 
-
 def main():
     cluster_url = os.getenv('OPENSEARCH_CLUSTER_URL')
     client_cert_path = os.getenv('OPENSEARCH_CLIENT_CERT_PATH')
     client_key_path = os.getenv('OPENSEARCH_CLIENT_KEY_PATH')
     ca_cert_path = os.getenv('OPENSEARCH_CA_CERT_PATH')
-    username = os.getenv('OPENSEARCH_USERNAME')
-    password = os.getenv('OPENSEARCH_PASSWORD')
-   
+
     model_group_name = os.getenv('OPENSEARCH_ML_MODEL_GROUP')
-    api = API(cluster_url, username, password, client_cert_path, client_key_path, ca_cert_path)  # Pass certificate details
-    api.configure()
+    api = API(cluster_url, client_cert_path, client_key_path, ca_cert_path)  # Pass certificate details
     model_group_id = api.register_model_group(
         model_group_name,
         description = "A model group for NLP models"
@@ -148,32 +111,24 @@ def main():
     current_model_id = api.get_model_group_model_id(
         model_group_id
     )
-    if username and password:
-        client = OpenSearch(
-            hosts=[cluster_url],
-            http_auth=(username, password),
-            use_ssl=True,  # Enable SSL/TLS
-            verify_certs=True,  # Verify server certificate against CA
-            ssl_assert_hostname=False,  # Disable hostname verification (if needed)
-            ca_certs=ca_cert_path,
-        )
-    else:
-        client = OpenSearch(
-            hosts=[cluster_url],
-            use_ssl=True,  # Enable SSL/TLS
-            verify_certs=True,  # Verify server certificate against CA
-            ssl_assert_hostname=False,  # Disable hostname verification (if needed)
-            client_cert=client_cert_path,
-            client_key=client_key_path,
-            ca_certs=ca_cert_path,
-        ) if client_cert_path else OpenSearch(
-            hosts=[cluster_url],
-        )
-    
+    client = OpenSearch(
+        hosts=[cluster_url],
+        use_ssl=True,  # Enable SSL/TLS
+        verify_certs=True,  # Verify server certificate against CA
+        ssl_assert_hostname=False, # Disable hostname verification (if needed)
+        client_cert=client_cert_path,
+        client_key=client_key_path,
+        ca_certs=ca_cert_path,
+    ) if client_cert_path else OpenSearch(
+        hosts=[cluster_url],
+    )
+
     ml_client = MLCommonClient(client)
     if current_model_id is not None:
+        print(f"Current model id: {current_model_id}")
         current_model_info = ml_client.get_model_info(current_model_id)
         print(current_model_info)
+
         current_model_content_hash_value = current_model_info.get('model_content_hash_value', '000')
         current_model_all_config = current_model_info.get('model_config', {'all_config':{}})['all_config']
         with open(os.getenv('TORCHSCRIPT_MODEL_CONFIG_PATH'), 'r') as f:
@@ -185,32 +140,20 @@ def main():
             current_model_content_hash_value == next_model_content_hash_value
             and current_model_all_config == next_model_all_config
         ):
-            print("Current model and config have no changes")
-            current_model_state = current_model_info['model_state']
-            if current_model_state == 'DEPLOYED':
-                # OK state. Exiting
-                print("Model is deployed")
-                return
-            if current_model_state == 'DEPLOY_FAILED':
-                # Attempt to execute deploy
-                print("Redeploying a model")
-                deploy_result = ml_client.deploy_model(
-                    current_model_id,
-                    wait_until_deployed = True
-                )
-                assert deploy_result['state'] == 'COMPLETED'
-                return
-            assert False, f"Unexpected model state {current_model_state}"
+            print("Use current model.")
+            return
+    else:
+        print("No current model found.")
 
 
+    print("Registering new model...")
     model_id = ml_client.register_model(
         model_group_id = model_group_id,
         model_path = os.getenv('TORCHSCRIPT_MODEL_PATH'),
         model_config_path = os.getenv('TORCHSCRIPT_MODEL_CONFIG_PATH'),
-        deploy_model = True,
-        wait_until_deployed = True
+        deploy_model = False
     )
-    print(model_id)
+    print(f"New model id: {model_id}")
     current_model_info = ml_client.get_model_info(model_id)
     print(current_model_info)
 
