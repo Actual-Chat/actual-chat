392da2fcd90782204e13d0e3f4d2bdedd1abcd3b
Dmitry Filippov <crui3er@gmail.com>
2025-01-20 16:02:22 +0400

    chore: use constant names for layout slots

---
+++ src/dotnet/UI.Blazor/Layouts/LayoutSlots.cs
@@ -0,0 +1,11 @@
+namespace ActualChat.UI.Blazor;
+
+public static class LayoutSlots
+{
+    public const string MainLeftDrawer = nameof(MainLeftDrawer);
+    public const string MainHeader = nameof(MainHeader);
+    public const string SubHeader = nameof(SubHeader);
+    public const string MainFooter = nameof(MainFooter);
+    public const string SubFooter = nameof(SubFooter);
+    public const string MainRightDrawer = nameof(MainRightDrawer);
+}
