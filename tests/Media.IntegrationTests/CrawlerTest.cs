2a7dc659a74b4b58f04bdf55dd5feb9c232df9ad
Frol <4425748+FrolYo@users.noreply.github.com>
2025-01-29 21:06:50 +0300

    feat: caching robots.txt + crawling enhancements #2777

--- tests/Media.IntegrationTests/CrawlerTest.cs
+++ tests/Media.IntegrationTests/CrawlerTest.cs
@@ -9,14 +9,16 @@ public class CrawlerTest(AppHostFixture fixture, ITestOutputHelper @out)
 {
     private static readonly RandomStringGenerator RandomStringGenerator = new (5, Alphabet.AlphaNumericLower);
 
+    private string Authority1 { get; } = $"https://domain_{RandomStringGenerator.Next(3)}.some";
+    private string Authority2 { get; } = $"https://domain_{RandomStringGenerator.Next(3)}.some";
     private HttpHandlerMock Http { get; } = fixture.AppHost.Services.GetRequiredService<HttpHandlerMock>();
 
     [Fact]
     public async Task SameLinkShouldReuseExistingMedia()
     {
         // arrange
-        var url = $"https://domain.some/{RandomStringGenerator.Next()}";
-        var imgUrl = $"https://domain2.some/images/{RandomStringGenerator.Next()}.jpg";
+        var url = $"{Authority1}/{RandomStringGenerator.Next()}";
+        var imgUrl = $"{Authority2}/images/{RandomStringGenerator.Next()}.jpg";
         Http.SetupImage(imgUrl)
             .SetupHtml(url, h => h.Title("Title 1").Description("Description 1").Image(imgUrl))
             .SetupEmptyRobots(url);
@@ -37,9 +39,9 @@ public class CrawlerTest(AppHostFixture fixture, ITestOutputHelper @out)
     public async Task DifferentLinksShouldReuseExistingMedia()
     {
         // arrange
-        var url1 = $"https://domain.some/{RandomStringGenerator.Next()}";
-        var url2 = $"https://domain.some/{RandomStringGenerator.Next()}";
-        var imgUrl = $"https://domain2.some/images/{RandomStringGenerator.Next()}.jpg";
+        var url1 = $"{Authority1}/{RandomStringGenerator.Next()}";
+        var url2 = $"{Authority1}/{RandomStringGenerator.Next()}";
+        var imgUrl = $"{Authority2}/images/{RandomStringGenerator.Next()}.jpg";
         Http.SetupImage(imgUrl)
             .SetupHtml(url1, h => h.Title("Title 1").Description("Description 1").Image(imgUrl))
             .SetupHtml(url2, h => h.Title("Title 2").Description("Description 2").Image(imgUrl))
@@ -51,8 +53,8 @@ public class CrawlerTest(AppHostFixture fixture, ITestOutputHelper @out)
         var meta2 = await sut.Crawl(url2, CancellationToken.None);
 
         // assert
-        meta2.PreviewMediaId.Should().Be(meta.PreviewMediaId);
         meta.PreviewMediaId.LocalId.Should().Be("Uv2NmtPVQJIu5C079WYUgcX2OMHocEu7GA60NljCb38");
+        meta2.PreviewMediaId.Should().Be(meta.PreviewMediaId);
         meta.OpenGraph.Title.Should().Be("Title 1");
         meta.OpenGraph.Description.Should().Be("Description 1");
         meta2.OpenGraph.Title.Should().Be("Title 2");
@@ -63,8 +65,8 @@ public class CrawlerTest(AppHostFixture fixture, ITestOutputHelper @out)
     public async Task ShouldNotUseInvalidImage()
     {
         // arrange
-        var url = $"https://domain.some/{RandomStringGenerator.Next()}";
-        var imgUrl = $"https://domain2.some/images/{RandomStringGenerator.Next()}.jpg";
+        var url = $"{Authority1}/{RandomStringGenerator.Next()}";
+        var imgUrl = $"{Authority2}/images/{RandomStringGenerator.Next()}.jpg";
         Http.SetupImage(imgUrl, "non-image.jpg")
             .SetupHtml(url, h => h.Title("Title 1").Description("Description 1").Image(imgUrl))
             .SetupAllAllowedRobots(url);
@@ -83,9 +85,9 @@ public class CrawlerTest(AppHostFixture fixture, ITestOutputHelper @out)
     public async Task ShouldNotCrawlIfRobotsTxtDisallowsForAllUserAgents()
     {
         // arrange
-        var url1 = $"https://domain.some/{RandomStringGenerator.Next()}";
-        var url2 = $"https://domain.some/{RandomStringGenerator.Next()}";
-        var imgUrl = $"https://domain2.some/images/{RandomStringGenerator.Next()}.jpg";
+        var url1 = $"{Authority1}/{RandomStringGenerator.Next()}";
+        var url2 = $"{Authority1}/{RandomStringGenerator.Next()}";
+        var imgUrl = $"{Authority2}/images/{RandomStringGenerator.Next()}.jpg";
         Http.SetupImage(imgUrl)
             .SetupHtml(url1, h => h.Title("Title 1").Description("Description 1").Image(imgUrl))
             .SetupHtml(url2, h => h.Title("Title 2").Description("Description 2").Image(imgUrl))
