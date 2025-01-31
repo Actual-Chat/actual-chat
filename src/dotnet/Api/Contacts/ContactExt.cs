44e9a4bad3e097936a79f1f4924adf47c249e92a
Dmitry Filippov <crui3er@gmail.com>
2025-01-31 12:59:34 +0400

    feat: get rid of 'aka.' in peer chat names

--- src/dotnet/Api/Contacts/ContactExt.cs
+++ src/dotnet/Api/Contacts/ContactExt.cs
@@ -3,10 +3,7 @@ namespace ActualChat.Contacts;
 public static class ContactExt
 {
     public static string GetPeerContactName(Contact? contact, string peerAvatarName)
-    {
-        if (contact is null || contact.PeerContactName.IsNullOrEmpty())
-            return peerAvatarName;
-
-        return contact.PeerContactName + " aka. " + peerAvatarName;
-    }
+        => !string.IsNullOrWhiteSpace(contact?.PeerContactName)
+            ? contact.PeerContactName
+            : peerAvatarName;
 }
