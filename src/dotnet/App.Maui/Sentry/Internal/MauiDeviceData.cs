c5a4c6b0cb40c95611a70f6b28eb53ed1d76e9cb
Dmitrii Filippov <crui3er@gmail.com>
2025-02-11 13:07:15 +0400

    fix: enriching sentry event data with display info fails in windows

--- src/dotnet/App.Maui/Sentry/Internal/MauiDeviceData.cs
+++ src/dotnet/App.Maui/Sentry/Internal/MauiDeviceData.cs
@@ -6,6 +6,10 @@ namespace Sentry.Maui.Internal;
 
 internal static class MauiDeviceData
 {
+#if ANDROID
+    private static readonly Lock JniLock = new();
+#endif
+
     public static void ApplyMauiDeviceData(this Device device, IDiagnosticLogger? logger)
     {
         try {
@@ -20,7 +24,16 @@ internal static class MauiDeviceData
             device.Name ??= deviceInfo.Name;
             device.Manufacturer ??= deviceInfo.Manufacturer;
             device.Model ??= deviceInfo.Model;
+#if ANDROID
+            // DeviceInfo.Idiom is not threadsafe on Android
+            // See: https://github.com/getsentry/sentry-dotnet/issues/3627
+            lock (JniLock)
+            {
+                device.DeviceType ??= deviceInfo.Idiom.ToString();
+            }
+#else
             device.DeviceType ??= deviceInfo.Idiom.ToString();
+#endif
             device.Simulator ??= deviceInfo.DeviceType switch {
                 DeviceType.Virtual => true,
                 DeviceType.Physical => false,
@@ -57,18 +70,27 @@ internal static class MauiDeviceData
                 logger?.LogDebug("No permission to read network state from the device");
             }
 
-            // https://docs.microsoft.com/dotnet/maui/platform-integration/device/display
-            var display = DeviceDisplay.MainDisplayInfo;
-            device.ScreenResolution ??= $"{(int)display.Width}x{(int)display.Height}";
-            device.ScreenDensity ??= (float)display.Density;
-            device.Orientation ??= display.Orientation switch {
-                DisplayOrientation.Portrait => DeviceOrientation.Portrait,
-                DisplayOrientation.Landscape => DeviceOrientation.Landscape,
-                _ => null
-            };
-            // device.ScreenDpi ??= ?
-            // ? = display.RefreshRate;
-            // ? = display.Rotation;
+#if MACCATALYST || IOS || WINDOWS
+            if (MainThread.IsMainThread)
+                CaptureDisplayInfo();
+            else
+            {
+                // On iOS and Mac Catalyst, Accessing DeviceDisplay.Current must be done on the UI thread or else an
+                // exception will be thrown.
+                // See https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/device/display?view=net-maui-8.0&tabs=macios#platform-differences
+                using var resetEvent = new ManualResetEventSlim(false);
+                // ReSharper disable once AccessToDisposedClosure - not disposed until lambda completes
+                MainThread.BeginInvokeOnMainThread(() => CaptureDisplayInfo(resetEvent));
+                resetEvent.Wait();
+            }
+#elif ANDROID
+            // DeviceDisplay.Current is not threadsafe on Android.
+            // See: https://github.com/getsentry/sentry-dotnet/issues/3627
+            lock (JniLock)
+            {
+                CaptureDisplayInfo();
+            }
+#endif
 
             // https://docs.microsoft.com/dotnet/maui/platform-integration/device/vibrate
             device.SupportsVibration ??= Vibration.Default.IsSupported;
@@ -103,6 +125,24 @@ internal static class MauiDeviceData
         catch (Exception ex) {
             // Log, but swallow the exception so we can continue sending events
             logger?.LogError(ex, "Error getting MAUI device information");
+        }
+
+        void CaptureDisplayInfo(ManualResetEventSlim? resetEvent = null)
+        {
+            // https://docs.microsoft.com/dotnet/maui/platform-integration/device/display
+            var display = DeviceDisplay.MainDisplayInfo;
+            device.ScreenResolution ??= $"{(int)display.Width}x{(int)display.Height}";
+            device.ScreenDensity ??= (float)display.Density;
+            device.Orientation ??= display.Orientation switch
+            {
+                DisplayOrientation.Portrait => DeviceOrientation.Portrait,
+                DisplayOrientation.Landscape => DeviceOrientation.Landscape,
+                _ => null
+            };
+            // device.ScreenDpi ??= ?
+            // ? = display.RefreshRate;
+            // ? = display.Rotation;
+            resetEvent?.Set();
         }
     }
 }
