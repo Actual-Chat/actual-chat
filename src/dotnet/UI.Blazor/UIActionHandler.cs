772e2b73d4c889defa2584a99bbb41db2c745bcd
Alex Yakunin <alex.yakunin@gmail.com>
2025-02-21 00:24:50 -0800

    fix: UIActionHandler.cs - formatting + "make class static" warning

--- src/dotnet/UI.Blazor/UIActionHandler.cs
+++ src/dotnet/UI.Blazor/UIActionHandler.cs
@@ -1,42 +1,45 @@
 namespace ActualChat.UI.Blazor;
 
-public interface IUIActionHandler {
+public interface IUIActionHandler
+{
     public bool IsBusy { get; }
     public bool IsReady { get; }
     public Task Execute();
 }
 
-public interface IUIActionHandler<in TParameter> {
+public interface IUIActionHandler<in TParameter>
+{
     public bool IsBusy { get; }
     public bool IsReady { get; }
     public Task Execute(TParameter parameter);
 }
 
-public class UIActionHandler {
-        public static IUIActionHandler<TArg> Create<TArg>(ComponentBase component, Func<TArg, Task> taskFactory)
-            => new UIActionHandler<TArg>(taskFactory, component.NotifyStateHasChanged);
+public static class UIActionHandler
+{
+    public static IUIActionHandler<TArg> Create<TArg>(ComponentBase component, Func<TArg, Task> taskFactory)
+        => new UIActionHandler<TArg>(taskFactory, component.NotifyStateHasChanged);
 
-        public static IUIActionHandler Create(ComponentBase component, Func<Task> taskFactory)
-            => new UIActionHandlerVoid(taskFactory, component.NotifyStateHasChanged);
-    }
+    public static IUIActionHandler Create(ComponentBase component, Func<Task> taskFactory)
+        => new UIActionHandlerVoid(taskFactory, component.NotifyStateHasChanged);
+}
 
-public class UIActionHandlerVoid : IUIActionHandler {
-    private readonly UIActionHandler<string> _innerHandler;
+public class UIActionHandlerVoid(Func<Task> taskFactory, Action stateHasChanged) : IUIActionHandler
+{
+    private readonly UIActionHandler<string> _innerHandler = new (_ => taskFactory.Invoke(), stateHasChanged);
 
     public bool IsBusy => _innerHandler.IsBusy;
     public bool IsReady => _innerHandler.IsReady;
     public Task Execute()
         => _innerHandler.Execute(string.Empty);
-
-    public UIActionHandlerVoid(Func<Task> taskFactory, Action stateHasChanged)
-        => _innerHandler = new UIActionHandler<string>(_ => taskFactory.Invoke(), stateHasChanged);
 }
 
-public class UIActionHandler<TArg>(Func<TArg, Task> taskFactory, Action stateHasChanged) : IUIActionHandler<TArg> {
+public class UIActionHandler<TArg>(Func<TArg, Task> taskFactory, Action stateHasChanged) : IUIActionHandler<TArg>
+{
     public bool IsBusy { get; private set; }
     public bool IsReady => !IsBusy;
 
-    public async Task Execute(TArg arg) {
+    public async Task Execute(TArg arg)
+    {
         try {
             if (IsBusy)
                 return;
