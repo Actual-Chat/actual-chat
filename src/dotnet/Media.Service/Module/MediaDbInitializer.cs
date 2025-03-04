9dd1a8a843e8dbc7bafae543a9b9c4e6c138fdc3
Frol <4425748+FrolYo@users.noreply.github.com>
2025-03-04 18:29:42 +0300

    chore: pass cancelationToken to MediaUploader #2849

--- src/dotnet/Media.Service/Module/MediaDbInitializer.cs
+++ src/dotnet/Media.Service/Module/MediaDbInitializer.cs
@@ -12,23 +12,23 @@ public class MediaDbInitializer(IServiceProvider services) : DbInitializer<Media
         // Add default chat images
         await new MediaUploader(GetType())
             .Upload(async x => {
-                await x.AddMedia("system-icons:family", Resource.FamilySvg).ConfigureAwait(false);
-                await x.AddMedia("system-icons:coworkers", Resource.CoworkersSvg).ConfigureAwait(false);
-                await x.AddMedia("system-icons:friends", Resource.FriendsSvg).ConfigureAwait(false);
-                await x.AddMedia("system-icons:alumni", Resource.AlumniSvg).ConfigureAwait(false);
-                await x.AddMedia("system-icons:notes", Resource.NotesSvg).ConfigureAwait(false);
-            }).ConfigureAwait(false);
+                    await x.AddMedia("system-icons:family", Resource.FamilySvg).ConfigureAwait(false);
+                    await x.AddMedia("system-icons:coworkers", Resource.CoworkersSvg).ConfigureAwait(false);
+                    await x.AddMedia("system-icons:friends", Resource.FriendsSvg).ConfigureAwait(false);
+                    await x.AddMedia("system-icons:alumni", Resource.AlumniSvg).ConfigureAwait(false);
+                    await x.AddMedia("system-icons:notes", Resource.NotesSvg).ConfigureAwait(false);
+                },
+                cancellationToken)
+            .ConfigureAwait(false);
 
         // Add Sherlock image
         await new MediaUploader(GetType())
-            .Upload(async c => {
-                await c.AddMedia(Constants.User.Sherlock.MediaId.Value, Resource.Sherlock).ConfigureAwait(false);
-            }).ConfigureAwait(false);
+            .Upload(c => c.AddMedia(Constants.User.Sherlock.MediaId.Value, Resource.Sherlock), cancellationToken)
+            .ConfigureAwait(false);
 
         // Add Chat Roulette image
         await new MediaUploader(GetType())
-            .Upload(async c => {
-                await c.AddMedia(ChatRoulette.MediaId.Value, Resource.ChatRoulette).ConfigureAwait(false);
-            }).ConfigureAwait(false);
+            .Upload(c => c.AddMedia(ChatRoulette.MediaId.Value, Resource.ChatRoulette), cancellationToken)
+            .ConfigureAwait(false);
     }
 }
