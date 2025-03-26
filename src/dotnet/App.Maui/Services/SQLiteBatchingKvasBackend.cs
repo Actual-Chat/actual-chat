051d7d9491d25ba64674f07d674779fe5fb70a80
Alex Yakunin <alex.yakunin@gmail.com>
2025-03-26 16:43:58 -0700

    perf: speedup SQLiteBatchingKvasBackend.cs

--- src/dotnet/App.Maui/Services/SQLiteBatchingKvasBackend.cs
+++ src/dotnet/App.Maui/Services/SQLiteBatchingKvasBackend.cs
@@ -21,7 +21,7 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
 
     private IServiceProvider Services { get; }
     [field: MaybeNull, AllowNull]
-    private ILogger? Log => field ??= Services.LogFor(GetType());
+    private ILogger Log => field ??= Services.LogFor(GetType());
 
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(SQLiteBatchingKvasBackend))]
     public SQLiteBatchingKvasBackend(FilePath dbPath, string version, IServiceProvider services)
@@ -32,29 +32,61 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
 
     public ValueTask<byte[]?[]> GetMany(string[] keys, CancellationToken cancellationToken = default)
     {
+        if (keys.Length == 0)
+            return ValueTask.FromResult<byte[]?[]>([]);
+
         var result = new byte[]?[keys.Length];
         if (_connectionPool == null)
             return ValueTask.FromResult(result);
 
         using var lease = _connectionPool.Rent();
-        for (var i = 0; i < keys.Length; i++) {
-            var dbItem = lease.Resource.Find<DbItem?>(keys[i]);
-            result[i] = dbItem?.Value;
+        var connection = lease.Resource;
+        if (keys.Length == 1)
+            result[0] = DbHelpers.Find(connection, keys[0])?.Value;
+        else if (keys.Length < 16) {
+            // Small number of keys, use a simple loop
+            foreach (var dbItem in DbHelpers.FindMany(connection, keys))
+                result[FindIndex(keys, dbItem.Key)] = dbItem.Value;
         }
+        else {
+            // Large number of keys, use a dictionary
+            var keyIndexes = new Dictionary<string, int>(StringComparer.Ordinal);
+            for (var i = 0; i < keys.Length; i++)
+                keyIndexes[keys[i]] = i;
+            foreach (var dbItem in DbHelpers.FindMany(connection, keys))
+                result[keyIndexes[dbItem.Key]] = dbItem.Value;
+        }
+        // Log.LogDebug("GetMany({KeyCount} keys) -> {Count} items", keys.Length, result.Count(x => x != null));
         return ValueTask.FromResult(result);
+
+        static int FindIndex(string[] keys, string key) {
+            for (var i = 0; i < keys.Length; i++)
+                if (OrdinalEquals(keys[i], key))
+                    return i;
+            return -1;
+        }
     }
 
     public Task SetMany(List<(string Key, byte[]? Value)> updates, CancellationToken cancellationToken = default)
     {
-        if (_connectionPool == null)
+        if (_connectionPool == null || updates.Count == 0)
             return Task.CompletedTask;
 
         using var lease = _connectionPool.Rent();
-        foreach (var (key, value) in updates) {
-            if (value == null)
-                lease.Resource.Delete<DbItem>(key);
-            else
-                lease.Resource.InsertOrReplace(new DbItem { Key = key, Value = value });
+        var connection = lease.Resource;
+        if (updates.Count == 1)
+            DbHelpers.Set(connection, updates[0]);
+        else {
+            var savepoint = connection.SaveTransactionPoint();
+            try {
+                foreach (var update in updates)
+                    DbHelpers.Set(connection, update);
+                connection.Release(savepoint);
+            }
+            catch {
+                connection.Rollback();
+                throw;
+            }
         }
         return Task.CompletedTask;
     }
@@ -65,7 +97,8 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
             return Task.CompletedTask;
 
         using var lease = _connectionPool.Rent();
-        lease.Resource.DeleteAll<DbItem>();
+        var connection = lease.Resource;
+        connection.DeleteAll<DbItem>();
         return Task.CompletedTask;
     }
 
@@ -76,12 +109,16 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
         try {
             var connectionCount = HardwareInfo.ProcessorCount + 2;
             var connections = new SimpleConcurrentPool<SQLiteConnection>(
-                () => new(dbPath, OpenFlags),
+                () => DbHelpers.OpenConnection(dbPath),
                 static c => !c.IsInTransaction,
                 connectionCount);
+
             using var lease = connections.Rent();
             var connection = lease.Resource;
-            connection.EnableWriteAheadLogging();
+            // connection.EnableWriteAheadLogging();
+            connection.ExecuteScalar<string>("PRAGMA journal_mode=WAL");
+            connection.ExecuteScalar<string>("PRAGMA synchronous=normal");
+            connection.ExecuteScalar<string>("PRAGMA journal_size_limit=2048000");
             var versionBytes = Encoding.UTF8.GetEncoder().Convert(version);
             if (connection.CreateTable<DbItem>() == CreateTableResult.Migrated) {
                 var existingVersionBytes = connection.Find<DbItem>(VersionKey)?.Value ?? [];
@@ -94,7 +131,7 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
             return connections;
         }
         catch (Exception e) {
-            Log?.LogError(e, "Failed to initialize SQLite database");
+            Log.LogError(e, "Failed to initialize SQLite database");
             return null;
         }
     }
@@ -106,5 +143,58 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
     {
         [PrimaryKey] public string Key { get; set; } = "";
         public byte[] Value { get; set; } = null!;
+    }
+
+    public static class DbHelpers
+    {
+        private static readonly Lock Lock = new();
+        private static volatile object? _initializedTag;
+
+        public static TableMapping Mapping = null!;
+        public static string FindSql = null!;
+        public static string FindManySql = null!;
+        public static string DeleteSql = null!;
+        public static string UpsertSql = null!;
+
+        [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(DbHelpers))]
+        public static SQLiteConnection OpenConnection(FilePath dbPath)
+        {
+            var connection = new SQLiteConnection(dbPath, OpenFlags);
+            if (_initializedTag == null) {
+                using var _ = Lock.EnterScope();
+                if (_initializedTag == null) {
+                    Mapping = connection.GetMapping(typeof(DbItem));
+                    FindSql = Mapping.GetByPrimaryKeySql;
+                    FindManySql =
+                        $"select * from {Mapping.TableName} where Key in (select e.value from json_each(?) e)";
+                    DeleteSql = $"delete from {Mapping.TableName} where Key = ?";
+                    UpsertSql = $"insert or replace into {Mapping.TableName} (Key, Value) values (?, ?)";
+                    _initializedTag = new();
+                }
+            }
+            return connection;
+        }
+
+        public static DbItem? Find(SQLiteConnection connection, string key)
+        {
+            var cmd = connection.CreateCommand(FindSql, key);
+            return cmd.ExecuteDeferredQuery<DbItem>(Mapping).FirstOrDefault();
+        }
+
+        [UnconditionalSuppressMessage("Trimming", "IL2026:RequiresUnreferencedCodeAttribute", Justification = "string[] is always JSON-serializable")]
+        public static IEnumerable<DbItem> FindMany(SQLiteConnection connection, string[] keys)
+        {
+            var keysJson = JsonSerializer.Serialize(keys);
+            var cmd = connection.CreateCommand(FindManySql, keysJson);
+            return cmd.ExecuteDeferredQuery<DbItem>(Mapping);
+        }
+
+        public static void Set(SQLiteConnection connection, (string Key, byte[]? Value) item)
+        {
+            if (item.Value == null)
+                connection.Execute(DeleteSql, item.Key);
+            else
+                connection.Execute(UpsertSql, item.Key, item.Value);
+        }
     }
 }
