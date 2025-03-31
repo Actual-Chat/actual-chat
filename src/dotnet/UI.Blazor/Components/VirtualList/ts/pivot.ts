586489f87ca193874fd6ddf9417c037631163cb7
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-03-31 18:03:56 +0300

    feat: virtual list positioning for expandable elements

--- src/dotnet/UI.Blazor/Components/VirtualList/ts/pivot.ts
+++ src/dotnet/UI.Blazor/Components/VirtualList/ts/pivot.ts
@@ -1,7 +1,10 @@
+import { NumberRange } from './range';
+
 export interface Pivot {
     itemKey: string;
     offset: number;
     time: number;
+    range: NumberRange;
     isVisible: boolean;
     isInteractive: boolean;
 }
