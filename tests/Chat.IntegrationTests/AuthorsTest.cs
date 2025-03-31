9bc0bc7549731290ca5529908cfd267de0cad700
Frol <4425748+FrolYo@users.noreply.github.com>
2025-03-31 18:05:20 +0300

    chore: authors tests for anonymous case #2912

--- tests/Chat.IntegrationTests/AuthorsTest.cs
+++ tests/Chat.IntegrationTests/AuthorsTest.cs
@@ -1,4 +1,5 @@
-﻿using ActualChat.Testing.Host;
+﻿using System.Diagnostics.CodeAnalysis;
+using ActualChat.Testing.Host;
 
 namespace ActualChat.Chat.IntegrationTests;
 
@@ -6,6 +7,24 @@ namespace ActualChat.Chat.IntegrationTests;
 public class AuthorsTest(ChatCollection.AppHostFixture fixture, ITestOutputHelper @out)
     : SharedAppHostTestBase<AppHostFixture>(fixture, @out)
 {
+    [field: AllowNull, MaybeNull]
+    private WebClientTester Alice => field ??= fixture.AppHost.NewWebClientTester(Out);
+    [field: AllowNull, MaybeNull]
+    private WebClientTester Anonymous => field ??= fixture.AppHost.NewWebClientTester(Out);
+
+    protected override async Task InitializeAsync()
+    {
+        await base.InitializeAsync();
+        await Alice.SignInAsAlice();
+    }
+
+    protected override async Task DisposeAsync()
+    {
+        await Alice.DisposeSilentlyAsync();
+        await Anonymous.DisposeSilentlyAsync();
+        await base.DisposeAsync();
+    }
+
     [Fact(Skip = "Fails on CI")]
     public async Task NullAuthorResult()
     {
@@ -19,5 +38,24 @@ public class AuthorsTest(ChatCollection.AppHostFixture fixture, ITestOutputHelpe
         var author = await authors.GetOwn(session, Constants.Chat.DefaultChatId, default);
         Out.WriteLine($"{startedAt}: get author");
         author.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task ShouldNotFailForAnonymousAuthors()
+    {
+        // arrange
+        var (chatId, inviteId) = await Alice.CreateChat(x => x with {
+            IsPublic = true,
+            AllowAnonymousAuthors = true,
+            AllowGuestAuthors = true,
+        });
+        var (anonymousAuthorId, _) = await Anonymous.JoinChat(chatId, inviteId, true);
+
+        // act
+        var anonymousAuthor = await Anonymous.GetOwnAuthor(chatId).Require();
+        var anonymousAuthorForAlice = await Alice.GetAuthor(anonymousAuthorId).Require();
+
+        // assert
+        anonymousAuthorForAlice.Should().BeEquivalentTo(anonymousAuthor.ToAuthor());
     }
 }
--- tests/Testing.Host/AuthorOperations.cs
+++ tests/Testing.Host/AuthorOperations.cs
@@ -6,4 +6,10 @@ public static class AuthorOperations
 {
     public static Task PromoteToOwner(this IWebTester tester, AuthorId authorId)
         => tester.Commander.Call(new Authors_PromoteToOwner(tester.Session, authorId));
+
+    public static Task<Author?> GetAuthor(this IWebTester tester, AuthorId authorId, CancellationToken cancellationToken = default)
+        => tester.Authors.Get(tester.Session, authorId.ChatId, authorId, cancellationToken);
+
+    public static Task<AuthorFull?> GetOwnAuthor(this IWebTester tester, ChatId chatId, CancellationToken cancellationToken = default)
+        => tester.Authors.GetOwn(tester.Session, chatId, cancellationToken);
 }
