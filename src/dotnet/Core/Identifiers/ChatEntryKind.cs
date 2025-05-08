e3abaedba83b7d2fbc78c647fdc25bbbd7a25ba1
Dmitrii Filippov <crui3er@gmail.com>
2025-05-08 12:38:33 +0700

    fix: ChatEntryIdTest, take out of service 'Video' chat entry kind

--- src/dotnet/Core/Identifiers/ChatEntryKind.cs
+++ src/dotnet/Core/Identifiers/ChatEntryKind.cs
@@ -4,5 +4,5 @@ public enum ChatEntryKind
 {
     Text = 0,
     Audio,
-    Video,
+    //Video,
 }
--- tests/Core.UnitTests/Identifiers/ChatEntryIdTest.cs
+++ tests/Core.UnitTests/Identifiers/ChatEntryIdTest.cs
@@ -5,11 +5,9 @@ public class ChatEntryIdTest(ITestOutputHelper @out) : StringIdentifierTestBase<
     public override string[] ValidIdentifiers => [
         "thisIsChatId:0:0",
         "thisIsChatId:1:0",
-        "thisIsChatId:2:0",
-        "thisIsChatId:2:10",
         "p-admin1-admin2:0:10",
         "p-admin1-admin2:1:100",
-        "p-admin1-admin2:2:1000",
+
     ];
     public override string[] InvalidIdentifiers => [
         "x:0:0",
@@ -20,5 +18,8 @@ public class ChatEntryIdTest(ITestOutputHelper @out) : StringIdentifierTestBase<
         "thisIsChatId:-1:0",
         "thisIsChatId:9:0",
         "thisIsChatId:1:-1",
+        "thisIsChatId:2:0", // Video chat entries are invalid so far.
+        "thisIsChatId:2:10",
+        "p-admin1-admin2:2:1000",
     ];
 }
