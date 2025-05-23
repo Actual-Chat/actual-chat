4481ec2736d2ccc869ce4b0e47e8b25637455f48
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-05-23 10:17:30 +0300

    chore: add migration for user_link -> alias_id

---
+++ src/dotnet/Users.Service.Migration/Migrations/20250523070411_IdMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Users.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_accounts_alias_id",
+                table: "accounts");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_accounts_alias_id",
+                table: "accounts",
+                column: "alias_id",
+                unique: true,
+                filter: "alias_id <> ''");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_accounts_alias_id",
+                table: "accounts");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_accounts_alias_id",
+                table: "accounts",
+                column: "alias_id",
+                unique: true,
+                filter: "user_link_id <> ''");
+        }
+    }
+}
