ba906ed570a3e4af3ceea9dc545df7637195f731
Alex Yakunin <alex.yakunin@gmail.com>
2025-05-27 19:54:53 -0700

    migration: remove obsolete members from "chats" and "text_entry_attachments" tables

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20250528025350_Remove_Obsolete.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_Obsolete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "content_id",
+                table: "text_entry_attachments");
+
+            migrationBuilder.DropColumn(
+                name: "metadata_json",
+                table: "text_entry_attachments");
+
+            migrationBuilder.DropColumn(
+                name: "picture",
+                table: "chats");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "content_id",
+                table: "text_entry_attachments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "metadata_json",
+                table: "text_entry_attachments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "picture",
+                table: "chats",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
