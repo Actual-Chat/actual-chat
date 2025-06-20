c3622090fb49d6e2f90b3aa1bf55486366fdd5e7
Frol <4425748+FrolYo@users.noreply.github.com>
2025-06-20 20:52:11 +0300

    feat: skip non-word messages + command.uuid for language detection #3043

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20250620152512_AddContentHashToChatEntryLanguage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContentHashToChatEntryLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_chat_entry_languages_id",
+                table: "chat_entry_languages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "entry_content_hash",
+                table: "chat_entry_languages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "entry_content_hash",
+                table: "chat_entry_languages");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_chat_entry_languages_id",
+                table: "chat_entry_languages",
+                column: "id",
+                filter: "languages = ''");
+        }
+    }
+}
