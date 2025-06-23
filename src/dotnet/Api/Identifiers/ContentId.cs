c9c995d9c087b071497a907b83ff57f46df530c0
Dmitrii Filippov <crui3er@gmail.com>
2025-06-23 13:44:55 +0700

    feat: support og tags for link to text entry

--- src/dotnet/Api/Identifiers/ContentId.cs
+++ src/dotnet/Api/Identifiers/ContentId.cs
@@ -1,11 +1,48 @@
 namespace ActualChat;
 
-public record ContentId(ContentKind Kind, StringIdentifier Id);
+public class ContentId
+{
+    public ContentKind Kind { get; }
+    public StringIdentifier Id { get; }
+
+    private ContentId(ContentKind kind, StringIdentifier id)
+    {
+        Kind = kind;
+        Id = id;
+    }
+
+    public static ContentId New(StringIdentifier id)
+    {
+        var kind = GetKind(id);
+        if (kind is null)
+            throw new ArgumentOutOfRangeException(nameof(id));
+        return new ContentId(kind.Value, id);
+    }
+
+    public static bool IsValid(StringIdentifier id)
+        => GetKind(id) is not null;
+
+    private static ContentKind? GetKind(StringIdentifier id)
+    {
+        if (id is ChatId)
+            return ContentKind.Chat;
+        if (id is UserId)
+            return ContentKind.User;
+        if (id is TextEntryId)
+            return ContentKind.TextEntry;
+        if (id is AuthorId)
+            return ContentKind.Author;
+        if (id is PlaceId)
+            return ContentKind.Place;
+        return null;
+    }
+}
 
 public enum ContentKind
 {
     User,
     Chat,
+    TextEntry,
     Author,
     Place
 }
@@ -13,5 +50,5 @@ public enum ContentKind
 public record ContentLinkInfo(ContentId Id, string Title, Picture? Picture, string Description)
 {
     public static ContentLinkInfo RemovedOrUnknown(ContentId id)
-        => new ContentLinkInfo(id, "Removed or Unknown", null, "");
+        => new (id, "Removed or Unknown", null, "");
 }
--- src/dotnet/Chat.Service/OpenGraphTagsProvider.cs
+++ src/dotnet/Chat.Service/OpenGraphTagsProvider.cs
@@ -10,15 +10,24 @@ public class OpenGraphTagsProvider(IServiceProvider services)
         LocalUrl localUrl,
         CancellationToken cancellationToken)
     {
+        ContentId? contentId = null;
         if (localUrl.IsChat(out var chatId)) {
             var chat = await Chats.Get(session, chatId, cancellationToken).ConfigureAwait(false);
             if (chat is not null) {
-                var contentId = new ContentId(ContentKind.Chat, chatId);
-                return await ContentLinksBackend
-                    .GetContentInfo(contentId, cancellationToken)
-                    .ConfigureAwait(false);
+                if (localUrl.IsChat(out _, out long entryLid)) {
+                    var textEntryId = TextEntryId.New(chatId, entryLid);
+                    var chatEntry = await Chats.GetEntry(session, textEntryId, cancellationToken).ConfigureAwait(false);
+                    if (chatEntry is not null)
+                        contentId = ContentId.New(textEntryId);
+                }
+                else
+                    contentId = ContentId.New(chatId);
             }
         }
+        if (contentId is not null)
+            return await ContentLinksBackend
+                .GetContentInfo(contentId, cancellationToken)
+                .ConfigureAwait(false);
         return null;
     }
 }
