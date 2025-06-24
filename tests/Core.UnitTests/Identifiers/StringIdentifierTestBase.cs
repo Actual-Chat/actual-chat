80f28e990ab77a08ef63285c209f009a32e770fa
Alex Yakunin <alex.yakunin@gmail.com>
2025-06-23 22:11:38 -0700

    fix: ChatMessageEditor.UpdateChatId - pass string instead of ChatId + extra tests on identifier to JSON conversion

--- tests/Core.UnitTests/Identifiers/StringIdentifierTestBase.cs
+++ tests/Core.UnitTests/Identifiers/StringIdentifierTestBase.cs
@@ -39,10 +39,16 @@ public abstract class StringIdentifierTestBase<TIdentifier>(ITestOutputHelper @o
         var identifiers = ValidIdentifiers.Select(TIdentifier.Parse).Concat([null]).ToArray();
         foreach (var id in identifiers) {
             id.AssertPassesThroughAllSerializers(Out);
+
             var s1 = new NewtonsoftJsonSerializer();
             s1.Write(id).Should().Be(GetJson(id));
-            var s2 = new SystemJsonSerializer();
+            s1.Write(new object[] { id }).Should().Be($"[{GetJson(id)}]");
+            var s2 = new SystemJsonSerializer(JsonSerializerOptions.Default);
             s2.Write(id).Should().Be(GetJson(id));
+            s2.Write(new object[] { id }).Should().Be($"[{GetJson(id)}]");
+            var s3 = new SystemJsonSerializer(JsonSerializerOptions.Web);
+            s3.Write(id).Should().Be(GetJson(id));
+            s3.Write(new object[] { id }).Should().Be($"[{GetJson(id)}]");
         }
         return;
 
