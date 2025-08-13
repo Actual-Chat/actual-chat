27d8485a32eca1a372d8e6aa1f4b61f2a0ba005b
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-08-13 16:18:16 +0300

    fix: do not suspend ConversationSplitFlow when there are immature entries + Summarize in parallel

--- src/dotnet/Core.Server/Sharding/ShardScheme.cs
+++ src/dotnet/Core.Server/Sharding/ShardScheme.cs
@@ -19,7 +19,7 @@ public sealed class ShardScheme(
     public static readonly ShardScheme None = new(nameof(None), 0, HostRole.None, ShardSchemeFlags.Special | ShardSchemeFlags.Queue);
     public static readonly ShardScheme Undefined = new(nameof(Undefined), 0, HostRole.None, ShardSchemeFlags.Special | ShardSchemeFlags.Queue);
     public static readonly ShardScheme EventQueue = new(nameof(EventQueue), N, HostRole.EventQueue, ShardSchemeFlags.Queue);
-    public static readonly ShardScheme SummarizeQueue = new(nameof(SummarizeQueue), 1, HostRole.EventQueue, ShardSchemeFlags.SlowQueue) { DegreeOfParallelism = 1 };
+    public static readonly ShardScheme SummarizeQueue = new(nameof(SummarizeQueue), 1, HostRole.EventQueue, ShardSchemeFlags.SlowQueue);
     public static readonly ShardScheme FlowsBackend = new(nameof(FlowsBackend), N, HostRole.FlowsBackend);
     public static readonly ShardScheme MediaBackend = new(nameof(MediaBackend), N, HostRole.MediaBackend);
     public static readonly ShardScheme AudioBackend = new(nameof(AudioBackend), N, HostRole.AudioBackend);
