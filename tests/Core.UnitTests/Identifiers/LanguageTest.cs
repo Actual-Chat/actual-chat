6c197875ee3e6b2b1a041701a5c5f30a27eee07a
Dmitrii Filippov <crui3er@gmail.com>
2025-08-15 00:57:21 +0500

    fix: fix ISO 639 Short titles

--- tests/Core.UnitTests/Identifiers/LanguageTest.cs
+++ tests/Core.UnitTests/Identifiers/LanguageTest.cs
@@ -2,6 +2,6 @@ namespace ActualChat.Core.UnitTests.Identifiers;
 
 public class LanguageTest(ITestOutputHelper @out) : StringIdentifierTestBase<Language>(@out)
 {
-    public override string[] ValidIdentifiers => [ "eN-Us", "UA", "rU" ];
+    public override string[] ValidIdentifiers => [ "eN-Us", "UK", "rU" ];
     public override string[] InvalidIdentifiers => [ "X", "~" ];
 }
