8f3daae16212b0d9e65affb4b626ffc4f0131f65
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-09-05 18:31:33 +0300

    fix: remove user settings that cause deserialization issues even when overwriten

--- src/dotnet/Api/Users/UserNavbarSettings.cs
+++ src/dotnet/Api/Users/UserNavbarSettings.cs
@@ -4,27 +4,10 @@ using MemoryPack;
 namespace ActualChat.Users;
 
 [DataContract, MemoryPackable(GenerateType.VersionTolerant)]
-public sealed partial record UserNavbarSettings : IHasOrigin
+public sealed partial record UserNavbarSettings : IHasOrigin, IHasKvasKey<UserNavbarSettings>
 {
-    public const string KvasKey = nameof(UserNavbarSettings);
-
-    [DataMember, MemoryPackOrder(1), MemoryPackInclude]
-    private ApiArray<LegacyId> LegacyPinnedChats {
-        get => PinnedChats.Select(x => LegacyId.Parse(x.Value)).ToApiArray();
-        init => PinnedChats = value.Select(x => ChatId.Parse(x.Value)).ToArray()!;
-    }
-
-    [DataMember, MemoryPackOrder(2), MemoryPackInclude]
-    private ApiArray<LegacyId> LegacyPlacesOrder {
-        get => PlacesOrder.Select(x => LegacyId.Parse(x.Value)).ToApiArray();
-        init => PlacesOrder = value.Select(x => PlaceId.Parse(x.Value)).ToArray()!;
-    }
-
+    public static string KvasKey => nameof(UserNavbarSettings);
     [DataMember, MemoryPackOrder(0)] public string Origin { get; init; } = "";
-
-    // Actually used properties
-    [IgnoreDataMember, MemoryPackIgnore]
-    public ChatId[] PinnedChats { get; init; } = [];
-    [IgnoreDataMember, MemoryPackIgnore]
-    public PlaceId[] PlacesOrder { get; init; } = [];
+    [DataMember, MemoryPackOrder(1)] public ChatId[] PinnedChats { get; init; } = [];
+    [DataMember, MemoryPackOrder(2)] public PlaceId[] PlacesOrder { get; init; } = [];
 }
