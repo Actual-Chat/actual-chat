a03c729a5ef8ce1f4d745085ff61b5be6d64a349
Iq Mulator <iqmulator@gmail.com>
2025-09-09 21:45:12 +0500

    refactoring

--- src/dotnet/Chat.Service/Conversations.cs
+++ src/dotnet/Chat.Service/Conversations.cs
@@ -17,18 +17,29 @@ public class Conversations(IServiceProvider services) : IConversations
         return await Backend.GetTile(chatId, idTileRange, cancellationToken).ConfigureAwait(false);
     }
 
-    public virtual Task<Conversation?> Get(ConversationId conversationId, CancellationToken cancellationToken)
-        => Backend.Get(conversationId, cancellationToken);
-
-    public virtual async Task<Conversation?> ReSummarize(ConversationId conversationId, CancellationToken cancellationToken)
+    public virtual async Task<Conversation?> Get(Session session, ConversationId conversationId, CancellationToken cancellationToken)
     {
-        var conversation = await Backend.Get(conversationId, cancellationToken).ConfigureAwait(false);
-        if (conversation == null)
+        var rules = await Chats.GetRules(session, conversationId.ChatId, cancellationToken).ConfigureAwait(false);
+        if (!rules.CanRead())
             return null;
 
+        return await Backend.Get(conversationId, cancellationToken).ConfigureAwait(false);
+    }
+
+    // [CommandHandler]
+    public virtual async Task OnReSummarize(Conversations_Summarize command, CancellationToken cancellationToken)
+    {
+        var (session, conversationId) = command;
         var chatId = conversationId.ChatId;
+        var chat = await Chats.Get(session, chatId, cancellationToken).Require().ConfigureAwait(false);
+        chat.Rules.Permissions.Require(ChatPermissions.Owner);
+
+        var conversation = await Backend.Get(conversationId, cancellationToken).ConfigureAwait(false);
+        if (conversation == null)
+            throw new NotFoundException<Conversation>();
+
         var entryIdRange = conversation.EntryRange;
-        var command = new ConversationBackend_Summarize(chatId, [entryIdRange]);
-        return await Commander.Call(command, cancellationToken).ConfigureAwait(false);
+        var reSummarizeCommand = new ConversationBackend_Summarize(chatId, [entryIdRange]);
+        await Commander.Call(reSummarizeCommand, cancellationToken).ConfigureAwait(false);
     }
 }
