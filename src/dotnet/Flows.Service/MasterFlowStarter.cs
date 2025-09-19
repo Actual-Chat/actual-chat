9e69ff4c8148db54d006180e8611697872c18108
Alex Yakunin <alex.yakunin@gmail.com>
2025-09-18 17:10:44 -0700

    refactor: replace Dictionary with ConcurrentDictionary in MasterFlowStarter and improve flow handling logic

--- src/dotnet/Flows.Service/MasterFlowStarter.cs
+++ src/dotnet/Flows.Service/MasterFlowStarter.cs
@@ -4,42 +4,45 @@ namespace ActualChat.Flows;
 
 internal class MasterFlowStarter(IServiceProvider services) : LegacyShardWorker(services, ShardScheme.FlowsBackend)
 {
-    private readonly Dictionary<Type, (FlowId FlowId, int RequiredShardIndex)> _masterFlows = new ();
-    private bool _isCompleted;
+    private readonly ConcurrentDictionary<Type, FlowId> _flowsToStart = new();
 
     [field: AllowNull, MaybeNull]
     private FlowRegistry FlowRegistry => field ??= Services.GetRequiredService<FlowRegistry>();
     [field: AllowNull, MaybeNull]
     private IFlows Flows => field ??= Services.GetRequiredService<IFlows>();
+    [field: AllowNull, MaybeNull]
+    private ShardKeyResolver<FlowId> FlowIdShardKeyResolver
+        => field ??= ShardKeyResolvers.Get<FlowId>(typeof(MasterFlowStarter));
 
     protected override Task OnStart(CancellationToken cancellationToken)
     {
-        var shardKeyResolver = ShardKeyResolvers.Get<FlowId>(typeof(MasterFlowStarter));
         var masterFlowTypes = FlowRegistry.NameByType.Keys.Where(x => x.IsAssignableTo(typeof(IMasterFlow)));
         foreach (var masterFlowType in masterFlowTypes) {
             var flowId = FlowRegistry.NewId(masterFlowType, "");
-            var requiredShardIndex = new ShardRef(ShardScheme, shardKeyResolver.Invoke(flowId)).Normalize().Key;
-            _masterFlows.Add(masterFlowType, (flowId, requiredShardIndex));
+            _flowsToStart[masterFlowType] = flowId;
         }
-
         return Task.CompletedTask;
     }
 
     protected override async Task OnRun(int shardIndex, CancellationToken cancellationToken)
     {
-        if (_masterFlows.Values.All(x => x.RequiredShardIndex != shardIndex) || _isCompleted) {
-            using var dTask = cancellationToken.ToTask();
-            await dTask.Resource.SilentAwait(false);
-            return;
-        }
+        var tasks = new List<Task>();
+        foreach (var (masterFlowType, flowId) in _flowsToStart) {
+            var requiredShardIndex = FlowIdShardKeyResolver.Invoke(flowId);
+            if (shardIndex != requiredShardIndex)
+                continue;
 
-        foreach (var (masterFlowType, (flowId, _)) in _masterFlows)
-            await Flows.StartOrReset(masterFlowType,
-                    flowId.Arguments,
-                    null,
-                    nameof(MasterFlowStarter),
-                    cancellationToken)
-                .ConfigureAwait(false);
-        _isCompleted = true;
+            var task = StartMasterFlow(masterFlowType, flowId, cancellationToken);
+            tasks.Add(task);
+        }
+        await Task.WhenAll(tasks).ConfigureAwait(false);
+    }
+
+    private async Task StartMasterFlow(Type flowType, FlowId flowId, CancellationToken cancellationToken)
+    {
+        await Flows
+            .StartOrReset(flowType, flowId.Arguments, null, nameof(MasterFlowStarter), cancellationToken)
+            .ConfigureAwait(false);
+        _flowsToStart.TryRemove(flowType, out _);
     }
 }
