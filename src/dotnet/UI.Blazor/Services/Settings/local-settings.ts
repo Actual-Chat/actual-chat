7af83dfb363bdc09d5c5dfaeab40dbf1687719da
Dmitrii Filippov <crui3er@gmail.com>
2025-09-22 14:05:35 +0400

    refactor: remove unused localStorage retrieval in local-settings.ts added by accident

--- src/dotnet/UI.Blazor/Services/Settings/local-settings.ts
+++ src/dotnet/UI.Blazor/Services/Settings/local-settings.ts
@@ -2,4 +2,3 @@ import { Kvas } from 'kvas';
 import { WebKvasBackend } from '../Kvas/web-kvas-backend';
 
 export const localSettings = new WebKvasBackend(new Kvas('local-settings', true), "1.0", false, true);
-localStorage.getItem('local-settings');
