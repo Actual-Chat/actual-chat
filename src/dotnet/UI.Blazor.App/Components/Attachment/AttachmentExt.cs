0fde6d785f981ba3e1375f0347c425162b82d704
Dmitrii Filippov <crui3er@gmail.com>
2025-09-22 19:49:09 +0400

    feat: update uploads progress on sending message start

--- src/dotnet/UI.Blazor.App/Components/Attachment/AttachmentExt.cs
+++ src/dotnet/UI.Blazor.App/Components/Attachment/AttachmentExt.cs
@@ -6,12 +6,25 @@ public static class AttachmentExt
 {
     public static void ObserveUploadProgress(UploadProgressTracker progressTracker, Action<Func<Attachment, Attachment>> updateAttachment)
     {
-        progressTracker.ProgressChanged += (_, value) => {
+        progressTracker.ProgressChanged += (_, value) => { UpdateProgress(value); };
+        _ = progressTracker.Task.ContinueWith(UpdateResult, TaskScheduler.Default);
+        if (progressTracker.Progress > 0)
+            UpdateProgress(progressTracker.Progress);
+        if (progressTracker.Task.IsCompleted)
+            UpdateResult(progressTracker.Task);
+        return;
+
+        void UpdateProgress(double value)
+        {
             updateAttachment(x => x with { Progress = (int)value });
-        };
-        _ = progressTracker.Task.ContinueWith(t => {
+        }
+
+        void UpdateResult(Task<MediaContent> t)
+        {
             if (t.IsCompletedSuccessfully) {
+ #pragma warning disable VSTHRD002
                 var mediaContent = t.Result;
+ #pragma warning restore VSTHRD002
                 updateAttachment(x => x with {
                     MediaId = mediaContent.MediaId,
                     ThumbnailMediaId = mediaContent.ThumbnailMediaId,
@@ -22,6 +35,6 @@ public static class AttachmentExt
                     Failed = true,
                 });
             }
-        }, TaskScheduler.Default);
+        }
     }
 }
