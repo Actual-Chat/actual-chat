5dc38d6f007ab7689c4e8a116d2b6ca65e2bf20b
Dmitrii Filippov <crui3er@gmail.com>
2025-09-30 17:46:27 +0400

    feat: SQLiteBatchingKvasBackend.GetAll

--- src/dotnet/App.Maui/Services/SQLiteBatchingKvasBackend.cs
+++ src/dotnet/App.Maui/Services/SQLiteBatchingKvasBackend.cs
@@ -68,7 +68,16 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
     }
 
     public ValueTask<(string Key, byte[] Value)[]> GetAll(CancellationToken cancellationToken = default)
-        => throw new NotImplementedException();
+    {
+        if (_connectionPool == null)
+            return ValueTask.FromResult(Array.Empty<(string Key, byte[] Value)>());
+
+        using var lease = _connectionPool.Rent();
+        var connection = lease.Resource;
+        var dbItems = DbHelpers.SelectAll(connection);
+        var result = dbItems.Select(dbItem => (dbItem.Key, dbItem.Value)).ToArray();
+        return ValueTask.FromResult(result);
+    }
 
     public Task SetMany(List<(string Key, byte[]? Value)> updates, CancellationToken cancellationToken = default)
     {
@@ -154,6 +163,7 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
         private static volatile object? _initializedTag;
 
         public static TableMapping Mapping = null!;
+        public static string SelectAllSql = null!;
         public static string FindSql = null!;
         public static string FindManySql = null!;
         public static string DeleteSql = null!;
@@ -167,6 +177,7 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
                 using var _ = Lock.EnterScope();
                 if (_initializedTag == null) {
                     Mapping = connection.GetMapping(typeof(DbItem));
+                    SelectAllSql = $"select * from {Mapping.TableName}";
                     FindSql = Mapping.GetByPrimaryKeySql;
                     FindManySql =
                         $"select * from {Mapping.TableName} where Key in (select e.value from json_each(?) e)";
@@ -176,6 +187,12 @@ public sealed class SQLiteBatchingKvasBackend : IBatchingKvasBackend
                 }
             }
             return connection;
+        }
+
+        public static IEnumerable<DbItem> SelectAll(SQLiteConnection connection)
+        {
+            var cmd = connection.CreateCommand(SelectAllSql);
+            return cmd.ExecuteDeferredQuery<DbItem>(Mapping);
         }
 
         public static DbItem? Find(SQLiteConnection connection, string key)
