71eae3e4d227d5bb0e9bed0c9a5bfa07ca34698c
Dmitrii Filippov <crui3er@gmail.com>
2025-10-03 18:52:14 +0400

    fix: IndexingFlowTests fail due to HardResumeIn is not in range (incorrect HardResumeIn calculation) chore: use string instead of Symbol in IndexingFlowContextBase

--- tests/Core.Server.IntegrationTests/Flows/IndexingFlowTestContext.cs
+++ tests/Core.Server.IntegrationTests/Flows/IndexingFlowTestContext.cs
@@ -4,7 +4,7 @@ namespace ActualChat.Core.Server.IntegrationTests.Flows;
 
 public sealed class IndexingFlowTestContext(MomentClockSet clocks) : IndexingFlowContextBase<BatchIndexingResult<long>>(clocks)
 {
-    public override BatchIndexingResult<long> Next(Symbol id)
+    public override BatchIndexingResult<long> Next(string id)
         => Batches[id].Dequeue();
 
     protected override bool HasProcessedAnyItems(BatchIndexingResult<long> batch)
