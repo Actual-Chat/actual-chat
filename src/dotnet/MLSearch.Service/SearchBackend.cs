095743b93346efc85db90ebdc419236d793772bd
Frol <4425748+FrolYo@users.noreply.github.com>
2025-10-16 18:13:51 +0300

    Revert "fix: Both user IDs are the same. (Parameter 'userId2') exception during search #3290"
    This reverts commit 4d53b865688e86795abfab04e35c1ae088025e86.

--- src/dotnet/MLSearch.Service/SearchBackend.cs
+++ src/dotnet/MLSearch.Service/SearchBackend.cs
@@ -322,7 +322,7 @@ public class SearchBackend(IServiceProvider services) : DbServiceBase<MLSearchDb
         if (searchResponse.ApiCall.HttpStatusCode == StatusCodes.Status404NotFound)
             return ContactSearchResultPage.Empty;
         return new ContactSearchResultPage {
-            Hits = searchResponse.Hits.Select(ToSearchResult).SkipNullItems().ToArray(),
+            Hits = searchResponse.Hits.Select(ToSearchResult).ToArray(),
             Offset = query.Skip,
         };
 
@@ -338,14 +338,8 @@ public class SearchBackend(IServiceProvider services) : DbServiceBase<MLSearchDb
                         : null)
                 .MustNot(qc => qc.HasChild<IndexedUserContact>(c => c.Query(q => q.MatchAll())));
 
-        ContactSearchResult? ToSearchResult(IHit<IndexedUser> hit)
-        {
-            var otherUserId = hit.Source.Id;
-            if (otherUserId == userId)
-                return null;
-            var contactId = ContactId.NewUser(userId, otherUserId);
-            return new ContactSearchResult(contactId, hit.GetSearchMatch());
-        }
+        ContactSearchResult ToSearchResult(IHit<IndexedUser> hit)
+            => new(ContactId.NewUser(userId, hit.Source.Id), hit.GetSearchMatch());
     }
 
     private async Task<ContactSearchResultPage> FindGroups(
