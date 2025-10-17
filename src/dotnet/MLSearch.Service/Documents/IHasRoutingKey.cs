e2596b290e25d3e42bae166df0304757cf4a6365
Frol <4425748+FrolYo@users.noreply.github.com>
2025-10-17 07:39:43 +0300

    chore: cleanup user contact indexing and search logic #3290

--- src/dotnet/MLSearch.Service/Documents/IHasRoutingKey.cs
+++ src/dotnet/MLSearch.Service/Documents/IHasRoutingKey.cs
@@ -3,5 +3,5 @@ namespace ActualChat.MLSearch.Documents;
 internal interface IHasRoutingKey<in TId>
     where TId : StringIdentifier
 {
-    static virtual string? GetRoutingKey(TId? id) => id?.Value;
+    static virtual string GetRoutingKey(TId id) => id.Value;
 }
