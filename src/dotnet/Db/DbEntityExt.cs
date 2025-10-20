910e7b2f7d2fbb2425228026206a577faab30d54
Alex Yakunin <alex.yakunin@gmail.com>
2025-10-20 15:58:41 -0700

    fix: disable C# 14 code

--- src/dotnet/Db/DbEntityExt.cs
+++ src/dotnet/Db/DbEntityExt.cs
@@ -1,5 +1,9 @@
 namespace ActualChat.Db;
 
+// NOTE(AY): This code requires C# 14, which isn't enabled on GitHub builders yet;
+//           on the other hand, the type isn't used.
+/*
+
 public static class DbEntityExt
 {
     extension<TDbEntity, TModel>(IDbEntity<TDbEntity, TModel> entity)
@@ -13,3 +17,5 @@ public static class DbEntityExt
         }
     }
 }
+
+*/
