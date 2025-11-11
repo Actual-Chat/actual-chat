b76ff7d65d1703dc6f28868d0a4a456b02012e54
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-11-11 19:56:11 +0300

    feat: sign-in by multiple identities - email and phone

--- src/dotnet/Users.Service/Email/EmailAuth.cs
+++ src/dotnet/Users.Service/Email/EmailAuth.cs
@@ -5,6 +5,7 @@ using ActualChat.Users.Module;
 using ActualLab.Fusion.EntityFramework;
 using ActualChat.Users.Phone;
 using ActualChat.Users.Templates;
+using ActualLab.Fusion.Authentication.Services;
 using ActualLab.Redis;
 using Mjml.Net;
 using StackExchange.Redis;
@@ -22,6 +23,9 @@ public class EmailAuth(IServiceProvider services) : DbServiceBase<UsersDbContext
     private TotpCodes TotpCodes { get; } = services.GetRequiredService<TotpCodes>();
     private TotpSecrets TotpSecrets { get; } = services.GetRequiredService<TotpSecrets>();
     private RedisDb<UsersDbContext> RedisDb { get; } = services.GetRequiredService<RedisDb<UsersDbContext>>();
+    private IDbUserRepo<UsersDbContext, DbUser, string> DbUsers { get; } = services.GetRequiredService<IDbUserRepo<UsersDbContext, DbUser, string>>();
+    [field: AllowNull, MaybeNull]
+    private IDbEntityConverter<DbUser, User> UserConverter => field ??= Services.DbEntityConverter<DbUser, User>();
 
     // [ComputeMethod]
     public virtual Task<string> ValidateCanSendToEmail(Session session, ActualChat.Email email, TotpPurpose purpose, CancellationToken cancellationToken)
@@ -85,6 +89,22 @@ public class EmailAuth(IServiceProvider services) : DbServiceBase<UsersDbContext
     }
 
     // [CommandHandler]
+    public virtual async Task<bool> OnValidateTotp(EmailAuth_ValidateTotp command, CancellationToken cancellationToken)
+    {
+        if (Invalidation.IsActive)
+            return false; // It just spawns other commands, so nothing to do here
+
+        var (session, email, totp) = command;
+        if (!await ValidateCode(session, email.Value, totp, TotpPurpose.SignInEmail, cancellationToken).ConfigureAwait(false))
+            return false;
+
+        var user = new User(Symbol.Empty, string.Empty).WithEmail(email);
+        var signInCommand = new AuthBackend_SignIn(session, user, user.GetEmailIdentity());
+        await Commander.Call(signInCommand, true, cancellationToken).ConfigureAwait(false);
+        return true;
+    }
+
+    // [CommandHandler]
     public virtual async Task<bool> OnVerifyEmail(EmailAuth_VerifyEmail command, CancellationToken cancellationToken)
     {
         if (Invalidation.IsActive)
@@ -99,35 +119,33 @@ public class EmailAuth(IServiceProvider services) : DbServiceBase<UsersDbContext
         }
 
         var (session, email, totp) = command;
-        var account = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
         if (!await ValidateCode(session, email.Value, totp, TotpPurpose.VerifyEmail, cancellationToken).ConfigureAwait(false))
             return false;
 
+        var account = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
         account = account with { IsEmailVerified = true };
         await Accounts.AssertCanUpdate(session, account, cancellationToken).ConfigureAwait(false);
+
         var cmd = new AccountsBackend_Update(account, account.Version);
         await Commander.Call(cmd, cancellationToken).ConfigureAwait(false);
 
+        // save email identity + email claim
+        var dbContext = await DbHub.CreateOperationDbContext(cancellationToken).ConfigureAwait(false);
+        await using var __ = dbContext.ConfigureAwait(false);
+
+        var dbUser = await DbUsers.Get(dbContext, account.Id.Value, true, cancellationToken).ConfigureAwait(false);
+        if (dbUser == null)
+            return default; // Should never happen, but if it somehow does, there is no extra to do in this case
+
+        var user = account.User.WithEmail(email);
+        var conflictingDbUser = await DbUsers.GetByUserIdentity(dbContext, user.GetEmailIdentity(), false, cancellationToken).ConfigureAwait(false);
+        if (conflictingDbUser != null && !OrdinalEquals(conflictingDbUser.Id, dbUser.Id))
+            throw StandardError.Unauthorized("Email has already been taken by another account.");
+
+        UserConverter.UpdateEntity(user, dbUser);
         context.Operation.Items.KeylessSet(account.Id);
-        return true;
-    }
 
-    // [CommandHandler]
-    public virtual async Task<bool> OnValidateTotp(EmailAuth_ValidateTotp command, CancellationToken cancellationToken)
-    {
-        if (Invalidation.IsActive)
-            return false; // It just spawns other commands, so nothing to do here
-
-        var (session, email, totp) = command;
-
-        // Try both purposes: SignIn and VerifyEmail. We accept either successful validation
-        if (!await ValidateCode(session, email.Value, totp, TotpPurpose.SignInEmail, cancellationToken).ConfigureAwait(false)
-            && !await ValidateCode(session, email.Value, totp, TotpPurpose.VerifyEmail, cancellationToken).ConfigureAwait(false))
-            return false;
-
-        var user = new User(Symbol.Empty, string.Empty).WithEmail(email);
-        var signInCommand = new AuthBackend_SignIn(session, user, user.GetEmailIdentity());
-        await Commander.Call(signInCommand, true, cancellationToken).ConfigureAwait(false);
+        await dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         return true;
     }
 
