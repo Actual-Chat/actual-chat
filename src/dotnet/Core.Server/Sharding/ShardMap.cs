565126fe83660120065408ab6179a1ae572dd582
Alex Yakunin <alex.yakunin@gmail.com>
2025-11-23 17:41:48 -0800

    fix: build error

--- src/dotnet/Core.Server/Sharding/ShardMap.cs
+++ src/dotnet/Core.Server/Sharding/ShardMap.cs
@@ -3,7 +3,7 @@ using ActualLab.Scalability;
 
 namespace ActualChat.Sharding;
 
-public sealed class ShardMap(ShardScheme shardScheme, ImmutableArray<MeshNode> nodes)
+public sealed class ShardMap(ShardScheme shardScheme, MeshNode[] nodes)
     : ShardMap<MeshNode>(shardScheme.ShardCount, nodes, static node => node.GetHashes<int>())
 {
     public ShardScheme ShardScheme { get; } = shardScheme;
