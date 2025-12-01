f56ba1cf3dd823dd3fcacabc5155e5060b688ce6
Dmitrii Filippov <crui3er@gmail.com>
2025-12-01 19:04:12 +0400

    refactor: remove `FileUploader` and related logic in favor of `ChunkedFileUploader`

--- src/dotnet/UI.Blazor.App/Services/FileProviders/MauiFileProvider.cs
+++ src/dotnet/UI.Blazor.App/Services/FileProviders/MauiFileProvider.cs
@@ -12,7 +12,6 @@ public partial class MauiFileProvider : IFileProvider
     [DataMember, MemoryPackOrder(1)]
     public string FileRef { get; init; } = "";
 
-    private FileUploader Uploader => _services.GetRequiredService<FileUploader>();
     private ChunkedFileUploader ChunkedFileUploader => _services.GetRequiredService<ChunkedFileUploader>();
     private IMauiFileProviderImpl Impl => field ??= _services.GetRequiredService<IMauiFileProviderImplFactory>().Create(FileRef);
     private ILogger Log => field ??= _services.LogFor<MauiFileProvider>();
@@ -25,22 +24,6 @@ public partial class MauiFileProvider : IFileProvider
 
     public Task PrepareForSaving()
         => Impl.PrepareForSaving();
-
-    [UnconditionalSuppressMessage("Trimming", "IL2026:RequiresUnreferencedCode", Justification = "Upload doesn't use reflection.")]
-    public async Task<IFileUploadOperation> CreateUploadOperation(ChatId chatId)
-    {
-        var stream = await OpenRead().ConfigureAwait(false);
-        if (stream is null)
-            throw new InvalidOperationException("No file access.");
-        var fileUploadOperation = Uploader.CreateUploadOperation(chatId, stream, Metadata.FileType, Metadata.FileName);
-        var task = fileUploadOperation.ProgressTracker.Task;
-        _ = task.ContinueWith(async _ => {
-            await task.SilentAwait(false);
-            // NOTE: dispose stream when upload completed or canceled.
-            await stream.DisposeSilentlyAsync().ConfigureAwait(false);
-        }, TaskScheduler.Default);
-        return fileUploadOperation;
-    }
 
     [UnconditionalSuppressMessage("Trimming", "IL2026:RequiresUnreferencedCode", Justification = "Upload doesn't use reflection.")]
     public async Task<IFileUploadOperation> CreateUploadOperation(UploadId uploadId)
