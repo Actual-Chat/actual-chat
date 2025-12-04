aee395d401e3e23e78613e70c90714983e6e8a63
Dmitrii Filippov <crui3er@gmail.com>
2025-12-04 16:56:04 +0400

    refactor: restructure `UploadsBackend` for modularity by moving storage logic to `UploadsStorage` and updating related methods and contracts

--- src/dotnet/Media.Contracts/IUploadsBackend.cs
+++ src/dotnet/Media.Contracts/IUploadsBackend.cs
@@ -7,23 +7,29 @@ public interface IUploadsBackend : IComputeService, IBackendService
 {
     [ComputeMethod]
     Task<Upload?> Get(UploadId uploadId, CancellationToken cancellationToken);
+    Task<long> GetOffset(UploadId uploadId, CancellationToken cancellationToken);
     [CommandHandler]
-    Task<UploadId> OnCreate(UploadsBackend_Create command, CancellationToken cancellationToken);
+    Task OnCreate(UploadsBackend_Create command, CancellationToken cancellationToken);
     [CommandHandler]
     Task OnRemove(UploadsBackend_Remove command, CancellationToken cancellationToken);
+    [CommandHandler]
+    Task<long> OnAppend(UploadsBackend_Append command, CancellationToken cancellationToken);
+    [CommandHandler]
+    Task<MediaContent> OnConvertToMediaContent(UploadsBackend_ConvertToMediaContent command, CancellationToken cancellationToken);
 }
 
 [DataContract, MemoryPackable(GenerateType.VersionTolerant)]
 // ReSharper disable once InconsistentNaming
 public sealed partial record UploadsBackend_Create(
-    [property: DataMember, MemoryPackOrder(0)] UserId UserId,
-    [property: DataMember, MemoryPackOrder(1)] long? Length,
-    [property: DataMember, MemoryPackOrder(2)] string Tag,
+    [property: DataMember, MemoryPackOrder(0)] UploadId UploadId,
+    [property: DataMember, MemoryPackOrder(1)] UserId UserId,
+    [property: DataMember, MemoryPackOrder(2)] long? Length,
+    [property: DataMember, MemoryPackOrder(3)] string Tag,
     [property: DataMember, MemoryPackOrder(10)] PropertyBag Metadata
-) : ICommand<UploadId>, IBackendCommand, IHasShardKey<UserId>
+) : ICommand<Unit>, IBackendCommand, IHasShardKey<UploadId>
 {
     [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
-    public UserId ShardKey => UserId;
+    public UploadId ShardKey => UploadId;
 }
 
 // How should I apply sharding?
@@ -38,4 +44,24 @@ public sealed partial record UploadsBackend_Remove(
     public UploadId ShardKey => Id;
 }
 
+[DataContract, MemoryPackable(GenerateType.VersionTolerant)]
+// ReSharper disable once InconsistentNaming
+public sealed partial record UploadsBackend_Append(
+    [property: DataMember, MemoryPackOrder(0)] UploadId UploadId,
+    [property: DataMember, MemoryPackOrder(1)] byte[] Chunk,
+    [property: DataMember, MemoryPackOrder(2)] long Offset
+) : ICommand<long>, IBackendCommand, IHasShardKey<UploadId>
+{
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
+    public UploadId ShardKey => UploadId;
+}
 
+[DataContract, MemoryPackable(GenerateType.VersionTolerant)]
+// ReSharper disable once InconsistentNaming
+public sealed partial record UploadsBackend_ConvertToMediaContent(
+    [property: DataMember, MemoryPackOrder(0)] UploadId UploadId
+) : ICommand<MediaContent>, IBackendCommand, IHasShardKey<UploadId>
+{
+    [JsonIgnore, Newtonsoft.Json.JsonIgnore, IgnoreDataMember, MemoryPackIgnore]
+    public UploadId ShardKey => UploadId;
+}
--- src/dotnet/Media.Service/Uploads.cs
+++ src/dotnet/Media.Service/Uploads.cs
@@ -1,5 +1,3 @@
-using ActualChat.Chat;
-using ActualChat.Uploads;
 using ActualChat.Users;
 
 namespace ActualChat.Media;
@@ -8,8 +6,6 @@ public class Uploads(IServiceProvider services) : IUploads
 {
     private IAccounts Accounts { get; } = services.GetRequiredService<IAccounts>();
     private IUploadsBackend Backend { get; } = services.GetRequiredService<IUploadsBackend>();
-    private UploadsStorage UploadsStorage { get; } = services.GetRequiredService<UploadsStorage>();
-    private IMediaProcessor MediaProcessor { get; } = services.GetRequiredService<IMediaProcessor>();
     private ICommander Commander { get; } = services.Commander();
 
     public virtual async Task<long> GetOffset(Session session, UploadId uploadId, CancellationToken cancellationToken)
@@ -17,7 +13,7 @@ public class Uploads(IServiceProvider services) : IUploads
         var user = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
         var upload = await Backend.Get(uploadId, cancellationToken).ConfigureAwait(false);
         EnsureCanAccessUpload(upload, user);
-        var offset = await UploadsStorage.GetUploadOffset(uploadId, cancellationToken).ConfigureAwait(false);
+        var offset = await Backend.GetOffset(uploadId, cancellationToken).ConfigureAwait(false);
         return offset;
     }
 
@@ -29,10 +25,12 @@ public class Uploads(IServiceProvider services) : IUploads
             throw StandardError.NotSupported("Defer upload length is not supported yet.");
         if (length > Constants.Attachments.FileSizeLimit)
             throw StandardError.Constraint("File is too big.");
-        // TODO: add validation by tag and user permissions
 
         var user = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
-        return await Commander.Call(new UploadsBackend_Create(user.Id, length, tag, metadata), cancellationToken).ConfigureAwait(false);
+        EnsureCanCreate(user, length.Value, tag, metadata);
+        var uploadId = UploadId.New();
+        await Commander.Call(new UploadsBackend_Create(uploadId, user.Id, length, tag, metadata), cancellationToken).ConfigureAwait(false);
+        return uploadId;
     }
 
     // [CommandHandler]
@@ -54,20 +52,7 @@ public class Uploads(IServiceProvider services) : IUploads
         var user = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
         var upload = await Backend.Get(uploadId, cancellationToken).Require().ConfigureAwait(false);
         EnsureCanAccessUpload(upload, user);
-
-        var currentOffset = await UploadsStorage.GetUploadOffset(uploadId, cancellationToken).ConfigureAwait(false);
-        if (offset != currentOffset)
-            throw StandardError.Constraint("Offset mismatch.");
-        if (offset + data.Length > upload.Length)
-            throw StandardError.Constraint("Upload length mismatch.");
-
-        var stream = MemoryStreamManager.Default.GetStream(nameof(Uploads), data.Length);
-        await using (stream.ConfigureAwait(false)) {
-            stream.Write(data);
-            stream.Position = 0;
-            await UploadsStorage.AppendDataAsync(uploadId, stream, cancellationToken).ConfigureAwait(false);
-        }
-        return currentOffset + data.Length;
+        return await Commander.Call(new UploadsBackend_Append(uploadId, data, offset), cancellationToken).ConfigureAwait(false);
     }
 
     // [CommandHandler]
@@ -77,17 +62,7 @@ public class Uploads(IServiceProvider services) : IUploads
         var user = await Accounts.GetOwn(session, cancellationToken).ConfigureAwait(false);
         var upload = await Backend.Get(uploadId, cancellationToken).ConfigureAwait(false);
         EnsureCanAccessUpload(upload, user);
-        var chatId = GetChatIdFromUploadTag(upload.Tag);
-        var offset = await UploadsStorage.GetUploadOffset(uploadId, cancellationToken).ConfigureAwait(false);
-        if (offset != upload.Length)
-            throw StandardError.Constraint("Upload length mismatch.");
-
-        var uploadedStreamFile = new UploadedStreamFile(
-            upload.FileName,
-            upload.ContentType,
-            upload.Length!.Value,
-            () => UploadsStorage.GetDataFile(uploadId, cancellationToken));
-        return await MediaProcessor.ProcessAttachment(chatId, uploadedStreamFile, cancellationToken).ConfigureAwait(false);
+        return await Commander.Call(new UploadsBackend_ConvertToMediaContent(uploadId), cancellationToken).ConfigureAwait(false);
     }
 
     private static void EnsureCanAccessUpload([NotNull] Upload? upload, Account user)
@@ -96,15 +71,8 @@ public class Uploads(IServiceProvider services) : IUploads
             throw StandardError.Constraint("Upload not found.");
     }
 
-    private ChatId GetChatIdFromUploadTag(string uploadTag)
+    private void EnsureCanCreate(AccountFull user, long length, string tag, PropertyBag metadata)
     {
-        var parts = uploadTag.Split('/');
-        if (parts.Length == 3
-            && OrdinalEquals(parts[0], nameof(TextEntryAttachment))
-            && OrdinalEquals(parts[1], "v1")
-            && ChatId.TryParse(parts[2], out var chatId))
-            return chatId;
-
-        throw StandardError.Constraint("Invalid upload tag.");
+        // Add validation by tag and user permissions
     }
 }
