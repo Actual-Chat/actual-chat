dc9f5e5540fafedc896341c20696bcd528c9f221
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-12-09 11:34:46 +0300

    fix: trying to make DebounceTest working on GH Agent

--- tests/Core.UnitTests/DebouncerTest.cs
+++ tests/Core.UnitTests/DebouncerTest.cs
@@ -13,7 +13,7 @@ public class DebouncerTest(ITestOutputHelper @out) : TestBase(@out)
         var sw = Stopwatch.StartNew();
         Action<string> log = msg => WriteLine(sw.Elapsed.ToString("c") + " " + msg);
         WriteLine($"start: {sw.Elapsed}");
-        var debouncer = Debouncer.New<int>(clock, TimeSpan.FromMilliseconds(1000), i => {
+        var debouncer = Debouncer.New<int>(clock, TimeSpan.FromMilliseconds(2000), i => {
             log($"debounce invoked, value ='{i}'");
             lock (results)
                 results.Add(i);
@@ -28,12 +28,12 @@ public class DebouncerTest(ITestOutputHelper @out) : TestBase(@out)
         debouncer.Debounce(3);
         log.Invoke("after debounce 3");
         clock.OffsetBy(100);
-        await Task.Delay(300); // Just to make sure async ops complete
-        log.Invoke("after delay 300");
+        await Task.Delay(1); // Just to make sure async ops complete
+        log.Invoke("after delay 1");
         debouncer.Debounce(4);
         log.Invoke("after debounce 4");
-        clock.OffsetBy(2000);
-        log.Invoke("after OffsetBy(2000)");
+        clock.OffsetBy(3000);
+        log.Invoke("after OffsetBy(3000)");
 
         await debouncer.WhenCompleted();
         log.Invoke("after WhenCompleted");
