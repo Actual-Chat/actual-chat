bd251f242b366433c03bd7bb115d070bda696ade
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-12-09 11:08:46 +0300

    fix: revert back SummarizeQueue as it is still in use by ConversationBackend_Summarize, etc.

--- src/dotnet/Core.Server/Sharding/ShardScheme.cs
+++ src/dotnet/Core.Server/Sharding/ShardScheme.cs
@@ -19,6 +19,7 @@ public sealed class ShardScheme(
     public static readonly ShardScheme None = new(nameof(None), 0, HostRole.None, ShardSchemeFlags.Special | ShardSchemeFlags.Queue);
     public static readonly ShardScheme Undefined = new(nameof(Undefined), 0, HostRole.None, ShardSchemeFlags.Special | ShardSchemeFlags.Queue);
     public static readonly ShardScheme EventQueue = new(nameof(EventQueue), N, HostRole.EventQueue, ShardSchemeFlags.Queue);
+    public static readonly ShardScheme SummarizeQueue = new(nameof(SummarizeQueue), 1, HostRole.EventQueue, ShardSchemeFlags.SlowQueue);
     public static readonly ShardScheme FlowsBackend = new(nameof(FlowsBackend), N, HostRole.FlowsBackend);
     public static readonly ShardScheme MediaBackend = new(nameof(MediaBackend), N, HostRole.MediaBackend);
     public static readonly ShardScheme AudioBackend = new(nameof(AudioBackend), N, HostRole.AudioBackend);
@@ -37,6 +38,7 @@ public sealed class ShardScheme(
         { None.Id, None },
         // { Undefined.Id, Undefined }, // Shouldn't be listed here
         { EventQueue.Id, EventQueue },
+        { SummarizeQueue.Id, SummarizeQueue },
         { MediaBackend.Id, MediaBackend },
         { AudioBackend.Id, AudioBackend },
         { ChatBackend.Id, ChatBackend },
