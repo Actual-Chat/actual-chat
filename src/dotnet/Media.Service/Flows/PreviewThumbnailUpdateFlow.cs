f5d7ed43a5b1ff17680ab620f92bac9117120a6b
Alex Yakunin <alex.yakunin@gmail.com>
2025-12-17 19:40:04 -0800

    chore: cosmetic edits

--- src/dotnet/Media.Service/Flows/PreviewThumbnailUpdateFlow.cs
+++ src/dotnet/Media.Service/Flows/PreviewThumbnailUpdateFlow.cs
@@ -14,7 +14,7 @@ public sealed partial class PreviewThumbnailUpdateFlow : PeriodicFlow
         => url.ToBase64();
 
     protected override ValueTask<Moment> GetNextRunAt(CancellationToken cancellationToken)
-        => new(LastRunAt.EpochOffsetTicks == 0 ? ResumedAt : Moment.MaxValue);
+        => new(LastRunAt = default ? ResumedAt : Moment.MaxValue);
 
     protected override async Task Run(CancellationToken cancellationToken)
     {
