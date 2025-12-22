f0d8278b18c273304e10e55532f830ba17469731
Alexey Kochetov <alexis.kochetov@gmail.com>
2025-12-22 18:24:36 +0300

    feat: conversation indices migration

---
+++ src/dotnet/Chat.Service.Migration/Migrations/20251222152051_ConversationIndexes.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConversationIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_conversations_chat_id_end_entry_lid",
+                table: "conversations",
+                columns: new[] { "chat_id", "end_entry_lid" },
+                unique: true,
+                descending: new[] { false, true })
+                .Annotation("Npgsql:IndexInclude", new[] { "start_entry_lid" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_conversations_chat_id_start_entry_lid",
+                table: "conversations",
+                columns: new[] { "chat_id", "start_entry_lid" },
+                unique: true)
+                .Annotation("Npgsql:IndexInclude", new[] { "end_entry_lid" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_conversations_chat_id_end_entry_lid",
+                table: "conversations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_conversations_chat_id_start_entry_lid",
+                table: "conversations");
+        }
+    }
+}
