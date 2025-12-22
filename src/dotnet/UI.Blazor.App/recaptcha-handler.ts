b2823eecd50c9439bb3163ad3383f7f0c3bf0b1d
Iq Mulator <iqmulator@gmail.com>
2025-12-22 20:20:14 +0500

    feat: hide recaptcha error message for user

---
+++ src/dotnet/UI.Blazor.App/recaptcha-handler.ts
@@ -0,0 +1,63 @@
+import { Log } from 'logging';
+
+const { warnLog } = Log.get('RecaptchaHandler');
+
+export class RecaptchaHandler {
+    public static observer: MutationObserver;
+
+    public static init() {
+        this.observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                for (const node of Array.from(mutation.addedNodes)) {
+                    if (node.nodeType !== Node.ELEMENT_NODE)
+                        continue;
+
+                    const outerDiv = node as HTMLElement;
+                    if (outerDiv.parentElement !== document.body)
+                        continue;
+
+                    if (outerDiv.tagName !== 'DIV')
+                        continue;
+
+                    this.checkOuterDiv(outerDiv);
+                }
+            }
+        });
+
+        this.observer.observe(document.body, {
+            childList: true
+        });
+    }
+
+    private static checkOuterDiv(outerDiv: HTMLElement) {
+        if (outerDiv.style.display == 'none')
+            return;
+
+        if (outerDiv.children.length !== 1)
+            return;
+
+        const innerDiv = outerDiv.children[0] as HTMLElement;
+        if (innerDiv.tagName !== 'DIV')
+            return;
+
+        const rawText = innerDiv.textContent;
+        if (!rawText)
+            return;
+
+        const text = this.normalizeText(rawText);
+        if (text.includes('recaptcha') && text.includes('connect')) {
+            outerDiv.style.display = 'none';
+            warnLog?.log('reCAPTCHA error detected and hidden:', rawText);
+        }
+    }
+
+    private static normalizeText(text: string): string {
+        return text
+            .toLowerCase()
+            .replace(/\u00a0/g, ' ')
+            .replace(/\s+/g, ' ')
+            .trim();
+    }
+}
+
+RecaptchaHandler.init();
--- src/nodejs/src/logging-init.ts
+++ src/nodejs/src/logging-init.ts
@@ -31,6 +31,7 @@ export type LogScope =
     | 'OnDeviceAwake'
     | 'promises'
     | 'Rpc'
+    | 'RecaptchaHandler'
     | 'ScreenSize'
     | 'ServiceWorker'
     | 'SessionTokens'
