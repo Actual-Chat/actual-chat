437a1bff59e673fdcc30312be2014c9808b17e15
Dmitrii Filippov <crui3er@gmail.com>
2025-12-26 23:04:50 +0400

    feat: show all nodes info in mesh diag page

--- src/dotnet/Chat.Service/Diagnostics.cs
+++ src/dotnet/Chat.Service/Diagnostics.cs
@@ -2,16 +2,10 @@ using ActualChat.Users;
 
 namespace ActualChat.Chat;
 
-public class Diagnostics : IDiagnostics
+public class Diagnostics(IServiceProvider services) : IDiagnostics
 {
-    public Diagnostics(IServiceProvider services)
-    {
-        LocalBackend = services.GetRequiredService<DiagnosticsBackendLocal>();
-        Accounts = services.GetRequiredService<IAccounts>();
-    }
-
-    private DiagnosticsBackendLocal LocalBackend { get; }
-    private IAccounts Accounts { get; }
+    private DiagnosticsBackendLocal LocalBackend { get; } = services.GetRequiredService<DiagnosticsBackendLocal>();
+    private IAccounts Accounts { get; } = services.GetRequiredService<IAccounts>();
 
     public virtual async Task<MeshDiagInfo> GetMeshDiagInfo(Session session, string tag, CancellationToken cancellationToken)
     {
@@ -19,6 +13,23 @@ public class Diagnostics : IDiagnostics
         if (!account.IsAdmin)
             throw StandardError.Unauthorized("Only admins can access.");
 
-        return await LocalBackend.GetMeshDiagInfo(tag, 1, cancellationToken).ConfigureAwait(false);
+        var nodeIds = new HashSet<string>(StringComparer.Ordinal);
+        var info = await LocalBackend.GetMeshDiagInfo(tag, 1, cancellationToken).ConfigureAwait(false);
+        nodeIds.Add(info.ThisNodeId);
+        return info with {
+            Others = Flatten(info.Others, nodeIds).ToArray(),
+        };
+    }
+
+    private IEnumerable<MeshDiagInfo> Flatten(MeshDiagInfo[] infos, HashSet<string> nodeIds)
+    {
+        foreach (var info in infos) {
+            if (!nodeIds.Add(info.ThisNodeId))
+                continue;
+
+            yield return info;
+            foreach (var child in Flatten(info.Others, nodeIds))
+                yield return child;
+        }
     }
 }
