db43a62dd2aca9f1ad37fa6a9a0fa4f7d0cf9eb7
Frol <4425748+FrolYo@users.noreply.github.com>
2026-01-16 19:52:08 +0300

    chore(test): ensure translation retains target language #3449

--- tests/Testing.Host/TranslationOperations.cs
+++ tests/Testing.Host/TranslationOperations.cs
@@ -5,43 +5,41 @@ namespace ActualChat.Testing.Host;
 
 public static class TranslationOperations
 {
-    public static Task<Translation?> GetTranslation(
-        this IWebTester tester,
-        TextEntryId entryId,
-        Language language,
-        bool translateIfMissing,
-        CancellationToken cancellationToken = default)
-        => tester.GetTranslation(TranslationId.New(entryId, language), translateIfMissing, cancellationToken);
+    extension(IWebTester tester)
+    {
+        public Task<Translation?> GetTranslation(
+            TextEntryId entryId,
+            Language language,
+            bool translateIfMissing,
+            CancellationToken cancellationToken = default)
+            => tester.GetTranslation(TranslationId.New(entryId, language), translateIfMissing, cancellationToken);
 
-    public static Task<Translation?> GetTranslation(
-        this IWebTester tester,
-        TranslationId id,
-        bool translateIfMissing,
-        CancellationToken cancellationToken = default)
-        => tester.Translations.Get(tester.Session, id, translateIfMissing, cancellationToken);
+        public Task<Translation?> GetTranslation(
+            TranslationId id,
+            bool translateIfMissing,
+            CancellationToken cancellationToken = default)
+            => tester.Translations.Get(tester.Session, id, translateIfMissing, cancellationToken);
 
-    public static Task<ChatEntryLanguage?> GetEntryLanguage(
-        this IWebTester tester,
-        ChatEntryId id,
-        CancellationToken cancellationToken = default)
-        => tester.Translations.GetLanguage(tester.Session, (TextEntryId)id, cancellationToken);
+        public Task<ChatEntryLanguage?> GetEntryLanguage(
+            ChatEntryId id,
+            CancellationToken cancellationToken = default)
+            => tester.Translations.GetLanguage(tester.Session, (TextEntryId)id, cancellationToken);
 
-    public static Task<ChatEntryLanguage?> CreateEntryLanguage(
-        this IWebTester tester,
-        TextEntryId id,
-        Language language,
-        HashString entryContentHash,
-        CancellationToken cancellationToken = default)
-        => tester.Commander.Call(new ChatEntryLanguagesBackend_Change(id, null, Change.Create(new ChatEntryLanguage(id) {
-            Languages = [language],
-            EntryContentHash = entryContentHash,
-        })), cancellationToken);
+        public Task<ChatEntryLanguage?> CreateEntryLanguage(
+            TextEntryId id,
+            Language? language,
+            HashString entryContentHash,
+            CancellationToken cancellationToken = default)
+            => tester.Commander.Call(new ChatEntryLanguagesBackend_Change(id, null, Change.Create(new ChatEntryLanguage(id) {
+                Languages = language is not null ? [language] : [],
+                EntryContentHash = entryContentHash,
+            })), cancellationToken);
 
-    public static Task<ChatEntryLanguage> UpdateEntryLanguage(
-        this IWebTester tester,
-        ChatEntryLanguage entryLanguage,
-        CancellationToken cancellationToken = default)
-        => tester.Commander.Call(
-            new ChatEntryLanguagesBackend_Change(entryLanguage.Id, null, Change.Update(entryLanguage)),
-            cancellationToken).Require();
+        public Task<ChatEntryLanguage> UpdateEntryLanguage(
+            ChatEntryLanguage entryLanguage,
+            CancellationToken cancellationToken = default)
+            => tester.Commander.Call(
+                new ChatEntryLanguagesBackend_Change(entryLanguage.Id, null, Change.Update(entryLanguage)),
+                cancellationToken).Require();
+    }
 }
