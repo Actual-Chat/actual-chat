884d6b91ced1826e03b6522a2ce404615854af0d
Alex Yakunin <alex.yakunin@gmail.com>
2026-01-23 07:18:52 -0800

    fix(auth): add `AddAuthEndpoints` to configure authentication endpoints

--- src/dotnet/Users.Service/Module/UsersServiceModule.cs
+++ src/dotnet/Users.Service/Module/UsersServiceModule.cs
@@ -102,6 +102,7 @@ public sealed class UsersServiceModule(IServiceProvider moduleServices)
             });
             */
 
+            fusionWebServer.AddAuthEndpoints();
             fusionWebServer.ConfigureAuthEndpoint(_ => new() {
                 DefaultSignInScheme = GoogleDefaults.AuthenticationScheme,
                 SignInPropertiesBuilder = (_, properties) => {
