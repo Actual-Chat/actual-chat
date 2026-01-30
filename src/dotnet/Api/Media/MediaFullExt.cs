a91644c6af5e24f493f3343d9efe5ceb0197156d
Dmitrii Filippov <crui3er@gmail.com>
2026-01-30 16:50:00 +0400

    fix: attachment's metadata is empty

--- src/dotnet/Api/Media/MediaFullExt.cs
+++ src/dotnet/Api/Media/MediaFullExt.cs
@@ -5,13 +5,6 @@ public static class MediaFullExt
     public static Media ToMedia(this MediaFull media)
         => new (media.Id) {
             ContentId = media.ContentId,
-            Metadata = Clone(media.Metadata),
+            Metadata = media.Metadata,
         };
-
-    private static PropertyBag Clone(PropertyBag metadata)
-    {
-        var clone = new PropertyBag();
-        clone.SetMany(metadata);
-        return clone;
-    }
 }
