7d3c45ee35943f7f283f67f01d6ea4161a5ed245
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-09 13:36:29 +0300

    feat(ios): introduce UIWorkerBase and UIServiceBase for share extension #3519

--- src/dotnet/App.Maui.IosShareExt/UI/UIServiceBase.cs
+++ src/dotnet/App.Maui.IosShareExt/UI/UIServiceBase.cs
@@ -2,6 +2,7 @@ using ActualChat.App.Maui.IosShareExt.UI.Fusion.Ios;
 
 namespace ActualChat.App.Maui.IosShareExt.UI;
 
+// TODO: if possible get rid of duplicating UIServiceBase
 public class UIServiceBase(IosHub hub) : ProcessorBase
 {
     protected IosHub Hub => hub;
---
+++ src/dotnet/App.Maui.IosShareExt/UI/UIWorkerBase.cs
@@ -0,0 +1,37 @@
+using ActualChat.App.Maui.IosShareExt.UI.Fusion.Ios;
+
+namespace ActualChat.App.Maui.IosShareExt.UI;
+
+// TODO: if possible get rid of duplicating UIWorkerBase
+public abstract class UIWorkerBase(IosHub hub) : UIServiceBase(hub)
+{
+    private volatile Task? _whenRunning;
+
+    public Task? WhenRunning => _whenRunning;
+
+    public virtual Task Run()
+    {
+        if (_whenRunning != null)
+            return _whenRunning;
+        lock (Lock) {
+            if (_whenRunning != null)
+                return _whenRunning;
+            if (StopToken.IsCancellationRequested)
+                return _whenRunning = Task.CompletedTask;
+
+            // ReSharper disable once PossibleMultipleWriteAccessInDoubleCheckLocking
+            _whenRunning = Task.Run(async () => {
+                try {
+                    await OnRun(StopToken).ConfigureAwait(false);
+                }
+                catch {
+                    // Intended: WhenRunning should behave similarly
+                    // to how it behaves in WorkerBase, i.e. never throw.
+                }
+            }, CancellationToken.None);
+        }
+        return _whenRunning;
+    }
+
+    protected abstract Task OnRun(CancellationToken cancellationToken);
+}
