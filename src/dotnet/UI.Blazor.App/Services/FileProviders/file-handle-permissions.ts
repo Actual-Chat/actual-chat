5fdeb4dfadc81f17628b620bc8b69bf885ab47be
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-10 22:59:19 +0300

    chore: suppress eslint errors and add TODOs in multiple .ts files #3534

--- src/dotnet/UI.Blazor.App/Services/FileProviders/file-handle-permissions.ts
+++ src/dotnet/UI.Blazor.App/Services/FileProviders/file-handle-permissions.ts
@@ -1,3 +1,5 @@
+// TODO: remove eslint-disables and fix errors
+/* eslint-disable @typescript-eslint/no-unnecessary-condition,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call */
 import { DelayedInvoker } from '../../../UI.Blazor/Components/delayed-invoker';
 
 export async function requestFileHandlePermission(handle: FileSystemFileHandle, mode : FileSystemPermissionMode = 'read') : Promise<GetFilePermissionsRequest> {
