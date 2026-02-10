1983b2167b00275606335677c63982fd8887c0f3
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-10 21:20:01 +0300

    chore: refactor TS files for eslint compliance and formatting updates #3534

--- src/dotnet/UI.Blazor.App/Components/ChatActivityPanel/chat-activity-panel-icon-svg.lit.ts
+++ src/dotnet/UI.Blazor.App/Components/ChatActivityPanel/chat-activity-panel-icon-svg.lit.ts
@@ -7,14 +7,12 @@ class ChatActivityPanelIconSvg extends LitElement {
         return this;
     }
 
-    @property()
-    size = 4;
-    @property({ type: Boolean })
-    isActive = false;
+    @property() size = 4;
+    @property({ type: Boolean }) isActive = false;
 
     protected render(): unknown {
         return html`
-            <div class="equalizer ${this.isActive ? "active" : ""}" style="width: ${this.size * 4}px; height: ${this.size * 4}px;">
+            <div class="equalizer ${this.isActive ? 'active' : ''}" style="width: ${this.size * 4}px; height: ${this.size * 4}px;">
                 <div class="bar bar-1"></div>
                 <div class="bar bar-2"></div>
                 <div class="bar bar-3"></div>
