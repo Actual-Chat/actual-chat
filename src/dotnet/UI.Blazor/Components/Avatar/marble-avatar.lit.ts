89eb98b96d53d3bbc5643a674006ade4cd54f664
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-10 18:50:25 +0300

    chore: refactor TypeScript files for improved clarity and consistency in avatar components #3534

--- src/dotnet/UI.Blazor/Components/Avatar/marble-avatar.lit.ts
+++ src/dotnet/UI.Blazor/Components/Avatar/marble-avatar.lit.ts
@@ -9,10 +9,10 @@ const ELEMENTS = 3;
 let id = 0;
 
 @customElement('marble-avatar')
-class MarbleAvatar extends LitElement {
+export class MarbleAvatar extends LitElement {
     @property() key: string;
     @property() title: string;
-    @property({ type: Boolean }) doNotBlur: boolean = false;
+    @property({ type: Boolean }) doNotBlur = false;
     @property() colors: string[] = ['F56095', 'F5CD65', '00B27D', '37D3F5', '2F89EB'];
 
     private maskId = `marble-avatar-${++id}`;
@@ -27,7 +27,7 @@ class MarbleAvatar extends LitElement {
 
         return html`
             <svg
-                viewBox='${'0 0 ' + SIZE + ' ' + SIZE}'
+                viewBox='${`0 0 ${SIZE} ${SIZE}`}'
                 fill='none'
                 role='img'
                 xmlns='http://www.w3.org/2000/svg'
@@ -44,42 +44,14 @@ class MarbleAvatar extends LitElement {
                         filter='url(#prefix__filter0_f)'
                         d='M32.414 59.35L50.376 70.5H72.5v-71H33.728L26.5 13.381l19.057 27.08L32.414 59.35z'
                         fill='#${properties[1].color}'
-                        transform='${
-                            'translate(' +
-                            properties[1].translateX +
-                            ' ' +
-                            properties[1].translateY +
-                            ') rotate(' +
-                            properties[1].rotate +
-                            ' ' +
-                            SIZE / 2 +
-                            ' ' +
-                            SIZE / 2 +
-                            ') scale(' +
-                            properties[2].scale +
-                            ')'
-                        }'
+                        transform='${`translate(${properties[1].translateX} ${properties[1].translateY}) rotate(${properties[1].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${properties[2].scale})`}'
                     />
                     <path
                         filter='url(#prefix__filter0_f)'
                         style='mix-blend-mode: overlay;'
                         d='M22.216 24L0 46.75l14.108 38.129L78 86l-3.081-59.276-22.378 4.005 12.972 20.186-23.35 27.395L22.215 24z'
                         fill='#${properties[2].color}'
-                        transform='${
-                            'translate(' +
-                            properties[2].translateX +
-                            ' ' +
-                            properties[2].translateY +
-                            ') rotate(' +
-                            properties[2].rotate +
-                            ' ' +
-                            SIZE / 2 +
-                            ' ' +
-                            SIZE / 2 +
-                            ') scale(' +
-                            properties[2].scale +
-                            ')'
-                        }'
+                        transform='${`translate(${properties[2].translateX} ${properties[2].translateY}) rotate(${properties[2].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${properties[2].scale})`}'
                     />
                 </g>
                 <text x="50%"
@@ -108,7 +80,7 @@ class MarbleAvatar extends LitElement {
 
     private generateColors(key: string, colors: string[]) {
         const numFromName = hashCode(key);
-        const range = colors && colors.length;
+        const range = colors.length;
         return Array.from({ length: ELEMENTS }, (_, i) => ({
             color: getRandomColor(numFromName + i, colors, range),
             translateX: getUnit(numFromName * (i + 1), SIZE / 10, 1),
