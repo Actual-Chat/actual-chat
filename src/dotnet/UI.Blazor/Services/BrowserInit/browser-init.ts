2487cadb9ebb9188ee36204724674d3ec21940f3
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-10 21:19:15 +0300

    chore: refactor TS files for eslint compliance and string literal updates #3534

--- src/dotnet/UI.Blazor/Services/BrowserInit/browser-init.ts
+++ src/dotnet/UI.Blazor/Services/BrowserInit/browser-init.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/dot-notation */
+ 
 import { Connectivity } from 'connectivity';
 import { EventHandlerSet } from 'event-handling';
 import { delayAsync, PromiseSource } from 'promises';
