a5ec679316868379c8599a7142d40982e6415999
Dmitrii Filippov <crui3er@gmail.com>
2026-02-12 00:46:41 +0400

    fix: fixing build  - update Media constructor usage in MediaSerializationTest to match updated parameters

--- tests/Media.UnitTests/MediaSerializationTest.cs
+++ tests/Media.UnitTests/MediaSerializationTest.cs
@@ -1,5 +1,3 @@
-using ActualChat.Media;
-
 namespace ActualChat.Media.UnitTests;
 
 public class MediaSerializationTest(ITestOutputHelper @out) : TestBase(@out)
@@ -11,7 +9,7 @@ public class MediaSerializationTest(ITestOutputHelper @out) : TestBase(@out)
     public void Media_Basic()
     {
         var mediaId = MediaId.New(TestUserId.Value, "local1");
-        var media = new Media(mediaId, "content-1", new PropertyBag());
+        var media = new Media(mediaId, 0, "content-1", new PropertyBag());
 
         var s = media.PassThroughAllSerializers(Out);
         s.Id.Should().Be(media.Id);
