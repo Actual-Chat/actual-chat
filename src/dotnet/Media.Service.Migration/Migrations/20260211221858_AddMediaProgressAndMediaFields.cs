d9d3ab6cb3d848fde521aea11ff2e7672b3a8678
Dmitrii Filippov <crui3er@gmail.com>
2026-02-12 02:20:30 +0400

    feat: add media progress tracking and new fields in Media table
     - Introduced `media_progresses` table for tracking media progress.
     - Added `user_id` and `version` fields to `media` table schema.

---
+++ src/dotnet/Media.Service.Migration/Migrations/20260211221858_AddMediaProgressAndMediaFields.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActualChat.Media.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaProgressAndMediaFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "user_id",
+                table: "media",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                collation: "C");
+
+            migrationBuilder.AddColumn<long>(
+                name: "version",
+                table: "media",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "media_progresses",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false, collation: "C"),
+                    version = table.Column<long>(type: "bigint", nullable: false),
+                    stage = table.Column<int>(type: "integer", nullable: false),
+                    stage_progress = table.Column<double>(type: "double precision", nullable: false),
+                    error_message = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_media_progresses", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "media_progresses");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "media");
+
+            migrationBuilder.DropColumn(
+                name: "version",
+                table: "media");
+        }
+    }
+}
