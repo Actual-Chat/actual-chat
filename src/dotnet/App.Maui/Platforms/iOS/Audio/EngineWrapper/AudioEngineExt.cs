8baefe9493729e0d147db55d17d1eb255a69f228
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-12 13:29:12 +0300

    fix: try resolve iOS AVAudioFile close crash in AudioEngine #3542

--- src/dotnet/App.Maui/Platforms/iOS/Audio/EngineWrapper/AudioEngineExt.cs
+++ src/dotnet/App.Maui/Platforms/iOS/Audio/EngineWrapper/AudioEngineExt.cs
@@ -7,20 +7,22 @@ public static class AudioEngineExt
 {
     private static readonly ConcurrentDictionary<string, NSUrl> Urls = new (StringComparer.OrdinalIgnoreCase);
 
+    private static ILogger Log => field ??= StaticLog.For(typeof(AudioEngineExt));
+
     public static Task PlayResourceFile(this AudioEngine engine, string resourceFileName)
         => BackgroundTask.Run(async () => {
             var url = Urls.GetOrAdd(resourceFileName, GetUrl);
-            using var audioFile = new AVAudioFile(url, out var error);
+            var audioFile = new AVAudioFile(url, out var error);
             error.Assert();
 
+            using var _1 = Disposable.New(audioFile, f => f.Close());
             using var node = engine.NewPlayer(audioFile.ProcessingFormat);
             engine.EnsureRunning();
             node.Play();
             using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
             var cancellationToken = cts.Token;
             await node.ScheduleFileAndWait(audioFile, cancellationToken).ConfigureAwait(false);
-            node.Stop();
-        });
+        }, Log, $"Failed to play resource file '{resourceFileName}'");
 
     private static NSUrl GetUrl(string soundName)
         => NSBundle.MainBundle.GetUrlForResource(soundName, "m4a", "sounds");
