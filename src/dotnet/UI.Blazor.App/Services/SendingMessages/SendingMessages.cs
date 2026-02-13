2095bdb71b214f801addadff0c00af046014dfc6
Dmitrii Filippov <crui3er@gmail.com>
2026-02-13 12:55:51 +0400

    chore: remove FakeSend stub usage from SendingMessages.Send

--- src/dotnet/UI.Blazor.App/Services/SendingMessages/SendingMessages.cs
+++ src/dotnet/UI.Blazor.App/Services/SendingMessages/SendingMessages.cs
@@ -85,15 +85,9 @@ public partial class SendingMessages : UIServiceBase<AppUIHub>, IComputeService,
         return _filesUploadRegistry.Register(upload, releaseUpload);
     }
 
-    private static async Task<ChatEntry?> FakeSend()
-    {
-        await Task.Delay(5000).ConfigureAwait(false);
-        return null;
-    }
-
     public async Task<Task<ChatEntry?>> Send(SendMessageRequest cmd, CancellationToken cancellationToken)
     {
-        return FakeSend();
+        // return FakeSend();
         DebugLog?.LogDebug("Post '{Text}'", cmd.Text);
         var now = Clocks.SystemClock.Now;
         var resultSource = TaskCompletionSourceExt.New<ChatEntry?>();
@@ -519,6 +513,14 @@ public partial class SendingMessages : UIServiceBase<AppUIHub>, IComputeService,
         var cmd1 = new Chats_RemoveTextEntry(Session, chatEntryId.ChatId, chatEntryId.LocalId);
         await Commander.Run(cmd1, cancellationToken).ConfigureAwait(false);
     }
+
+
+    private static async Task<ChatEntry?> FakeSend()
+    {
+        await Task.Delay(5000).ConfigureAwait(false);
+        return null;
+    }
+
     // Nested types
 
     public record PostMessageRequestInternal(string Uuid,
