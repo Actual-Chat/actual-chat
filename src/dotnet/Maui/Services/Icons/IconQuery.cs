9bb74c93d335e3141f8125898e4ca3356579df21
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-17 21:06:16 +0300

    feat(avatars): add support for avatar titles in generation logic #3541

--- src/dotnet/Maui/Services/Icons/IconQuery.cs
+++ src/dotnet/Maui/Services/Icons/IconQuery.cs
@@ -2,7 +2,7 @@ using ActualChat.UI;
 
 namespace ActualChat.Maui.Services;
 
-public record IconQuery(Picture? Picture, AvatarKind AvatarKind, string DefaultAvatarKey, int? AvatarSize = null)
+public record IconQuery(Picture? Picture, AvatarKind AvatarKind, string DefaultAvatarKey, int? AvatarSize = null, string AvatarTitle = "")
 {
     public string AvatarKey => Picture?.AvatarKey.NullIfEmpty() ?? DefaultAvatarKey;
 }
--- src/dotnet/Maui/Services/Icons/IconQueryExt.cs
+++ src/dotnet/Maui/Services/Icons/IconQueryExt.cs
@@ -1,4 +1,3 @@
-using System;
 using ActualChat.Chat;
 using ActualChat.Contacts;
 using ActualChat.Media;
@@ -8,7 +7,7 @@ namespace ActualChat.Maui.Services;
 
 public static class IconQueryExt
 {
-    public static IconQuery GetIconQuery(this Contact contact, int? avatarSize = null)
+    public static IconQuery GetIconQuery(this Contact contact, int? avatarSize = null, bool renderAvatarTitle = false)
     {
         var chatKind = contact.ChatId.IsThread(out var threadChatId)
             ? threadChatId.ParentChatId.Kind
@@ -20,12 +19,23 @@ public static class IconQueryExt
             return new IconQuery(contact.Account?.Avatar.Picture, AvatarKind.Beam, defaultAvatarKey, avatarSize);
         case ChatKind.Group:
         case ChatKind.Place:
-            return new IconQuery(contact.Chat.Picture.ToPicture(), AvatarKind.Marble, contact.ChatId.Value, avatarSize);
+            return new IconQuery(contact.Chat.Picture.ToPicture(),
+                AvatarKind.Marble,
+                contact.ChatId.Value,
+                avatarSize,
+                renderAvatarTitle ? GetInitial(contact.Chat.Title) : "");
         default:
             throw new ArgumentOutOfRangeException();
         }
     }
 
-    public static IconQuery GetIconQuery(this Place place, int? avatarSize = null)
-        => new (place.Picture.ToPicture(), AvatarKind.Marble, place.Id.Value, avatarSize);
+    public static IconQuery GetIconQuery(this Place place, int? avatarSize = null, bool renderAvatarTitle = false)
+        => new (place.Picture.ToPicture(),
+            AvatarKind.Marble,
+            place.Id.Value,
+            avatarSize,
+            renderAvatarTitle ? GetInitial(place.Title) : "");
+
+    private static string GetInitial(string title)
+        => title.Length > 0 ? title[0].ToString().ToUpperInvariant() : "";
 }
