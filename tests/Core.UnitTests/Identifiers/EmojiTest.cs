422b4cd203b8f6773c12557231abadf1a203fe85
Iq Mulator <iqmulator@gmail.com>
2026-02-18 17:36:13 +0500

    fix: emoji, add new emojis

--- tests/Core.UnitTests/Identifiers/EmojiTest.cs
+++ tests/Core.UnitTests/Identifiers/EmojiTest.cs
@@ -6,17 +6,40 @@ public class EmojiTest
     public void Parse()
     {
         Emoji[] emojis = [
-            Emojis.ThumbsUp,
-            Emojis.RedHeart,
+            // Positive
             Emojis.Lol,
-            Emojis.Surprise,
-            Emojis.Sad,
+            Emojis.Awesome,
+            Emojis.Cool,
+            Emojis.Smile,
+            Emojis.Nerd,
+            Emojis.SmileRotated,
+            Emojis.Kiss,
+            // Negative
             Emojis.Angry,
+            Emojis.Sad,
+            Emojis.Cry,
+            Emojis.Melt,
+            Emojis.Devil,
+            Emojis.Clown,
+            // Love
+            Emojis.RedHeart,
+            Emojis.InLove,
+            Emojis.BrokenHeart,
+            Emojis.Please,
+            // Gestures
+            Emojis.ThumbsUp,
+            Emojis.Done,
+            Emojis.Eyes,
             Emojis.Poo,
+            Emojis.Boom,
+            Emojis.Surprise,
+            Emojis.Mysterious,
+            Emojis.Stone,
         ];
 
         foreach (var emoji in emojis) {
-            Emoji.TryParse(emoji.Value, out var emoji2);
+            var parsed = Emoji.TryParse(emoji.Value, out var emoji2);
+            parsed.Should().BeTrue();
             emoji2.Should().Be(emoji);
         }
     }
