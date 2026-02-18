300270f2cf4f88fbc28658c4837f1cd6a10a5231
Iq Mulator <iqmulator@gmail.com>
2026-02-18 17:36:13 +0500

    fix: emoji, add new emojis

---
+++ src/dotnet/Api/Identifiers/EmojiGroup.cs
@@ -0,0 +1,16 @@
+namespace ActualChat;
+
+/// <summary>
+/// Defines groups for categorizing emojis.
+/// </summary>
+public enum EmojiGroup
+{
+    /// <summary>Positive face emojis - laughter, smirks, happy.</summary>
+    Positive,
+    /// <summary>Negative face emojis - anger, sadness, devil, clown.</summary>
+    Negative,
+    /// <summary>Hearts and love emojis.</summary>
+    Love,
+    /// <summary>Gestures and symbols - thumbs up, checkmark, eyes, poo.</summary>
+    Gestures,
+}
