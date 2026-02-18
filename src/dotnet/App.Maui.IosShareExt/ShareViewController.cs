c95f9015a7814d5a25829aa172fdf6bb118a7c34
Frol <4425748+frolyo@users.noreply.github.com>
2026-02-18 18:16:26 +0300

    chore(ios): suppress CA1010 warning in ShareViewController #3551

--- src/dotnet/App.Maui.IosShareExt/ShareViewController.cs
+++ src/dotnet/App.Maui.IosShareExt/ShareViewController.cs
@@ -4,6 +4,7 @@ using ActualChat.App.Maui.IosShareExt.UI.Fusion.Ios;
 namespace ActualChat.App.Maui.IosShareExt;
 
 [Register("ShareViewController")]
+[SuppressMessage("Design", "CA1010:Generic interface should also be implemented")]
 public class ShareViewController : UIViewController
 {
     private ShareExtensionApplication? _app;
