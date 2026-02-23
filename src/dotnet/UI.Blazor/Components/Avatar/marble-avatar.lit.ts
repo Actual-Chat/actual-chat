364587c920c0a95ec9b1476304fb820c9473b7ed
Alex Yakunin <alex.yakunin@gmail.com>
2026-02-23 11:25:42 -0800

    wip: this change boosts re-renders of SVG avatars w/ weird BG on Android (i.e., makes the issue much more visible)

--- src/dotnet/UI.Blazor/Components/Avatar/marble-avatar.lit.ts
+++ src/dotnet/UI.Blazor/Components/Avatar/marble-avatar.lit.ts
@@ -10,12 +10,18 @@ let id = 0;
 
 @customElement('marble-avatar')
 export class MarbleAvatar extends LitElement {
+    protected createRenderRoot() {
+        return this; // Use light DOM to avoid Shadow DOM compositing layers on Android WebView
+    }
+
     @property() key: string;
     @property() title: string;
     @property({ type: Boolean }) doNotBlur = false;
     @property() colors: string[] = ['F56095', 'F5CD65', '00B27D', '37D3F5', '2F89EB'];
 
-    private maskId = `marble-avatar-${++id}`;
+    private instanceId = ++id;
+    private maskId = `marble-avatar-${this.instanceId}`;
+    private filterId = `marble-filter-${this.instanceId}`;
 
     render() {
         const properties = this.generateColors(this.key, this.colors);
@@ -41,13 +47,13 @@ export class MarbleAvatar extends LitElement {
                 <g mask='url(#${this.maskId})'>
                     <rect width='${SIZE}' height='${SIZE}' fill='#${properties[0].color}' />
                     <path
-                        filter='url(#prefix__filter0_f)'
+                        filter='url(#${this.filterId})'
                         d='M32.414 59.35L50.376 70.5H72.5v-71H33.728L26.5 13.381l19.057 27.08L32.414 59.35z'
                         fill='#${properties[1].color}'
                         transform='${`translate(${properties[1].translateX} ${properties[1].translateY}) rotate(${properties[1].rotate} ${SIZE / 2} ${SIZE / 2}) scale(${properties[2].scale})`}'
                     />
                     <path
-                        filter='url(#prefix__filter0_f)'
+                        filter='url(#${this.filterId})'
                         style='mix-blend-mode: overlay;'
                         d='M22.216 24L0 46.75l14.108 38.129L78 86l-3.081-59.276-22.378 4.005 12.972 20.186-23.35 27.395L22.215 24z'
                         fill='#${properties[2].color}'
@@ -65,7 +71,7 @@ export class MarbleAvatar extends LitElement {
                 </text>
                 <defs>
                     <filter
-                        id='prefix__filter0_f'
+                        id='${this.filterId}'
                         filterUnits='userSpaceOnUse'
                         color-interpolation-filters='sRGB'
                     >
