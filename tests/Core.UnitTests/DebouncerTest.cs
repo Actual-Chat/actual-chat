77af038fac18e16b6077ac4f6bf4c3665980b307
Alex Yakunin <alex.yakunin@gmail.com>
2026-02-28 18:36:44 -0800

    test: fix `DebouncerTest` by using TestClock there
    Updated debounce delays and clock offsets to larger values to test long-duration debounce behavior.

--- tests/Core.UnitTests/DebouncerTest.cs
+++ tests/Core.UnitTests/DebouncerTest.cs
@@ -12,7 +12,7 @@ public class DebouncerTest(ITestOutputHelper @out) : TestBase(@out)
         var sw = Stopwatch.StartNew();
         Action<string> log = msg => WriteLine(sw.Elapsed.ToString("c") + " " + msg);
         WriteLine($"start: {sw.Elapsed}");
-        var debouncer = Debouncer.New<int>(clock, TimeSpan.FromMilliseconds(2000), i => {
+        var debouncer = Debouncer.New<int>(clock, TimeSpan.FromMilliseconds(100_000), i => {
             log($"debounce invoked, value ='{i}'");
             lock (results)
                 results.Add(i);
@@ -31,8 +31,8 @@ public class DebouncerTest(ITestOutputHelper @out) : TestBase(@out)
         log.Invoke("after delay 1");
         debouncer.Debounce(4);
         log.Invoke("after debounce 4");
-        clock.OffsetBy(3000);
-        log.Invoke("after OffsetBy(3000)");
+        clock.OffsetBy(200_000);
+        log.Invoke("after OffsetBy(200_000)");
 
         await debouncer.WhenCompleted();
         log.Invoke("after WhenCompleted");
@@ -49,7 +49,7 @@ public class DebouncerTest(ITestOutputHelper @out) : TestBase(@out)
         clock.OffsetBy(100);
         await Task.Delay(300); // Just to make sure async ops complete
         debouncer.Debounce(4, true);
-        clock.OffsetBy(2000);
+        clock.OffsetBy(200_000);
 
         await debouncer.WhenCompleted();
         results.Count.Should().Be(1);
@@ -61,10 +61,10 @@ public class DebouncerTest(ITestOutputHelper @out) : TestBase(@out)
         debouncer.Debounce(2, true);
         clock.OffsetBy(100);
         debouncer.Debounce(3, true);
-        clock.OffsetBy(2000);
+        clock.OffsetBy(200_000);
         await Task.Delay(300); // Just to make sure async ops complete
         debouncer.Debounce(4, true);
-        clock.OffsetBy(2000);
+        clock.OffsetBy(200_000);
 
         await debouncer.WhenCompleted();
         results.Count.Should().Be(2);
