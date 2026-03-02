bf932b38615a26f023f418d9413983127c8bfac5
Frol <4425748+frolyo@users.noreply.github.com>
2026-03-02 17:53:02 +0300

    fix: update transcoded path check to use IsEmpty in UploadSession #3457

--- src/dotnet/UI.Blazor.App/Services/FileUploads/UploadSession.cs
+++ src/dotnet/UI.Blazor.App/Services/FileUploads/UploadSession.cs
@@ -171,7 +171,7 @@ public class UploadSession
                     .TranscodeIfNeeded(filePath, progress, cancellationToken)
                     .ConfigureAwait(false);
 
-                if (transcodedPath is not null)
+                if (!transcodedPath.IsEmpty)
                     await UpdateState(s => s with { TranscodedFilePath = transcodedPath.Value },
                             cancellationToken: cancellationToken)
                         .ConfigureAwait(false);
