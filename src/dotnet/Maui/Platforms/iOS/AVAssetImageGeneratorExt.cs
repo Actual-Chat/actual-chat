8320e67d6092c4f589f077a4fca39e59846b30d9
Frol <4425748+frolyo@users.noreply.github.com>
2026-03-04 15:07:55 +0300

    refactor: add cancellation support for MOV thumbnail generation on iOS #3611

--- src/dotnet/Maui/Platforms/iOS/AVAssetImageGeneratorExt.cs
+++ src/dotnet/Maui/Platforms/iOS/AVAssetImageGeneratorExt.cs
@@ -4,14 +4,22 @@ namespace ActualChat.Maui;
 
 public static class AVAssetImageGeneratorExt
 {
-    public static Task<CGImage?> GenerateCGImage(this AVAssetImageGenerator generator, TimeSpan time)
+    public static Task<CGImage?> GenerateCGImage(
+        this AVAssetImageGenerator generator,
+        TimeSpan time,
+        CancellationToken cancellationToken = default)
     {
         var tcs = new AsyncTaskMethodBuilder<CGImage?>();
+        var ctr = cancellationToken.Register(() => {
+            generator.CancelAllCGImageGeneration();
+            tcs.TrySetCanceled(cancellationToken);
+        });
         generator.GenerateCGImageAsynchronously(time.ToCMTime(), (image, _, error) => {
             if (error is null)
                 tcs.TrySetResult(image);
             else
                 tcs.TrySetException(error.ToException());
+            ctr.DisposeSilently();
         });
         return tcs.Task;
     }
