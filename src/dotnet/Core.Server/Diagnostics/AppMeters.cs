db1fa01d7f11b60d36c4b68fbf188a76f4ca7175
Alexey Kochetov <alexis.kochetov@gmail.com>
2026-03-04 13:28:11 +0300

    chore: rename metrics to check ingestion

--- src/dotnet/Core.Server/Diagnostics/AppMeters.cs
+++ src/dotnet/Core.Server/Diagnostics/AppMeters.cs
@@ -14,10 +14,10 @@ public static class AppMeters
     static AppMeters()
     {
         var m = AppInstruments.Meter;
-        AudioLatency = m.CreateHistogram<double>("AudioLatency", "ms", "Real-time audio recording to playback latency");
-        VideoLatency = m.CreateHistogram<double>("VideoLatency", "ms", "Video streaming latency");
-        AudioStreamCount = m.CreateUpDownCounter<int>("AudioStreamCount", null, "Audio stream count");
-        VideoStreamCount = m.CreateUpDownCounter<int>("VideoStreamCount", null, "Video stream count");
-        MessageCount = m.CreateCounter<long>("MessageCount", null, "Chat message count");
+        AudioLatency = m.CreateHistogram<double>("app.audio.latency", "ms", "Real-time audio recording to playback latency");
+        VideoLatency = m.CreateHistogram<double>("app.video.latency", "ms", "Video streaming latency");
+        AudioStreamCount = m.CreateUpDownCounter<int>("app.audio.stream.count", null, "Audio stream count");
+        VideoStreamCount = m.CreateUpDownCounter<int>("app.video.stream.count", null, "Video stream count");
+        MessageCount = m.CreateCounter<long>("app.message.count", null, "Chat message count");
     }
 }
