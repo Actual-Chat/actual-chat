817e843b066feb95d0a484ed6254178e6ad35a9f
Frol <4425748+frolyo@users.noreply.github.com>
2026-03-11 12:34:57 +0300

    test(avatars): standardize unit test formatting and comments #3498

--- tests/Users.UnitTests/AvatarSvgGenerationTest.cs
+++ tests/Users.UnitTests/AvatarSvgGenerationTest.cs
@@ -1,36 +1,29 @@
-using ActualChat.Users;
 using ActualChat.Users.AvatarIcons;
 
-namespace ActualChat.Testing.Users;
+namespace ActualChat.Users.UnitTests;
 
 public class AvatarSvgGenerationTest
 {
     [Fact]
-    public void BeamAvatar_GeneratesSvg_WithConsistentOutput()
+    public void BeamAvatarShouldGenerateConsistentSvg()
     {
-        // Arrange
         var key = "testuser123";
 
-        // Act
         var svg1 = BeamAvatars.GenerateSvg(key);
         var svg2 = BeamAvatars.GenerateSvg(key);
 
-        // Assert
         svg1.Should().NotBeNullOrEmpty();
         svg2.Should().NotBeNullOrEmpty();
         svg1.Should().Be(svg2, "same key should produce identical SVG");
     }
 
     [Fact]
-    public void BeamAvatar_GeneratesSvg_WithValidXml()
+    public void BeamAvatarShouldGenerateValidXml()
     {
-        // Arrange
         var key = "testuser456";
 
-        // Act
         var svg = BeamAvatars.GenerateSvg(key);
 
-        // Assert
         svg.Should().StartWith("<svg");
         svg.Should().EndWith("</svg>");
         svg.Should().Contain("xmlns='http://www.w3.org/2000/svg'");
@@ -38,15 +31,12 @@ public class AvatarSvgGenerationTest
     }
 
     [Fact]
-    public void BeamAvatar_GeneratesSvg_ContainsExpectedElements()
+    public void BeamAvatarShouldContainExpectedElements()
     {
-        // Arrange
         var key = "testuser789";
 
-        // Act
         var svg = BeamAvatars.GenerateSvg(key);
 
-        // Assert  - should contain mask, background, wrapper, and face elements
         svg.Should().Contain("<mask id='m'");
         svg.Should().Contain("<g mask='url(#m)'>");
         svg.Should().Contain("fill='#");  // Should have colored fills
@@ -55,61 +45,49 @@ public class AvatarSvgGenerationTest
     }
 
     [Fact]
-    public void BeamAvatar_GeneratesSvg_DifferentKeysProduceDifferentOutput()
+    public void BeamAvatarShouldProduceDifferentOutputForDifferentKeys()
     {
-        // Arrange
         var key1 = "user1";
         var key2 = "user2";
 
-        // Act
         var svg1 = BeamAvatars.GenerateSvg(key1);
         var svg2 = BeamAvatars.GenerateSvg(key2);
 
-        // Assert
         svg1.Should().NotBe(svg2, "different keys should produce different avatars");
     }
 
     [Fact]
-    public void BeamAvatar_GeneratesSvg_SquareParameter()
+    public void BeamAvatarShouldRespectSquareParameter()
     {
-        // Arrange
         var key = "squaretest";
 
-        // Act
         var svgRound = BeamAvatars.GenerateSvg(key, square: false);
         var svgSquare = BeamAvatars.GenerateSvg(key, square: true);
 
-        // Assert
         svgRound.Should().Contain("rx='72'", "round avatar should have border radius");
         svgSquare.Should().NotContain("rx='72'", "square avatar should not have border radius");
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_WithConsistentOutput()
+    public void MarbleAvatarShouldGenerateConsistentSvg()
     {
-        // Arrange
         var key = "marbleuser123";
 
-        // Act
         var svg1 = MarbleAvatars.GenerateSvg(key);
         var svg2 = MarbleAvatars.GenerateSvg(key);
 
-        // Assert
         svg1.Should().NotBeNullOrEmpty();
         svg2.Should().NotBeNullOrEmpty();
         svg1.Should().Be(svg2, "same key should produce identical SVG");
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_WithValidXml()
+    public void MarbleAvatarShouldGenerateValidXml()
     {
-        // Arrange
         var key = "marbleuser456";
 
-        // Act
         var svg = MarbleAvatars.GenerateSvg(key);
 
-        // Assert
         svg.Should().StartWith("<svg");
         svg.Should().EndWith("</svg>");
         svg.Should().Contain("xmlns='http://www.w3.org/2000/svg'");
@@ -117,15 +95,12 @@ public class AvatarSvgGenerationTest
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_ContainsExpectedElements()
+    public void MarbleAvatarShouldContainExpectedElements()
     {
-        // Arrange
         var key = "marbleuser789";
 
-        // Act
         var svg = MarbleAvatars.GenerateSvg(key);
 
-        // Assert - should contain mask, paths, filter, and defs
         svg.Should().Contain("<mask id='m'");
         svg.Should().Contain("<path");
         svg.Should().Contain("<filter id='f'");
@@ -134,61 +109,49 @@ public class AvatarSvgGenerationTest
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_WithTitle()
+    public void MarbleAvatarShouldRenderTitle()
     {
-        // Arrange
         var key = "titletest";
         var title = "A";
 
-        // Act
         var svg = MarbleAvatars.GenerateSvg(key, title: title);
 
-        // Assert
         svg.Should().Contain("<text");
         svg.Should().Contain(">A</text>", "should display uppercase first letter of title");
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_WithoutTitleWhenEmpty()
+    public void MarbleAvatarShouldRenderEmptyTextWhenTitleIsEmpty()
     {
-        // Arrange
         var key = "notitle";
 
-        // Act
         var svg = MarbleAvatars.GenerateSvg(key, title: "");
 
-        // Assert
         svg.Should().Contain("<text");
         svg.Should().Contain("></text>", "should have empty text element when no title");
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_DoNotBlurParameter()
+    public void MarbleAvatarShouldRespectDoNotBlurParameter()
     {
-        // Arrange
         var key = "blurtest";
 
-        // Act
         var svgWithBlur = MarbleAvatars.GenerateSvg(key, doNotBlur: false);
         var svgWithoutBlur = MarbleAvatars.GenerateSvg(key, doNotBlur: true);
 
-        // Assert
         svgWithBlur.Should().Contain("feGaussianBlur", "should have blur effect by default");
         svgWithoutBlur.Should().NotContain("feGaussianBlur", "should not have blur when doNotBlur is true");
     }
 
     [Fact]
-    public void MarbleAvatar_GeneratesSvg_DifferentKeysProduceDifferentOutput()
+    public void MarbleAvatarShouldProduceDifferentOutputForDifferentKeys()
     {
-        // Arrange
         var key1 = "marble1";
         var key2 = "marble2";
 
-        // Act
         var svg1 = MarbleAvatars.GenerateSvg(key1);
         var svg2 = MarbleAvatars.GenerateSvg(key2);
 
-        // Assert
         svg1.Should().NotBe(svg2, "different keys should produce different avatars");
     }
 }
