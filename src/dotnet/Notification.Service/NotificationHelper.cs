a83a3c9a03e1dcf020800ccab2bf426dd2294b03
Alexey Kochetov <alexis.kochetov@gmail.com>
2026-03-19 15:03:20 +0300

    feat: refactor GetIconUrl method to simplify icon URL retrieval for chats

--- src/dotnet/Notification.Service/NotificationHelper.cs
+++ src/dotnet/Notification.Service/NotificationHelper.cs
@@ -12,15 +12,10 @@ public static class NotificationHelper
         };
 
     public static string GetIconUrl(Chat.Chat chat, AuthorFull author, UrlMapper urlMapper)
-        => chat.Kind switch {
-            ChatKind.Group or ChatKind.Place or ChatKind.Thread => chat.Picture?.BlobId.IsNullOrEmpty() == false
-                ? urlMapper.ContentUrl(chat.Picture.BlobId)
-                : "/favicon_voxt.ico",
-            ChatKind.Peer => author.Avatar.Media?.BlobId.IsNullOrEmpty() == false
-                ? urlMapper.ContentUrl(author.Avatar.Media.BlobId)
-                : "/favicon_voxt.ico",
-            _ => throw new ArgumentOutOfRangeException($"{nameof(chat)}.{nameof(chat.Kind)}", chat.Kind, null),
-        };
+    {
+        var query = chat.GetIconQuery(author);
+        return urlMapper.IconUrl(query);
+    }
 
     public static async ValueTask<(string Content, HashSet<MentionId> MentionIds)> GetText(
         ChatEntry entry,
