d8ad26e770a120e17a54e9df144b7bdf5c1bd11e
Alexey Kochetov <alexis.kochetov@gmail.com>
2026-03-26 17:58:21 +0300

    chore: interface formatting

--- src/dotnet/Api.Contracts/Streaming/ILiveAudioStreams.cs
+++ src/dotnet/Api.Contracts/Streaming/ILiveAudioStreams.cs
@@ -4,19 +4,30 @@ using ActualLab.Rpc;
 namespace ActualChat.Streaming;
 
 /// <summary>
-/// RPC service for multiplexed real-time and replay audio streaming.
+///     RPC service for multiplexed real-time and replay audio streaming.
 /// </summary>
 public interface ILiveAudioStreams : IComputeService
 {
     [ComputeMethod]
-    Task<ApiArray<LiveStreamInfo>> List(
-        Session session, ChatId chatId, CancellationToken cancellationToken);
+    Task<ApiArray<LiveStreamInfo>> List(Session session, ChatId chatId, CancellationToken cancellationToken);
 
     Task<RpcStream<LiveStreamItem>> GetStream(
-        Session session, ChatId chatId, LiveStreamSettings settings, CancellationToken cancellationToken);
+        Session session,
+        ChatId chatId,
+        LiveStreamSettings settings,
+        CancellationToken cancellationToken);
+
     Task ChangeSettings(
-        Session session, ChatId chatId, LiveStreamSettings settings, CancellationToken cancellationToken);
+        Session session,
+        ChatId chatId,
+        LiveStreamSettings settings,
+        CancellationToken cancellationToken);
 
     Task<RpcStream<LiveStreamItem>> GetReplayStream(
-        Session session, ChatId chatId, Moment startAt, TimeSpan rewindOffset, double speed, CancellationToken cancellationToken);
+        Session session,
+        ChatId chatId,
+        Moment startAt,
+        TimeSpan rewindOffset,
+        double speed,
+        CancellationToken cancellationToken);
 }
--- src/dotnet/Api.Contracts/Streaming/ILiveVideoStreams.cs
+++ src/dotnet/Api.Contracts/Streaming/ILiveVideoStreams.cs
@@ -5,17 +5,29 @@ namespace ActualChat.Streaming;
 
 public interface ILiveVideoStreams : IComputeService
 {
-    Task<RpcStream<VideoFrame>?> GetStream(Session session, StreamId streamId, TimeSpan skipTo, CancellationToken cancellationToken);
+    Task<RpcStream<VideoFrame>?> GetStream(
+        Session session,
+        StreamId streamId,
+        TimeSpan skipTo,
+        CancellationToken cancellationToken);
 
     [ComputeMethod]
     Task<ApiArray<VideoStreamInfo>> List(Session session, ChatId chatId, CancellationToken cancellationToken);
+
     [ComputeMethod]
     Task<int> GetMemberCount(Session session, ChatId chatId, CancellationToken cancellationToken);
+
     [ComputeMethod]
     Task<ApiArray<string>> GetSupportedCodecs(Session session, ChatId chatId, CancellationToken cancellationToken);
+
     [ComputeMethod]
     Task<VideoQualityPreset> GetQualityPreset(Session session, StreamId streamId, CancellationToken cancellationToken);
 
-    Task RegisterMember(Session session, ChatId chatId, ApiArray<string> supportedDecoderCodecs, CancellationToken cancellationToken);
+    Task RegisterMember(
+        Session session,
+        ChatId chatId,
+        ApiArray<string> supportedDecoderCodecs,
+        CancellationToken cancellationToken);
+
     Task UnregisterMember(Session session, ChatId chatId, CancellationToken cancellationToken);
 }
