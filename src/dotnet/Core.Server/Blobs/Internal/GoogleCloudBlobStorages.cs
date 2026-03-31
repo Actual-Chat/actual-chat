232a46773e69666ba566305b977bab2d939b7f3d
Dmitrii Filippov <crui3er@gmail.com>
2026-03-31 12:26:00 +0400

    feat: speed up large media displaying in viewer by using SeekableGcsStream for efficient ranged GCS reads #3689

--- src/dotnet/Core.Server/Blobs/Internal/GoogleCloudBlobStorages.cs
+++ src/dotnet/Core.Server/Blobs/Internal/GoogleCloudBlobStorages.cs
@@ -1,13 +1,9 @@
-using Microsoft.IO;
-
 namespace ActualChat.Blobs.Internal;
 
 public class GoogleCloudBlobStorages(string blobBucketName) : IBlobStorages
 {
-    private readonly RecyclableMemoryStreamManager _streamManager = new ();
-
     public string BucketName => blobBucketName;
 
     public IBlobStorage this[Symbol blobScope]
-        => new GoogleCloudBlobStorage(blobBucketName, _streamManager);
+        => new GoogleCloudBlobStorage(blobBucketName);
 }
