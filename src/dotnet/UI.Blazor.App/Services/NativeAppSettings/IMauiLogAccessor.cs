64ae171c15f9ed62bf0fea8d2d55625421ab703b
Dmitrii Filippov <crui3er@gmail.com>
2026-03-31 17:39:06 +0400

    feat: improve information about saving log file

--- src/dotnet/UI.Blazor.App/Services/NativeAppSettings/IMauiLogAccessor.cs
+++ src/dotnet/UI.Blazor.App/Services/NativeAppSettings/IMauiLogAccessor.cs
@@ -3,5 +3,5 @@ namespace ActualChat.UI.Blazor.App.Services;
 public interface IMauiLogAccessor
 {
     string ActionName { get; }
-    Func<Task<bool>>? GetLogFile { get; }
+    Func<Task>? GetLogFile { get; }
 }
