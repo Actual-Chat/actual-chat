5e6a71abad6d31d4feb2c1635401fa2f26b8584d
Frol <4425748+frolyo@users.noreply.github.com>
2026-04-02 23:25:49 +0300

    refactor: unify image processing helper usage in upload processors #3680

--- src/dotnet/Core.Server/Uploads/UploadProcessorHelper.cs
+++ src/dotnet/Core.Server/Uploads/UploadProcessorHelper.cs
@@ -3,6 +3,8 @@ using ActualLab.IO;
 using FFMpegCore;
 using MediaFormat = FFMpegCore.MediaFormat;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
 
 namespace ActualChat.Uploads;
 
@@ -84,6 +86,44 @@ public static class UploadProcessorHelper
         await source.CopyToAsync(target, cancellationToken).ConfigureAwait(false);
         target.Position = 0;
         return new UploadedTempFile(file.FileName, file.ContentType, tempFilePath);
+    }
+
+    public static async Task<ProcessedFile> ProcessRasterImage(
+        UploadedFile upload,
+        int maxSize,
+        bool convertToPng = false,
+        IProgress<double>? progress = null,
+        CancellationToken cancellationToken = default)
+    {
+        var inputStream = await upload.Open().ConfigureAwait(false);
+        await using var _ = inputStream.ConfigureAwait(false);
+
+        using var image = await Image.LoadAsync(inputStream, cancellationToken).ConfigureAwait(false);
+        progress?.Report(30);
+
+        var resizeRequired = image.Width > maxSize || image.Height > maxSize;
+        var mustProcess = convertToPng || image.Metadata.ExifProfile != null || resizeRequired;
+        if (!mustProcess)
+            return new ProcessedFile(upload, image.Size);
+
+        image.Mutate(img => {
+            img.AutoOrient();
+            if (resizeRequired)
+                img.Resize(new ResizeOptions { Mode = ResizeMode.Max, Size = new Size(maxSize) });
+        });
+        image.Metadata.ExifProfile = null;
+        var imageSize = image.Size;
+        progress?.Report(60);
+
+        var outPath = (FilePath.GetApplicationTempDirectory() & upload.FileName).ToUnique(randomLength: 10);
+        if (convertToPng)
+            outPath = outPath.ChangeExtension(".png");
+        var outStream = File.OpenWrite(outPath);
+        await using var _1 = outStream.ConfigureAwait(false);
+        var format = convertToPng ? PngFormat.Instance : image.Metadata.DecodedImageFormat!;
+        await image.SaveAsync(outStream, format, cancellationToken).ConfigureAwait(false);
+        var contentType = convertToPng ? "image/png" : upload.ContentType;
+        return new ProcessedFile(new UploadedTempFile(outPath.FileName, contentType, outPath), imageSize);
     }
 
     // Private helpers
