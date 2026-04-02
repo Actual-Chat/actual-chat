f85cda69bfc82fe3c073829e81f022832928d5e5
Frol <4425748+frolyo@users.noreply.github.com>
2026-04-02 22:06:21 +0300

    refactor(tests): consolidate test image creation helpers in TestImages #3680

--- tests/Testing/TestImages/TestImages.cs
+++ tests/Testing/TestImages/TestImages.cs
@@ -1,4 +1,10 @@
 using ActualChat.Uploads;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace ActualChat.Testing;
 
@@ -17,4 +23,39 @@ public static class TestImages
         var stream = GetImage(name);
         return new UploadedStreamFile(name, "image/jpeg", stream.Length, () => Task.FromResult(stream));
     }
+
+    public static byte[] CreatePng(int width, int height)
+    {
+        using var image = new Image<Rgba32>(width, height);
+        using var ms = new MemoryStream();
+        image.Save(ms, new PngEncoder());
+        return ms.ToArray();
+    }
+
+    public static byte[] CreateJpeg(int width, int height)
+    {
+        using var image = new Image<Rgba32>(width, height);
+        using var ms = new MemoryStream();
+        image.Save(ms, new JpegEncoder());
+        return ms.ToArray();
+    }
+
+    public static byte[] CreateWebp(int width, int height)
+    {
+        using var image = new Image<Rgba32>(width, height);
+        using var ms = new MemoryStream();
+        image.Save(ms, new WebpEncoder());
+        return ms.ToArray();
+    }
+
+    public static byte[] CreateBmp(int width, int height)
+    {
+        using var image = new Image<Rgba32>(width, height);
+        using var ms = new MemoryStream();
+        image.Save(ms, new BmpEncoder());
+        return ms.ToArray();
+    }
+
+    public static UploadedStreamFile CreateUploadedFile(string fileName, string contentType, byte[] data)
+        => new(fileName, contentType, data.Length, () => Task.FromResult<Stream>(new MemoryStream(data)));
 }
