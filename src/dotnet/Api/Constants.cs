be43a8ab09df3a8246ac0392316ae32dca629441
Frol <4425748+frolyo@users.noreply.github.com>
2026-04-06 18:36:18 +0300

    refactor: replace hardcoded max size with Constants.MaxIconSize #3680

--- src/dotnet/Api/Constants.cs
+++ src/dotnet/Api/Constants.cs
@@ -171,6 +171,7 @@ public static partial class Constants
         public const int MaxThumbnailHeight = 36; // In pixels
         public const int MaxAvatarWidth = 720;
         public const int MaxAvatarHeight = 720;
+        public const int MaxIconSize = 1024;
         public static readonly Vector2 MaxResolution = new(MaxImageWidth, MaxImageHeight);
         public static readonly Vector2 MaxActualResolution = MaxResolution * 2;
         public static readonly Vector2 MaxThumbnailResolution = new(MaxThumbnailWidth, MaxThumbnailHeight);
