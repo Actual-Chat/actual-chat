b58e0333ce83b4f294be693e8fff8d997d16f27d
Dmitrii Filippov <crui3er@gmail.com>
2026-04-09 14:17:40 +0400

    chore: remove unused IsAnonymousChatEnabled method from FeaturesExt

--- src/dotnet/UI.Blazor/Services/Features/FeaturesExt.cs
+++ src/dotnet/UI.Blazor/Services/Features/FeaturesExt.cs
@@ -7,11 +7,6 @@ public static class FeaturesExt
         CancellationToken cancellationToken = default)
         => features.Get<Features_EnableIncompleteUI>(cancellationToken);
 
-    public static ValueTask<bool> IsAnonymousChatEnabled(
-        this IFeatures features,
-        CancellationToken cancellationToken = default)
-        => features.Get<Features_EnableAnonymousChat>(cancellationToken);
-
     public static ValueTask<bool> IsVideoStreamingEnabled(
         this IFeatures features,
         CancellationToken cancellationToken = default)
