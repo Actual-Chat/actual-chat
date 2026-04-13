67fc23837a630456af702d1dff9a07e9ec544e44
Dmitrii Filippov <crui3er@gmail.com>
2026-04-13 12:17:50 +0400

    feat: do not block stop listening request while video panel is showing

--- src/dotnet/UI.Blazor.App/Services/ChatAudioUI.cs
+++ src/dotnet/UI.Blazor.App/Services/ChatAudioUI.cs
@@ -108,13 +108,10 @@ public partial class ChatAudioUI : UIWorkerBase<AppUIHub>, IComputeService, INot
     public virtual Task<ImmutableHashSet<ChatId>> GetListeningChatIds()
         => Task.FromResult(ActiveChatsUI.ActiveChats.Value.Where(c => c.IsListening).Select(c => c.ChatId).ToImmutableHashSet());
 
-    public async ValueTask SetListeningState(ChatId chatId, bool mustListen)
+    public ValueTask SetListeningState(ChatId chatId, bool mustListen)
     {
-        // Don't stop listening while watching video in this chat
-        if (!mustListen && await Hub.ChatVideoUI.GetWatchingChatId().ConfigureAwait(false) == chatId)
-            return;
         var now = CpuNow;
-        await ActiveChatsUI.UpdateActiveChats(activeChats => {
+        return ActiveChatsUI.UpdateActiveChats(activeChats => {
             if (activeChats.TryGetValue(chatId, out var chat)) {
                 if (chat.IsListening == mustListen)
                     return activeChats;
@@ -132,15 +129,12 @@ public partial class ChatAudioUI : UIWorkerBase<AppUIHub>, IComputeService, INot
         });
     }
 
-    public async ValueTask ClearListeningChats()
-    {
-        var videoWatchingChatId = await Hub.ChatVideoUI.GetWatchingChatId().ConfigureAwait(false);
-        await ActiveChatsUI.UpdateActiveChats(activeChats => {
+    public ValueTask ClearListeningChats()
+        => ActiveChatsUI.UpdateActiveChats(activeChats => {
             var newActiveChats = new List<ActiveChat>(activeChats.Length);
             var isUpdated = false;
             foreach (var chat in activeChats) {
-                // Don't stop listening in chats with active video watching
-                if (chat.IsListening && chat.ChatId != videoWatchingChatId) {
+                if (chat.IsListening) {
                     newActiveChats.Add(chat with { IsListening = false });
                     isUpdated = true;
                 }
@@ -149,7 +143,6 @@ public partial class ChatAudioUI : UIWorkerBase<AppUIHub>, IComputeService, INot
             }
             return isUpdated ? newActiveChats.ToArray() : activeChats;
         });
-    }
 
     [ComputeMethod] // Synced
     public virtual Task<ChatId?> GetRecordingChatId()
