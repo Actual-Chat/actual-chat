325247c62ee5382ae01d8b6eb8bf8a6beb082586
Alex Yakunin <alex.yakunin@gmail.com>
2026-04-17 10:26:32 -0700

    chore: remove `UserPresence` and `CheckIn` classes from Users.Service (they are unused after last commit)

--- src/dotnet/Users.Service/Internal/CheckIn.cs
+++
@@ -1,7 +0,0 @@
-namespace ActualChat.Users.Internal;
-
-public record CheckIn(Moment At, Moment? LastActiveAt)
-{
-    public CheckIn(Moment at, bool isActive, CheckIn? prev) : this(at, isActive ? at : prev?.LastActiveAt)
-    { }
-}
