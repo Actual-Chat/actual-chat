aabc52a97626b7b5b763b5d6d847c98bc1dc1f41
Alex Yakunin <alex.yakunin@gmail.com>
2026-04-29 21:17:04 -0700

    fix: ensure `server.log` is deleted before starting the server in development mode

--- server-run-logging.cmd
+++ server-run-logging.cmd
@@ -7,6 +7,7 @@ if not exist tmp mkdir tmp
 set ActualChat_DevLog=%CD%\tmp\server.log
 set ASPNETCORE_ENVIRONMENT=Development
 
+if exist "%ActualChat_DevLog%" del "%ActualChat_DevLog%"
 dotnet run --configuration Debug --no-launch-profile --project src/dotnet/App.Server/App.Server.csproj
 
 popd
