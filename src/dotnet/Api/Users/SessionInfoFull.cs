d5b1763ab2c355c2edbd7cb845d1e897cb68f6ea
Alex Yakunin <alex.yakunin@gmail.com>
2026-05-02 05:34:26 -0700

    fix: handle null `Session` in `SessionInfoFull` constructor
    - Updated `SessionInfoFull` constructor to handle potential `null` `Session` values by using a conditional check for `Session.IdPrefix`.

--- src/dotnet/Api/Users/SessionInfoFull.cs
+++ src/dotnet/Api/Users/SessionInfoFull.cs
@@ -9,7 +9,7 @@ namespace ActualChat.Users;
 [method: JsonConstructor, Newtonsoft.Json.JsonConstructor, MemoryPackConstructor]
 public partial record SessionInfoFull(
     [property: DataMember(Order = 10), MemoryPackOrder(10), Key(10)] Session Session
-    ) : SessionInfo(Session.IdPrefix), IRequirementTarget, IHasVersion<long>
+    ) : SessionInfo(ReferenceEquals(Session, null) ? "" : Session.IdPrefix), IRequirementTarget, IHasVersion<long>
 {
     [DataMember(Order = 11), MemoryPackOrder(11), Key(11)] public ImmutableOptionSet Options { get; init; }
 
