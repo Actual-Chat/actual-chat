4f9bb8d25fb496a8882b242eab1bbda683526304
Frol <4425748+frolyo@users.noreply.github.com>
2026-05-14 19:01:02 +0300

    feat(ui): add LogUI.DumpToTempFile for exporting logs #3730

--- src/dotnet/UI.Blazor/Services/LogUI/LogUIExt.cs
+++ src/dotnet/UI.Blazor/Services/LogUI/LogUIExt.cs
@@ -1,7 +1,25 @@
+using ActualLab.IO;
+
 namespace ActualChat.UI.Blazor.Services;
 
 public static class LogUIExt
 {
+    public static async Task<FilePath> DumpToTempFile(this LogUI logUI, CancellationToken cancellationToken)
+    {
+        var fileName = $"{MomentClockSet.Default.SystemClock.UtcNow:yyyyMMdd-HHmmss}.log";
+        FilePath logFile = (FilePath)Path.GetTempPath() | fileName;
+        try {
+            var stream = File.Create(logFile);
+            await using var _1 = stream.ConfigureAwait(false);
+            await logUI.Save(stream, cancellationToken).ConfigureAwait(false);
+        }
+        catch {
+            logFile.DeleteSilently();
+            throw;
+        }
+        return logFile;
+    }
+
     public static async Task Save(this LogUI logUI, Stream stream, CancellationToken cancellationToken)
     {
         var idRange = await logUI.GetIdRange(cancellationToken).ConfigureAwait(false);
