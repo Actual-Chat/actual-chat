557705080a6f33c6d324349f390c9c22e92c1d90
Iq Mulator <iqmulator@gmail.com>
2026-06-08 18:06:33 +0500

    fix: landing page 1

--- src/nodejs/index.ts
+++ src/nodejs/index.ts
@@ -1,6 +1,7 @@
 // Styles
 import './fonts/svgtofont/icon.css';
 import './fonts/tt-commons-pro.css';
+import './fonts/nothing-you-could-do.css';
 import 'fork-awesome/css/fork-awesome.min.css';
 import './styles/index.css';
 
