ae77572a958e96266d31114d818e874263d8829f
Alexey Kochetov <alexis.kochetov@gmail.com>
2026-07-07 13:59:52 +0300

    chore: update dotnet watch for linux-windows compat

--- dotnet-watch.cmd
+++ dotnet-watch.cmd
@@ -1,1 +1,15 @@
-dotnet watch run --project src/dotnet/App.Server
+:<<BATCH
+    @echo off
+    if not exist tmp mkdir tmp
+    set ASPNETCORE_ENVIRONMENT=Development
+    set EnableAnalyzer=false
+    set EnableNETAnalyzers=false
+    dotnet watch run --project src/dotnet/App.Server 2>&1 | powershell -NoProfile -Command "$input | Tee-Object -FilePath tmp/watch-dotnet.log"
+    exit /b %ERRORLEVEL%
+BATCH
+
+#!/bin/sh
+mkdir -p tmp
+ASPNETCORE_ENVIRONMENT=Development EnableAnalyzer=false EnableNETAnalyzers=false \
+    dotnet watch run --project src/dotnet/App.Server 2>&1 | tee tmp/watch-dotnet.log
+exit $?
