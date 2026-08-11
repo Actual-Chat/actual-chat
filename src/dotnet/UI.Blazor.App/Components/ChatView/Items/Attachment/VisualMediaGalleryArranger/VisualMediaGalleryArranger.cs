325c0f175c9aa2edbaa1f9b249a63d536afa7e48
Iq Mulator <iqmulator@gmail.com>
2026-08-11 23:38:00 +0500

    fix: update uploads, gallery, progress, file size badge

--- src/dotnet/UI.Blazor.App/Components/ChatView/Items/Attachment/VisualMediaGalleryArranger/VisualMediaGalleryArranger.cs
+++ src/dotnet/UI.Blazor.App/Components/ChatView/Items/Attachment/VisualMediaGalleryArranger/VisualMediaGalleryArranger.cs
@@ -12,4 +12,9 @@ public static class VisualMediaGalleryArranger
     public static IVisualMediaGalleryArranger Default { get; } = new JustifiedGalleryArranger(targetRowRatio: 3f, maxPerRow: 5);
     public static IVisualMediaGalleryArranger Narrow { get; } = new JustifiedGalleryArranger(targetRowRatio: 2.5f, maxPerRow: 3);
     public static IVisualMediaGalleryArranger ConversationWideScreen { get; } = new JustifiedGalleryArranger(targetRowRatio: 4f, maxPerRow: 6);
+
+    public static IVisualMediaGalleryArranger Pick(bool isConversation, bool isNarrow)
+        => isNarrow ? Narrow
+            : isConversation ? ConversationWideScreen
+            : Default;
 }
